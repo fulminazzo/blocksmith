9e988134bdcec87ac5f87964b68e7676174fc59e
user <fulminazzo21@gmail.com>
2026-02-03 23:09:48 +0100

    Added default values and made fields private

--- config-starter/src/test/java/it/fulminazzo/config/jackson/Person.java
+++ config-starter/src/test/java/it/fulminazzo/config/jackson/Person.java
@@ -1,16 +1,21 @@
 package it.fulminazzo.config.jackson;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Person {
 
-    String name;
+    String name = "Alex";
 
-    String lastname;
+    String lastname = "Fulminazzo";
 
-    int age;
+    int age = 23;
 
 }
