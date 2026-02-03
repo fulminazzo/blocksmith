dfa8240312242c715d2d3736ba50af5eef9a4907
user <fulminazzo21@gmail.com>
2026-02-03 22:56:45 +0100

    Overridden deserialization methods to catch and handle DeserializationExceptions

--- config-starter/src/main/java/it/fulminazzo/config/jackson/LoggerSettableBeanProperty.java
+++ config-starter/src/main/java/it/fulminazzo/config/jackson/LoggerSettableBeanProperty.java
@@ -1,11 +1,15 @@
 package it.fulminazzo.config.jackson;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.SettableBeanProperty;
 import com.fasterxml.jackson.databind.introspect.AnnotatedField;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+
+import java.io.IOException;
 
 /**
  * A special type of {@link SettableBeanProperty} that will not throw
@@ -35,6 +39,39 @@ final class LoggerSettableBeanProperty extends SettableBeanProperty.Delegating {
     @Override
     protected SettableBeanProperty withDelegate(final @NotNull SettableBeanProperty delegate) {
         return new LoggerSettableBeanProperty(delegate, logger, field);
+    }
+
+    @Override
+    public void deserializeAndSet(final @NotNull JsonParser parser,
+                                  final @NotNull DeserializationContext context,
+                                  final @NotNull Object instance) throws IOException {
+        try {
+            super.deserializeAndSet(parser, context, instance);
+        } catch (DeserializationException e) {
+            handleDeserializationException(instance, e);
+        }
+    }
+
+    @Override
+    public Object deserializeSetAndReturn(final @NotNull JsonParser parser,
+                                          final @NotNull DeserializationContext context,
+                                          final @NotNull Object instance) throws IOException {
+        try {
+            return super.deserializeSetAndReturn(parser, context, instance);
+        } catch (DeserializationException e) {
+            return handleDeserializationException(instance, e);
+        }
+    }
+
+    private Object handleDeserializationException(final @NotNull Object instance,
+                                                  final @NotNull DeserializationException exception) {
+        logger.warn(exception.getMessage()
+                .replace("<name>", field.getName())
+                .replace("<type>", field.getRawType().getCanonicalName())
+        );
+        Object defaultValue = field.getValue(instance);
+        logger.warn("Using default value: {}", defaultValue);
+        return defaultValue;
     }
 
     /**
