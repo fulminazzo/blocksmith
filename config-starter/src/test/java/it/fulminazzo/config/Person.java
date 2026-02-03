72ace0014e101a0ceb2e2d8b44f2724670bc2a49
user <fulminazzo21@gmail.com>
2026-02-03 16:34:05 +0100

    Created Person object for testing purposes

---
+++ config-starter/src/test/java/it/fulminazzo/config/Person.java
@@ -0,0 +1,16 @@
+package it.fulminazzo.config;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    String name;
+
+    String lastname;
+
+    int age;
+
+}
