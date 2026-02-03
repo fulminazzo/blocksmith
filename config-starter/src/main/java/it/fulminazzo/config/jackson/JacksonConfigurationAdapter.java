cc74c5e40761a684e44f0ee32054b1790fb69c69
user <fulminazzo21@gmail.com>
2026-02-03 23:00:36 +0100

    Updated JacksonConfigurationBeanDeserializerModifier to support LoggerSettableBeanProperty

--- config-starter/src/main/java/it/fulminazzo/config/jackson/JacksonConfigurationAdapter.java
+++ config-starter/src/main/java/it/fulminazzo/config/jackson/JacksonConfigurationAdapter.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerBuilder;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.deser.SettableBeanProperty;
 import com.fasterxml.jackson.databind.deser.std.MapDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.type.MapType;
@@ -13,8 +15,10 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 import java.io.File;
+import java.util.Iterator;
 
 /**
  * A special implementation of {@link ConfigurationAdapter}
@@ -41,17 +45,44 @@ final class JacksonConfigurationAdapter implements ConfigurationAdapter {
      * A special {@link SimpleModule} to apply all the rules preventing
      * throwing exceptions on errors.
      */
+    @RequiredArgsConstructor
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
     static final class JacksonConfigurationModule extends SimpleModule {
+        @NotNull Logger logger;
 
         @Override
         public void setupModule(final SetupContext context) {
             super.setupModule(context);
-            context.addBeanDeserializerModifier(new JacksonConfigurationBeanDeserializerModifier());
+            context.addBeanDeserializerModifier(new JacksonConfigurationBeanDeserializerModifier(logger));
         }
 
     }
 
+    @RequiredArgsConstructor
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
     private static final class JacksonConfigurationBeanDeserializerModifier extends BeanDeserializerModifier {
+        @NotNull Logger logger;
+
+        @Override
+        public BeanDeserializerBuilder updateBuilder(final DeserializationConfig config,
+                                                     final BeanDescription beanDescription,
+                                                     final BeanDeserializerBuilder builder) {
+            Iterator<SettableBeanProperty> it = builder.getProperties();
+            while (it.hasNext()) {
+                SettableBeanProperty property = it.next();
+                beanDescription.findProperties().stream()
+                        .filter(p -> p.getName().equals(property.getName()))
+                        .findFirst().ifPresent(description -> {
+                            LoggerSettableBeanProperty beanProperty = new LoggerSettableBeanProperty(
+                                    property,
+                                    logger,
+                                    description.getField()
+                            );
+                            builder.addOrReplaceProperty(beanProperty, true);
+                        });
+            }
+            return builder;
+        }
 
         @Override
         public JsonDeserializer<?> modifyMapDeserializer(final DeserializationConfig config,
