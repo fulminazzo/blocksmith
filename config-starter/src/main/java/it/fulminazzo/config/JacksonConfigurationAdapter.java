320ceee0d1e11a93ad765c595b27d35a4fe08797
user <fulminazzo21@gmail.com>
2026-02-03 15:58:06 +0100

    Created JacksonConfigurationAdapter

---
+++ config-starter/src/main/java/it/fulminazzo/config/JacksonConfigurationAdapter.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * A special implementation of {@link ConfigurationAdapter}
+ * that uses the <a href="https://github.com/FasterXML/jackson">jackson project</a>
+ * for serialization and deserialization.
+ */
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+final class JacksonConfigurationAdapter implements ConfigurationAdapter {
+    @NotNull ObjectMapper mapper;
+
+    @Override
+    public @NotNull <T> T load(final @NotNull File file, final @NotNull Class<T> type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> void store(final @NotNull T configuration, final @NotNull File file) {
+        throw new UnsupportedOperationException();
+    }
+
+}
