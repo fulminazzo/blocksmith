efd6d6ff1c88fdedee4142a82f171cc4a7fbd900
user <fulminazzo21@gmail.com>
2026-02-04 01:20:08 +0100

    Created Comment annotation

---
+++ config-starter/src/main/java/it/fulminazzo/blacksmith/config/Comment.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.blacksmith.config;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.*;
+
+/**
+ * Any annotated field will be serialized with a prefixed
+ * comment with {@link #value()} as text.
+ * <br>
+ * Only for data formats languages that supports comments (such as YAML).
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+public @interface Comment {
+
+    /**
+     * The text of the comment.
+     * If on JDK < 15, it is possible to use the newline character <code>\n</code>
+     * to create multiple lines comments.
+     *
+     * @return the text
+     */
+    @NotNull String value();
+
+}
