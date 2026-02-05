50161f01141b2df69fdfe5e9dbfeffedd8ceeb51
user <fulminazzo21@gmail.com>
2026-02-06 00:35:04 +0100

    Fixed setComments not considering TOML default naming strategy

--- config-starter/src/main/java/it/fulminazzo/blocksmith/config/nightconfig/ConfigUtils.java
+++ config-starter/src/main/java/it/fulminazzo/blocksmith/config/nightconfig/ConfigUtils.java
@@ -2,6 +2,7 @@ package it.fulminazzo.blocksmith.config.nightconfig;
 
 import com.electronwill.nightconfig.core.CommentedConfig;
 import com.electronwill.nightconfig.core.Config;
+import com.fasterxml.jackson.databind.util.NamingStrategyImpls;
 import it.fulminazzo.blocksmith.config.Comment;
 import it.fulminazzo.blocksmith.util.ReflectionUtils;
 import lombok.AccessLevel;
@@ -16,6 +17,7 @@ import java.util.Collection;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ConfigUtils {
+    static final @NotNull NamingStrategyImpls NAMING_STRATEGY = NamingStrategyImpls.SNAKE_CASE;
 
     /**
      * Loops all the given object fields in search for the {@link Comment} annotation.
@@ -28,16 +30,16 @@ public final class ConfigUtils {
                                    final @NotNull CommentedConfig configuration) {
         Collection<Field> fields = ReflectionUtils.getInstanceFields(reference.getClass());
         for (Field field : fields) {
-            String fieldName = field.getName();
+            String propertyName = NAMING_STRATEGY.translate(field.getName());
             if (field.isAnnotationPresent(Comment.class)) {
                 Comment comment = field.getAnnotation(Comment.class);
-                configuration.setComment(fieldName, comment.value());
+                configuration.setComment(propertyName, comment.value());
             }
             Class<?> fieldType = field.getType();
             if (fieldType.isPrimitive() || fieldType.equals(Object.class)) continue;
             Object fieldValue = ReflectionUtils.getFieldValue(reference, field);
             if (fieldValue == null || fieldValue.equals(reference)) continue;
-            Object configValue = configuration.get(fieldName);
+            Object configValue = configuration.get(propertyName);
             if (configValue instanceof CommentedConfig)
                 setComments(fieldValue, (CommentedConfig) configValue);
         }
