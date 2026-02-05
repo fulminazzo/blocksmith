3eab44815e623357128fd85b52d20699d1e23a7d
user <fulminazzo21@gmail.com>
2026-02-06 00:44:28 +0100

    Updated TomlConfigurationAdapter to delegate comments to ConfigUtils

--- config-starter/src/main/java/it/fulminazzo/blocksmith/config/TomlConfigurationAdapter.java
+++ config-starter/src/main/java/it/fulminazzo/blocksmith/config/TomlConfigurationAdapter.java
@@ -1,21 +1,24 @@
 package it.fulminazzo.blocksmith.config;
 
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.toml.TomlFormat;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 import it.fulminazzo.blocksmith.config.jackson.CommentPropertyWriter;
 import it.fulminazzo.blocksmith.config.jackson.JacksonConfigurationAdapter;
-import lombok.experimental.Delegate;
+import it.fulminazzo.blocksmith.config.nightconfig.ConfigUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Implementation of {@link ConfigurationAdapter} for TOML.
  */
 final class TomlConfigurationAdapter implements ConfigurationAdapter {
-    @Delegate
     private final @NotNull ConfigurationAdapter delegate;
 
     /**
@@ -25,14 +28,34 @@ final class TomlConfigurationAdapter implements ConfigurationAdapter {
      */
     public TomlConfigurationAdapter(final @NotNull Logger logger) {
         this.delegate = new JacksonConfigurationAdapter(
-                new TomlMapper(),
+                new TomlMapper()
+                        .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE),
                 logger,
                 TomlCommentPropertyWriter.class
         );
     }
 
+    @Override
+    public @NotNull <T> T load(@NotNull File file, @NotNull Class<T> type) throws IOException {
+        return delegate.load(file, type);
+    }
+
+    @Override
+    public <T> void store(@NotNull File file, @NotNull T configuration) throws IOException {
+        delegate.store(file, configuration);
+        try (CommentedFileConfig config = CommentedFileConfig.builder(file, TomlFormat.instance())
+                .sync()
+                .preserveInsertionOrder()
+                .build()) {
+            config.load();
+            ConfigUtils.setComments(configuration, config);
+            config.save();
+        }
+    }
+
     /**
      * An implementation of {@link CommentPropertyWriter} for handling TOML comments.
+     * (These are actually handled by night-config)
      */
     static final class TomlCommentPropertyWriter extends CommentPropertyWriter {
 
@@ -49,10 +72,7 @@ final class TomlConfigurationAdapter implements ConfigurationAdapter {
 
         @Override
         protected void writeComment(final @NotNull JsonGenerator generator,
-                                    final @NotNull Comment comment) throws IOException {
-            String commentText = comment.value().replace("\\n", "\n");
-            for (String t : commentText.split("\n"))
-                generator.writeRaw("# " + t + "\n");
+                                    final @NotNull Comment comment) {
         }
 
     }
