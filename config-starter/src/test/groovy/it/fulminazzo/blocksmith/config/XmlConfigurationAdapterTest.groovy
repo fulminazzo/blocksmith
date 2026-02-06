a78f66dee1ef3fc3784bbef275ab294d601b6ecf
user <fulminazzo21@gmail.com>
2026-02-06 02:02:26 +0100

    Added tests for XmlConfigurationAdapter

---
+++ config-starter/src/test/groovy/it/fulminazzo/blocksmith/config/XmlConfigurationAdapterTest.groovy
@@ -0,0 +1,70 @@
+package it.fulminazzo.blocksmith.config
+
+import groovy.util.logging.Slf4j
+import spock.lang.Specification
+
+@Slf4j
+class XmlConfigurationAdapterTest extends Specification {
+
+    def 'test that load correctly loads file'() {
+        given:
+        def file = new File('build/resources/test/load.xml')
+
+        and:
+        def adapter = new XmlConfigurationAdapter(log)
+
+        when:
+        def actual = adapter.load(file, MockConfig)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        actual == new MockConfig(
+                false,
+                'Blocksmith',
+                null,
+                ['Fulminazzo', 'Camilla', 'Alex'],
+                new MockConfig.Internal(1.0)
+        )
+    }
+
+    def 'test that store correctly saves file'() {
+        given:
+        def file = new File('build/resources/test/store.xml')
+        if (file.exists()) file.delete()
+
+        and:
+        def adapter = new XmlConfigurationAdapter(log)
+
+        when:
+        adapter.store(file, new MockConfig())
+
+        then:
+        noExceptionThrown()
+
+        and:
+        file.readLines() == [
+                '<MockConfig>',
+                '    <!-- Example comment -->',
+                '    <CommentsEnabled>true</CommentsEnabled>',
+                '    <!-- This comment should be -->',
+                '    <!-- Multiline! -->',
+                '    <Name>blocksmith</Name>',
+                '    <Description>This is the description for the configuration file.',
+                '        Should be written in multiline format.',
+                '    </Description>',
+                '    <Authors>',
+                '        <Authors>Fulminazzo</Authors>',
+                '        <Authors>Camilla</Authors>',
+                '    </Authors>',
+                '    <Internal>',
+                '        <!-- This comment should be indented -->',
+                '        <!-- And should be multiline too -->',
+                '        <Version>1.0</Version>',
+                '    </Internal>',
+                '</MockConfig>'
+        ]
+    }
+
+}
