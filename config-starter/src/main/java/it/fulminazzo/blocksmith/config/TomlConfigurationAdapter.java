d9b9df43b3c73b4587cc2dad124c56724b0df913
user <fulminazzo21@gmail.com>
2026-02-06 01:00:34 +0100

    Added indentArrays function to properly indent arrays

--- config-starter/src/main/java/it/fulminazzo/blocksmith/config/TomlConfigurationAdapter.java
+++ config-starter/src/main/java/it/fulminazzo/blocksmith/config/TomlConfigurationAdapter.java
@@ -14,8 +14,9 @@ import it.fulminazzo.blocksmith.config.nightconfig.ConfigUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implementation of {@link ConfigurationAdapter} for TOML.
@@ -38,12 +39,12 @@ final class TomlConfigurationAdapter implements ConfigurationAdapter {
     }
 
     @Override
-    public @NotNull <T> T load(@NotNull File file, @NotNull Class<T> type) throws IOException {
+    public @NotNull <T> T load(final @NotNull File file, final @NotNull Class<T> type) throws IOException {
         return delegate.load(file, type);
     }
 
     @Override
-    public <T> void store(@NotNull File file, @NotNull T configuration) throws IOException {
+    public <T> void store(final @NotNull File file, final @NotNull T configuration) throws IOException {
         delegate.store(file, configuration);
         try (CommentedFileConfig config = CommentedFileConfig.builder(file, TomlFormat.instance())
                 .sync()
@@ -52,6 +53,29 @@ final class TomlConfigurationAdapter implements ConfigurationAdapter {
             config.load();
             ConfigUtils.setComments(configuration, config);
             newTomlWriter().write(config, file, WritingMode.REPLACE);
+        }
+        indentArrays(file);
+    }
+
+    /**
+     * Manually indents arrays in the given TOML file.
+     *
+     * @param file the file
+     */
+    static void indentArrays(final @NotNull File file) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (FileReader reader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+            bufferedReader.lines().forEach(lines::add);
+        }
+        try (FileWriter writer = new FileWriter(file);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+            String indent = "";
+            for (String line : lines) {
+                if (!indent.isEmpty() && line.matches(" *] *")) indent = "";
+                bufferedWriter.write(indent + line + "\n");
+                if (line.matches("^ *[A-Za-z.0-9_-]+ *= *\\[ *$")) indent = "    ";
+            }
         }
     }
 
