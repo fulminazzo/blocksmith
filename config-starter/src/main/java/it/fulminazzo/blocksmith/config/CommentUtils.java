b785313c54e690415e060120d32d706bbcc34421
user <fulminazzo21@gmail.com>
2026-02-06 01:45:57 +0100

    Created CommentUtils with helper functions

---
+++ config-starter/src/main/java/it/fulminazzo/blocksmith/config/CommentUtils.java
@@ -0,0 +1,38 @@
+package it.fulminazzo.blocksmith.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * A collection of utilities for {@link Comment}.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CommentUtils {
+
+    public static boolean isEmpty(final @NotNull Comment comment) {
+        return getText(comment).stream().allMatch(l -> l.trim().isEmpty());
+    }
+
+    /**
+     * Gets the text of the comment.
+     * Each entry of the collection will be a line of the comment.
+     *
+     * @param comment the comment
+     * @return the comment text
+     */
+    public static @NotNull Collection<String> getText(final @NotNull Comment comment) {
+        List<String> text = new ArrayList<>();
+        for (String t : comment.value()) {
+            String[] raw = t.replaceAll("\\n", "\n").split("\n");
+            text.addAll(Arrays.asList(raw));
+        }
+        return text;
+    }
+
+}
