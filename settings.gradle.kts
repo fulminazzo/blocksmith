6d99c7183360069591ee222a7c698d4a4e5688c6
user <fulminazzo21@gmail.com>
2026-02-08 15:33:03 +0100

    Created testing module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,3 +9,5 @@ include("config-starter:config-starter-properties")
 include("config-starter:config-starter-toml")
 include("config-starter:config-starter-xml")
 include("config-starter:config-starter-yaml")
+
+include("config-starter:config-starter-testing")
