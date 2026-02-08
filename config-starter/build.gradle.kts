3c85bf55298134ed7843c5ca34dd8b6b9f401b65
user <fulminazzo21@gmail.com>
2026-02-08 15:38:17 +0100

    Moved dependencies to according modules

--- config-starter/build.gradle.kts
+++ config-starter/build.gradle.kts
@@ -1,14 +1,12 @@
 dependencies {
     compileOnly(libs.slf4j)
 
-    api(libs.jackson.json)
-    api(libs.jackson.yaml)
-    api(libs.bundles.toml)
-    api(libs.jackson.xml)
-    api(libs.jackson.properties)
+    compileOnly(libs.jackson.json)
 
     api(libs.bundles.validation)
 
     testImplementation(libs.slf4j)
     testImplementation(libs.bundles.log4j)
+
+    testImplementation(libs.jackson.json)
 }
