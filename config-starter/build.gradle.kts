661892bc26c24ca121093de87c9961ace0d0dffc
user <fulminazzo21@gmail.com>
2026-02-08 15:42:39 +0100

    Made config-starter dependency for all submodules Made config-starter-testing dependency for languages submodules

--- config-starter/build.gradle.kts
+++ config-starter/build.gradle.kts
@@ -1,18 +1,28 @@
 dependencies {
+    val projectName = project.name
+
     compileOnly(libs.slf4j)
 
-    compileOnly(libs.jackson.json)
-
+    api(libs.jackson.json)
     api(libs.bundles.validation)
 
-    subprojects.forEach {
+    subprojects {
+
         dependencies {
-            api(project(":${project.name}"))
+            compileOnly(rootProject.libs.slf4j)
+            api(project(":$projectName"))
+
+            val testingModuleName = "$projectName-testing"
+            if (project.name != testingModuleName)
+                testImplementation(project(":$projectName:$testingModuleName"))
         }
+
     }
 
     testImplementation(libs.slf4j)
     testImplementation(libs.bundles.log4j)
 
     testImplementation(libs.jackson.json)
+    testImplementation(project(":$projectName:$projectName-testing"))
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,12 @@ rootProject.name = "blocksmith"
 
 include("base")
 
+
 include("config-starter")
+include("config-starter:config-starter-testing")
 
 include("config-starter:config-starter-json")
 include("config-starter:config-starter-properties")
 include("config-starter:config-starter-toml")
 include("config-starter:config-starter-xml")
 include("config-starter:config-starter-yaml")
-
-include("config-starter:config-starter-testing")
