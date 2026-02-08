81e8ce33671a24cf68c30b7de66ccc3885d14d1c
user <fulminazzo21@gmail.com>
2026-02-08 16:49:06 +0100

    Created config-starter-all module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,3 +11,5 @@ include("config-starter:config-starter-properties")
 include("config-starter:config-starter-toml")
 include("config-starter:config-starter-xml")
 include("config-starter:config-starter-yaml")
+
+include("config-starter:config-starter-all")
