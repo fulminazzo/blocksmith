4c9e74f5dc1d6e705d91e9a9af812eb9fb49508e
user <fulminazzo21@gmail.com>
2026-02-08 17:46:30 +0100

    Removed several unused functions from ReflectionUtils

--- base/src/main/java/it/fulminazzo/blocksmith/util/ReflectionUtils.java
+++ base/src/main/java/it/fulminazzo/blocksmith/util/ReflectionUtils.java
@@ -4,7 +4,8 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -16,40 +17,6 @@ import java.util.stream.Collectors;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ReflectionUtils {
-
-    /**
-     * Initializes a new instance of the given class
-     * through the constructor with matching parameters.
-     *
-     * @param <T>             the type of the object
-     * @param type            the type
-     * @param parametersTypes the parameters classes
-     * @param parameters      the actual parameters
-     * @return the object
-     */
-    public static <T> T initialize(final @NotNull Class<T> type,
-                                   final @NotNull Collection<Class<?>> parametersTypes,
-                                   final Object... parameters) {
-        Class<?>[] actualParametersTypes = parametersTypes.toArray(new Class[0]);
-        String parameterTypesNames = parametersTypes.stream()
-                .map(Class::getCanonicalName)
-                .collect(Collectors.joining(", "));
-        try {
-            Constructor<T> constructor = type.getDeclaredConstructor(actualParametersTypes);
-            constructor.setAccessible(true);
-            return constructor.newInstance(parameters);
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            if (cause instanceof RuntimeException) throw (RuntimeException) cause;
-            else throw new RuntimeException(cause);
-        } catch (NoSuchMethodException e) {
-            throw new ReflectionException("Could not get constructor %s(%s): no such constructor is present",
-                    type.getCanonicalName(), parameterTypesNames);
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new ReflectionException(e, "Could not initialize %s(%s): %s",
-                    type.getCanonicalName(), parameterTypesNames, e.getMessage());
-        }
-    }
 
     /**
      * Returns all the fields in the given class (or superclasses)
@@ -68,170 +35,6 @@ public final class ReflectionUtils {
             curr = curr.getSuperclass();
         }
         return fields;
-    }
-
-    /**
-     * Attempts to find a field in the given class (or superclasses)
-     * with the given name and arguments types.
-     * <br>
-     * The returned field can either be static or non-static.
-     *
-     * @param container the container of the method
-     * @param fieldName the field name
-     * @return the method
-     */
-    public static @NotNull Field getField(final @NotNull Class<?> container,
-                                          final @NotNull String fieldName) {
-        Class<?> curr = container;
-        while (curr != null && !curr.equals(Object.class)) {
-            try {
-                return curr.getDeclaredField(fieldName);
-            } catch (NoSuchFieldException ignored) {
-                curr = curr.getSuperclass();
-            }
-        }
-        throw new ReflectionException("Could not get field %s#%s: no such field is present",
-                container.getCanonicalName(), fieldName);
-    }
-
-    /**
-     * Wrapper for:
-     * <pre>
-     * Field field = getField(caller.getClass(), fieldName);
-     * return getFieldValue(caller, field);
-     * </pre>
-     * In case of error throws the unchecked {@link ReflectionException}.
-     *
-     * @param caller    the caller
-     * @param fieldName the name of the field to get
-     * @param <T>       the type of the field value
-     * @return the value of the field
-     */
-    public static <T> T getFieldValue(final @NotNull Object caller,
-                                      final @NotNull String fieldName) {
-        Field field = getField(caller.getClass(), fieldName);
-        return getFieldValue(caller, field);
-    }
-
-    /**
-     * Wrapper for:
-     * <pre>
-     * field.setAccessible(true);
-     * return field.get(caller);
-     * </pre>
-     * In case of error throws the unchecked {@link ReflectionException}.
-     *
-     * @param caller the caller
-     * @param field  the field to get
-     * @param <T>    the type of the field value
-     * @return the value of the field
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getFieldValue(final @NotNull Object caller,
-                                      final @NotNull Field field) {
-        try {
-            field.setAccessible(true);
-            return (T) field.get(caller);
-        } catch (IllegalAccessException e) {
-            throw new ReflectionException(e, "Could not get value of %s#%s: %s",
-                    caller.getClass().getCanonicalName(), field.getName(), e.getMessage());
-        }
-    }
-
-    /**
-     * Attempts to find a method in the given class (or superclasses)
-     * with the given name and arguments types.
-     * <br>
-     * The returned method can either be static or non-static.
-     *
-     * @param container      the container of the method
-     * @param methodName     the method name
-     * @param argumentsTypes the types of the arguments
-     * @return the method
-     */
-    public static @NotNull Method getMethod(final @NotNull Class<?> container,
-                                            final @NotNull String methodName,
-                                            final @NotNull Collection<Class<?>> argumentsTypes) {
-        Class<?> curr = container;
-        while (curr != null && !curr.equals(Object.class)) {
-            try {
-                return curr.getDeclaredMethod(methodName, argumentsTypes.toArray(new Class[0]));
-            } catch (NoSuchMethodException ignored) {
-                curr = curr.getSuperclass();
-            }
-        }
-        throw new ReflectionException("Could not get method %s#%s(%s): no such method is present",
-                container.getCanonicalName(), methodName,
-                argumentsTypes.stream()
-                        .map(Class::getCanonicalName)
-                        .collect(Collectors.joining(", "))
-        );
-    }
-
-    /**
-     * Invokes a method (regardless if it is static or not) from the given object.
-     *
-     * @param <T>            the return type
-     * @param caller         the object to call the method from
-     * @param methodName     the method name
-     * @param argumentsTypes the types of the arguments
-     * @param arguments      the arguments
-     * @return the returned object
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T invokeMethod(final @NotNull Object caller,
-                                     final @NotNull String methodName,
-                                     final @NotNull Collection<Class<?>> argumentsTypes,
-                                     final Object... arguments) {
-        Class<?> container = caller instanceof Class ? (Class<?>) caller : caller.getClass();
-        try {
-            Method function = getMethod(container, methodName, argumentsTypes);
-            function.setAccessible(true);
-            return (T) function.invoke(caller, arguments);
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            if (cause instanceof RuntimeException) throw (RuntimeException) cause;
-            else throw new RuntimeException(cause);
-        } catch (IllegalAccessException e) {
-            throw new ReflectionException(e, "Could not invoke method %s#%s(%s): %s",
-                    container.getCanonicalName(), methodName,
-                    argumentsTypes.stream()
-                            .map(Class::getCanonicalName)
-                            .collect(Collectors.joining(", ")),
-                    e.getMessage()
-            );
-        }
-    }
-
-    /**
-     * An exception thrown by {@link ReflectionUtils} failed operations.
-     */
-    static final class ReflectionException extends RuntimeException {
-
-        /**
-         * Instantiates a new Reflection exception.
-         *
-         * @param format    the format of the message
-         * @param arguments the arguments to parse in the message
-         */
-        public ReflectionException(final @NotNull String format,
-                                   final @NotNull Object... arguments) {
-            super(String.format(format, arguments));
-        }
-
-        /**
-         * Instantiates a new Reflection exception.
-         *
-         * @param cause     the cause
-         * @param format    the format of the message
-         * @param arguments the arguments to parse in the message
-         */
-        public ReflectionException(final @NotNull Throwable cause,
-                                   final @NotNull String format,
-                                   final @NotNull Object... arguments) {
-            super(String.format(format, arguments), cause);
-        }
-
     }
 
 }
