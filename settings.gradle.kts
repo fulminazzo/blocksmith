44e574560dacf9800e3152cb6f37b74e60fd0dee
user <fulminazzo21@gmail.com>
2026-02-08 15:31:08 +0100

    Created supported data format languages modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,9 @@ rootProject.name = "blocksmith"
 include("base")
 
 include("config-starter")
+
+include("config-starter:config-starter-json")
+include("config-starter:config-starter-properties")
+include("config-starter:config-starter-toml")
+include("config-starter:config-starter-xml")
+include("config-starter:config-starter-yaml")
