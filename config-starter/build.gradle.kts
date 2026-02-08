91fdd84934b7c5257f0bafd726c69ce6d461d431
user <fulminazzo21@gmail.com>
2026-02-08 16:14:23 +0100

    Fixed submodules tests configurations

--- config-starter/build.gradle.kts
+++ config-starter/build.gradle.kts
@@ -6,6 +6,18 @@ dependencies {
     api(libs.jackson.json)
     api(libs.bundles.validation)
 
+    allprojects {
+
+        dependencies {
+            testImplementation(rootProject.libs.slf4j)
+            testImplementation(rootProject.libs.bundles.log4j)
+
+            testImplementation(rootProject.libs.jackson.json)
+            testImplementation(project(":$projectName:$projectName-testing"))
+        }
+
+    }
+
     subprojects {
 
         dependencies {
@@ -18,11 +30,5 @@ dependencies {
         }
 
     }
-
-    testImplementation(libs.slf4j)
-    testImplementation(libs.bundles.log4j)
-
-    testImplementation(libs.jackson.json)
-    testImplementation(project(":$projectName:$projectName-testing"))
 
 }
