b4fb3f97f4695966ac5bd5cfd4f973a740a5d220
user <fulminazzo21@gmail.com>
2026-02-08 22:43:08 +0100

    Created data-starter-testing module for testing purposes

--- data-starter/build.gradle.kts
+++ data-starter/build.gradle.kts
@@ -1,5 +1,14 @@
+val testingModuleName: String by rootProject.extra
+
 dependencies {
     val projectName = project.name
+
+    allprojects {
+        dependencies {
+            if (!project.name.endsWith(testingModuleName))
+                testImplementation(project(":$projectName:$projectName-$testingModuleName"))
+        }
+    }
 
     subprojects {
         dependencies {
