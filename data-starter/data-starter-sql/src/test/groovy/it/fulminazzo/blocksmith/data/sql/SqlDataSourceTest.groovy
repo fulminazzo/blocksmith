6a38eed656c0372042e67052f596814ba60c34f4
user <fulminazzo21@gmail.com>
2026-02-09 02:39:21 +0100

    Added tests for RemoteDataSourceBuilder

--- data-starter/data-starter-sql/src/test/groovy/it/fulminazzo/blocksmith/data/sql/SqlDataSourceTest.groovy
+++ data-starter/data-starter-sql/src/test/groovy/it/fulminazzo/blocksmith/data/sql/SqlDataSourceTest.groovy
@@ -5,6 +5,42 @@ import spock.lang.Specification
 
 class SqlDataSourceTest extends Specification {
 
+    /*
+     * REMOTE
+     */
+
+    def 'test initialize #type connection'() {
+        given:
+        def methodName = (type == DatabaseType.MARIADB ?
+                DatabaseType.MYSQL : type)
+                .name().toLowerCase()
+
+        when:
+        def source = SqlDataSource.builder()
+                .setDatabase('sql_data_source')
+                .setUsername('user')
+                .setPassword('password')
+                .setDatabaseType(type)
+                .host('localhost')
+                .port(23692)
+                ."${methodName}"()
+                .build()
+
+        then:
+        thrown(Exception)
+
+        cleanup:
+        if (source != null) source.close()
+
+        where:
+        type << DatabaseType.values()
+    }
+
+
+    /*
+     * H2
+     */
+
     def 'test initialize h2 memory connection'() {
         when:
         def source = SqlDataSource.builder()
