9307898317b5dcafda01bd0553537a4c59a4eaf4
user <fulminazzo21@gmail.com>
2026-02-09 13:08:54 +0100

    Updated SqlDataSource with SQLDialect parameter

--- data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/SqlDataSource.java
+++ data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/SqlDataSource.java
@@ -1,18 +1,25 @@
 package it.fulminazzo.blocksmith.data.sql;
 
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 import org.jetbrains.annotations.NotNull;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 import javax.sql.DataSource;
 import java.io.Closeable;
 
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public final class SqlDataSource implements DataSource, Closeable {
     @Delegate
     private final @NotNull HikariDataSource delegate;
+    private final @NotNull DSLContext context;
+
+    SqlDataSource(final @NotNull HikariDataSource delegate,
+                  final @NotNull SQLDialect dialect) {
+        this.delegate = delegate;
+        this.context = DSL.using(this, dialect);
+    }
 
     public static @NotNull SqlDataSourceBuilder builder() {
         return new SqlDataSourceBuilder();
