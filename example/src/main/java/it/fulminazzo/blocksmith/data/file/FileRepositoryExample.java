a3185eade8a82fcff2df196cbd4d25984f2f170a
user <fulminazzo21@gmail.com>
2026-02-10 19:26:33 +0100

    Created simple FileRepository usage example

---
+++ example/src/main/java/it/fulminazzo/blocksmith/data/file/FileRepositoryExample.java
@@ -0,0 +1,36 @@
+package it.fulminazzo.blocksmith.data.file;
+
+import it.fulminazzo.blocksmith.config.ConfigurationFormat;
+import it.fulminazzo.blocksmith.data.Repository;
+import it.fulminazzo.blocksmith.data.User;
+import it.fulminazzo.blocksmith.util.TestUtils;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public final class FileRepositoryExample {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        try {
+            Repository<User, Long> repository = FileRepository.builder(User.class)
+                    .dataDirectory(new File("build/resources/test/files"))
+                    .executor(executor)
+                    .dataLanguageFormat(ConfigurationFormat.YAML)
+                    .logger(LoggerFactory.getLogger(FileRepositoryExample.class))
+                    .idMapper(User::getId)
+                    .build();
+            User user = new User(1337L, "Alexander", "Drinkwater", "alex@fulminazzo.it", 23);
+            TestUtils.assertEquals(repository.existsById(user.getId()).join(), false, "User should not exist at start");
+            TestUtils.assertEquals(repository.save(user).join(), user, "Saved user should be equal to current");
+            TestUtils.assertEquals(repository.existsById(user.getId()).join(), true, "User should exist after save");
+            repository.delete(user.getId()).join();
+            TestUtils.assertEquals(repository.existsById(user.getId()).join(), false, "User should not exist after delete");
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+}
