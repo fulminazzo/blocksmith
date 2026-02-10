9a04dc4ad8ded903e3a71fd44b339e4dc20258fb
user <fulminazzo21@gmail.com>
2026-02-10 21:08:45 +0100

    Created SerializableMapper implementation of Mapper for Serializable objects

---
+++ data-starter/data-starter-mapper/src/main/java/it/fulminazzo/blocksmith/data/mapper/SerializableMapper.java
@@ -0,0 +1,54 @@
+package it.fulminazzo.blocksmith.data.mapper;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.Base64;
+
+/**
+ * A basic implementation of {@link Mapper} for {@link java.io.Serializable} objects.
+ */
+final class SerializableMapper implements Mapper {
+
+    @Override
+    public @NotNull <T> String serialize(final @NotNull T data) {
+        try (
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                ObjectOutputStream objectStream = new ObjectOutputStream(output)
+        ) {
+            objectStream.writeObject(data);
+            byte[] raw = output.toByteArray();
+            return Base64.getEncoder().encodeToString(raw);
+        } catch (IOException e) {
+            throw new SerializationException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public @NotNull <T> T deserialize(final @NotNull String serialized) {
+        byte[] raw = Base64.getDecoder().decode(serialized);
+        try (
+                ByteArrayInputStream input = new ByteArrayInputStream(raw);
+                ObjectInputStream inputStream = new ObjectInputStream(input)
+        ) {
+            return (T) inputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new SerializationException(e);
+        }
+    }
+
+    public static final class SerializationException extends RuntimeException {
+
+        /**
+         * Instantiates a new Serialization exception.
+         *
+         * @param cause the cause
+         */
+        public SerializationException(final @NotNull Throwable cause) {
+            super(cause.getMessage(), cause);
+        }
+
+    }
+
+}
