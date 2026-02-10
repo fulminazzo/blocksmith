e4e2b5603b9a41f131e60eb05b69c0731934e7cd
user <fulminazzo21@gmail.com>
2026-02-10 19:35:19 +0100

    Created custom FileRepository usage example

---
+++ example/src/main/java/it/fulminazzo/blocksmith/data/file/CustomFileRepositoryExample.java
@@ -0,0 +1,71 @@
+package it.fulminazzo.blocksmith.data.file;
+
+import it.fulminazzo.blocksmith.config.ConfigurationFormat;
+import it.fulminazzo.blocksmith.data.User;
+import it.fulminazzo.blocksmith.util.TestUtils;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public final class CustomFileRepositoryExample {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        try {
+            User first = new User(1L, "Alexander", "Drinkwater", "alex@fulminazzo.it", 23);
+            User second = new User(1L, "Camilla", "Drinkwater", "cami@fulminazzo.it", 20);
+            UserRepository repository = new UserRepository(
+                    new File("build/resources/test/files"),
+                    executor,
+                    LoggerFactory.getLogger(FileRepositoryExample.class),
+                    ConfigurationFormat.YAML
+            );
+            repository.save(first).join();
+            repository.save(second).join();
+
+            Optional<User> result = repository.findYoungestUser().join();
+            TestUtils.assertEquals(result.isEmpty(), false, "Could not find youngest user");
+            TestUtils.assertEquals(result.get(), second, "Youngest user was not youngest");
+
+            repository.delete(first.getId()).join();
+            repository.delete(second.getId()).join();
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    public static final class UserRepository extends FileRepository<User, Long> {
+
+        UserRepository(final @NotNull File dataDirectory,
+                       final @NotNull Executor executor,
+                       final @NotNull Logger logger,
+                       final @NotNull ConfigurationFormat format) {
+            super(
+                    dataDirectory,
+                    User.class,
+                    User::getId,
+                    executor,
+                    logger,
+                    format
+            );
+        }
+
+        public @NotNull CompletableFuture<Optional<User>> findYoungestUser() {
+            return findAll().thenApply(l ->
+                    l.stream()
+                            .min(Comparator.comparingInt(User::getAge))
+                            .stream().findFirst()
+            );
+        }
+
+    }
+
+}
