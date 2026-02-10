7cf54c9eee08bb45ee256f63d9db8b18d6700778
user <fulminazzo21@gmail.com>
2026-02-10 12:42:31 +0100

    Added tests for ConfigurationFormat#newAdapter failure

---
+++ config-starter/config-starter-base/src/test/java/it/fulminazzo/blocksmith/config/YamlConfigurationAdapter.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.blocksmith.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+final class YamlConfigurationAdapter extends AbstractConfigurationAdapter {
+
+    public YamlConfigurationAdapter(final @NotNull Logger logger) {
+        throw new RuntimeException("Test runtime exception");
+    }
+
+}
