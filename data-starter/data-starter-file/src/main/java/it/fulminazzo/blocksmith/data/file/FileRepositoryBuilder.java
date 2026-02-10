19343af9395a027914047d83a4cab033aa91513b
user <fulminazzo21@gmail.com>
2026-02-10 17:15:50 +0100

    Added documentation

--- data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/FileRepositoryBuilder.java
+++ data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/FileRepositoryBuilder.java
@@ -4,6 +4,52 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * A builder for {@link FileRepository}.
+ * <br>
+ * Example usage:
+ * <pre>{@code
+ * FileRepository.builder(User.class)
+ *         .dataDirectory(new File("./users"))
+ *         .executor(ForkJoinPool.commonPool()) // proper executor should be used
+ *         .dataLanguageFormat(ConfigurationFormat.JSON)
+ *         .logger(LoggerFactory.getLogger(FileRepository.class))
+ *         .idMapper(User::getId)
+ *         .build();
+ * }</pre>
+ * <br>
+ * Given the Java POJO
+ * <pre>{@code
+ * public class User {
+ *
+ *     private UUID id;
+ *
+ *     private String username;
+ *
+ *     public User() {
+ *
+ *     }
+ *
+ *     public User(UUID id, String username) {
+ *         this.id = id;
+ *         this.username = username;
+ *     }
+ *
+ *     public UUID getId() {
+ *         return id;
+ *     }
+ *
+ *     public void setId(UUID id) {
+ *         this.id = id;
+ *     }
+ *
+ *     public String getUsername() {
+ *         return username;
+ *     }
+ *
+ *     public void setUsername(String username) {
+ *         this.username = username;
+ *     }
+ *
+ * }}</pre>
  *
  * @param <T> the type of the data
  */
