f385153de15929adc815f06470a58f7afdaae249
user <fulminazzo21@gmail.com>
2026-02-10 14:47:18 +0100

    Added tests for FileRepository

---
+++ data-starter/data-starter-file/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    testImplementation(libs.slf4j)
+    testImplementation(project(":config-starter:config-starter-json"))
+}
---
+++ data-starter/data-starter-file/src/test/groovy/it/fulminazzo/blocksmith/data/file/FileRepositoryTest.groovy
@@ -0,0 +1,98 @@
+package it.fulminazzo.blocksmith.data.file
+
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.config.ConfigurationAdapter
+import it.fulminazzo.blocksmith.config.ConfigurationFormat
+import it.fulminazzo.blocksmith.data.Repository
+import it.fulminazzo.blocksmith.data.RepositoryTest
+import it.fulminazzo.blocksmith.data.User
+import it.fulminazzo.blocksmith.function.ConsumerException
+import it.fulminazzo.blocksmith.function.FunctionException
+import org.jetbrains.annotations.NotNull
+
+import java.util.concurrent.CompletionException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@Slf4j
+class FileRepositoryTest extends RepositoryTest {
+    private static final ConfigurationFormat FORMAT = ConfigurationFormat.JSON
+
+    private final File workingDir = new File('build/resources/test/file_repository')
+
+    private final ConfigurationAdapter adapter = ConfigurationAdapter.newAdapter(log, FORMAT)
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor()
+
+    void setup() {
+        setupRepository()
+    }
+
+    void cleanup() {
+        executor.shutdown()
+    }
+
+    def 'test executeOnMany throws CompletionException'() {
+        given:
+        def ids = [1L, 2L]
+
+        when:
+        repository.executeOnMany(ids, function).join()
+
+        then:
+        def e = thrown(CompletionException)
+        (e.cause instanceof IOException)
+
+        where:
+        function << [
+                (ConsumerException<File, IOException>) (f -> {
+                    throw new IOException('test exception')
+                }),
+                (FunctionException<File, User, IOException>) (f -> {
+                    throw new IOException('test exception')
+                })
+        ]
+    }
+
+    def 'test executeOnSingle throws CompletionException'() {
+        given:
+        def id = 1L
+
+        when:
+        repository.executeOnSingle(id, function).join()
+
+        then:
+        def e = thrown(CompletionException)
+        (e.cause instanceof IOException)
+
+        where:
+        function << [
+                (ConsumerException<File, IOException>) (f -> {
+                    throw new IOException('test exception')
+                }),
+                (FunctionException<File, User, IOException>) (f -> {
+                    throw new IOException('test exception')
+                })
+        ]
+    }
+
+    @Override
+    Repository<User, Long> initializeRepository() {
+        return new FileRepository<>(
+                workingDir,
+                User::getId,
+                executor
+        )
+    }
+
+    @Override
+    boolean exists(final @NotNull Long id) {
+        return FORMAT.getFile(workingDir, id.toString()).exists()
+    }
+
+    @Override
+    void insert(final @NotNull User data) {
+        adapter.store(workingDir, data.id.toString(), data)
+    }
+
+}
