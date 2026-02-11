d098f478b795cd9c29af45cd0682cc01b7cc6e44
user <fulminazzo21@gmail.com>
2026-02-11 19:45:34 +0100

    Updated MongoRepository with implementation of Repository Implemented single methods

--- data-starter/data-starter-mongo/src/main/java/it/fulminazzo/blocksmith/data/mongodb/MongoRepository.java
+++ data-starter/data-starter-mongo/src/main/java/it/fulminazzo/blocksmith/data/mongodb/MongoRepository.java
@@ -1,19 +1,85 @@
 package it.fulminazzo.blocksmith.data.mongodb;
 
 import com.mongodb.Function;
+import com.mongodb.client.model.CountOptions;
 import com.mongodb.reactivestreams.client.MongoCollection;
+import it.fulminazzo.blocksmith.data.Repository;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
+import static com.mongodb.client.model.Filters.eq;
+
+/**
+ * A basic implementation of {@link Repository} for MongoDB databases.
+ *
+ * @param <T>  the type of the data
+ * @param <ID> the type of the id of the data (should be unique)
+ */
 @RequiredArgsConstructor
-public class MongoRepository<T, ID> {
+public class MongoRepository<T, ID> implements Repository<T, ID> {
     private final @NotNull MongoCollection<T> collection;
     private final @NotNull String idFieldName;
     private final @NotNull Function<T, ID> idMapper;
+
+    @Override
+    public @NotNull CompletableFuture<Optional<T>> findById(final @NonNull ID id) {
+        return query(collection ->
+                collection.find(eq(idFieldName, id))
+        ).thenApply(Optional::ofNullable);
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Boolean> existsById(final @NonNull ID id) {
+        return query(collection ->
+                collection.countDocuments(eq(idFieldName, id), new CountOptions().limit(1))
+        ).thenApply(c -> c > 0);
+    }
+
+    @Override
+    public @NotNull CompletableFuture<T> save(final @NonNull T data) {
+        return query(collection ->
+                collection.insertOne(data)
+        ).thenApply(r -> data);
+    }
+
+    @Override
+    public @NotNull CompletableFuture<?> delete(final @NonNull ID id) {
+        return query(collection ->
+                collection.deleteOne(eq(idFieldName, id))
+        );
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Collection<T>> findAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Collection<T>> findAllById(final @NotNull Collection<ID> ids) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Collection<T>> saveAll(final @NotNull Collection<T> entries) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NotNull CompletableFuture<?> deleteAll(final @NotNull Collection<ID> ids) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Long> count() {
+        throw new UnsupportedOperationException();
+    }
 
     /**
      * Executes a general query and returns the result.
