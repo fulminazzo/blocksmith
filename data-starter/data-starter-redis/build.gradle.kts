9ade1097e93a99966eb571d93c28016475cf4a2b
user <fulminazzo21@gmail.com>
2026-02-11 02:48:09 +0100

    Added dependency to base module

--- data-starter/data-starter-redis/build.gradle.kts
+++ data-starter/data-starter-redis/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     api(libs.lettuce)
 
+    api(project(":data-starter:data-starter-base"))
     api(project(":data-starter:data-starter-mapper:data-starter-mapper-json"))
 
     testImplementation(libs.bundles.log4j)
