dc5918d9891314b5f445c345eed9f5e779e4ec72
user <fulminazzo21@gmail.com>
2026-02-11 02:34:02 +0100

    Added more validation functions

--- data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/util/ValidationUtils.java
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/util/ValidationUtils.java
@@ -9,6 +9,55 @@ import org.jetbrains.annotations.NotNull;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ValidationUtils {
+    static final int MIN_PORT = 1;
+    static final int MAX_PORT = 65535;
+
+    /**
+     * Checks if the given value is positive and not <code>0</code>.
+     *
+     * @param value     the value
+     * @param valueName the value name
+     */
+    public static void checkNatural(final int value,
+                                    final @NotNull String valueName) {
+        checkGreaterEqualThan(value, valueName, 1);
+    }
+
+    /**
+     * Checks if the given value is positive.
+     *
+     * @param value     the value
+     * @param valueName the value name
+     */
+    public static void checkPositive(final int value,
+                                     final @NotNull String valueName) {
+        checkGreaterEqualThan(value, valueName, 0);
+    }
+
+    /**
+     * Checks if the given value is greater or equal than the minimum value.
+     *
+     * @param value     the value
+     * @param valueName the value name
+     * @param min       the minimum allowed value
+     */
+    public static void checkGreaterEqualThan(final int value,
+                                             final @NotNull String valueName,
+                                             final int min) {
+        if (value < min)
+            throw new IllegalArgumentException(String.format("Invalid %s %s. Must be at least %s",
+                    valueName, value, min
+            ));
+    }
+
+    /**
+     * Checks if the given value is a valid port.
+     *
+     * @param port the port
+     */
+    public static void checkPort(final int port) {
+        checkInRange(port, "port", MIN_PORT, MAX_PORT);
+    }
 
     /**
      * Checks if the given value is in range.
