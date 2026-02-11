2ba12068b8f7ed584421c3a9d4cbafdde3b85560
user <fulminazzo21@gmail.com>
2026-02-11 19:18:42 +0100

    Fixed server not supporting Manjaro for package resolving

--- data-starter/data-starter-mongo/src/test/groovy/it/fulminazzo/blocksmith/data/mongodb/MongoRepositoryTest.groovy
+++ data-starter/data-starter-mongo/src/test/groovy/it/fulminazzo/blocksmith/data/mongodb/MongoRepositoryTest.groovy
@@ -6,8 +6,16 @@ import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
 import com.mongodb.reactivestreams.client.MongoCollection
 import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.mongo.packageresolver.PlatformPackageResolver
 import de.flapdoodle.embed.mongo.transitions.Mongod
+import de.flapdoodle.embed.mongo.transitions.PackageOfCommandDistribution
 import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess
+import de.flapdoodle.embed.process.distribution.Distribution
+import de.flapdoodle.embed.process.distribution.PackageResolver
+import de.flapdoodle.os.CommonOS
+import de.flapdoodle.os.ImmutablePlatform
+import de.flapdoodle.os.linux.LinuxDistribution
+import de.flapdoodle.os.linux.UbuntuVersion
 import de.flapdoodle.reverse.TransitionWalker
 import it.fulminazzo.blocksmith.data.RepositoryTest
 import it.fulminazzo.blocksmith.data.User
@@ -25,7 +33,31 @@ class MongoRepositoryTest extends RepositoryTest<MongoRepository<User, Long>> {
     private MongoCollection<User> collection
 
     void setup() {
-        server = Mongod.instance().start(Version.V7_0_0)
+        server = Mongod.builder()
+                .packageOfDistribution(PackageOfCommandDistribution.builder()
+                        .commandPackageResolver(command -> {
+                            def resolver = new PlatformPackageResolver(command)
+                            return (PackageResolver) ((distribution) -> {
+                                try {
+                                    return resolver.packageFor(distribution)
+                                } catch (IllegalArgumentException e) {
+                                    if (distribution.platform().operatingSystem() == CommonOS.Linux) {
+                                        def fallbackDist = Distribution.of(
+                                                distribution.version(),
+                                                ImmutablePlatform.copyOf(distribution.platform())
+                                                        .withDistribution(LinuxDistribution.Ubuntu)
+                                                        .withVersion(UbuntuVersion.Ubuntu_22_04)
+                                        )
+                                        return resolver.packageFor(fallbackDist)
+                                    }
+                                    throw e
+                                }
+                            })
+                        })
+                        .build()
+                )
+                .build()
+                .start(Version.Main.V7_0)
 
         def pojoCodec = CodecRegistries.fromRegistries(MongoClientSettings.defaultCodecRegistry,
                 CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
