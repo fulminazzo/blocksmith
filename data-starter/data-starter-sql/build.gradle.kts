15f1cd09a4b338f5518aa04a8dc5c5db529a940d
user <fulminazzo21@gmail.com>
2026-02-11 02:40:26 +0100

    Added dependency to base module

--- data-starter/data-starter-sql/build.gradle.kts
+++ data-starter/data-starter-sql/build.gradle.kts
@@ -2,6 +2,8 @@ dependencies {
     api(libs.jooq)
     api(libs.hikaricp)
 
+    api(project(":data-starter:data-starter-base"))
+
     testImplementation(libs.h2)
     testImplementation(libs.sqlite)
 }
