35022dbeed3f0091cc91e1d6fe4d4a057c73f48d
user <fulminazzo21@gmail.com>
2026-02-11 19:27:39 +0100

    Added slf4j and log4j dependencies

--- data-starter/data-starter-mongo/build.gradle.kts
+++ data-starter/data-starter-mongo/build.gradle.kts
@@ -4,5 +4,7 @@ dependencies {
     api(libs.mongodb.driver.async)
     api(libs.reactor)
 
+    testImplementation(libs.slf4j)
+    testImplementation(libs.bundles.log4j)
     testImplementation(libs.embedded.mongodb)
 }
