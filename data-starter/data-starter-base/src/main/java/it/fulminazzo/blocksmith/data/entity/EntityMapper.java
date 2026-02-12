eb710ec8f61fe7b6a78d76d5bad296649aa9f152
user <fulminazzo21@gmail.com>
2026-02-12 14:33:10 +0100

    Created EntityMapper

---
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/entity/EntityMapper.java
@@ -0,0 +1,55 @@
+package it.fulminazzo.blocksmith.data.entity;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+/**
+ * Responsible for accessing information of an entity.
+ *
+ * @param <T>  the type of the entity
+ * @param <ID> the type of the id of the entity (should be unique)
+ */
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class EntityMapper<T, ID> {
+    private final @NotNull Class<T> dataType;
+    private final @NotNull Function<T, ID> idMapper;
+
+    /**
+     * Gets the entity Java class.
+     *
+     * @return the type
+     */
+    public @NotNull Class<T> getType() {
+        return dataType;
+    }
+
+    /**
+     * Gets the ID of the entity.
+     *
+     * @param entity the entity
+     * @return the id
+     */
+    public @NotNull ID getId(final @NotNull T entity) {
+        return idMapper.apply(entity);
+    }
+
+    /**
+     * Creates a new entity mapper.
+     *
+     * @param <T>      the type of the entity
+     * @param <ID>     the type of the id of the data (should be unique)
+     * @param type     the entity Java class
+     * @param idMapper the function to get the ID of the entity
+     * @return the entity mapper
+     */
+    public static <T, ID> @NotNull EntityMapper<T, ID> create(
+            final @NotNull Class<T> type,
+            final @NotNull Function<T, ID> idMapper
+    ) {
+        return new EntityMapper<>(type, idMapper);
+    }
+
+}
