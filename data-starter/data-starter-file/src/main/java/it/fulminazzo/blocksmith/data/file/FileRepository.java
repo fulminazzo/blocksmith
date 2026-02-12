82d6a105185d362df3cd185a6d8526ce31050df1
user <fulminazzo21@gmail.com>
2026-02-12 23:59:25 +0100

    Updated FileRepository with FileQueryEngine field Totally re-implemented methods with new QueryEngine#query method

--- data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/FileRepository.java
+++ data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/FileRepository.java
@@ -1,428 +1,122 @@
 package it.fulminazzo.blocksmith.data.file;
 
 import it.fulminazzo.blocksmith.config.ConfigurationAdapter;
-import it.fulminazzo.blocksmith.config.ConfigurationFormat;
 import it.fulminazzo.blocksmith.data.AbstractRepository;
 import it.fulminazzo.blocksmith.data.Repository;
 import it.fulminazzo.blocksmith.data.entity.EntityMapper;
-import it.fulminazzo.blocksmith.function.*;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-import java.util.concurrent.Executor;
-import java.util.stream.Collectors;
 
 /**
- * A basic implementation of {@link Repository} that stores data on disk.
+ * Implementation of {@link Repository} that stores data on disk.
  *
  * @param <T>  the type of the entities
  * @param <ID> the type of the id of the entities (will be used as files names)
  */
-public class FileRepository<T, ID> extends AbstractRepository<T, ID> {
-    protected final @NotNull ConfigurationAdapter adapter;
-    private final @NotNull File dataDirectory;
-    private final @NotNull Executor executor;
-    private final @NotNull ConfigurationFormat format;
+public class FileRepository<T, ID> extends AbstractRepository<T, ID, FileQueryEngine<T, ID>> {
 
-    protected FileRepository(final @NotNull File dataDirectory,
-                             final @NotNull EntityMapper<T, ID> entityMapper,
-                             final @NotNull Executor executor,
-                             final @NotNull Logger logger,
-                             final @NotNull ConfigurationFormat format) {
-        super(entityMapper);
-        this.adapter = ConfigurationAdapter.newAdapter(logger, format);
-        this.dataDirectory = dataDirectory;
-        this.executor = executor;
-        this.format = format;
+    /**
+     * Instantiates a new File repository.
+     *
+     * @param queryEngine  the query engine
+     * @param entityMapper the entity mapper
+     */
+    protected FileRepository(final @NotNull FileQueryEngine<T, ID> queryEngine,
+                             final @NotNull EntityMapper<T, ID> entityMapper) {
+        super(queryEngine, entityMapper);
     }
 
     @Override
     public @NotNull CompletableFuture<Optional<T>> findById(final @NotNull ID id) {
-        return executeOnSingle(id, f -> {
-            if (f.exists()) return Optional.of(adapter.load(f, entityMapper.getType()));
+        return queryEngine.query(a -> {
+            File file = queryEngine.getDataFile(id);
+            if (file.exists()) return Optional.of(a.load(file, entityMapper.getType()));
             else return Optional.empty();
         });
     }
 
     @Override
     public @NotNull CompletableFuture<Boolean> existsById(final @NotNull ID id) {
-        return executeOnSingle(id, File::exists);
+        return queryEngine.query(a -> queryEngine.getDataFile(id).exists());
     }
 
     @Override
     public @NotNull CompletableFuture<T> save(final @NotNull T entity) {
-        return executeOnSingleData(entity, f -> {
-            adapter.store(f, entity);
+        return queryEngine.query(a -> {
+            saveSingle(a, entity);
             return entity;
         });
     }
 
     @Override
     protected @NotNull CompletableFuture<?> deleteImpl(final @NotNull ID id) {
-        return executeOnSingle(id, f -> {
-            Files.deleteIfExists(f.toPath());
+        return queryEngine.query(a -> {
+            File file = queryEngine.getDataFile(id);
+            return Files.deleteIfExists(file.toPath());
         });
     }
 
     @Override
     public @NotNull CompletableFuture<Collection<T>> findAll() {
-        return executeOnMany(f -> {
-            return adapter.load(f, entityMapper.getType());
+        return queryEngine.query(a -> {
+            List<T> result = new ArrayList<>();
+            for (File file : queryEngine.getFiles())
+                result.add(a.load(file, entityMapper.getType()));
+            return result;
         });
     }
 
     @Override
     protected @NotNull CompletableFuture<Collection<T>> findAllByIdImpl(final @NotNull Collection<ID> ids) {
-        return executeOnMany(ids, (f, i) -> {
-            return adapter.load(f, entityMapper.getType());
+        return queryEngine.query(a -> {
+            List<File> files = new ArrayList<>();
+            for (ID id : ids)
+                files.add(queryEngine.getDataFile(id));
+            List<T> result = new ArrayList<>();
+            for (File file : files)
+                if (file.exists())
+                    result.add(a.load(file, entityMapper.getType()));
+            return result;
         });
     }
 
     @Override
     protected @NotNull CompletableFuture<Collection<T>> saveAllImpl(final @NotNull Collection<T> entities) {
-        return executeOnManyData(entities, (f, t) -> {
-            adapter.store(f, t);
-            return t;
+        return queryEngine.query(a -> {
+            for (T e : entities) saveSingle(a, e);
+            return entities;
         });
     }
 
     @Override
     protected @NotNull CompletableFuture<?> deleteAllImpl(final @NotNull Collection<ID> ids) {
-        return executeOnMany(ids, (f, i) -> {
-            Files.deleteIfExists(f.toPath());
+        return queryEngine.query(a -> {
+            for (ID id : ids) {
+                File file = queryEngine.getDataFile(id);
+                Files.deleteIfExists(file.toPath());
+            }
+            return null;
         });
     }
 
     @Override
     public @NotNull CompletableFuture<Long> count() {
-        return execute(() -> (long) getFiles().size());
+        return queryEngine.query(a -> (long) queryEngine.getFiles().size());
     }
 
-    /*
-     * MULTIPLE
-     */
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnMany(
-            final @NotNull ConsumerException<File, IOException> function
-    ) {
-        return execute(() -> {
-            for (File dataFile : getFiles()) function.accept(dataFile);
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param <R>      the type of the result
-     * @param function the function to execute
-     * @return a collection containing the results for each id
-     */
-    protected <R> @NotNull CompletableFuture<Collection<R>> executeOnMany(
-            final @NotNull FunctionException<File, R, IOException> function
-    ) {
-        return execute(() -> {
-            final List<R> result = new ArrayList<>();
-            for (File dataFile : getFiles()) result.add(function.apply(dataFile));
-            return result;
-        });
-    }
-
-    /*
-     * MULTIPLE FILTERED
-     */
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param entries  the entries of the files
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnManyData(
-            final @NotNull Collection<T> entries,
-            final @NotNull ConsumerException<File, IOException> function
-    ) {
-        return executeOnManyData(entries, (f, t) -> {
-            function.accept(f);
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param entries  the entries of the files
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnManyData(
-            final @NotNull Collection<T> entries,
-            final @NotNull BiConsumerException<File, T, IOException> function
-    ) {
-        return execute(() -> {
-            for (T data : entries) {
-                ID id = entityMapper.getId(data);
-                File dataFile = getDataFile(id);
-                function.accept(dataFile, data);
-            }
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param <R>      the type of the result
-     * @param entries  the entries of the files
-     * @param function the function to execute
-     * @return a collection containing the results for each data
-     */
-    protected <R> @NotNull CompletableFuture<Collection<R>> executeOnManyData(
-            final @NotNull Collection<T> entries,
-            final @NotNull FunctionException<File, R, IOException> function
-    ) {
-        return executeOnManyData(entries, (f, t) -> {
-            return function.apply(f);
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param <R>      the type of the result
-     * @param entries  the entries of the files
-     * @param function the function to execute
-     * @return a collection containing the results for each data
-     */
-    protected <R> @NotNull CompletableFuture<Collection<R>> executeOnManyData(
-            final @NotNull Collection<T> entries,
-            final @NotNull BiFunctionException<File, T, R, IOException> function
-    ) {
-        return execute(() -> {
-            final List<R> result = new ArrayList<>();
-            for (T data : entries) {
-                ID id = entityMapper.getId(data);
-                File dataFile = getDataFile(id);
-                result.add(function.apply(dataFile, data));
-            }
-            return result;
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param ids      the ids of the files
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnMany(
-            final @NotNull Collection<ID> ids,
-            final @NotNull ConsumerException<File, IOException> function
-    ) {
-        return executeOnMany(ids, (f, i) -> {
-            function.accept(f);
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param ids      the ids of the files
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnMany(
-            final @NotNull Collection<ID> ids,
-            final @NotNull BiConsumerException<File, ID, IOException> function
-    ) {
-        return execute(() -> {
-            for (ID id : ids) {
-                File dataFile = getDataFile(id);
-                function.accept(dataFile, id);
-            }
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param <R>      the type of the result
-     * @param ids      the ids of the files
-     * @param function the function to execute
-     * @return a collection containing the results for each id
-     */
-    protected <R> @NotNull CompletableFuture<Collection<R>> executeOnMany(
-            final @NotNull Collection<ID> ids,
-            final @NotNull FunctionException<File, R, IOException> function
-    ) {
-        return executeOnMany(ids, (f, i) -> {
-            return function.apply(f);
-        });
-    }
-
-    /**
-     * Executes the given function on multiple data files.
-     *
-     * @param <R>      the type of the result
-     * @param ids      the ids of the files
-     * @param function the function to execute
-     * @return a collection containing the results for each id
-     */
-    protected <R> @NotNull CompletableFuture<Collection<R>> executeOnMany(
-            final @NotNull Collection<ID> ids,
-            final @NotNull BiFunctionException<File, ID, R, IOException> function
-    ) {
-        return execute(() -> {
-            final List<R> result = new ArrayList<>();
-            for (ID id : ids) {
-                File dataFile = getDataFile(id);
-                result.add(function.apply(dataFile, id));
-            }
-            return result;
-        });
-    }
-
-    /*
-     * SINGLE
-     */
-
-    /**
-     * Executes the given function on a single data file.
-     *
-     * @param data     the data of the file
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnSingleData(
-            final @NotNull T data,
-            final @NotNull ConsumerException<File, IOException> function
-    ) {
-        return executeOnSingle(
-                entityMapper.getId(data),
-                function
-        );
-    }
-
-    /**
-     * Executes the given function on a single data file.
-     *
-     * @param <R>      the type of the result
-     * @param data     the data of the file
-     * @param function the function to execute
-     * @return the result
-     */
-    protected <R> @NotNull CompletableFuture<R> executeOnSingleData(
-            final @NotNull T data,
-            final @NotNull FunctionException<File, R, IOException> function
-    ) {
-        return executeOnSingle(
-                entityMapper.getId(data),
-                function
-        );
-    }
-
-    /**
-     * Executes the given function on a single data file.
-     *
-     * @param id       the id of the file
-     * @param function the function to execute
-     * @return the result
-     */
-    protected @NotNull CompletableFuture<?> executeOnSingle(
-            final @NotNull ID id,
-            final @NotNull ConsumerException<File, IOException> function
-    ) {
-        return execute(() -> function.accept(getDataFile(id)));
-    }
-
-    /**
-     * Executes the given function on a single data file.
-     *
-     * @param <R>      the type of the result
-     * @param id       the id of the file
-     * @param function the function to execute
-     * @return the result
-     */
-    protected <R> @NotNull CompletableFuture<R> executeOnSingle(
-            final @NotNull ID id,
-            final @NotNull FunctionException<File, R, IOException> function
-    ) {
-        return execute(() -> function.apply(getDataFile(id)));
-    }
-
-    private @NotNull CompletableFuture<?> execute(
-            final @NotNull RunnableException<IOException> function
-    ) {
-        return CompletableFuture.runAsync(() -> {
-            try {
-                function.run();
-            } catch (IOException e) {
-                throw new CompletionException(e);
-            }
-        }, executor);
-    }
-
-    private <R> @NotNull CompletableFuture<R> execute(
-            final @NotNull SupplierException<R, IOException> function
-    ) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                return function.get();
-            } catch (IOException e) {
-                throw new CompletionException(e);
-            }
-        }, executor);
-    }
-
-    /**
-     * Gets all the files of the data.
-     *
-     * @return the files
-     */
-    protected @NotNull Collection<File> getFiles() {
-        if (dataDirectory.exists()) {
-            File[] files = dataDirectory.listFiles();
-            if (files != null)
-                return Arrays.stream(files)
-                        .filter(f -> f.getName().endsWith("." + format.getFileExtension()))
-                        .collect(Collectors.toList());
-        }
-        return new ArrayList<>();
-    }
-
-    /**
-     * Gets the corresponding data file to the id.
-     *
-     * @param id the id
-     * @return the data file
-     * @throws IOException in case of any exception
-     */
-    protected @NotNull File getDataFile(final @NotNull ID id) throws IOException {
-        if (!dataDirectory.isDirectory())
-            Files.createDirectories(dataDirectory.toPath());
-        return format.getFile(dataDirectory, id.toString());
-    }
-
-    /**
-     * Gets a new builder for this class.
-     *
-     * @param <T>      the type of the data
-     * @param dataType the data type
-     * @return the builder
-     */
-    public static <T> @NotNull FileRepositoryBuilder<T> builder(final @NotNull Class<T> dataType) {
-        return new FileRepositoryBuilder<>(dataType);
+    private void saveSingle(final @NotNull ConfigurationAdapter adapter,
+                            final @NotNull T entity) throws IOException {
+        ID id = entityMapper.getId(entity);
+        File file = queryEngine.getDataFile(id);
+        adapter.store(file, entity);
     }
 
 }
