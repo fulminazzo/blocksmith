eeb0d88a6072f4c1cb594cd81d2298a34e503290
user <fulminazzo21@gmail.com>
2026-02-12 23:39:44 +0100

    Updated SqlRepository with usage of queryEngine Extracted saveSingle private method Removed unused fields

--- data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/SqlRepository.java
+++ data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/SqlRepository.java
@@ -5,162 +5,120 @@ import it.fulminazzo.blocksmith.data.Repository;
 import it.fulminazzo.blocksmith.data.entity.EntityMapper;
 import org.jetbrains.annotations.NotNull;
 import org.jooq.DSLContext;
-import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.Table;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
- * A basic implementation of {@link Repository} for SQL databases.
+ * Implementation of {@link Repository} for SQL databases.
  *
  * @param <T>  the type of the entities
  * @param <ID> the type of the id of the entities (should be unique)
  * @param <TB> the type of the table
  */
 @SuppressWarnings({"resource"})
-public class SqlRepository<T, ID, TB extends Table<?>> extends AbstractRepository<T, ID> {
-    private final @NotNull DSLContext context;
-    protected final @NotNull TB table;
-    protected final @NotNull Field<ID> idColumn;
-    private final @NotNull Executor executor;
+public class SqlRepository<T, ID, TB extends Table<?>> extends AbstractRepository<T, ID, SqlQueryEngine<T, ID, TB>> {
 
-    protected SqlRepository(final @NotNull DSLContext context,
-                            final @NotNull TB table,
-                            final @NotNull Field<ID> idColumn,
-                            final @NotNull EntityMapper<T, ID> entityMapper,
-                            final @NotNull Executor executor) {
-        super(entityMapper);
-        this.context = context;
-        this.table = table;
-        this.idColumn = idColumn;
-        this.executor = executor;
+    /**
+     * Instantiates a new SQL repository.
+     *
+     * @param queryEngine  the query engine
+     * @param entityMapper the entity mapper
+     */
+    protected SqlRepository(final @NotNull SqlQueryEngine<T, ID, TB> queryEngine,
+                            final @NotNull EntityMapper<T, ID> entityMapper) {
+        super(queryEngine, entityMapper);
     }
 
     @Override
     public @NotNull CompletableFuture<Optional<T>> findById(final @NotNull ID id) {
-        return query(dsl ->
-                dsl.selectFrom(table())
-                        .where(idColumn.eq(id))
+        return queryEngine.query((dsl, table) ->
+                dsl.selectFrom(table)
+                        .where(queryEngine.idEquals(id))
                         .fetchOptionalInto(entityMapper.getType())
         );
     }
 
     @Override
     public @NotNull CompletableFuture<Boolean> existsById(final @NotNull ID id) {
-        return query(dsl -> dsl.fetchExists(
+        return queryEngine.query((dsl, table) -> dsl.fetchExists(
                 dsl.selectOne()
-                        .from(table())
-                        .where(idColumn.eq(id))
+                        .from(table)
+                        .where(queryEngine.idEquals(id))
         ));
     }
 
     @Override
     public @NotNull CompletableFuture<T> save(final @NotNull T entity) {
-        return query(dsl -> {
-            Record record = dsl.newRecord(table(), entity);
-            return dsl.insertInto(table())
-                    .set(record)
-                    .onConflict(idColumn)
-                    .doUpdate()
-                    .set(record)
-                    .returning()
-                    .fetchOneInto(entityMapper.getType());
-        });
+        return queryEngine.query((dsl, table) -> saveSingle(dsl, table, entity));
     }
 
     @Override
     protected @NotNull CompletableFuture<?> deleteImpl(final @NotNull ID id) {
-        return query(dsl ->
-                dsl.deleteFrom(table())
-                        .where(idColumn.eq(id))
+        return queryEngine.query((dsl, table) ->
+                dsl.deleteFrom(table)
+                        .where(queryEngine.idEquals(id))
                         .execute()
         );
     }
 
     @Override
     public @NotNull CompletableFuture<Collection<T>> findAll() {
-        return query(dsl ->
-                dsl.selectFrom(table())
+        return queryEngine.query((dsl, table) ->
+                dsl.selectFrom(table)
                         .fetchInto(entityMapper.getType())
         );
     }
 
     @Override
     protected @NotNull CompletableFuture<Collection<T>> findAllByIdImpl(final @NotNull Collection<ID> ids) {
-        return query(dsl -> dsl.selectFrom(table())
-                .where(idColumn.in(ids))
+        return queryEngine.query((dsl, table) -> dsl.selectFrom(table)
+                .where(queryEngine.idIn(ids))
                 .fetchInto(entityMapper.getType())
         );
     }
 
     @Override
     protected @NotNull CompletableFuture<Collection<T>> saveAllImpl(final @NotNull Collection<T> entities) {
-        return query(dsl -> {
-            List<Record> records = entities.stream()
-                    .map(entry -> dsl.newRecord(table(), entry))
-                    .collect(Collectors.toList());
-            dsl.batch(
-                            dsl.insertInto(table())
-                                    .set(records.get(0))
-                                    .onConflict(idColumn)
-                                    .doUpdate()
-                                    .set(records.get(0))
-                    ).bind(
-                            records.stream()
-                                    .map(r -> {
-                                        Object[] original = r.intoArray();
-                                        Object[] doubled = new Object[original.length * 2];
-                                        System.arraycopy(original, 0, doubled, 0, original.length);
-                                        System.arraycopy(original, 0, doubled, original.length, original.length);
-                                        return doubled;
-                                    })
-                                    .toArray(Object[][]::new)
-                    )
-                    .execute();
-            return records.stream()
-                    .map(r -> r.get(idColumn))
-                    .collect(Collectors.toList());
-        }).thenCompose(this::findAllById);
+        return queryEngine.batched((dsl, table) -> {
+            List<T> result = new ArrayList<>();
+            for (T e : entities)
+                result.add(saveSingle(dsl, table, e));
+            return result;
+        });
     }
 
     @Override
     protected @NotNull CompletableFuture<?> deleteAllImpl(final @NotNull Collection<ID> ids) {
-        if (ids.isEmpty()) return CompletableFuture.completedFuture(Collections.emptyList());
-        return query(dsl ->
-                dsl.deleteFrom(table())
-                        .where(idColumn.in(ids))
+        return queryEngine.query((dsl, table) ->
+                dsl.deleteFrom(table)
+                        .where(queryEngine.idIn(ids))
                         .execute()
         );
     }
 
     @Override
     public @NotNull CompletableFuture<Long> count() {
-        return query(dsl -> dsl.selectCount()
-                .from(table())
-                .fetchOne(0, Long.class));
+        return queryEngine.query((dsl, table) ->
+                dsl.selectCount()
+                        .from(table)
+                        .fetchOne(0, Long.class)
+        );
     }
 
-    /**
-     * Executes a general query and returns the result.
-     *
-     * @param <R>           the type of the result
-     * @param queryFunction the query
-     * @return the result
-     */
-    protected <R> @NotNull CompletableFuture<R> query(final @NotNull Function<DSLContext, R> queryFunction) {
-        return CompletableFuture.supplyAsync(() -> queryFunction.apply(context), executor);
-    }
-
-    private @NotNull Table<?> table() {
-        return table;
+    private @NotNull T saveSingle(final @NotNull DSLContext dsl,
+                                  final @NotNull Table<?> table,
+                                  final @NotNull T entity) {
+        Record record = dsl.newRecord(table, entity);
+        return Objects.requireNonNull(dsl.insertInto(table)
+                .set(record)
+                .onConflict(queryEngine.getIdColumn())
+                .doUpdate()
+                .set(record)
+                .returning()
+                .fetchOneInto(entityMapper.getType()), "The insertInto query did not return the stored entity accordingly");
     }
 
 }
