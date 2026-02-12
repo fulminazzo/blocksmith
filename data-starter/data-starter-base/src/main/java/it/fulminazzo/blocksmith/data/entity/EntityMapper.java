efd9381e2d2ecfa2c81857f837256d1dbd1f8896
user <fulminazzo21@gmail.com>
2026-02-12 14:45:13 +0100

    Added create function with explicit id field name

--- data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/entity/EntityMapper.java
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/entity/EntityMapper.java
@@ -3,6 +3,8 @@ package it.fulminazzo.blocksmith.data.entity;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.joor.Reflect;
+import org.joor.ReflectException;
 
 import java.util.function.Function;
 
@@ -34,6 +36,32 @@ public final class EntityMapper<T, ID> {
      */
     public @NotNull ID getId(final @NotNull T entity) {
         return idMapper.apply(entity);
+    }
+
+    /**
+     * Creates a new entity mapper.
+     *
+     * @param <T>         the type of the entity
+     * @param <ID>        the type of the id of the entity (should be unique)
+     * @param type        the entity Java class
+     * @param idFieldName the name of the field that identifies the entity
+     * @return the entity mapper
+     */
+    public static <T, ID> @NotNull EntityMapper<T, ID> create(
+            final @NotNull Class<T> type,
+            final @NotNull String idFieldName
+    ) {
+        try {
+            Reflect.onClass(type).field(idFieldName);
+            return create(
+                    type,
+                    e -> Reflect.on(e).field(idFieldName).get()
+            );
+        } catch (ReflectException e) {
+            throw new IllegalArgumentException(String.format("Could not find field '%s' in %s",
+                    idFieldName, type.getCanonicalName())
+            );
+        }
     }
 
     /**
