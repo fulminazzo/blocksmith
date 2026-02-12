92a4f442acd63f1509b33281a8be440eae1f5673
user <fulminazzo21@gmail.com>
2026-02-12 14:56:59 +0100

    Added tests for new create function

--- data-starter/data-starter-base/src/test/groovy/it/fulminazzo/blocksmith/data/entity/EntityMapperTest.groovy
+++ data-starter/data-starter-base/src/test/groovy/it/fulminazzo/blocksmith/data/entity/EntityMapperTest.groovy
@@ -16,8 +16,21 @@ class EntityMapperTest extends Specification {
 
         where:
         mapper                                                   | object      || expectedType | expectedId
+        EntityMapper.create(SimpleEntity, 'uuid') |
+                new SimpleEntity(UUID.nameUUIDFromBytes('Alex'.bytes), 'Alex') || SimpleEntity | UUID.nameUUIDFromBytes('Alex'.bytes)
+        EntityMapper.create(SimpleEntity, 'name') |
+                new SimpleEntity(UUID.nameUUIDFromBytes('Alex'.bytes), 'Alex') || SimpleEntity | 'Alex'
         EntityMapper.create(SimpleEntity, SimpleEntity::getUuid) |
                 new SimpleEntity(UUID.nameUUIDFromBytes('Alex'.bytes), 'Alex') || SimpleEntity | UUID.nameUUIDFromBytes('Alex'.bytes)
+    }
+
+    def 'test that entity mapper throws IllegalArgumentException if could not find argument'() {
+        when:
+        EntityMapper.create(SimpleEntity, 'invalid')
+
+        then:
+        def e = thrown(IllegalArgumentException)
+        e.message == "Could not find field 'invalid' in ${SimpleEntity.canonicalName}"
     }
 
 }
