a96179fc0310dcaaf08cb43bfdde0cc3f468a4e5
user <fulminazzo21@gmail.com>
2026-02-13 01:36:56 +0100

    Created FileDataSource with internal newRepository methods

---
+++ data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/FileDataSource.java
@@ -0,0 +1,73 @@
+package it.fulminazzo.blocksmith.data.file;
+
+import it.fulminazzo.blocksmith.config.ConfigurationAdapter;
+import it.fulminazzo.blocksmith.config.ConfigurationFormat;
+import it.fulminazzo.blocksmith.data.Repository;
+import it.fulminazzo.blocksmith.data.RepositoryDataSource;
+import it.fulminazzo.blocksmith.data.entity.EntityMapper;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.util.concurrent.Executor;
+
+/**
+ * Implementation of {@link RepositoryDataSource} for file-based repositories.
+ */
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public final class FileDataSource implements RepositoryDataSource {
+    private final @NotNull Executor executor;
+
+    /**
+     * Creates a new repository.
+     *
+     * @param <T>           the type of the entities
+     * @param <ID>          the type of the id of the entities
+     * @param entityType     the entity Java class
+     * @param dataDirectory the directory where all the data is stored
+     * @param logger        the logger
+     * @param format        the configuration format to use for storing
+     * @return the repository
+     */
+    public <T, ID> @NotNull Repository<T, ID> newRepository(
+            final @NotNull Class<T> entityType,
+            final @NotNull File dataDirectory,
+            final @NotNull Logger logger,
+            final @NotNull ConfigurationFormat format
+    ) {
+        return newRepository(EntityMapper.create(entityType), dataDirectory, logger, format);
+    }
+
+    /**
+     * Creates a new repository.
+     *
+     * @param <T>           the type of the entities
+     * @param <ID>          the type of the id of the entities
+     * @param entityMapper  the entity mapper
+     * @param dataDirectory the directory where all the data is stored
+     * @param logger        the logger
+     * @param format        the configuration format to use for storing
+     * @return the repository
+     */
+    public <T, ID> @NotNull Repository<T, ID> newRepository(
+            final @NotNull EntityMapper<T, ID> entityMapper,
+            final @NotNull File dataDirectory,
+            final @NotNull Logger logger,
+            final @NotNull ConfigurationFormat format
+    ) {
+        FileQueryEngine<T, ID> engine = new FileQueryEngine<>(
+                ConfigurationAdapter.newAdapter(logger, format),
+                format,
+                dataDirectory,
+                executor
+        );
+        return new FileRepository<>(engine, entityMapper);
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
