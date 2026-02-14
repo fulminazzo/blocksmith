16fb921a796de6e863a1136a7678a636b4e35d34
user <fulminazzo21@gmail.com>
2026-02-14 15:12:27 +0100

    Added Jakarta validations

--- data-starter/data-starter-base/src/test/java/it/fulminazzo/blocksmith/Cat.java
+++ data-starter/data-starter-base/src/test/java/it/fulminazzo/blocksmith/Cat.java
@@ -1,5 +1,7 @@
 package it.fulminazzo.blocksmith;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public final class Cat {
 
+    @NotEmpty(message = "Cat name should not be empty")
     String name;
 
+    @Min(value = 1, message = "Cat age should at least be 1")
     int age;
 
     boolean hasOwner;
