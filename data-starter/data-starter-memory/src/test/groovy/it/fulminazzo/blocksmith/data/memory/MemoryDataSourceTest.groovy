115c654c2a42a04fdb7880888147e73842fc0561
user <fulminazzo21@gmail.com>
2026-02-14 15:51:31 +0100

    Added tests for MemoryDataSource

---
+++ data-starter/data-starter-memory/src/test/groovy/it/fulminazzo/blocksmith/data/memory/MemoryDataSourceTest.groovy
@@ -0,0 +1,31 @@
+package it.fulminazzo.blocksmith.data.memory
+
+
+import it.fulminazzo.blocksmith.data.User
+import spock.lang.Specification
+
+import java.util.concurrent.Executors
+
+class MemoryDataSourceTest extends Specification {
+
+    def 'test datasource life cycle'() {
+        given:
+        def executor = Executors.newSingleThreadExecutor()
+
+        and:
+        def dataSource = MemoryDataSource.create(executor)
+
+        when:
+        def repository = dataSource.newRepository(User)
+
+        then:
+        repository != null
+
+        when:
+        dataSource.close()
+
+        then:
+        executor.isShutdown()
+    }
+
+}
