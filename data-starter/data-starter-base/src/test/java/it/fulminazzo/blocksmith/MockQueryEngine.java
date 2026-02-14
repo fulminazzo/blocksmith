bd51d461f2755113a92eb1611ba3aa408af995cd
user <fulminazzo21@gmail.com>
2026-02-14 15:09:25 +0100

    Added query method

--- data-starter/data-starter-base/src/test/java/it/fulminazzo/blocksmith/MockQueryEngine.java
+++ data-starter/data-starter-base/src/test/java/it/fulminazzo/blocksmith/MockQueryEngine.java
@@ -1,17 +1,30 @@
 package it.fulminazzo.blocksmith;
 
 import it.fulminazzo.blocksmith.data.QueryEngine;
-import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 /**
  * Mock {@link QueryEngine} for testing purposes.
  */
 public class MockQueryEngine implements QueryEngine<Cat, String> {
-    @Getter
     private final @NotNull Map<String, Cat> map = new ConcurrentHashMap<>();
+
+    /**
+     * Executes a mock query asynchronously.
+     *
+     * @param <R>           the type of the query result
+     * @param queryFunction the query
+     * @return the query result
+     */
+    public <R> @NotNull CompletableFuture<R> query(
+            final @NotNull Function<Map<String, Cat>, R> queryFunction
+    ) {
+        return CompletableFuture.supplyAsync(() -> queryFunction.apply(map));
+    }
 
 }
