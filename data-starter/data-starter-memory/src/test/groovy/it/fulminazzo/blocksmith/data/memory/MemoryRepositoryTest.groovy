0246651192ac7dcf5bc170e4b613fba05a28b9f0
user <fulminazzo21@gmail.com>
2026-02-14 15:47:26 +0100

    Added tests for MemoryRepository

---
+++ data-starter/data-starter-memory/src/test/groovy/it/fulminazzo/blocksmith/data/memory/MemoryRepositoryTest.groovy
@@ -0,0 +1,50 @@
+package it.fulminazzo.blocksmith.data.memory
+
+import it.fulminazzo.blocksmith.data.RepositoryTest
+import it.fulminazzo.blocksmith.data.User
+import it.fulminazzo.blocksmith.data.entity.EntityMapper
+import org.jetbrains.annotations.NotNull
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MemoryRepositoryTest extends RepositoryTest<MemoryRepository<User, Long>> {
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor()
+    private static final MemoryQueryEngine<User, Long> engine = new MemoryQueryEngine<>(executor)
+
+    void setup() {
+        setupRepository()
+    }
+
+    void cleanup() {
+        clearData()
+    }
+
+    void cleanupSpec() {
+        executor?.shutdown()
+    }
+
+    @Override
+    MemoryRepository<User, Long> initializeRepository() {
+        return new MemoryRepository<>(
+                engine,
+                EntityMapper.create(User)
+        )
+    }
+
+    @Override
+    boolean exists(final @NotNull Long id) {
+        return engine.internalMap.containsKey(id)
+    }
+
+    @Override
+    void insert(final @NotNull User entity) {
+        engine.internalMap.put(entity.id, entity)
+    }
+
+    @Override
+    void remove(final @NotNull Long id) {
+        engine.internalMap.remove(id)
+    }
+
+}
