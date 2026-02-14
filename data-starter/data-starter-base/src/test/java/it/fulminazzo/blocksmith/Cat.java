1cd7f93b9756e98742506b0108094720ff614c6d
user <fulminazzo21@gmail.com>
2026-02-14 15:06:51 +0100

    Created Cat bean for testing purposes

---
+++ data-starter/data-starter-base/src/test/java/it/fulminazzo/blocksmith/Cat.java
@@ -0,0 +1,18 @@
+package it.fulminazzo.blocksmith;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public final class Cat {
+
+    String name;
+
+    int age;
+
+    boolean hasOwner;
+
+}
