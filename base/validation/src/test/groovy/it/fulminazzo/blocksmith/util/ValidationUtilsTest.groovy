5e2b2dc1a5a3441f27824540df1fb1b3077d13d9
user <fulminazzo21@gmail.com>
2026-02-14 14:41:56 +0100

    Added more tests for ValidationUtils

--- base/validation/src/test/groovy/it/fulminazzo/blocksmith/util/ValidationUtilsTest.groovy
+++ base/validation/src/test/groovy/it/fulminazzo/blocksmith/util/ValidationUtilsTest.groovy
@@ -4,6 +4,67 @@ import spock.lang.Specification
 
 class ValidationUtilsTest extends Specification {
 
+    def 'test that validate does not throw on valid bean'() {
+        given:
+        def person = new Person('Alex', 'Fulminazzo', 18)
+
+        when:
+        ValidationUtils.validate(person)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'test that validate fails verification for #person'() {
+        when:
+        ValidationUtils.validate(person)
+
+        then:
+        def e = thrown(ValidationUtils.ViolationException)
+        e.message == expected
+
+        where:
+        person                               || expected
+        new Person(null, 'Fulminazzo', 18)   || 'Name cannot be empty'
+        new Person('', 'Fulminazzo', 18)     || 'Name cannot be empty'
+        new Person('Alex', null, 18)         || 'Lastname cannot be empty'
+        new Person('Alex', '', 18)           || 'Lastname cannot be empty'
+        new Person('Alex', 'Fulminazzo', -1) || 'Must be at least 18 years old to use the application'
+        new Person('Alex', 'Fulminazzo', 0)  || 'Must be at least 18 years old to use the application'
+    }
+
+    def 'test that validateField does not throw on valid #fieldName'() {
+        when:
+        ValidationUtils.validateField(Person, fieldName, fieldValue)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        fieldName  | fieldValue
+        'name'     | 'Alex'
+        'lastname' | 'Fulminazzo'
+        'age'      | 18
+    }
+
+    def 'test that validateField fails verification for #fieldName'() {
+        when:
+        ValidationUtils.validateField(Person, fieldName, fieldValue)
+
+        then:
+        def e = thrown(ValidationUtils.ViolationException)
+        e.message == expected
+
+        where:
+        fieldName  | fieldValue || expected
+        'name'     | null       || 'Name cannot be empty'
+        'name'     | ''         || 'Name cannot be empty'
+        'lastname' | null       || 'Lastname cannot be empty'
+        'lastname' | ''         || 'Lastname cannot be empty'
+        'age'      | -1         || 'Must be at least 18 years old to use the application'
+        'age'      | 0          || 'Must be at least 18 years old to use the application'
+    }
+
     def 'test that checkNatural throws for #value'() {
         when:
         ValidationUtils.checkNatural(value, 'value')
