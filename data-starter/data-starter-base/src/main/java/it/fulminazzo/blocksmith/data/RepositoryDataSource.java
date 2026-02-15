48e6270dad3a48e321405e319d6fc9521b687826
user <fulminazzo21@gmail.com>
2026-02-15 02:02:20 +0100

    Updated RepositoryDataSource with common newRepository methods and RepositorySettings

--- data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/RepositoryDataSource.java
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/RepositoryDataSource.java
@@ -1,5 +1,8 @@
 package it.fulminazzo.blocksmith.data;
 
+import it.fulminazzo.blocksmith.data.entity.EntityMapper;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Closeable;
 
 /**
@@ -8,7 +11,39 @@ import java.io.Closeable;
  * Identifies all objects that can create repositories for entities.
  * This is a marker interface - each implementation varies in its methods
  * because each backend has different requirements.
+ *
+ * @param <S> the type of the repository settings (to build new repositories)
  */
-public interface RepositoryDataSource extends Closeable {
+public interface RepositoryDataSource<S extends RepositorySettings> extends Closeable {
+
+    /**
+     * Creates a new repository.
+     *
+     * @param <T>        the type of the entities
+     * @param <ID>       the type of the id of the entities
+     * @param entityType the entity Java class
+     * @param settings   the settings to build the repository
+     * @return the repository
+     */
+    default <T, ID> @NotNull Repository<T, ID> newRepository(
+            final @NotNull Class<T> entityType,
+            final @NotNull S settings
+    ) {
+        return newRepository(EntityMapper.create(entityType), settings);
+    }
+
+    /**
+     * Creates a new repository.
+     *
+     * @param <T>          the type of the entities
+     * @param <ID>         the type of the id of the entities
+     * @param entityMapper the entity mapper
+     * @param settings     the settings to build the repository
+     * @return the repository
+     */
+    <T, ID> @NotNull Repository<T, ID> newRepository(
+            final @NotNull EntityMapper<T, ID> entityMapper,
+            final @NotNull S settings
+    );
 
 }
