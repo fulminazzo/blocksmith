fb6fd1f343e07b3a084ae376b49368ad27629cdc
user <fulminazzo21@gmail.com>
2026-02-15 02:20:08 +0100

    Created RedisRepositorySettings

---
+++ data-starter/data-starter-redis/src/main/java/it/fulminazzo/blocksmith/data/redis/RedisRepositorySettings.java
@@ -0,0 +1,42 @@
+package it.fulminazzo.blocksmith.data.redis;
+
+import it.fulminazzo.blocksmith.data.RepositorySettings;
+import it.fulminazzo.blocksmith.data.entity.EntityMapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public final class RedisRepositorySettings extends RepositorySettings {
+    private @Nullable String databaseName;
+    private @Nullable String collectionName;
+    private @Nullable EntityMapper<?, ?> entityMapper;
+
+    public @NotNull String getDatabaseName() {
+        return Objects.requireNonNull(databaseName, "database name has not been specified yet");
+    }
+
+    public @NotNull String getCollectionName() {
+        return Objects.requireNonNull(collectionName, "collection name has not been specified yet");
+    }
+
+    public @NotNull EntityMapper<?, ?> getEntityMapper() {
+        return Objects.requireNonNull(entityMapper, "entity mapper has not been specified yet");
+    }
+
+    public @NotNull RedisRepositorySettings withDatabaseName(final @NotNull String databaseName) {
+        this.databaseName = databaseName;
+        return this;
+    }
+
+    public @NotNull RedisRepositorySettings withCollectionName(final @NotNull String collectionName) {
+        this.collectionName = collectionName;
+        return this;
+    }
+
+    public @NotNull RedisRepositorySettings withEntityMapper(final @NotNull EntityMapper<?, ?> entityMapper) {
+        this.entityMapper = entityMapper;
+        return this;
+    }
+
+}
