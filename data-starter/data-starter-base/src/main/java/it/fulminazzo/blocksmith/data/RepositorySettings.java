f0f01c1a9531a4c9d02e035cd4d38b27f77d0af2
user <fulminazzo21@gmail.com>
2026-02-15 02:01:15 +0100

    Created RepositorySettings abstract empty class

---
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/RepositorySettings.java
@@ -0,0 +1,9 @@
+package it.fulminazzo.blocksmith.data;
+
+/**
+ * Represents a general data holder for repository settings.
+ * Implementation may vary according to repository type.
+ */
+public abstract class RepositorySettings {
+
+}
