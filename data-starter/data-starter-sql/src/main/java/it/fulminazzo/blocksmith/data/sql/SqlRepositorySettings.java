d4aaf8d8c65baea311dc1693f61db67369a73794
user <fulminazzo21@gmail.com>
2026-02-15 02:31:52 +0100

    Created SqlRepositorySettings

---
+++ data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/SqlRepositorySettings.java
@@ -0,0 +1,33 @@
+package it.fulminazzo.blocksmith.data.sql;
+
+import it.fulminazzo.blocksmith.data.RepositorySettings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jooq.Table;
+import org.jooq.TableField;
+
+import java.util.Objects;
+
+public class SqlRepositorySettings extends RepositorySettings {
+    private @Nullable Table<?> table;
+    private @Nullable TableField<?, ?> idColumn;
+
+    public @NotNull Table<?> getTable() {
+        return Objects.requireNonNull(table, "table has not been specified yet");
+    }
+
+    public @NotNull TableField<?, ?> getIdColumn() {
+        return Objects.requireNonNull(idColumn, "id column has not been specified yet");
+    }
+
+    public @NotNull SqlRepositorySettings setTable(final @NotNull Table<?> table) {
+        this.table = table;
+        return this;
+    }
+
+    public @NotNull SqlRepositorySettings setIdColumn(final @NotNull TableField<?, ?> idColumn) {
+        this.idColumn = idColumn;
+        return this;
+    }
+
+}
