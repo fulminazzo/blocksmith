e25b3902838207390d2de625c51308b7045b08ed
user <fulminazzo21@gmail.com>
2026-02-19 02:47:22 +0100

    Created CachedRepositorySettings

---
+++ data-starter/data-starter-cache/src/main/java/it/fulminazzo/blocksmith/data/cache/CachedRepositorySettings.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.blocksmith.data.cache;
+
+import it.fulminazzo.blocksmith.data.RepositorySettings;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CachedRepositorySettings<
+        CS extends RepositorySettings,
+        S extends RepositorySettings
+        > extends RepositorySettings {
+
+    private final @NotNull CS cachedRepositorySettings;
+    private final @NotNull S repositorySettings;
+
+    public static <
+            CS extends RepositorySettings,
+            S extends RepositorySettings
+            > @NotNull CachedRepositorySettings<CS, S> combine(
+            final @NotNull CS cacheRepositorySettings,
+            final @NotNull S repositorySettings
+    ) {
+        return new CachedRepositorySettings<>(
+                cacheRepositorySettings,
+                repositorySettings
+        );
+    }
+
+}
