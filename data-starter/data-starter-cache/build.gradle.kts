113388fdcc71f6ca537f2108f804a04999fd10b9
user <fulminazzo21@gmail.com>
2026-02-19 20:50:44 +0100

    Added sql and redis dependencies for testing purposes

--- data-starter/data-starter-cache/build.gradle.kts
+++ data-starter/data-starter-cache/build.gradle.kts
@@ -1,3 +1,11 @@
 dependencies {
     api(project(":data-starter:data-starter-memory"))
+
+    testImplementation(project(":data-starter:data-starter-redis"))
+    testImplementation(project(":data-starter:data-starter-sql"))
+
+    testImplementation(libs.bundles.log4j)
+
+    testImplementation(libs.embedded.redis)
+    testImplementation(libs.h2)
 }
