7f5f1065e258f7b62951aba4c30ac3dc11b2b4ec
user <fulminazzo21@gmail.com>
2026-02-19 02:59:21 +0100

    Created CacheRepositoryDataSource interface

---
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/CacheRepositoryDataSource.java
@@ -0,0 +1,28 @@
+package it.fulminazzo.blocksmith.data;
+
+import it.fulminazzo.blocksmith.data.entity.EntityMapper;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Special implementation of {@link RepositoryDataSource} for creating cache repositories.
+ *
+ * @param <S> the type of the repository settings (to build new repositories)
+ */
+public interface CacheRepositoryDataSource<S extends CacheRepositorySettings<S>> extends RepositoryDataSource<S> {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    default @NotNull <T, ID> CacheRepository<T, ID> newRepository(
+            final @NotNull Class<T> entityType,
+            final @NotNull S settings
+    ) {
+        return (CacheRepository<T, ID>) RepositoryDataSource.super.newRepository(entityType, settings);
+    }
+
+    @Override
+    @NotNull <T, ID> CacheRepository<T, ID> newRepository(
+            final @NotNull EntityMapper<T, ID> entityMapper,
+            final @NotNull S settings
+    );
+
+}
