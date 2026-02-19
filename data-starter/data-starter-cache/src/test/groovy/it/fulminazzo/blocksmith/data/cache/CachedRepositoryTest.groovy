842c272080ed7a269db90d1bf2da77026b2e8ab7
user <fulminazzo21@gmail.com>
2026-02-19 01:19:06 +0100

    Added more tests for CachedRepository

--- data-starter/data-starter-cache/src/test/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
+++ data-starter/data-starter-cache/src/test/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
@@ -8,6 +8,7 @@ import it.fulminazzo.blocksmith.data.memory.MemoryQueryEngine
 import it.fulminazzo.blocksmith.data.memory.MemoryRepository
 import org.jetbrains.annotations.NotNull
 
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -16,14 +17,32 @@ class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> 
     private static final ExecutorService executor = Executors.newSingleThreadExecutor()
 
     private static final EntityMapper<User, Long> entityMapper = EntityMapper.create(User)
-    private static final MemoryRepository<User, Long> first = new MemoryRepository(
+    private static final MemoryRepository<User, Long> cache = new MemoryRepository(
             new MemoryQueryEngine<>(executor),
             entityMapper
-    )
-    private static final MemoryRepository<User, Long> second = new MemoryRepository(
-            new MemoryQueryEngine<>(executor),
+    ).setExpiry(60_000L)
+    private static final MemoryRepository<User, Long> actual = new MemoryRepository<User, Long>(
+            new MemoryQueryEngine<User, Long>(executor),
             entityMapper
-    )
+    ) {
+
+        @Override
+        protected @NotNull
+        CompletableFuture<User> saveImpl(final @NotNull User entity) {
+            if (entity.id == -1L) entity.id = 3L
+            return super.saveImpl(entity)
+        }
+
+        @Override
+        protected @NotNull
+        CompletableFuture<Collection<User>> saveAllImpl(final @NotNull Collection<User> entities) {
+            entities.each {
+                if (it.id == -1L) it.id = 3L
+            }
+            return super.saveAllImpl(entities)
+        }
+
+    }
 
     void setup() {
         setupRepository()
@@ -37,30 +56,148 @@ class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> 
         executor.shutdown()
     }
 
+    def 'test that findById updates cache repository if not present'() {
+        given:
+        def entity = Users.SAVED1
+
+        expect:
+        !cache.existsById(entity).get()
+
+        when:
+        def found = repository.findById(entity.id).get()
+
+        then:
+        found.isPresent()
+
+        and:
+        found.get() == entity
+
+        and:
+        cache.existsById(entity.id).get()
+    }
+
+    def 'test that save saves on cache repository the updated version of entity'() {
+        given:
+        def expected = Users.NEW1
+        def entity = new User(-1L, expected.username, expected.age)
+
+        when:
+        repository.save(entity).get()
+
+        and:
+        def actual = cache.findById(expected.id).get()
+
+        then:
+        actual.isPresent()
+
+        when:
+        def actualEntity = actual.get()
+
+        then:
+        actualEntity == expected
+    }
+
+    def 'test that delete deletes from both repositories'() {
+        given:
+        def entityId = Users.SAVED2.id
+
+        expect:
+        cache.existsById(entityId).get()
+        actual.existsById(entityId).get()
+
+        when:
+        repository.delete(entityId).get()
+
+        then:
+        !cache.existsById(entityId).get()
+        !actual.existsById(entityId).get()
+    }
+
+    def 'test that findAll returns only all the entities of the main repository'() {
+        given:
+        cache.save(Users.NEW1).get()
+
+        when:
+        def actual = repository.findAll().get()
+
+        then:
+        actual == [Users.SAVED1, Users.SAVED2]
+    }
+
+    def 'test that findAllById queries main repository only for not found entities'() {
+        given:
+        def expected = [Users.SAVED2, Users.NEW1, Users.SAVED1]
+
+        and:
+        cache.save(Users.NEW1).get()
+
+        when:
+        def actual = repository.findAllById(
+                [*expected.collect { it.id }, 9L]
+        ).get()
+
+        then:
+        actual == expected
+    }
+
+    def 'test that saveAll saves on cache repository the updated version of entities'() {
+        given:
+        def expected = [Users.NEW1, Users.NEW2]
+        def entities = [
+                new User(-1L, Users.NEW1.username, Users.NEW1.age),
+                Users.NEW2
+        ]
+
+        when:
+        repository.saveAll(entities).get()
+
+        and:
+        def actual = cache.findAllById(expected.collect { it.id }).get()
+
+        then:
+        actual == expected
+    }
+
+    def 'test that deleteAll deletes from both repositories'() {
+        given:
+        def entitiesIds = [Users.SAVED2].collect { it.id }
+
+        expect:
+        cache.existsById(entitiesIds[0]).get()
+        actual.existsById(entitiesIds[0]).get()
+
+        when:
+        repository.deleteAll(entitiesIds).get()
+
+        then:
+        !cache.existsById(entitiesIds[0]).get()
+        !actual.existsById(entitiesIds[0]).get()
+    }
+
     @Override
     CachedRepository<User, Long> initializeRepository() {
         return new CachedRepository<User, Long>(
                 entityMapper,
-                first,
-                second
+                cache,
+                actual
         )
     }
 
     @Override
     boolean exists(final @NotNull Long id) {
-        return first.existsById(id).get() || second.existsById(id).get()
+        return cache.existsById(id).get() || actual.existsById(id).get()
     }
 
     @Override
     void insert(final @NotNull User entity) {
-        if (entity == Users.SAVED2) first.save(entity).get()
-        second.save(entity).get()
+        if (entity == Users.SAVED2) cache.save(entity).get()
+        actual.save(entity).get()
     }
 
     @Override
     void remove(final @NotNull Long id) {
-        first.delete(id).get()
-        second.delete(id).get()
+        cache.delete(id).get()
+        actual.delete(id).get()
     }
 
 }
