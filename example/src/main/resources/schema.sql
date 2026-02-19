d9b343cbddec2e30ae8c177f8c60d846044e4267
user <fulminazzo21@gmail.com>
2026-02-19 21:23:56 +0100

    Updated schema.sql

--- example/src/main/resources/schema.sql
+++ example/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id       BIGINT PRIMARY KEY,
+    id       BIGINT PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(50),
     lastname VARCHAR(50),
     email    VARCHAR(100) UNIQUE,
