c2c1e0613e057a7923d64349e79f83b87a5489e3
user <fulminazzo21@gmail.com>
2026-02-20 01:47:16 +0100

    Added tests for HybridCachedRepositoryExample

---
+++ example/src/test/groovy/it/fulminazzo/blocksmith/data/HybridCachedRepositoryExampleTest.groovy
@@ -0,0 +1,38 @@
+package it.fulminazzo.blocksmith.data
+
+import ch.vorburger.mariadb4j.DB
+import ch.vorburger.mariadb4j.DBConfigurationBuilder
+import redis.embedded.RedisServer
+import spock.lang.Specification
+
+class HybridCachedRepositoryExampleTest extends Specification {
+    private static DB mariadb
+    private static RedisServer redis
+
+    void setupSpec() {
+        mariadb = DB.newEmbeddedDB(
+                DBConfigurationBuilder.newBuilder()
+                        .setPort(3306)
+                        .setDataDir(new File('build/resources/test/mariadb'))
+                        .build()
+        )
+        mariadb.start()
+
+        redis = new RedisServer()
+        redis.start()
+    }
+
+    void cleanupSpec() {
+        redis?.stop()
+        mariadb?.stop()
+    }
+
+    def 'test that main functioning does not throw'() {
+        when:
+        HybridCachedRepositoryExample.main(new String[0])
+
+        then:
+        noExceptionThrown()
+    }
+
+}
