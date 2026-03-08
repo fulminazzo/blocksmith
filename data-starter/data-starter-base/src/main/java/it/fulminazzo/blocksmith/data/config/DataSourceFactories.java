7e27514a506fc0babd1541d97c2ce765b685519a
user <fulminazzo21@gmail.com>
2026-03-08 19:52:56 +0100

    Created DataSourceFactories to keep track of all the factories and build the datasource from the configuration

---
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/config/DataSourceFactories.java
@@ -0,0 +1,28 @@
+package it.fulminazzo.blocksmith.data.config;
+
+import it.fulminazzo.blocksmith.data.RepositoryDataSource;
+import it.fulminazzo.blocksmith.data.RepositorySettings;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DataSourceFactories {
+    private static final @NotNull Map<Class<? extends DataSourceConfig>, DataSourceFactory> factories = new HashMap<>();
+
+    public static <S extends RepositorySettings> @NotNull RepositoryDataSource<S> build(final @NotNull DataSourceConfig dataSourceConfig) {
+        DataSourceFactory dataSourceFactory = factories.get(dataSourceConfig.getClass());
+        if (dataSourceFactory == null)
+            throw new IllegalArgumentException("No RepositoryDataSource factory currently registered for configuration type: " + dataSourceConfig.getClass().getSimpleName());
+        return dataSourceFactory.build(dataSourceConfig);
+    }
+
+    public static void registerFactory(final @NotNull Class<? extends DataSourceConfig> configClass,
+                                       final @NotNull DataSourceFactory factory) {
+        factories.put(configClass, factory);
+    }
+
+}
