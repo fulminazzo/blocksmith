c0e0f8453e131f132fe649b65713c6de817ae361
user <fulminazzo21@gmail.com>
2026-03-08 19:59:48 +0100

    Created and registered FileDataSourceConfig

---
+++ data-starter/data-starter-file/src/main/java/it/fulminazzo/blocksmith/data/file/config/FileDataSourceConfig.java
@@ -0,0 +1,20 @@
+package it.fulminazzo.blocksmith.data.file.config;
+
+import it.fulminazzo.blocksmith.data.config.DataSourceConfig;
+import it.fulminazzo.blocksmith.data.config.DataSourceFactories;
+import it.fulminazzo.blocksmith.data.file.FileDataSource;
+import lombok.Value;
+
+import java.util.concurrent.Executors;
+
+@Value
+public class FileDataSourceConfig implements DataSourceConfig {
+
+    static {
+        DataSourceFactories.registerFactory(
+                FileDataSourceConfig.class,
+                c -> FileDataSource.create(Executors.newCachedThreadPool())
+        );
+    }
+
+}
