b4814e8cf7b2b93a3254f4a22ef979244deaef9b
user <fulminazzo21@gmail.com>
2026-03-08 20:28:41 +0100

    Added validations to SqlDataSourceConfig

--- data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/config/SqlDataSourceConfig.java
+++ data-starter/data-starter-sql/src/main/java/it/fulminazzo/blocksmith/data/sql/config/SqlDataSourceConfig.java
@@ -2,9 +2,9 @@ package it.fulminazzo.blocksmith.data.sql.config;
 
 import it.fulminazzo.blocksmith.data.config.DataSourceConfig;
 import it.fulminazzo.blocksmith.data.sql.IDatabaseType;
+import jakarta.validation.constraints.*;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Range;
 
@@ -13,51 +13,75 @@ import java.util.Map;
 @Value
 public class SqlDataSourceConfig implements DataSourceConfig {
 
-    @NotNull IDatabaseType databaseType;
+    @NotNull(message = "databaseType must be declared")
+    @org.jetbrains.annotations.NotNull
+    IDatabaseType databaseType;
 
-    @NotNull String database;
+    @NotEmpty(message = "database name must not be empty")
+    @org.jetbrains.annotations.NotNull
+    String database;
 
-    @Nullable String username;
+    @Nullable
+    String username;
 
-    @Nullable String password;
+    @Nullable
+    String password;
 
+    @Positive(message = "maximumPoolSize must be greater than 0")
     @Range(from = 1, to = Integer.MAX_VALUE)
-    @Nullable Integer maximumPoolSize;
+    @Nullable
+    Integer maximumPoolSize;
 
+    @PositiveOrZero(message = "minimumIdle must be greater than or equal to 0")
     @Range(from = 0, to = Integer.MAX_VALUE)
-    @Nullable Integer minimumIdle;
+    @Nullable
+    Integer minimumIdle;
 
+    @Positive(message = "connectionTimeout must be greater than 0")
     @Range(from = 1, to = Long.MAX_VALUE)
-    @Nullable Long connectionTimeout;
+    @Nullable
+    Long connectionTimeout;
 
+    @PositiveOrZero(message = "idleTimeout must be greater than or equal to 0")
     @Range(from = 0, to = Long.MAX_VALUE)
-    @Nullable Long idleTimeout;
+    @Nullable
+    Long idleTimeout;
 
+    @PositiveOrZero(message = "maxLifetime must be greater than or equal to 0")
     @Range(from = 0, to = Long.MAX_VALUE)
-    @Nullable Long maxLifetime;
+    @Nullable
+    Long maxLifetime;
 
-    @Nullable Map<String, Object> properties;
+    @Nullable
+    Map<String, Object> properties;
 
     /*
      * RemoteDataSource
      */
-    @Nullable String host;
+    @Nullable
+    String host;
 
+    @Min(value = 1, message = "port number must be greater than or equal to 1")
+    @Max(value = 1, message = "port number must be lower than or equal to 65535")
     @Range(from = 1, to = 65535)
-    @Nullable Integer port;
+    @Nullable
+    Integer port;
 
     /*
      * H2DataSource
      */
-    @Nullable String schemaName;
+    @Nullable
+    String schemaName;
 
-    @Nullable Map<String, Object> parameters;
+    @Nullable
+    Map<String, Object> parameters;
 
     /*
      * SqliteDataSource
      */
 
-    @Nullable ConnectionMode connectionMode;
+    @Nullable
+    ConnectionMode connectionMode;
 
     public static abstract class ConnectionMode {
 
@@ -67,14 +91,20 @@ public class SqlDataSourceConfig implements DataSourceConfig {
     @Value
     public static class H2ConnectionMode extends ConnectionMode {
 
-        @NotNull H2ConnectionModeType type;
+        @NotNull
+        H2ConnectionModeType type = H2ConnectionModeType.MEMORY;
 
-        @Nullable String directoryPath;
+        @Nullable
+        String directoryPath;
 
-        @Nullable String host;
+        @Nullable
+        String host;
 
+        @Min(value = 1, message = "port number must be greater than or equal to 1")
+        @Max(value = 1, message = "port number must be lower than or equal to 65535")
         @Range(from = 1, to = 65535)
-        @Nullable Integer port;
+        @Nullable
+        Integer port;
 
     }
 
@@ -86,9 +116,11 @@ public class SqlDataSourceConfig implements DataSourceConfig {
     @Value
     public static class SqliteConnectionMode extends ConnectionMode {
 
-        @NotNull SqliteConnectionModeType type;
+        @NotNull
+        SqliteConnectionModeType type = SqliteConnectionModeType.MEMORY;
 
-        @Nullable String directoryPath;
+        @Nullable
+        String directoryPath;
 
     }
 
