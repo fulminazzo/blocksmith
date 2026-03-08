7e82ecc3a284fe52b39221e3fafdfb029ec7e721
user <fulminazzo21@gmail.com>
2026-03-08 19:49:02 +0100

    Created DataSourceConfig

---
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/config/DataSourceConfig.java
@@ -0,0 +1,8 @@
+package it.fulminazzo.blocksmith.data.config;
+
+/**
+ * Represents the configuration for a {@link it.fulminazzo.blocksmith.data.RepositoryDataSource}.
+ */
+public interface DataSourceConfig {
+
+}
