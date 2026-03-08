f1b458d2cdb07bd0fb00f61c6657d049a43f0fe2
user <fulminazzo21@gmail.com>
2026-03-09 00:59:36 +0100

    Added tests for DataSourceType

---
+++ data-starter/data-starter-base/src/test/groovy/it/fulminazzo/blocksmith/data/config/DataSourceTypeTest.groovy
@@ -0,0 +1,25 @@
+package it.fulminazzo.blocksmith.data.config
+
+import spock.lang.Specification
+
+class DataSourceTypeTest extends Specification {
+
+    def 'test that newConfig throws for type #dataSourceType'() {
+        given:
+        def lowercaseType = dataSourceType.name().toLowerCase()
+        if (dataSourceType == DataSourceType.CACHED) lowercaseType = 'cache'
+
+        when:
+        dataSourceType.newConfig()
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == "Could not find suitable ${DataSourceConfig.simpleName} for ${dataSourceType.name().toLowerCase().capitalize()}. " +
+                "Please check that the module it.fulminazzo.blocksmith:data-starter-${lowercaseType} " +
+                "is correctly installed."
+
+        where:
+        dataSourceType << DataSourceType.values()
+    }
+
+}
