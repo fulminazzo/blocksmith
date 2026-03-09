bc2552fbf30869125268df81e7b59b653c908bf4
user <fulminazzo21@gmail.com>
2026-03-09 18:50:22 +0100

    Created ReceiverFactory

---
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/receiver/ReceiverFactory.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.blocksmith.message.receiver;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Creates {@link Receiver} objects accordingly.
+ */
+public interface ReceiverFactory {
+
+    /**
+     * Creates a new Receiver from the given one.
+     *
+     * @param <R>      the type of the receiver
+     * @param receiver the receiver
+     * @return the blocksmith receiver
+     */
+    <R> @NotNull Receiver<R> create(final @NotNull R receiver);
+
+    /**
+     * Checks if the current factory supports the given receiver type.
+     *
+     * @param receiverType the receiver type
+     * @return <code>true</code> if it does
+     */
+    boolean supports(final @NotNull Class<?> receiverType);
+
+}
