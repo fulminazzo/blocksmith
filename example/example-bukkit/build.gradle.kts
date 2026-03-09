3d06ee12cf797aeba80d313cd660f7a93ed983bf
user <fulminazzo21@gmail.com>
2026-03-09 23:16:29 +0100

    Added building configuration

--- example/example-bukkit/build.gradle.kts
+++ example/example-bukkit/build.gradle.kts
@@ -1,5 +1,32 @@
+plugins {
+    alias(libs.plugins.shadow)
+}
+
 dependencies {
     compileOnly(libs.spigot)
 
     testImplementation(libs.spigot)
+}
+
+tasks.jar {
+    dependsOn(tasks.shadowJar)
+    archiveClassifier = "original"
+}
+
+tasks.shadowJar {
+    archiveClassifier = ""
+}
+
+tasks.processResources {
+    val properties = mapOf(
+        "version" to version,
+        "group" to group,
+        "name" to rootProject.name,
+        "name_lower" to rootProject.name.lowercase()
+    )
+    inputs.properties(properties)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(properties)
+    }
 }
