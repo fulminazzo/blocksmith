6238f54ad6dfe2f8ab7908820838767b241e0279
user <fulminazzo21@gmail.com>
2026-03-09 23:29:55 +0100

    Created Blocksmith plugin entrypoint

---
+++ example/example-bukkit/src/main/java/it/fulminazzo/blocksmith/Blocksmith.java
@@ -0,0 +1,41 @@
+package it.fulminazzo.blocksmith;
+
+import it.fulminazzo.blocksmith.message.Messenger;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.jul.JDK14LoggerAdapter;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public final class Blocksmith extends JavaPlugin {
+    private final @NotNull Logger logger;
+
+    @Getter
+    private final @NotNull Messenger messenger;
+
+    public Blocksmith() {
+        try {
+            Constructor<JDK14LoggerAdapter> constructor = JDK14LoggerAdapter.class.getDeclaredConstructor(java.util.logging.Logger.class);
+            constructor.setAccessible(true);
+            this.logger = constructor.newInstance(getLogger());
+            this.messenger = new Messenger(logger);
+        } catch (InvocationTargetException | IllegalAccessException | InstantiationException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException("Could not instantiate SLF4J logger", e);
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        logger.info("{} successfully enabled", getDescription().getName());
+    }
+
+    @Override
+    public void onDisable() {
+        logger.info("{} disabled. Goodbye!", getDescription().getName());
+    }
+
+}
