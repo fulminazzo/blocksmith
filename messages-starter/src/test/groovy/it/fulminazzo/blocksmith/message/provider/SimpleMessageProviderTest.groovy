9088a25a0e048f4581b0b8374cc47f80aad77f15
user <fulminazzo21@gmail.com>
2026-03-09 16:37:19 +0100

    Added more tests for getMessage

--- messages-starter/src/test/groovy/it/fulminazzo/blocksmith/message/provider/SimpleMessageProviderTest.groovy
+++ messages-starter/src/test/groovy/it/fulminazzo/blocksmith/message/provider/SimpleMessageProviderTest.groovy
@@ -7,11 +7,11 @@ class SimpleMessageProviderTest extends Specification {
 
     def 'test getMessage correctly formats all color formats'() {
         given:
-        def message = '§cHello, &aworld! <aqua>How are you?'
+        def message = '§cHello&6, &aworld&d! <aqua>How &#ff00aaare §x§1§2§3§4§5§6you <#abcdef>?'
 
         and:
         def expected = MiniMessage.miniMessage().deserialize(
-                '<red>Hello, <green>world! <aqua>How are you?'
+                '<red>Hello<gold>, <green>world<light_purple>! <aqua>How <#ff00aa>are <#123456>you<#abcdef>?'
         )
 
         and:
