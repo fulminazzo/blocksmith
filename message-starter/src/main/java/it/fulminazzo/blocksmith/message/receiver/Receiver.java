0a7168a68f510e6df2403324c96d672d45ec452e
user <fulminazzo21@gmail.com>
2026-03-09 19:51:12 +0100

    Removed unused raw receiver parameters from Receiver

--- message-starter/src/main/java/it/fulminazzo/blocksmith/message/receiver/Receiver.java
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/receiver/Receiver.java
@@ -7,25 +7,21 @@ import java.util.Locale;
 
 /**
  * Represents a general message receiver.
- *
- * @param <R> the actual type of the receiver
  */
-public interface Receiver<R> {
+public interface Receiver {
 
     /**
      * Converts the given receiver to an Audience.
      *
-     * @param receiver the receiver of the message
      * @return the audience
      */
-    @NotNull Audience toAudience(final @NotNull R receiver);
+    @NotNull Audience toAudience();
 
     /**
      * Gets the locale of the receiver.
      *
-     * @param receiver the receiver of the message
      * @return the locale
      */
-    @NotNull Locale getLocale(final @NotNull R receiver);
+    @NotNull Locale getLocale();
 
 }
