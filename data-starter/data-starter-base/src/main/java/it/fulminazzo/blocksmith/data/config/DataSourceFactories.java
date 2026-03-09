74792dd524f6373b1064f915f2cacffcdd545bb3
user <fulminazzo21@gmail.com>
2026-03-09 02:09:28 +0100

    Updated build function to return RepositorySettings as generic type

--- data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/config/DataSourceFactories.java
+++ data-starter/data-starter-base/src/main/java/it/fulminazzo/blocksmith/data/config/DataSourceFactories.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.blocksmith.data.config;
 
 import it.fulminazzo.blocksmith.data.RepositoryDataSource;
+import it.fulminazzo.blocksmith.data.RepositorySettings;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -12,11 +13,12 @@ import java.util.Map;
 public final class DataSourceFactories {
     private static final @NotNull Map<Class<? extends DataSourceConfig>, DataSourceFactory> factories = new HashMap<>();
 
-    public static @NotNull RepositoryDataSource<?> build(final @NotNull DataSourceConfig dataSourceConfig) {
+    @SuppressWarnings("unchecked")
+    public static @NotNull RepositoryDataSource<RepositorySettings> build(final @NotNull DataSourceConfig dataSourceConfig) {
         DataSourceFactory dataSourceFactory = factories.get(dataSourceConfig.getClass());
         if (dataSourceFactory == null)
             throw new IllegalArgumentException("No RepositoryDataSource factory currently registered for configuration type: " + dataSourceConfig.getClass().getSimpleName());
-        return dataSourceFactory.build(dataSourceConfig);
+        return (RepositoryDataSource<RepositorySettings>) dataSourceFactory.build(dataSourceConfig);
     }
 
     public static void registerFactory(final @NotNull Class<? extends DataSourceConfig> configClass,
