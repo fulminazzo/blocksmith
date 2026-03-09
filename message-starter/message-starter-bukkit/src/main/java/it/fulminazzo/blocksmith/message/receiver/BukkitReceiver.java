e646e5a659f8a63e143908f329ced3d53ba0552d
user <fulminazzo21@gmail.com>
2026-03-09 19:59:17 +0100

    Created bukkit Receiver implementation

---
+++ message-starter/message-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/message/receiver/BukkitReceiver.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.blocksmith.message.receiver;
+
+import it.fulminazzo.blocksmith.message.util.LocaleUtils;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+final class BukkitReceiver implements Receiver {
+    private static final @NotNull BukkitAudiences adventure = BukkitAudiences.create(JavaPlugin.getProvidingPlugin(BukkitReceiver.class));
+
+    private final @NotNull CommandSender receiver;
+
+    @Override
+    public @NotNull Audience toAudience() {
+        if (receiver instanceof Audience) return (Audience) receiver;
+        else return adventure.sender(receiver);
+    }
+
+    @Override
+    public @NotNull Locale getLocale() {
+        if (receiver instanceof Player) return LocaleUtils.fromString(((Player) receiver).getLocale());
+        else return Locale.getDefault();
+    }
+
+}
