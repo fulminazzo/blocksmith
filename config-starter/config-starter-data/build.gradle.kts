2e9bb6e97fd3d27c37d60357c18224a7a674e4c7
user <fulminazzo21@gmail.com>
2026-03-09 01:29:58 +0100

    Added several dependencies

--- config-starter/config-starter-data/build.gradle.kts
+++ config-starter/config-starter-data/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
     api(project(":data-starter:data-starter-base"))
 
+    testImplementation(project(":config-starter:config-starter-yaml"))
+    testImplementation(project(":data-starter:data-starter-cache"))
+    testImplementation(project(":data-starter:data-starter-redis"))
+    testImplementation(project(":data-starter:data-starter-sql"))
 }
