e2ca112bec7082a565887965ad0ad1a1349ab72e
user <fulminazzo21@gmail.com>
2026-03-09 17:21:44 +0100

    Added translation function

--- message-starter/src/main/java/it/fulminazzo/blocksmith/message/provider/MessageProvider.java
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/provider/MessageProvider.java
@@ -2,6 +2,7 @@ package it.fulminazzo.blocksmith.message.provider;
 
 import it.fulminazzo.blocksmith.config.ConfigurationAdapter;
 import it.fulminazzo.blocksmith.config.ConfigurationFormat;
+import it.fulminazzo.blocksmith.message.util.LocaleUtils;
 import it.fulminazzo.blocksmith.util.MapUtils;
 import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +12,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Provides messages based on their code (path in the associated configuration).
@@ -52,7 +54,7 @@ public interface MessageProvider {
      *     <li>creates the new MessageProvider from the found map.</li>
      * </ol>
      *
-     * @param workingDir the working dir
+     * @param workingDir the working directory
      * @param resource   the resource
      * @return the message provider
      * @throws IOException in case of any errors
@@ -77,6 +79,37 @@ public interface MessageProvider {
         );
         Map<String, Object> messages = adapter.load(file, Map.class);
         return memory(messages);
+    }
+
+    /**
+     * Creates a new MessageProvider that supports translations.
+     * The provider expects a resource directory where all the default translations are stored.
+     * Will then load them from disk, based on the locale specified in their name.
+     *
+     * @param workingDir   the working directory
+     * @param resourcesDir the resources directory
+     * @param format       the format of the translation files
+     * @return the translation message provider
+     * @throws IOException in case of any errors
+     */
+    static @NotNull TranslationMessageProvider translation(final @NotNull File workingDir,
+                                                           final @NotNull String resourcesDir,
+                                                           final @NotNull ConfigurationFormat format) throws IOException {
+        File directory = new File(workingDir, resourcesDir);
+        if (!directory.isDirectory()); //TODO: resources lookup
+        TranslationMessageProvider provider = TranslationMessageProvider.newProvider();
+        for (File file : Objects.requireNonNull(directory.listFiles(), "Could not read files of directory: " + directory.getAbsolutePath())) {
+            if (format.isValidFile(file)) {
+                String fileName = file.getName();
+                fileName = fileName.substring(0, fileName.lastIndexOf('.'));
+                Locale locale = LocaleUtils.fromString(fileName);
+                provider.registerProvider(
+                        locale,
+                        resource(directory, file.getName())
+                );
+            }
+        }
+        return provider;
     }
 
 }
