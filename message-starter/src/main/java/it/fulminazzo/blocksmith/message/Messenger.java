257c8a4f8f4d0256872b552433035b9a27a0be2c
user <fulminazzo21@gmail.com>
2026-03-09 22:59:14 +0100

    Created Messenger

---
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/Messenger.java
@@ -0,0 +1,82 @@
+package it.fulminazzo.blocksmith.message;
+
+import it.fulminazzo.blocksmith.message.provider.MessageNotFoundException;
+import it.fulminazzo.blocksmith.message.provider.MessageProvider;
+import it.fulminazzo.blocksmith.message.receiver.Receiver;
+import it.fulminazzo.blocksmith.message.receiver.ReceiverFactories;
+import it.fulminazzo.blocksmith.message.receiver.ReceiverFactory;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+import java.util.Locale;
+
+/**
+ * Handles all the messages and translations to send to users.
+ */
+@RequiredArgsConstructor
+public final class Messenger {
+    private final @NotNull Logger logger;
+
+    private @Nullable MessageProvider messageProvider;
+
+    /**
+     * Sends a message to the given receiver.
+     * If the message could not be found, a warning will be displayed
+     * and no error will be returned.
+     *
+     * @param <R>         the type of the receiver
+     * @param receiver    the receiver
+     * @param messageCode the message code
+     */
+    public <R> void sendMessage(final @NotNull R receiver,
+                                final @NotNull String messageCode) {
+        try {
+            ReceiverFactory factory = ReceiverFactories.get(receiver.getClass());
+            Receiver rec = factory.create(receiver);
+            Component message = getComponent(messageCode, rec.getLocale());
+            rec.toAudience().sendMessage(message);
+        } catch (MessageNotFoundException e) {
+            logger.warn(e.getMessage());
+        }
+    }
+
+    /**
+     * Gets a Text Adventure component from the given message code.
+     * Uses the internal {@link MessageProvider}.
+     *
+     * @param messageCode the message code
+     * @param locale      the locale
+     * @return the component
+     * @throws MessageNotFoundException in case the message code was invalid
+     */
+    public @NotNull Component getComponent(final @NotNull String messageCode,
+                                           final @NotNull Locale locale) throws MessageNotFoundException {
+        return getMessageProvider().getMessage(messageCode, locale);
+    }
+
+    /**
+     * Sets the internal message provider.
+     *
+     * @param messageProvider the message provider
+     * @return this object (for method chaining)
+     */
+    public @NotNull Messenger setMessageProvider(final @Nullable MessageProvider messageProvider) {
+        this.messageProvider = messageProvider;
+        return this;
+    }
+
+    private @NotNull MessageProvider getMessageProvider() {
+        if (messageProvider == null)
+            throw new IllegalStateException("No message provider has been specified yet. " +
+                    String.format("Please use %s#setMessageProvider(%s)",
+                            Messenger.class.getSimpleName(),
+                            MessageProvider.class.getSimpleName()
+                    )
+            );
+        return messageProvider;
+    }
+
+}
