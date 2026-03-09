c53c280dcd75586d02bc0e61b22c609962307a77
user <fulminazzo21@gmail.com>
2026-03-09 21:30:34 +0100

    Extracted getAdventure function for testing purposes

--- message-starter/message-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/message/receiver/BukkitReceiver.java
+++ message-starter/message-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/message/receiver/BukkitReceiver.java
@@ -8,25 +8,32 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
 
 @RequiredArgsConstructor
 final class BukkitReceiver implements Receiver {
-    private static final @NotNull BukkitAudiences adventure = BukkitAudiences.create(JavaPlugin.getProvidingPlugin(BukkitReceiver.class));
+    private static @Nullable BukkitAudiences adventure;
 
     private final @NotNull CommandSender receiver;
 
     @Override
     public @NotNull Audience toAudience() {
         if (receiver instanceof Audience) return (Audience) receiver;
-        else return adventure.sender(receiver);
+        else return getAdventure().sender(receiver);
     }
 
     @Override
     public @NotNull Locale getLocale() {
         if (receiver instanceof Player) return LocaleUtils.fromString(((Player) receiver).getLocale());
         else return Locale.getDefault();
+    }
+
+    static @NotNull BukkitAudiences getAdventure() {
+        if (adventure == null)
+            adventure = BukkitAudiences.create(JavaPlugin.getProvidingPlugin(BukkitReceiver.class));
+        return adventure;
     }
 
 }
