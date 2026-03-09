3de24cee2f5a68ddd8ff50fee410c6e3f7ef4ed5
user <fulminazzo21@gmail.com>
2026-03-09 02:12:57 +0100

    Refactored code

--- config-starter/config-starter-all/build.gradle.kts
+++ config-starter/config-starter-all/build.gradle.kts
@@ -2,6 +2,6 @@ val testingModuleName: String by rootProject.extra
 
 dependencies {
     project.parent?.subprojects?.filter {
-        !it.name.endsWith("-${testingModuleName}") && !it.name.equals(project.name)
+        !it.name.endsWith("-${testingModuleName}") && it.name != project.name
     }?.forEach { api(it) }
 }
