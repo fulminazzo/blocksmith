7a1d9ecc18111ee9bd115eb126a94f44d0acaf23
user <fulminazzo21@gmail.com>
2026-03-10 02:23:03 +0100

    Created BlocksmithUser

---
+++ example/example-bukkit/src/main/java/it/fulminazzo/blocksmith/BlocksmithUser.java
@@ -0,0 +1,22 @@
+package it.fulminazzo.blocksmith;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public final class BlocksmithUser {
+
+    @NotNull UUID uuid;
+
+    @NotNull String username;
+
+    @NotNull Locale locale;
+
+}
