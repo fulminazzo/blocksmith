cc104c6234d6353cc47e04ba4999724c90e882a0
user <fulminazzo21@gmail.com>
2026-03-10 22:19:41 +0100

    Updated BukkitReceiver with internal instance adventure field Updated BukkitReceiverFactory to load BukkitAudiences upon creation of receiver

--- message-starter/message-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/message/receiver/BukkitReceiverFactory.java
+++ message-starter/message-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/message/receiver/BukkitReceiverFactory.java
@@ -1,21 +1,36 @@
 package it.fulminazzo.blocksmith.message.receiver;
 
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Implementation of {@link ReceiverFactory} for Bukkit platform.
  */
 public final class BukkitReceiverFactory implements ReceiverFactory {
+    private static @Nullable BukkitAudiences adventure;
 
     @Override
     public @NotNull <R> Receiver create(final @NotNull R receiver) {
-        return new BukkitReceiver((CommandSender) receiver);
+        return new BukkitReceiver(getAdventure(), (CommandSender) receiver);
     }
 
     @Override
     public boolean supports(final @NotNull Class<?> receiverType) {
         return CommandSender.class.isAssignableFrom(receiverType);
+    }
+
+    /**
+     * Gets the Text Adventure converter for command senders.
+     *
+     * @return the converter
+     */
+    static @NotNull BukkitAudiences getAdventure() {
+        if (adventure == null)
+            adventure = BukkitAudiences.create(JavaPlugin.getProvidingPlugin(BukkitReceiver.class));
+        return adventure;
     }
 
 }
