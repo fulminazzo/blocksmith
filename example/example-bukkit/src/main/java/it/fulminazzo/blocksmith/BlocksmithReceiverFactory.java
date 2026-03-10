3fce9dc86d9d91f14c6e9450fb4f2efcfaa710d3
user <fulminazzo21@gmail.com>
2026-03-10 02:52:32 +0100

    Created BlocksmithReceiverFactory

---
+++ example/example-bukkit/src/main/java/it/fulminazzo/blocksmith/BlocksmithReceiverFactory.java
@@ -0,0 +1,57 @@
+package it.fulminazzo.blocksmith;
+
+import it.fulminazzo.blocksmith.message.receiver.Receiver;
+import it.fulminazzo.blocksmith.message.receiver.ReceiverFactories;
+import it.fulminazzo.blocksmith.message.receiver.ReceiverFactory;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.audience.Audience;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
+public final class BlocksmithReceiverFactory implements ReceiverFactory {
+
+    static {
+        ReceiverFactories.registerCustomFactory(new BlocksmithReceiverFactory());
+    }
+
+    @Override
+    public @NotNull <R> Receiver create(final @NonNull R receiver) {
+        return new BlocksmithReceiver((Player) receiver);
+    }
+
+    @Override
+    public boolean supports(final @NotNull Class<?> receiverType) {
+        return Player.class.isAssignableFrom(receiverType);
+    }
+
+    @RequiredArgsConstructor
+    static final class BlocksmithReceiver implements Receiver {
+        private final @NotNull Player player;
+        private final @NotNull Receiver defaultReceiver = ReceiverFactories.get(CommandSender.class).create(player);
+
+        @Override
+        public @NotNull Audience toAudience() {
+            return defaultReceiver.toAudience();
+        }
+
+        @Override
+        public @NotNull Locale getLocale() {
+            try {
+                return Blocksmith.getInstance().getRepository().findById(player.getUniqueId())
+                        .thenApply(u -> u.map(BlocksmithUser::getLocale)
+                                .orElseGet(defaultReceiver::getLocale)
+                        )
+                        .get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
