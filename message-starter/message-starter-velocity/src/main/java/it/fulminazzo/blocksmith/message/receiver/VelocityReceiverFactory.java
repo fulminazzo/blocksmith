853b6308722d2b86a9258d48bc39ede073f6ac3f
user <fulminazzo21@gmail.com>
2026-03-10 22:22:46 +0100

    Added setup and getServer functions

--- message-starter/message-starter-velocity/src/main/java/it/fulminazzo/blocksmith/message/receiver/VelocityReceiverFactory.java
+++ message-starter/message-starter-velocity/src/main/java/it/fulminazzo/blocksmith/message/receiver/VelocityReceiverFactory.java
@@ -1,12 +1,17 @@
 package it.fulminazzo.blocksmith.message.receiver;
 
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 /**
  * Implementation of {@link ReceiverFactory} for Velocity platform.
  */
 public final class VelocityReceiverFactory implements ReceiverFactory {
+    private static @Nullable ProxyServer server;
 
     @Override
     public @NotNull <R> Receiver create(final @NotNull R receiver) {
@@ -16,6 +21,28 @@ public final class VelocityReceiverFactory implements ReceiverFactory {
     @Override
     public boolean supports(final @NotNull Class<?> receiverType) {
         return CommandSource.class.isAssignableFrom(receiverType);
+    }
+
+    /**
+     * Initializes the factory with the required fields.
+     *
+     * @param server the current server instance
+     */
+    public static void setup(final @NotNull ProxyServer server) {
+        VelocityReceiverFactory.server = server;
+    }
+
+    /**
+     * Gets the current server.
+     *
+     * @return the server
+     */
+    static @NotNull ProxyServer getServer() {
+        return Objects.requireNonNull(server,
+                "Message system has not been fully initialized yet. " +
+                        String.format("Please use %s#startup(%s)",
+                                VelocityReceiverFactory.class.getCanonicalName(), ProxyServer.class.getCanonicalName()
+                        ));
     }
 
 }
