b924dee98925a20f5253825962b36049ebc39243
user <fulminazzo21@gmail.com>
2026-03-10 22:21:17 +0100

    Updated BungeeReceiver with internal instance adventure field Updated BungeeReceiverFactory to require BungeeAudiences upon creation of receiver

--- message-starter/message-starter-bungeecord/src/main/java/it/fulminazzo/blocksmith/message/receiver/BungeeReceiver.java
+++ message-starter/message-starter-bungeecord/src/main/java/it/fulminazzo/blocksmith/message/receiver/BungeeReceiver.java
@@ -5,43 +5,24 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
-import java.util.Objects;
 
 @RequiredArgsConstructor
-public final class BungeeReceiver implements Receiver {
-    private static @Nullable BungeeAudiences adventure;
-
+final class BungeeReceiver implements Receiver {
+    private final @NotNull BungeeAudiences adventure;
     private final @NotNull CommandSender receiver;
 
     @Override
     public @NotNull Audience toAudience() {
-        return getAdventure().sender(receiver);
+        return adventure.sender(receiver);
     }
 
     @Override
     public @NotNull Locale getLocale() {
         if (receiver instanceof ProxiedPlayer) return ((ProxiedPlayer) receiver).getLocale();
         else return Locale.getDefault();
-    }
-
-    /**
-     * Initializes the internal serializer.
-     *
-     * @param plugin the plugin (using the receiver)
-     */
-    public static void setup(final @NotNull Plugin plugin) {
-        adventure = BungeeAudiences.create(plugin);
-    }
-
-    static @NotNull BungeeAudiences getAdventure() {
-        return Objects.requireNonNull(adventure, String.format("BungeeAudiences has not been initialized yet. Please use %s#startup(%s)",
-                BungeeReceiver.class.getCanonicalName(), Plugin.class.getCanonicalName()
-        ));
     }
 
 }
--- message-starter/message-starter-bungeecord/src/main/java/it/fulminazzo/blocksmith/message/receiver/BungeeReceiverFactory.java
+++ message-starter/message-starter-bungeecord/src/main/java/it/fulminazzo/blocksmith/message/receiver/BungeeReceiverFactory.java
@@ -1,21 +1,49 @@
 package it.fulminazzo.blocksmith.message.receiver;
 
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 /**
  * Implementation of {@link ReceiverFactory} for BungeeCord platform.
  */
 public final class BungeeReceiverFactory implements ReceiverFactory {
+    private static @Nullable BungeeAudiences adventure;
 
     @Override
     public @NotNull <R> Receiver create(final @NotNull R receiver) {
-        return new BungeeReceiver((CommandSender) receiver);
+        return new BungeeReceiver(getAdventure(), (CommandSender) receiver);
     }
 
     @Override
     public boolean supports(final @NotNull Class<?> receiverType) {
         return CommandSender.class.isAssignableFrom(receiverType);
+    }
+
+    /**
+     * Initializes the factory with the required fields.
+     *
+     * @param plugin the plugin using this factory
+     */
+    public static void setup(final @NotNull Plugin plugin) {
+        adventure = BungeeAudiences.create(plugin);
+    }
+
+    /**
+     * Gets the Text Adventure converter for command senders.
+     *
+     * @return the converter
+     */
+    static @NotNull BungeeAudiences getAdventure() {
+        return Objects.requireNonNull(adventure,
+                "Message system has not been fully initialized yet. " +
+                        String.format("Please use %s#startup(%s)",
+                                BungeeReceiverFactory.class.getCanonicalName(), Plugin.class.getCanonicalName()
+                        ));
     }
 
 }
