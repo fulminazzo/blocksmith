e2c24c0bd6e5752cde4983284f8ca91c707e50f1
user <fulminazzo21@gmail.com>
2026-03-11 17:46:13 +0100

    Created scheduler-starter-velocity module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,4 @@ include("scheduler-starter")
 
 include("scheduler-starter:scheduler-starter-bukkit")
 include("scheduler-starter:scheduler-starter-bungeecord")
+include("scheduler-starter:scheduler-starter-velocity")
