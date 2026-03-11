33e5aacaf415810bff46a34512170ad66fb9eb39
user <fulminazzo21@gmail.com>
2026-03-11 15:37:49 +0100

    Created TaskFactory

---
+++ scheduler-starter/src/main/java/it/fulminazzo/blocksmith/scheduler/TaskFactory.java
@@ -0,0 +1,21 @@
+package it.fulminazzo.blocksmith.scheduler;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * A factory to build and instantiate {@link Task} objects.
+ */
+public interface TaskFactory {
+
+    /**
+     * Schedules a new task.
+     *
+     * @param owner    the owner of the task
+     * @param function the function to run
+     * @return the task builder (to specify how the task should be run)
+     */
+    @NotNull TaskBuilder schedule(final @NotNull Object owner, final @NotNull Consumer<Task> function);
+
+}
