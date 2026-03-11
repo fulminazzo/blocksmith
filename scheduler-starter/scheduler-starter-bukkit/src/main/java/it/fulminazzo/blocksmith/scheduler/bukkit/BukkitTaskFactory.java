8593af0588c49be87a21e1cc7b1598ab2e77f081
user <fulminazzo21@gmail.com>
2026-03-11 23:38:11 +0100

    Added support for Pair objects

--- scheduler-starter/scheduler-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/scheduler/bukkit/BukkitTaskFactory.java
+++ scheduler-starter/scheduler-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/scheduler/bukkit/BukkitTaskFactory.java
@@ -3,6 +3,7 @@ package it.fulminazzo.blocksmith.scheduler.bukkit;
 import it.fulminazzo.blocksmith.scheduler.Task;
 import it.fulminazzo.blocksmith.scheduler.TaskBuilder;
 import it.fulminazzo.blocksmith.scheduler.TaskFactory;
+import it.fulminazzo.blocksmith.structure.Pair;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,10 +13,11 @@ public final class BukkitTaskFactory implements TaskFactory {
 
     @Override
     public @NotNull TaskBuilder schedule(final @NotNull Object owner, final @NotNull Consumer<Task> function) {
-        if (supportsOwner(owner.getClass())) return new BukkitTaskBuilder(owner, function);
+        if (owner instanceof Pair<?, ?>) return schedule(((Pair<?, ?>) owner).getFirst(), function);
+        else if (supportsOwner(owner.getClass())) return new BukkitTaskBuilder(owner, function);
         else throw new IllegalArgumentException(String.format("%s does not support owner type: %s",
-                BukkitTask.class.getSimpleName(), owner.getClass().getCanonicalName()
-        ));
+                    BukkitTask.class.getSimpleName(), owner.getClass().getCanonicalName()
+            ));
     }
 
     @Override
