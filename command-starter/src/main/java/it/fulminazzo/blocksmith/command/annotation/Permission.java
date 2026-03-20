08cab3ca8b4b459928258f88e58af72b92fa4fd6
user <fulminazzo21@gmail.com>
2026-03-20 16:14:15 +0100

    Created Permission annotation

---
+++ command-starter/src/main/java/it/fulminazzo/blocksmith/command/annotation/Permission.java
@@ -0,0 +1,24 @@
+package it.fulminazzo.blocksmith.command.annotation;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//TODO: pending documentation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Permission {
+
+    @NotNull String value() default "";
+
+    //TODO: better naming
+    @NotNull Default permissionDefault() default Default.OP;
+
+    enum Default {
+        ALL, OP, NONE
+    }
+
+}
