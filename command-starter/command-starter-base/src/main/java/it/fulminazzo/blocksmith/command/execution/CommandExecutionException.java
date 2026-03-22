5d9016e45f0b278551ab174ec5395e7cda7aeb48
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 22:32:20 +0100

    Created CommandExecutionException

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/execution/CommandExecutionException.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.blocksmith.command.execution;
+
+import lombok.experimental.StandardException;
+
+/**
+ * An exception thrown during execution of command nodes.
+ */
+@StandardException
+public final class CommandExecutionException extends Exception {
+
+}
