c056b4cd9a5c2b946cfbcfbc5ad8b96cd8087b8f
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 23:20:03 +0100

    Implemented validateInput method Overridden getDefaultValue method to return already parsed value

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/ArgumentNode.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/ArgumentNode.java
@@ -1,5 +1,7 @@
 package it.fulminazzo.blocksmith.command.node;
 
+import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext;
+import it.fulminazzo.blocksmith.command.execution.CommandExecutionException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -20,6 +22,26 @@ public final class ArgumentNode<T> extends CommandNode {
     private final boolean optional;
     private @Nullable String defaultValue;
     private boolean greedy;
+
+    /**
+     * Gets the default value (if given).
+     *
+     * @return the default value
+     * @throws CommandExecutionException in case of parsing errors
+     */
+    public @Nullable T getDefaultValue() throws CommandExecutionException {
+        return defaultValue == null ? null : parseArgument(defaultValue);
+    }
+
+    private @Nullable T parseArgument(final @NotNull String argument) throws CommandExecutionException {
+        //TODO: parsers
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void validateInput(final @NotNull CommandExecutionContext context) throws CommandExecutionException {
+        context.addParsedArgument(parseArgument(context.getCurrent()));
+    }
 
     @Override
     public boolean matches(final @NotNull String token) {
