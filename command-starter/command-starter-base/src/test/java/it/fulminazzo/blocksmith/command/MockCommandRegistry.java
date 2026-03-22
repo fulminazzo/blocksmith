d28aa84f012421bc58be7c9479e45a971c200c18
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 19:48:57 +0100

    Created MockCommandRegistry for testing purposes

---
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandRegistry.java
@@ -0,0 +1,71 @@
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.node.CommandInfo;
+import it.fulminazzo.blocksmith.command.node.LiteralNode;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+final class MockCommandRegistry extends CommandRegistry {
+    @Getter
+    private final @NotNull Map<String, CommandInfo> registeredCommands = new ConcurrentHashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public @NotNull Map<String, LiteralNode> getCommands() {
+        try {
+            Class<?> clazz = CommandRegistry.class;
+            Field field = clazz.getDeclaredField("commands");
+            field.setAccessible(true);
+            return (Map<String, LiteralNode>) field.get(this);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setState(final @NotNull State state) {
+        try {
+            Class<?> clazz = CommandRegistry.class;
+            Field field = clazz.getDeclaredField("state");
+            field.setAccessible(true);
+            field.set(this, state);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public State getState() {
+        try {
+            Class<?> clazz = CommandRegistry.class;
+            Field field = clazz.getDeclaredField("state");
+            field.setAccessible(true);
+            return (State) field.get(this);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void onRegister(final @NotNull String commandName,
+                              final @NotNull CommandInfo commandInfo) {
+        registeredCommands.put(commandName, commandInfo);
+    }
+
+    @Override
+    protected void onUnregister(final @NotNull String commandName) {
+        registeredCommands.remove(commandName);
+    }
+
+    @Override
+    protected @NotNull Class<?> getSenderType() {
+        return CommandSender.class;
+    }
+
+    @Override
+    protected @NotNull String getPrefix() {
+        return "blocksmith";
+    }
+
+}
