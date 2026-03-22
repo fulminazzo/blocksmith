7ecb64318b9beba3a266aaef9a78b412e1aa4d29
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 20:05:22 +0100

    Created ClanCommand and ClanAdminCommand for testing purposes

---
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/ClanAdminCommand.java
@@ -0,0 +1,29 @@
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.annotation.Command;
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+@Command("clan")
+public class ClanAdminCommand {
+
+    @Command("admin invite <target>")
+    public void adminInvite(final @NotNull CommandSender sender,
+                            final @NotNull Object target) {
+        // something
+    }
+
+    @Command("admin members")
+    @Permission(permissionDefault = Permission.Default.ALL)
+    public void adminMembers(final @NotNull CommandSender sender) {
+        // something
+    }
+
+    @Command("admin members kick <target>")
+    public void adminMembersKick(final @NotNull CommandSender sender,
+                                 final @NotNull Object target) {
+        // something
+    }
+
+}
---
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/ClanCommand.java
@@ -0,0 +1,21 @@
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.annotation.Command;
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+
+@SuppressWarnings("unused")
+@Command(value = "(clan|team|gang)", description = "Clan base command")
+@Permission(value = "blocksmith.clan", permissionDefault = Permission.Default.ALL)
+final class ClanCommand {
+
+    @Command
+    public void execute() {
+        // something
+    }
+
+    @Command(value = "admin", description = "Clan admin command")
+    public void admin() {
+        // something
+    }
+
+}
