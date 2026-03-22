6a777ae5b8c35abd39a8aa98b47b93b626017f17
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 13:18:19 +0100

    Added clone method

--- command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/node/MockNode.java
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/node/MockNode.java
@@ -5,15 +5,30 @@ import lombok.ToString;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Set;
+
 @Value
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class MockNode extends CommandNode {
+public class MockNode extends CommandNode implements Cloneable {
     @NotNull String name;
 
     @Override
     public boolean matches(final @NotNull String token) {
         return token.equals(name);
+    }
+
+    @Override
+    protected MockNode clone() throws CloneNotSupportedException {
+        MockNode clone = new MockNode(name);
+        @NotNull Set<CommandNode> children = clone.getChildren();
+        for (CommandNode child : getChildren())
+            if (child instanceof MockNode)
+                children.add(((MockNode) child).clone());
+            else
+                throw new CloneNotSupportedException("Clone not supported on node: " + child.getClass().getCanonicalName());
+        getExecutionInfo().ifPresent(clone::setExecutionInfo);
+        return clone;
     }
 
 }
