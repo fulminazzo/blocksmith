5579326da076499e7829fcf18fe61bbc8bc28c62
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 17:30:42 +0100

    Updated CommandInfo and PermissionInfo to merge data from other objects

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/CommandInfo.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/CommandInfo.java
@@ -1,11 +1,30 @@
 package it.fulminazzo.blocksmith.command.node;
 
-import lombok.Value;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
-@Value
-public class CommandInfo {
+/**
+ * Represents the command information.
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Setter(AccessLevel.NONE)
+public final class CommandInfo {
     @NotNull String description;
-    @NotNull PermissionInfo permission;
+    final @NotNull PermissionInfo permission;
+
+    /**
+     * Merges the given command information into the current one
+     * (only if the information are not given).
+     *
+     * @param commandInfo the command info to merge from
+     */
+    public void merge(final @NotNull CommandInfo commandInfo) {
+        if (description.isEmpty()) description = commandInfo.getDescription();
+        permission.merge(commandInfo.getPermission());
+    }
 
 }
--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/PermissionInfo.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/PermissionInfo.java
@@ -1,12 +1,31 @@
 package it.fulminazzo.blocksmith.command.node;
 
 import it.fulminazzo.blocksmith.command.annotation.Permission;
-import lombok.Value;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
-@Value
-public class PermissionInfo {
+/**
+ * Represents the permission information.
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Setter(AccessLevel.NONE)
+public final class PermissionInfo {
     @NotNull String permission;
-    @NotNull Permission.Default permissionDefault;
+    final @NotNull Permission.Default permissionDefault;
+
+    /**
+     * Merges the given permission information into the current one
+     * (only if the information are not given).
+     *
+     * @param permissionInfo the permission info to merge from
+     */
+    public void merge(final @NotNull PermissionInfo permissionInfo) {
+        if (permission.isEmpty()) permission = permissionInfo.getPermission();
+
+    }
 
 }
