db05051e2f830191bb9d3d0fb0681c6772c5a860
fulminazzo <fulminazzo21@gmail.com>
2026-03-22 20:32:27 +0100

    Updated PermissionInfo to mark when automatically computing permissions

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/PermissionInfo.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/PermissionInfo.java
@@ -1,9 +1,7 @@
 package it.fulminazzo.blocksmith.command.node;
 
 import it.fulminazzo.blocksmith.command.annotation.Permission;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,8 +12,43 @@ import org.jetbrains.annotations.NotNull;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Setter(AccessLevel.NONE)
 public final class PermissionInfo {
+
     @NotNull String permission;
     @NotNull Permission.Default permissionDefault;
+
+    @Getter(AccessLevel.PRIVATE)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    boolean autoComputed;
+
+    /**
+     * Instantiates a new Permission info.
+     *
+     * @param permission        the permission
+     * @param permissionDefault the permission default
+     */
+    public PermissionInfo(final @NotNull String permission,
+                          final @NotNull Permission.Default permissionDefault) {
+        this(permission, permissionDefault, false);
+    }
+
+    /**
+     * Instantiates a new Permission info.
+     *
+     * @param permission        the permission
+     * @param permissionDefault the permission default
+     * @param autoComputed      if <code>true</code>, will mark the permission as automatically computed,
+     *                          meaning it was not provided by the user, but was instead retrieved from the command route
+     * @deprecated FOR INTERNAL USE ONLY
+     */
+    @Deprecated
+    public PermissionInfo(final @NotNull String permission,
+                          final @NotNull Permission.Default permissionDefault,
+                          final boolean autoComputed) {
+        this.permission = permission;
+        this.permissionDefault = permissionDefault;
+        this.autoComputed = autoComputed;
+    }
 
     /**
      * Merges the given permission information into the current one
@@ -24,7 +57,10 @@ public final class PermissionInfo {
      * @param permissionInfo the permission info to merge from
      */
     public void merge(final @NotNull PermissionInfo permissionInfo) {
-        if (permission.isEmpty()) permission = permissionInfo.getPermission();
+        if (permission.isEmpty() || isAutoComputed()) {
+            permission = permissionInfo.getPermission();
+            autoComputed = permissionInfo.isAutoComputed();
+        }
         if (permissionDefault == Permission.Default.OP)
             permissionDefault = permissionInfo.getPermissionDefault();
     }
