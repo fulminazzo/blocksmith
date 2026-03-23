5d5eed05b3b2c56fd1bead3f8a7c3d78532af114
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 13:14:51 +0100

    Created bukkit implementation of CommandRegistry

---
+++ command-starter/command-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/command/BukkitCommandRegistry.java
@@ -0,0 +1,86 @@
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.node.CommandInfo;
+import it.fulminazzo.blocksmith.command.node.LiteralNode;
+import it.fulminazzo.blocksmith.message.Messenger;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
+import org.jetbrains.annotations.NotNull;
+import org.joor.Reflect;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+final class BukkitCommandRegistry extends CommandRegistry {
+    private final @NotNull SimpleCommandMap commandMap;
+    private final @NotNull Map<String, Command> knownCommands;
+
+    public BukkitCommandRegistry(final @NotNull Messenger messenger,
+                                 final @NotNull Logger logger,
+                                 final @NotNull String prefix) {
+        super(messenger, logger, prefix);
+        Reflect reflect = Reflect.on(Bukkit.getServer()).fields().values().stream()
+                .filter(f -> f.get() instanceof SimpleCommandMap)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Could not find SimpleCommandMap"));
+        this.commandMap = reflect.get();
+        this.knownCommands = reflect.field("knownCommands").get();
+    }
+
+    @Override
+    protected @NotNull CommandSenderWrapper wrapSender(final @NotNull Object executor) {
+        return new BukkitCommandSenderWrapper((CommandSender) executor);
+    }
+
+    @Override
+    protected void onRegister(final @NotNull String commandName, final @NotNull LiteralNode command) {
+        CommandInfo info = command.getCommandInfo().orElseThrow();
+        List<String> aliases = new ArrayList<>(command.getAliases());
+        aliases.remove(commandName);
+        knownCommands.remove(commandName);
+        commandMap.register(
+                commandName,
+                getPrefix(),
+                new Command(
+                        commandName,
+                        info.getDescription(),
+                        "", //TODO: usage
+                        aliases
+                ) {
+
+                    @Override
+                    public boolean execute(final @NonNull CommandSender sender,
+                                           final @NonNull String commandLabel,
+                                           final @NonNull String[] args) {
+                        BukkitCommandRegistry.this.execute(command, sender, commandLabel, args);
+                        return true;
+                    }
+
+                    @Override
+                    public @NonNull List<String> tabComplete(final @NonNull CommandSender sender,
+                                                             final @NonNull String alias,
+                                                             final @NonNull String[] args) throws IllegalArgumentException {
+                        return BukkitCommandRegistry.this.tabComplete(command, sender, alias, args);
+                    }
+
+                }
+        );
+    }
+
+    @Override
+    protected void onUnregister(final @NotNull String commandName) {
+        knownCommands.remove(commandName);
+        knownCommands.remove(getPrefix() + ":" + commandName);
+    }
+
+    @Override
+    protected @NotNull Class<?> getSenderType() {
+        return CommandSender.class;
+    }
+
+}
