c366ca2b4e8c1f9d2107e1e4c104b601cbcbcb7e
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 14:56:24 +0100

    Created BlocksmithApplication interface to uniform required parameters for CommandRegistry initialization

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/BlocksmithApplication.java
@@ -0,0 +1,33 @@
+package it.fulminazzo.blocksmith;
+
+import it.fulminazzo.blocksmith.message.Messenger;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+/**
+ * Represents a blocksmith application.
+ */
+public interface BlocksmithApplication {
+
+    /**
+     * Gets the handler for messages sending.
+     *
+     * @return the messenger
+     */
+    @NotNull Messenger getMessenger();
+
+    /**
+     * Gets the SLF4J logger.
+     *
+     * @return the logger
+     */
+    @NotNull Logger getLog();
+
+    /**
+     * Gets the name of this application.
+     *
+     * @return the name
+     */
+    @NotNull String getName();
+
+}
