c24308667d68031bec464fed41958740ab43408c
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 14:45:22 +0100

    Fixed tests

--- command-starter/command-starter-bukkit/src/test/groovy/it/fulminazzo/blocksmith/command/BukkitCommandRegistryFactoryTest.groovy
+++ command-starter/command-starter-bukkit/src/test/groovy/it/fulminazzo/blocksmith/command/BukkitCommandRegistryFactoryTest.groovy
@@ -1,11 +1,20 @@
 package it.fulminazzo.blocksmith.command
 
+import be.seeseemelk.mockbukkit.MockBukkit
 import groovy.util.logging.Slf4j
 import it.fulminazzo.blocksmith.message.Messenger
 import spock.lang.Specification
 
 @Slf4j
 class BukkitCommandRegistryFactoryTest extends Specification {
+
+    void setupSpec() {
+        MockBukkit.mock()
+    }
+
+    void cleanupSpec() {
+        MockBukkit.unmock()
+    }
 
     def 'test that newCommandRegistry returns BukkitCommandRegistry'() {
         when:
