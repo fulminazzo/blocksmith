654ee0e3342d076fd539ff692e02b0ba387b6328
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 19:17:39 +0100

    Added tests for onRegister and onUnregister methods

---
+++ command-starter/command-starter-bukkit/src/test/groovy/it/fulminazzo/blocksmith/command/BukkitCommandRegistryTest.groovy
@@ -0,0 +1,118 @@
+package it.fulminazzo.blocksmith.command
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.BlocksmithApplication
+import it.fulminazzo.blocksmith.command.annotation.Permission
+import it.fulminazzo.blocksmith.command.node.CommandInfo
+import it.fulminazzo.blocksmith.command.node.LiteralNode
+import it.fulminazzo.blocksmith.command.node.PermissionInfo
+import it.fulminazzo.blocksmith.message.Messenger
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import spock.lang.Specification
+
+@Slf4j
+class BukkitCommandRegistryTest extends Specification {
+
+    private BlocksmithApplication application
+
+    private BukkitCommandRegistry registry
+
+    void setup() {
+        MockBukkit.mock()
+
+        application = Mock(BlocksmithApplication)
+        application.server >> Bukkit.server
+        application.log >> log
+        application.messenger >> new Messenger(log)
+        application.name >> 'blocksmith'
+
+        registry = new BukkitCommandRegistry(application)
+    }
+
+    void cleanup() {
+        MockBukkit.unmock()
+    }
+
+    def 'test that onRegister correctly registers new command and aliases'() {
+        given:
+        def previousCommand = Mock(Command)
+        registry.knownCommands.put('?', previousCommand)
+
+        and:
+        def node = new LiteralNode('help', '?')
+        node.commandInfo = new CommandInfo(
+                'command.description.help',
+                new PermissionInfo('help', Permission.Grant.ALL)
+        )
+
+        when:
+        registry.onRegister('help', node)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        def knownCommands = registry.knownCommands
+        (knownCommands['help'] instanceof BukkitCommandRegistry.BukkitCommand)
+        (knownCommands['?'] instanceof BukkitCommandRegistry.BukkitCommand)
+        (knownCommands["${application.name}:help"] instanceof BukkitCommandRegistry.BukkitCommand)
+        (knownCommands["${application.name}:?"] instanceof BukkitCommandRegistry.BukkitCommand)
+
+        and:
+        def previousCommands = registry.previousCommands
+        previousCommands.size() == 1
+
+        and:
+        previousCommands['?'] == previousCommand
+    }
+
+    def 'test that onUnregister removes commands and restores previous commands'() {
+        given:
+        def previousCommand = Mock(Command)
+        registry.previousCommands.put('?', previousCommand)
+        registry.previousCommands.put('showhelp', previousCommand)
+
+        and:
+        def node = new LiteralNode('help', '?', 'showhelp', 'displayhelp')
+        node.commandInfo = new CommandInfo(
+                'command.description.help',
+                new PermissionInfo('help', Permission.Grant.ALL)
+        )
+        def command = new BukkitCommandRegistry.BukkitCommand(registry, 'help', node)
+
+        and:
+        registry.knownCommands['help'] = command
+        registry.knownCommands['?'] = command
+        registry.knownCommands["${application.name}:help"] = command
+        registry.knownCommands["${application.name}:?"] = command
+        registry.knownCommands['showhelp'] = Mock(Command)
+
+        when:
+        registry.onUnregister('help')
+
+        then:
+        noExceptionThrown()
+
+        and:
+        def knownCommands = registry.knownCommands
+        knownCommands['help'] == null
+        knownCommands['?'] == previousCommand
+        knownCommands["${application.name}help"] == null
+        knownCommands["${application.name}?"] == null
+
+        and:
+        def previousCommands = registry.previousCommands
+        previousCommands.isEmpty()
+    }
+
+    def 'test that onUnregister does not throw on unknown command'() {
+        when:
+        registry.onUnregister('unknown')
+
+        then:
+        noExceptionThrown()
+    }
+
+}
