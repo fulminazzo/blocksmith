416fee2f47fa07330e42a9334d308da1f0364c14
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 12:58:44 +0100

    Created bukkit implementation of CommandSenderWrapper

---
+++ command-starter/command-starter-bukkit/src/main/java/it/fulminazzo/blocksmith/command/BukkitCommandSenderWrapper.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+import it.fulminazzo.blocksmith.command.node.PermissionInfo;
+import lombok.*;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+final class BukkitCommandSenderWrapper extends CommandSenderWrapper {
+    private final @NotNull CommandSender actualSender;
+
+    @Override
+    public @NotNull String getName() {
+        return actualSender.getName();
+    }
+
+    @Override
+    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
+        if (permissionInfo.getGrant() == Permission.Grant.OP && actualSender.isOp()) return true;
+        else return actualSender.hasPermission(permissionInfo.getPermission());
+    }
+
+    @Override
+    public boolean isPlayer() {
+        return actualSender instanceof Player;
+    }
+
+}
