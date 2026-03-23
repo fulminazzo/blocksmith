500e52b06d9f76f42e82d191355130d98d1bb923
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 01:13:19 +0100

    Added more helper methods and fields for CommandSender

--- command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/CommandSender.java
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/CommandSender.java
@@ -1,4 +1,36 @@
 package it.fulminazzo.blocksmith.command;
 
-public final class CommandSender {
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+public class CommandSender {
+    private final @NotNull String name;
+    private final @NotNull List<String> permissions = new ArrayList<>();
+    private boolean op;
+
+    public CommandSender() {
+        this("temp");
+    }
+
+    public @NotNull CommandSender addPermissions(final String @NotNull ... permissions) {
+        this.permissions.addAll(Arrays.asList(permissions));
+        return this;
+    }
+
+    public @NotNull CommandSender setOp(final boolean op) {
+        this.op = op;
+        return this;
+    }
+
+    public boolean hasPermission(final @NotNull String permission) {
+        return permissions.stream().anyMatch(permission::equalsIgnoreCase);
+    }
+
 }
