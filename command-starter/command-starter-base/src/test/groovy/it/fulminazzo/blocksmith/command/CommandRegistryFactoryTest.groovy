60c1b9de13afede7ea25f1662ccba72138713bd3
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 13:03:37 +0100

    Updated initialization of registries

--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/CommandRegistryFactoryTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/CommandRegistryFactoryTest.groovy
@@ -1,12 +1,15 @@
 package it.fulminazzo.blocksmith.command
 
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.message.Messenger
 import spock.lang.Specification
 
+@Slf4j
 class CommandRegistryFactoryTest extends Specification {
 
     def 'test that newCommandRegistry with no service throws'() {
         when:
-        CommandRegistryFactory.newCommandRegistry('blocksmith')
+        CommandRegistryFactory.newCommandRegistry(new Messenger(log), log)
 
         then:
         thrown(IllegalStateException)
