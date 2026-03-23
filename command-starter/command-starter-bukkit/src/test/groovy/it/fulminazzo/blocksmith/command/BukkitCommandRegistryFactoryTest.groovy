c60316009e37ea4d1555547b15d0ec09f8235242
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 15:14:58 +0100

    Fixed tests with mock of BlocksmithApplication

--- command-starter/command-starter-bukkit/src/test/groovy/it/fulminazzo/blocksmith/command/BukkitCommandRegistryFactoryTest.groovy
+++ command-starter/command-starter-bukkit/src/test/groovy/it/fulminazzo/blocksmith/command/BukkitCommandRegistryFactoryTest.groovy
@@ -2,7 +2,10 @@ package it.fulminazzo.blocksmith.command
 
 import be.seeseemelk.mockbukkit.MockBukkit
 import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.BlocksmithApplication
 import it.fulminazzo.blocksmith.message.Messenger
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
 import spock.lang.Specification
 
 @Slf4j
@@ -18,10 +21,19 @@ class BukkitCommandRegistryFactoryTest extends Specification {
 
     def 'test that newCommandRegistry returns BukkitCommandRegistry'() {
         when:
-        def registry = CommandRegistryFactory.newCommandRegistry(Mock(Messenger), log, 'prefix')
+        def registry = CommandRegistryFactory.newCommandRegistry(mockApplication())
 
         then:
         (registry instanceof BukkitCommandRegistry)
+    }
+
+    private BlocksmithApplication mockApplication() {
+        BlocksmithApplication application = Mock(Plugin, additionalInterfaces: [BlocksmithApplication]) as BlocksmithApplication
+        application.messenger >> new Messenger(log)
+        application.log >> log
+        application.name >> 'blocksmith'
+        application.server >> Bukkit.server
+        return application
     }
 
 }
--- command-starter/command-starter-bungeecord/src/test/groovy/it/fulminazzo/blocksmith/command/BungeeCommandRegistryFactoryTest.groovy
+++ command-starter/command-starter-bungeecord/src/test/groovy/it/fulminazzo/blocksmith/command/BungeeCommandRegistryFactoryTest.groovy
@@ -2,6 +2,9 @@ package it.fulminazzo.blocksmith.command
 
 import groovy.util.logging.Slf4j
 import it.fulminazzo.blocksmith.BlocksmithApplication
+import it.fulminazzo.blocksmith.message.Messenger
+import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.plugin.Plugin
 import spock.lang.Specification
 
 @Slf4j
@@ -9,10 +12,19 @@ class BungeeCommandRegistryFactoryTest extends Specification {
 
     def 'test that newCommandRegistry returns BungeeCommandRegistry'() {
         when:
-        def registry = CommandRegistryFactory.newCommandRegistry(Mock(BlocksmithApplication))
+        def registry = CommandRegistryFactory.newCommandRegistry(mockApplication())
 
         then:
         (registry instanceof BungeeCommandRegistry)
+    }
+
+    private BlocksmithApplication mockApplication() {
+        BlocksmithApplication application = Mock(Plugin, additionalInterfaces: [BlocksmithApplication]) as BlocksmithApplication
+        application.messenger >> new Messenger(log)
+        application.log >> log
+        application.name >> 'blocksmith'
+        application.proxy >> Mock(ProxyServer)
+        return application
     }
 
 }
