78cfb767b23e5a68377b556c1a3965b015e2773e
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 13:03:34 +0100

    Updated initialization of registries

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/CommandRegistryFactory.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/CommandRegistryFactory.java
@@ -1,6 +1,8 @@
 package it.fulminazzo.blocksmith.command;
 
+import it.fulminazzo.blocksmith.message.Messenger;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 import java.util.ServiceLoader;
 
@@ -12,22 +14,26 @@ public interface CommandRegistryFactory {
     /**
      * Creates a new Command registry.
      *
-     * @param prefix the prefix to prepend to automatically computed permissions
+     * @param messenger the messenger (to handle internal messages)
+     * @param logger    the logger
      * @return the command registry
      */
-    @NotNull CommandRegistry newRegistry(final @NotNull String prefix);
+    @NotNull CommandRegistry newRegistry(final @NotNull Messenger messenger,
+                                         final @NotNull Logger logger);
 
     /**
      * Looks up the available services for a valid factory.
      * Then, it creates a new registry with the given prefix.
      *
-     * @param prefix the prefix to prepend to automatically computed permissions
+     * @param messenger the messenger (to handle internal messages)
+     * @param logger    the logger
      * @return the command registry
      */
-    static @NotNull CommandRegistry newCommandRegistry(final @NotNull String prefix) {
+    static @NotNull CommandRegistry newCommandRegistry(final @NotNull Messenger messenger,
+                                                       final @NotNull Logger logger) {
         ServiceLoader<CommandRegistryFactory> loader = ServiceLoader.load(CommandRegistryFactory.class);
         CommandRegistryFactory factory = loader.findFirst().orElseThrow(() -> new IllegalStateException("No valid CommandRegistryFactory was found"));
-        return factory.newRegistry(prefix);
+        return factory.newRegistry(messenger, logger);
     }
 
 }
