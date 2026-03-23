b26e5e1ee3b44d266f881bc0c7840a1bd281b515
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 01:23:27 +0100

    Created ConsoleCommandSender for testing purposes

---
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/ConsoleCommandSender.java
@@ -0,0 +1,9 @@
+package it.fulminazzo.blocksmith.command;
+
+public final class ConsoleCommandSender extends CommandSender {
+
+    public ConsoleCommandSender() {
+        super("CONSOLE");
+    }
+
+}
