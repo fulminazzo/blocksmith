9c928835bdb964a1925ca7a42d67a7d5e5c4213b
fulminazzo <fulminazzo21@gmail.com>
2026-03-24 00:02:29 +0100

    Added tests for NumberArgumentParser

---
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/argument/NumberArgumentParserTest.groovy
@@ -0,0 +1,57 @@
+package it.fulminazzo.blocksmith.command.argument
+
+import it.fulminazzo.blocksmith.command.CommandSender
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper
+import it.fulminazzo.blocksmith.command.MockCommandSenderWrapper
+import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext
+import it.fulminazzo.blocksmith.command.execution.CommandExecutionException
+import it.fulminazzo.blocksmith.message.argument.Placeholder
+import spock.lang.Specification
+
+class NumberArgumentParserTest extends Specification {
+
+    private final NumberArgumentParser parser = new NumberArgumentParser<>(Integer.MIN_VALUE, Integer.MAX_VALUE, Integer::valueOf)
+
+    private final CommandSenderWrapper sender = new MockCommandSenderWrapper(new CommandSender())
+
+    def 'test that parse with valid argument does not throw'() {
+        when:
+        parser.parse('1')
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'test that parse throws for invalid argument'() {
+        when:
+        parser.parse('a')
+
+        then:
+        def e = thrown(CommandExecutionException)
+        e.arguments.toList() == [Placeholder.of("min", Integer.MIN_VALUE.toString()),
+                                 Placeholder.of("max", Integer.MAX_VALUE.toString())]
+    }
+
+    def 'test that getCompletions returns #expected for argument #argument'() {
+        given:
+        def context = new CommandExecutionContext(sender).addInput(argument)
+
+        when:
+        def actual = parser.getCompletions(context)
+
+        then:
+        actual == expected
+
+        where:
+        argument             || expected
+        ''                   || (0..9).collect { it.toString() }
+        '1'                  || (0..9).collect { "1$it".toString() }
+        '12'                 || (0..9).collect { "12$it".toString() }
+        '-1'                 || (0..9).collect { "-1$it".toString() }
+        '-12'                || (0..9).collect { "-12$it".toString() }
+        'a'                  || []
+        "$Integer.MAX_VALUE" || []
+        "$Integer.MIN_VALUE" || []
+    }
+
+}
