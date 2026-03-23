7d47f00fcf319f0694f6c4843eb1c61126982961
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 15:27:14 +0100

    Created velocity implementation of CommandRegistry

---
+++ command-starter/command-starter-velocity/src/main/java/it/fulminazzo/blocksmith/command/VelocityCommandRegistry.java
@@ -0,0 +1,70 @@
+package it.fulminazzo.blocksmith.command;
+
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.command.CommandMeta;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+import com.velocitypowered.api.proxy.ProxyServer;
+import it.fulminazzo.blocksmith.BlocksmithApplication;
+import it.fulminazzo.blocksmith.command.node.CommandInfo;
+import it.fulminazzo.blocksmith.command.node.LiteralNode;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+final class VelocityCommandRegistry extends CommandRegistry {
+    private final @NotNull CommandManager commandManager;
+
+    public VelocityCommandRegistry(final @NotNull BlocksmithApplication application) {
+        super(application.getMessenger(), application.getLog(), application.getName().toLowerCase());
+        this.commandManager = ((ProxyServer) application.getServer()).getCommandManager();
+    }
+
+    @Override
+    protected @NotNull CommandSenderWrapper wrapSender(final @NotNull Object executor) {
+        return new VelocityCommandSenderWrapper((CommandSource) executor);
+    }
+
+    @Override
+    protected void onRegister(final @NotNull String commandName, final @NotNull LiteralNode command) {
+        CommandInfo info = command.getCommandInfo().orElseThrow();
+        List<String> aliases = new ArrayList<>(command.getAliases());
+        aliases.remove(commandName);
+        CommandMeta meta = commandManager.metaBuilder(commandName)
+                .aliases(aliases.toArray(new String[0]))
+                .build();
+        commandManager.register(meta, new SimpleCommand() {
+
+            @Override
+            public void execute(final @NotNull Invocation invocation) {
+                VelocityCommandRegistry.this.execute(command, invocation.source(), invocation.alias(), invocation.arguments());
+            }
+
+            @Override
+            public @NotNull CompletableFuture<List<String>> suggestAsync(final @NotNull Invocation invocation) {
+                return CompletableFuture.supplyAsync(() ->
+                        VelocityCommandRegistry.this.tabComplete(command, invocation.source(), invocation.alias(), invocation.arguments())
+                );
+            }
+
+            @Override
+            public boolean hasPermission(final @NotNull Invocation invocation) {
+                return wrapSender(invocation.source()).hasPermission(info.getPermission());
+            }
+
+        });
+    }
+
+    @Override
+    protected void onUnregister(final @NotNull String commandName) {
+        commandManager.unregister(commandName);
+    }
+
+    @Override
+    protected @NotNull Class<?> getSenderType() {
+        return CommandSource.class;
+    }
+
+}
