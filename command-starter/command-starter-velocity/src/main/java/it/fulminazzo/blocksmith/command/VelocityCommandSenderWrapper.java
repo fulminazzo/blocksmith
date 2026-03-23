7a1f3ad17233b00b15d28b38d5bc22dd6c6d92ee
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 15:21:53 +0100

    Created velocity implementation of CommandSenderWrapper

---
+++ command-starter/command-starter-velocity/src/main/java/it/fulminazzo/blocksmith/command/VelocityCommandSenderWrapper.java
@@ -0,0 +1,30 @@
+package it.fulminazzo.blocksmith.command;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
+import it.fulminazzo.blocksmith.command.node.PermissionInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.jetbrains.annotations.NotNull;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+final class VelocityCommandSenderWrapper extends CommandSenderWrapper {
+    private final @NotNull CommandSource actualSender;
+
+    @Override
+    public @NotNull String getName() {
+        return isPlayer() ? ((Player) actualSender).getUsername() : "console";
+    }
+
+    @Override
+    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
+        return actualSender.hasPermission(permissionInfo.getPermission());
+    }
+
+    @Override
+    public boolean isPlayer() {
+        return actualSender instanceof Player;
+    }
+
+}
