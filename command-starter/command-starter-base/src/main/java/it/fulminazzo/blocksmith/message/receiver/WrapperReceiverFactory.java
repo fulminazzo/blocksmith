ceeffe3968458e0595b317549c08362b70795a08
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 12:42:23 +0100

    Created and registered WrapperReceiverFactory to handle messages sent to CommandSenderWrapper objects

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/message/receiver/WrapperReceiverFactory.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.blocksmith.message.receiver;
+
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Implementation of {@link ReceiverFactory} for {@link CommandSenderWrapper} objects.
+ */
+public final class WrapperReceiverFactory implements ReceiverFactory {
+
+    @Override
+    public @NotNull Collection<Receiver> getAllReceivers() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public @NotNull <R> Receiver create(final @NotNull R receiver) {
+        CommandSenderWrapper wrapper = (CommandSenderWrapper) receiver;
+        Object internal = wrapper.getActualSender();
+        return ReceiverFactories.get(internal.getClass()).create(internal);
+    }
+
+    @Override
+    public boolean supports(final @NotNull Class<?> receiverType) {
+        return CommandSenderWrapper.class.isAssignableFrom(receiverType);
+    }
+
+}
