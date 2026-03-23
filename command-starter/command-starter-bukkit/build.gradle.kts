d03883d7e2adbf5d532ef6393ce71dd7a50aa40d
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 14:45:09 +0100

    Added MockBukkit 1.16 dependency for testing purposes

--- command-starter/command-starter-bukkit/build.gradle.kts
+++ command-starter/command-starter-bukkit/build.gradle.kts
@@ -4,4 +4,9 @@ dependencies {
     compileOnly(libs.spigot)
 
     testImplementation(libs.spigot)
+    testImplementation(libs.mockbukkit16) {
+        exclude(group = "io.papermc.paper")
+        exclude(group = "net.bytebuddy")
+        exclude(group = "org.junit.jupiter")
+    }
 }
