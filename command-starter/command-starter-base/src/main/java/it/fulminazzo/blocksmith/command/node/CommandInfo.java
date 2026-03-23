b6fc1e0350ce7881840e94ace0ada7df902b936b
fulminazzo <fulminazzo21@gmail.com>
2026-03-23 02:08:00 +0100

    Updated CommandInfo with autoComputed field and logic

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/CommandInfo.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/CommandInfo.java
@@ -1,8 +1,6 @@
 package it.fulminazzo.blocksmith.command.node;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +14,40 @@ public final class CommandInfo {
     @NotNull String description;
     final @NotNull PermissionInfo permission;
 
+    @Getter(AccessLevel.PRIVATE)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    boolean autoComputed;
+
+    /**
+     * Instantiates a new Command info.
+     *
+     * @param description the description
+     * @param permission  the permission
+     */
+    public CommandInfo(final @NotNull String description,
+                       final @NotNull PermissionInfo permission) {
+        this(description, permission, false);
+    }
+
+    /**
+     * Instantiates a new Command info.
+     *
+     * @param description  the description
+     * @param permission   the permission
+     * @param autoComputed if <code>true</code>, will mark the permission as automatically computed,
+     *                     meaning it was not provided by the user, but was instead retrieved from the command route
+     * @deprecated FOR INTERNAL USE ONLY
+     */
+    @Deprecated
+    public CommandInfo(final @NotNull String description,
+                       final @NotNull PermissionInfo permission,
+                       final boolean autoComputed) {
+        this.description = description;
+        this.permission = permission;
+        this.autoComputed = autoComputed;
+    }
+
     /**
      * Merges the given command information into the current one
      * (only if the information are not given).
@@ -23,7 +55,8 @@ public final class CommandInfo {
      * @param commandInfo the command info to merge from
      */
     public void merge(final @NotNull CommandInfo commandInfo) {
-        if (description.isEmpty()) description = commandInfo.getDescription();
+        if (description.isEmpty() || isAutoComputed())
+            description = commandInfo.getDescription();
         permission.merge(commandInfo.getPermission());
     }
 
