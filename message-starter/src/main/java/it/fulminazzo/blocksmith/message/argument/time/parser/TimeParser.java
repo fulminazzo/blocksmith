e4c43b3a565177521761269eba594321bdf2e4b0
fulminazzo <fulminazzo21@gmail.com>
2026-03-25 16:23:23 +0100

    Added parseOptionalArgument and parseShownArgument methods with parseGeneralArgument support method

--- message-starter/src/main/java/it/fulminazzo/blocksmith/message/argument/time/parser/TimeParser.java
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/argument/time/parser/TimeParser.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.blocksmith.message.argument.time.parser;
 
 import it.fulminazzo.blocksmith.message.argument.time.node.ArgumentNode;
+import it.fulminazzo.blocksmith.message.argument.time.node.TimeNode;
 import it.fulminazzo.blocksmith.structure.Pair;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,6 +22,79 @@ final class TimeParser {
     TimeParser(final @NotNull String timeFormat) {
         this.timeFormat = timeFormat;
         this.tokenizer = new TimeTokenizer(timeFormat);
+    }
+
+    /**
+     * OPTIONAL_ARGUMENT := [ GENERAL_ARGUMENT ]
+     *
+     * @return the node
+     */
+    @NotNull TimeNode parseOptionalArgument() {
+        consume(TimeToken.OPEN_BRACKET);
+        ArgumentNode node = parseGeneralArgument(TimeToken.CLOSE_BRACKET);
+        node.setOptional(true);
+        consume(TimeToken.CLOSE_BRACKET);
+        return node;
+    }
+
+    /**
+     * SHOWN_ARGUMENT := ( GENERAL_ARGUMENT )
+     *
+     * @return the node
+     */
+    @NotNull TimeNode parseShownArgument() {
+        consume(TimeToken.OPEN_PHARENTHESIS);
+        ArgumentNode node = parseGeneralArgument(TimeToken.CLOSE_PARENTHESIS);
+        consume(TimeToken.CLOSE_PARENTHESIS);
+        return node;
+    }
+
+    /**
+     * GENERAL_ARGUMENT := ( SINGULAR_AND_PLURAL | {@link TimeToken#TEXT} )* UNIT_PLACEHOLDER ( SINGULAR_AND_PLURAL | {@link TimeToken#TEXT} )*
+     *
+     * @param closeToken the token upon which the reading should stop
+     * @return the node
+     */
+    @NotNull ArgumentNode parseGeneralArgument(final @NotNull TimeToken closeToken) {
+        StringBuilder text = new StringBuilder();
+        ArgumentNode.TimeUnit unit = null;
+        Pair<String, String> singularAndPlural = null;
+        Boolean full = null;
+
+        TimeToken lastRead;
+        while ((lastRead = tokenizer.next()) != closeToken && lastRead != TimeToken.EOF) {
+            if (full == null) {
+                if (lastRead == TimeToken.EXCLAMATION_MARK) {
+                    full = true;
+                    continue;
+                } else full = false;
+            }
+            if (lastRead == TimeToken.PERCENTAGE) {
+                if (unit == null) {
+                    unit = parseUnitPlaceholder();
+                    text.append("%unit%");
+                }
+                else throw parseException("invalid argument '%s': unit was already specified", text);
+            } else if (lastRead == TimeToken.OPEN_BRACE) {
+                singularAndPlural = parseSingularAndPlural();
+                text.append("%name%");
+            } else text.append(tokenizer.getLastRead());
+        }
+
+        if (unit == null)
+            throw parseException("no time unit was specified for argument '%s'. Please use %%<unit>%%", text);
+        if (singularAndPlural == null) {
+            String first = unit.getName().charAt(0) + "";
+            singularAndPlural = Pair.of(first, first);
+        }
+
+        return new ArgumentNode(
+                text.toString(),
+                unit,
+                singularAndPlural.getFirst(),
+                singularAndPlural.getSecond(),
+                full
+        );
     }
 
     /**
