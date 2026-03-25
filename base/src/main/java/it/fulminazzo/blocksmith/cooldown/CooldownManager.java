1c19797e64de4042275daceac10a50fb233320a0
fulminazzo <fulminazzo21@gmail.com>
2026-03-25 13:06:07 +0100

    Added getRemainingCooldown method

--- base/src/main/java/it/fulminazzo/blocksmith/cooldown/CooldownManager.java
+++ base/src/main/java/it/fulminazzo/blocksmith/cooldown/CooldownManager.java
@@ -36,9 +36,24 @@ public final class CooldownManager<T> {
     public boolean isOnCooldown(final @NotNull T entity) {
         Long last = lastUsage.get(entity);
         if (last == null) return false;
-        if (last > System.currentTimeMillis()) return true;
+        if (last > getNow()) return true;
         lastUsage.remove(entity);
         return false;
+    }
+
+    /**
+     * Gets the remaining time before the cooldown is exhausted.
+     *
+     * @param entity the entity
+     * @return the time in milliseconds
+     */
+    public long getRemainingCooldown(final @NotNull T entity) {
+        if (isOnCooldown(entity)) return lastUsage.get(entity) - getNow();
+        else throw new IllegalArgumentException(String.format("Entity '%s' is not on cooldown", entity));
+    }
+
+    private static long getNow() {
+        return System.currentTimeMillis();
     }
 
 }
