1bddf9e933a12b65b1936cad4bfac6bab8fea2c1
fulminazzo <fulminazzo21@gmail.com>
2026-03-25 22:34:28 +0100

    Created DynamicClanCommand for testing purposes

---
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/parser/DynamicClanCommand.java
@@ -0,0 +1,36 @@
+package it.fulminazzo.blocksmith.command.parser;
+
+import it.fulminazzo.blocksmith.command.CommandSender;
+import it.fulminazzo.blocksmith.command.annotation.Command;
+import it.fulminazzo.blocksmith.command.annotation.Default;
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("unused")
+@Command(dynamic = true)
+final class DynamicClanCommand {
+
+    @Command
+    public void execute(
+            final @NotNull CommandSender sender
+    ) {
+        // something
+    }
+
+    @Command("help <verbose>")
+    @Permission(grant = Permission.Grant.ALL)
+    public void help(
+            final @NotNull CommandSender sender,
+            final boolean verbose
+    ) {
+        // something
+    }
+
+    public @NotNull List<String> getAliases() {
+        return Arrays.asList("clan", "gang", "team");
+    }
+
+}
