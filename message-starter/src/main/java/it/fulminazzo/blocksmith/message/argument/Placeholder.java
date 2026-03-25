a2bf619dc615beae14f38afc2a8065f33df091d7
fulminazzo <fulminazzo21@gmail.com>
2026-03-25 17:05:18 +0100

    Updated Javadoc

--- message-starter/src/main/java/it/fulminazzo/blocksmith/message/argument/Placeholder.java
+++ message-starter/src/main/java/it/fulminazzo/blocksmith/message/argument/Placeholder.java
@@ -47,7 +47,7 @@ public final class Placeholder implements Argument {
     /**
      * Creates a new Placeholder.
      *
-     * @param placeholder the placeholder (without ampersands)
+     * @param placeholder the placeholder (without percentages)
      * @param value       the value to replace
      * @return the placeholder
      */
@@ -59,7 +59,7 @@ public final class Placeholder implements Argument {
     /**
      * Creates a new Placeholder.
      *
-     * @param placeholder the placeholder (without ampersands)
+     * @param placeholder the placeholder (without percentages)
      * @param value       the value to replace
      * @return the placeholder
      */
@@ -71,7 +71,7 @@ public final class Placeholder implements Argument {
     /**
      * Creates a new Placeholder.
      *
-     * @param placeholder the placeholder (without ampersands)
+     * @param placeholder the placeholder (without percentages)
      * @param value       the value to replace
      * @return the placeholder
      */
