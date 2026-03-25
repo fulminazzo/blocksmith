f21ed8c619822e258a72b4097147020a8b7398c9
fulminazzo <fulminazzo21@gmail.com>
2026-03-25 14:27:12 +0100

    Added more tests for Placeholder parsing

--- message-starter/src/test/groovy/it/fulminazzo/blocksmith/message/argument/PlaceholderTest.groovy
+++ message-starter/src/test/groovy/it/fulminazzo/blocksmith/message/argument/PlaceholderTest.groovy
@@ -1,10 +1,30 @@
 package it.fulminazzo.blocksmith.message.argument
 
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.message.MessageParseContext
+import it.fulminazzo.blocksmith.message.Messenger
+import it.fulminazzo.blocksmith.message.provider.MessageNotFoundException
+import it.fulminazzo.blocksmith.message.provider.MessageProvider
 import it.fulminazzo.blocksmith.message.util.ComponentUtils
 import net.kyori.adventure.text.Component
 import spock.lang.Specification
 
+@Slf4j
 class PlaceholderTest extends Specification {
+
+    private Messenger messenger
+
+    void setup() {
+        def provider = Mock(MessageProvider)
+        provider.getMessage(_, _) >> { a ->
+            def code = a[0]
+            if (code == 'second') return Component.text('Alex')
+            else throw new MessageNotFoundException(code, a[1])
+        }
+
+        messenger = new Messenger(log)
+        messenger.messageProvider = provider
+    }
 
     def 'test that apply correctly applies with #placeholder and #value'() {
         given:
@@ -17,7 +37,7 @@ class PlaceholderTest extends Specification {
         def p = Placeholder.of(placeholder, value)
 
         when:
-        def actual = p.apply(component)
+        def actual = p.apply(new MessageParseContext(messenger, Locale.ITALY, component))
 
         then:
         ComponentUtils.toString(actual) == expected
@@ -28,6 +48,20 @@ class PlaceholderTest extends Specification {
         Component.text('name') | 'Alex'
         Component.text('name') | Component.text('Alex')
         'name'                 | Component.text('Alex')
+    }
+
+    def 'test that apply correctly applies recursively placeholder'() {
+        given:
+        def component = Component.text('Hello, %name%!')
+
+        and:
+        def p = Placeholder.of('name', 'second')
+
+        when:
+        def actual = p.apply(new MessageParseContext(messenger, Locale.ITALY, component))
+
+        then:
+        ComponentUtils.toString(actual) == 'Hello, Alex!'
     }
 
 }
