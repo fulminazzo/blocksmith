6305ee56f754647e83c1f99327af3c5a97d7b7df
fulminazzo <fulminazzo21@gmail.com>
2026-03-26 17:09:30 +0100

    Added more tests for quoting of results

--- base/src/test/groovy/it/fulminazzo/blocksmith/util/StringUtilsTest.groovy
+++ base/src/test/groovy/it/fulminazzo/blocksmith/util/StringUtilsTest.groovy
@@ -4,43 +4,63 @@ import spock.lang.Specification
 
 class StringUtilsTest extends Specification {
 
-    def 'test that split #string, #regex, #quotes returns #expected'() {
+    def 'test that split #string, #regex, #quoted and #quotes returns #expected'() {
         when:
-        def actual = StringUtils.split(string, regex, *quotes)
+        def actual = StringUtils.split(string, regex, quoted, *quotes)
 
         then:
         actual == expected
 
         where:
-        string                          | regex  | quotes     || expected
-        'a,b,c'                         | ','    | []         || ['a', 'b', 'c']
-        'hello world'                   | ' '    | []         || ['hello', 'world']
-        'one'                           | ','    | []         || ['one']
-        ''                              | ','    | []         || ['']
-        'a,,b'                          | ','    | []         || ['a', '', 'b']
+        string                          | regex  | quoted | quotes     || expected
+        'a,b,c'                         | ','    | true   | []         || ['a', 'b', 'c']
+        'a,b,c'                         | ','    | false  | []         || ['a', 'b', 'c']
+        'hello world'                   | ' '    | true   | []         || ['hello', 'world']
+        'hello world'                   | ' '    | false  | []         || ['hello', 'world']
+        'one'                           | ','    | true   | []         || ['one']
+        'one'                           | ','    | false  | []         || ['one']
+        ''                              | ','    | true   | []         || ['']
+        ''                              | ','    | false  | []         || ['']
+        'a,,b'                          | ','    | true   | []         || ['a', '', 'b']
+        'a,,b'                          | ','    | false  | []         || ['a', '', 'b']
 
-        'a1b2c'                         | '\\d'  | []         || ['a', 'b', 'c']
-        'a1b2c3'                        | '\\d'  | []         || ['a', 'b', 'c', '']
-        'one  two'                      | '\\s+' | []         || ['one', 'two']
+        'a1b2c'                         | '\\d'  | true   | []         || ['a', 'b', 'c']
+        'a1b2c'                         | '\\d'  | false  | []         || ['a', 'b', 'c']
+        'a1b2c3'                        | '\\d'  | true   | []         || ['a', 'b', 'c', '']
+        'a1b2c3'                        | '\\d'  | false  | []         || ['a', 'b', 'c', '']
+        'one  two'                      | '\\s+' | true   | []         || ['one', 'two']
+        'one  two'                      | '\\s+' | false  | []         || ['one', 'two']
 
-        '"a,b",c'                       | ','    | ['"']      || ['"a,b"', 'c']
-        '"a,b",c"'                      | ','    | ['"']      || ['"a,b"', 'c"']
-        "'a,b',c"                       | ','    | ["'"]      || ["'a,b'", 'c']
-        '"a,b","c,d"'                   | ','    | ['"']      || ['"a,b"', '"c,d"']
-        '"a,b","c,d'                    | ','    | ['"']      || ['"a,b"', '"c,d']
+        '"a,b",c'                       | ','    | true   | ['"']      || ['"a,b"', 'c']
+        '"a,b",c'                       | ','    | false  | ['"']      || ['a,b', 'c']
+        '"a,b",c"'                      | ','    | true   | ['"']      || ['"a,b"', 'c"']
+        '"a,b",c"'                      | ','    | false  | ['"']      || ['a,b', 'c"']
+        "'a,b',c"                       | ','    | true   | ["'"]      || ["'a,b'", 'c']
+        "'a,b',c"                       | ','    | false  | ["'"]      || ["a,b", 'c']
+        '"a,b","c,d"'                   | ','    | true   | ['"']      || ['"a,b"', '"c,d"']
+        '"a,b","c,d"'                   | ','    | false  | ['"']      || ['a,b', 'c,d']
+        '"a,b","c,d'                    | ','    | true   | ['"']      || ['"a,b"', '"c,d']
+        '"a,b","c,d'                    | ','    | false  | ['"']      || ['a,b', '"c,d']
 
-        '"a,b",\'c,d\',e'               | ','    | ['"', "'"] || ['"a,b"', "'c,d'", 'e']
+        '"a,b",\'c,d\',e'               | ','    | true   | ['"', "'"] || ['"a,b"', "'c,d'", 'e']
+        '"a,b",\'c,d\',e'               | ','    | false  | ['"', "'"] || ['a,b', "c,d", 'e']
 
-        'first "and \"second\"" third' || ' '    | ['"']      || ['first', '"and \"second\""', 'third']
+        'first "and \"second\"" third' || ' '    | true   | ['"']      || ['first', '"and \"second\""', 'third']
+        'first "and \"second\"" third' || ' '    | false  | ['"']      || ['first', 'and \"second\"', 'third']
 
-        '"a,b,c'                        | ','    | ['"']      || ['"a,b,c']
+        '"a,b,c'                        | ','    | true   | ['"']      || ['"a,b,c']
+        '"a,b,c'                        | ','    | false  | ['"']      || ['"a,b,c']
 
-        'nodivision'                    | ','    | ['"']      || ['nodivision']
+        'nodivision'                    | ','    | true   | ['"']      || ['nodivision']
+        'nodivision'                    | ','    | false  | ['"']      || ['nodivision']
 
-        'a,b,c'                         | ','    | []         || ['a', 'b', 'c']
+        'a,b,c'                         | ','    | true   | []         || ['a', 'b', 'c']
+        'a,b,c'                         | ','    | false  | []         || ['a', 'b', 'c']
 
-        ',a,b'                          | ','    | []         || ['', 'a', 'b']
-        'a,b,'                          | ','    | []         || ['a', 'b', '']
+        ',a,b'                          | ','    | true   | []         || ['', 'a', 'b']
+        ',a,b'                          | ','    | false  | []         || ['', 'a', 'b']
+        'a,b,'                          | ','    | true   | []         || ['a', 'b', '']
+        'a,b,'                          | ','    | false  | []         || ['a', 'b', '']
     }
 
 }
