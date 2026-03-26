528718419866c21d97f9bb639b084430e223c936
fulminazzo <fulminazzo21@gmail.com>
2026-03-26 13:37:11 +0100

    Added tests for executes method

--- command-starter/command-starter-brigadier/src/test/groovy/it/fulminazzo/blocksmith/command/BrigadierParserTest.groovy
+++ command-starter/command-starter-brigadier/src/test/groovy/it/fulminazzo/blocksmith/command/BrigadierParserTest.groovy
@@ -1,17 +1,58 @@
 package it.fulminazzo.blocksmith.command
 
 import com.mojang.brigadier.arguments.*
+import com.mojang.brigadier.context.CommandContext
 import it.fulminazzo.blocksmith.command.node.ArgumentNode
+import it.fulminazzo.blocksmith.command.node.LiteralNode
 import spock.lang.Specification
 
 class BrigadierParserTest extends Specification {
+
+    def 'test that executes calls on delegate with root node'() {
+        given:
+        def delegate = Mock(CommandRegistry)
+        def node = Mock(LiteralNode)
+
+        and:
+        def source = new Object()
+        def input = 'Hello, world! How are you?'
+        def context = new CommandContext(
+                source,
+                input,
+                [:],
+                null,
+                null,
+                [],
+                null,
+                null,
+                null,
+                false
+        )
+
+        and:
+        def parser = new BrigadierParser(delegate)
+
+        when:
+        def command = parser.executes(node).run(context)
+
+        then:
+        command == 1
+
+        and:
+        1 * delegate.execute(
+                node,
+                source,
+                'Hello,',
+                'world! How are you?'.split(' ')
+        )
+    }
 
     def 'test that getArgumentType of #node returns #expected'() {
         when:
         def actual = BrigadierParser.getArgumentType(node as ArgumentNode)
 
         then:
-        actual.class == expected
+        actual == null ? expected == null : actual.class == expected
 
         where:
         node                                                           || expected
