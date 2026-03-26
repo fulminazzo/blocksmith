58e401a4a1d72ea3c9cfb35e89221bfc5a72a15b
fulminazzo <fulminazzo21@gmail.com>
2026-03-26 15:01:20 +0100

    Added tests for parseChild of ArgumentNode and unknown ArgumentType

--- command-starter/command-starter-brigadier/src/test/groovy/it/fulminazzo/blocksmith/command/BrigadierParserTest.groovy
+++ command-starter/command-starter-brigadier/src/test/groovy/it/fulminazzo/blocksmith/command/BrigadierParserTest.groovy
@@ -3,10 +3,12 @@ package it.fulminazzo.blocksmith.command
 import com.mojang.brigadier.arguments.*
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.suggestion.SuggestionProvider
+import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import com.mojang.brigadier.tree.ArgumentCommandNode
-import com.mojang.brigadier.tree.RootCommandNode
 import it.fulminazzo.blocksmith.command.node.ArgumentNode
 import it.fulminazzo.blocksmith.command.node.LiteralNode
+import org.jetbrains.annotations.NotNull
 import spock.lang.Specification
 
 class BrigadierParserTest extends Specification {
@@ -51,6 +53,75 @@ class BrigadierParserTest extends Specification {
         (child.type instanceof BoolArgumentType)
     }
 
+    def 'test parseChild of unknown argument type'() {
+        given:
+        def delegate = Mock(CommandRegistry)
+        def node = ArgumentNode.newNode('argument', Object, false)
+        node.addChild(ArgumentNode.newNode('value', boolean, false))
+
+        and:
+        def builder = LiteralArgumentBuilder.literal('sentinel')
+
+        and:
+        def source = new Object()
+        def context = mockContext(source, input)
+
+        and:
+        def parser = new BrigadierParser(delegate)
+
+        and:
+        def root = new LiteralNode('sentinel')
+
+        when:
+        parser.parseChild(root, builder, node)
+
+        and:
+        def arguments = builder.arguments
+
+        then:
+        arguments.size() == 1
+
+        and:
+        def argument = arguments.first()
+        (argument instanceof ArgumentCommandNode)
+        argument.name == 'argument'
+        (argument.type instanceof StringArgumentType)
+
+        when:
+        SuggestionProvider suggestion = argument.customSuggestions
+
+        then:
+        suggestion != null
+
+        when:
+        suggestion.getSuggestions(context, new SuggestionsBuilder(input, 0))
+
+        then:
+        1 * delegate.tabComplete(
+                root,
+                source,
+                'argument',
+                args.toArray(new String[args.size()])
+        ) >> []
+
+        when:
+        def children = argument.children
+
+        then:
+        children.size() == 1
+
+        and:
+        def child = children.first()
+        (child instanceof ArgumentCommandNode)
+        child.name == 'value'
+        (child.type instanceof BoolArgumentType)
+
+        where:
+        input                  || args
+        'argument Hello '      || ['Hello', '']
+        'argument Hello world' || ['Hello', 'world']
+    }
+
     def 'test that executes calls on delegate with root node'() {
         given:
         def delegate = Mock(CommandRegistry)
@@ -59,18 +130,7 @@ class BrigadierParserTest extends Specification {
         and:
         def source = new Object()
         def input = 'Hello, world! How are you?'
-        def context = new CommandContext(
-                source,
-                input,
-                [:],
-                null,
-                null,
-                [],
-                null,
-                null,
-                null,
-                false
-        )
+        def context = mockContext(source, input)
 
         and:
         def parser = new BrigadierParser(delegate)
@@ -110,6 +170,21 @@ class BrigadierParserTest extends Specification {
         ArgumentNode.newNode('number', Integer, false).setGreedy(true) || StringArgumentType
         ArgumentNode.newNode('string', String, false).setGreedy(true)  || StringArgumentType
         ArgumentNode.newNode('object', Object, false)                  || null
+    }
+
+    private CommandContext mockContext(final @NotNull Object source, final @NotNull String input) {
+        return new CommandContext(
+                source,
+                input,
+                [:],
+                null,
+                null,
+                [],
+                null,
+                null,
+                null,
+                false
+        )
     }
 
 }
