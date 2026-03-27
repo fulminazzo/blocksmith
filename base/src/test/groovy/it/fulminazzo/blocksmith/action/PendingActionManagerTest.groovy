60d5d8adc13bc6341784a3ee572b32e1d3dfccae
fulminazzo <fulminazzo21@gmail.com>
2026-03-27 23:07:08 +0100

    Added tests for PendingActionManager

---
+++ base/src/test/groovy/it/fulminazzo/blocksmith/action/PendingActionManagerTest.groovy
@@ -0,0 +1,70 @@
+package it.fulminazzo.blocksmith.action
+
+import spock.lang.Specification
+
+import java.time.Duration
+
+class PendingActionManagerTest extends Specification {
+
+    private boolean executed = false
+
+    def 'test that PendingActionManager works'() {
+        given:
+        def manager = new PendingActionManager()
+
+        and:
+        def target = new Object()
+
+        when:
+        def result = manager.execute(target)
+
+        then:
+        result == PendingActionManager.Result.NOT_FOUND
+
+        when:
+        manager.register(target, Duration.ofSeconds(10), () -> executed = true)
+
+        then:
+        noExceptionThrown()
+
+        when:
+        result = manager.execute(target)
+
+        then:
+        result == PendingActionManager.Result.SUCCESS
+
+        and:
+        executed
+
+        when:
+        result = manager.execute(target)
+
+        then:
+        result == PendingActionManager.Result.NOT_FOUND
+
+        when:
+        executed = false
+
+        and:
+        manager.register(target, Duration.ofNanos(1), () -> executed = true)
+
+        then:
+        noExceptionThrown()
+
+        when:
+        result = manager.execute(target)
+
+        then:
+        result == PendingActionManager.Result.EXPIRED
+
+        and:
+        !executed
+
+        when:
+        result = manager.execute(target)
+
+        then:
+        result == PendingActionManager.Result.NOT_FOUND
+    }
+
+}
