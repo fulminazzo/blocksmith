856aabf1048b0586a388ce5d1c831fea2d6bf2b6
fulminazzo <fulminazzo21@gmail.com>
2026-03-28 15:34:52 +0100

    Created Tab annotation

---
+++ command-starter/src/main/java/it/fulminazzo/blocksmith/command/annotation/Tab.java
@@ -0,0 +1,17 @@
+package it.fulminazzo.blocksmith.command.annotation;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//TODO: pending documentation
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface Tab {
+
+    @NotNull String value();
+
+}
