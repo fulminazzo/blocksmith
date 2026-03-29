3c8cca02aee0997bbadce0427d9b8f4b846e7ed7
fulminazzo <fulminazzo21@gmail.com>
2026-03-29 13:16:39 +0200

    Added tests for Reflect methods

---
+++ base/src/test/groovy/it/fulminazzo/blocksmith/reflect/ReflectTest.groovy
@@ -0,0 +1,130 @@
+package it.fulminazzo.blocksmith.reflect
+
+import spock.lang.Specification
+
+import java.lang.reflect.Field
+import java.util.function.Predicate
+
+class ReflectTest extends Specification {
+    private static final Field DEFAULT_NAME = NamedEntity.getDeclaredField('DEFAULT_NAME')
+    private static final Field DEFAULT_AGE = Person.getDeclaredField('DEFAULT_AGE')
+    private static final Field name = NamedEntity.getDeclaredField('name')
+    private static final Field age = Person.getDeclaredField('age')
+
+    private static final String nameValue = 'Alex'
+    private static final int ageValue = 23
+
+    static {
+        DEFAULT_NAME.accessible = true
+        DEFAULT_AGE.accessible = true
+    }
+
+    private Reflect reflect
+
+    void setup() {
+        reflect = new Reflect(Person, new Person(nameValue, ageValue))
+    }
+
+    def 'test that #method with #arguments returns #expected'() {
+        when:
+        def actual = reflect."$method"(*arguments)
+
+        then:
+        actual == expected
+
+        where:
+        method                    | arguments                                                     || expected
+        // getFieldsObject
+        'getInstanceFieldsObject' | []                                                            || [ageValue, nameValue].collect { new Reflect(it.class, it) }
+        'getStaticFieldsObject'   | []                                                            || [DEFAULT_AGE.get(null), DEFAULT_NAME.get(null)].collect { new Reflect(it.class, it) }
+        'getFieldsObject'         | [((Predicate<Field>) (f) -> false)]                           || [].collect { new Reflect(it.class, it) }
+        'getFieldsObject'         | [((Predicate<Field>) (f) -> f.declaringClass == NamedEntity)] || [DEFAULT_NAME.get(null), nameValue].collect { new Reflect(it.class, it) }
+        'getFieldsObject'         | [((Predicate<Field>) (f) -> f.declaringClass == Person)]      || [DEFAULT_AGE.get(null), ageValue].collect { new Reflect(it.class, it) }
+        'getFieldsObject'         | [((Predicate<Field>) (f) -> true)]                            || [DEFAULT_AGE.get(null), ageValue, DEFAULT_NAME.get(null), nameValue].collect { new Reflect(it.class, it) }
+        'getFieldsObject'         | []                                                            || [DEFAULT_AGE.get(null), ageValue, DEFAULT_NAME.get(null), nameValue].collect { new Reflect(it.class, it) }
+        // getFieldObject
+        'getInstanceFieldObject'  | [name.name]                                                   || new Reflect(name.type, nameValue)
+        'getInstanceFieldObject'  | [age.name]                                                    || new Reflect(age.type, ageValue)
+        'getStaticFieldObject'    | [DEFAULT_NAME.name]                                           || new Reflect(DEFAULT_NAME.type, DEFAULT_NAME.get(null))
+        'getStaticFieldObject'    | [DEFAULT_AGE.name]                                            || new Reflect(DEFAULT_AGE.type, DEFAULT_AGE.get(null))
+        'getFieldObject'          | [DEFAULT_NAME.name]                                           || new Reflect(DEFAULT_NAME.type, DEFAULT_NAME.get(null))
+        'getFieldObject'          | [DEFAULT_AGE.name]                                            || new Reflect(DEFAULT_AGE.type, DEFAULT_AGE.get(null))
+        'getFieldObject'          | [name.name]                                                   || new Reflect(name.type, nameValue)
+        'getFieldObject'          | [age.name]                                                    || new Reflect(age.type, ageValue)
+        'getFieldObject'          | [((Predicate<Field>) (f) -> f.declaringClass == NamedEntity)] || new Reflect(DEFAULT_NAME.type, DEFAULT_NAME.get(null))
+        'getFieldObject'          | [((Predicate<Field>) (f) -> f.declaringClass == Person)]      || new Reflect(DEFAULT_AGE.type, DEFAULT_AGE.get(null))
+        'getFieldObject'          | [((Predicate<Field>) (f) -> true)]                            || new Reflect(DEFAULT_AGE.type, DEFAULT_AGE.get(null))
+        // getField
+        'getInstanceField'        | [name.name]                                                   || name
+        'getInstanceField'        | [age.name]                                                    || age
+        'getStaticField'          | [DEFAULT_NAME.name]                                           || DEFAULT_NAME
+        'getStaticField'          | [DEFAULT_AGE.name]                                            || DEFAULT_AGE
+        'getField'                | [DEFAULT_NAME.name]                                           || DEFAULT_NAME
+        'getField'                | [DEFAULT_AGE.name]                                            || DEFAULT_AGE
+        'getField'                | [name.name]                                                   || name
+        'getField'                | [age.name]                                                    || age
+        'getField'                | [((Predicate<Field>) (f) -> f.declaringClass == NamedEntity)] || DEFAULT_NAME
+        'getField'                | [((Predicate<Field>) (f) -> f.declaringClass == Person)]      || DEFAULT_AGE
+        'getField'                | [((Predicate<Field>) (f) -> true)]                            || DEFAULT_AGE
+        // getFields
+        'getInstanceFields'       | []                                                            || [age, name]
+        'getStaticFields'         | []                                                            || [DEFAULT_AGE, DEFAULT_NAME]
+        'getFields'               | [((Predicate<Field>) (f) -> false)]                           || []
+        'getFields'               | [((Predicate<Field>) (f) -> f.declaringClass == NamedEntity)] || [DEFAULT_NAME, name]
+        'getFields'               | [((Predicate<Field>) (f) -> f.declaringClass == Person)]      || [DEFAULT_AGE, age]
+        'getFields'               | [((Predicate<Field>) (f) -> true)]                            || [DEFAULT_AGE, age, DEFAULT_NAME, name]
+        'getFields'               | []                                                            || [DEFAULT_AGE, age, DEFAULT_NAME, name]
+    }
+
+    def 'test that #method with #arguments throws ReflectException with #expected'() {
+        when:
+        reflect."$method"(*arguments)
+
+        then:
+        def e = thrown(ReflectException)
+        e.message == expected.message
+
+        where:
+        method                   | arguments                                                || expected
+        // getFieldObject
+        'getInstanceFieldObject' | [DEFAULT_NAME.name]                                      || ReflectException.cannotFindField(Person, DEFAULT_NAME.name)
+        'getInstanceFieldObject' | [DEFAULT_AGE.name]                                       || ReflectException.cannotFindField(Person, DEFAULT_AGE.name)
+        'getInstanceFieldObject' | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getStaticFieldObject'   | [name.name]                                              || ReflectException.cannotFindField(Person, name.name)
+        'getStaticFieldObject'   | [age.name]                                               || ReflectException.cannotFindField(Person, age.name)
+        'getStaticFieldObject'   | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getFieldObject'         | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getFieldObject'         | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getFieldObject'         | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getFieldObject'         | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getFieldObject'         | [((Predicate<Field>) (f) -> f.declaringClass == String)] || ReflectException.cannotFindField(Person)
+        // getField
+        'getInstanceField'       | [DEFAULT_NAME.name]                                      || ReflectException.cannotFindField(Person, DEFAULT_NAME.name)
+        'getInstanceField'       | [DEFAULT_AGE.name]                                       || ReflectException.cannotFindField(Person, DEFAULT_AGE.name)
+        'getInstanceField'       | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getStaticField'         | [name.name]                                              || ReflectException.cannotFindField(Person, name.name)
+        'getStaticField'         | [age.name]                                               || ReflectException.cannotFindField(Person, age.name)
+        'getStaticField'         | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getField'               | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getField'               | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getField'               | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getField'               | ['notExisting']                                          || ReflectException.cannotFindField(Person, 'notExisting')
+        'getField'               | [((Predicate<Field>) (f) -> f.declaringClass == String)] || ReflectException.cannotFindField(Person)
+        'getField'               | [((Predicate<Field>) (f) -> false)]                      || ReflectException.cannotFindField(Person)
+    }
+
+    def 'test that getField object throws ReflectException on IllegalAccessException'() {
+        given:
+        def field = Mock(Field)
+        field.get(_) >> {
+            throw new IllegalAccessException()
+        }
+
+        when:
+        reflect.getFieldObject(field)
+
+        then:
+        thrown(ReflectException)
+    }
+
+}
