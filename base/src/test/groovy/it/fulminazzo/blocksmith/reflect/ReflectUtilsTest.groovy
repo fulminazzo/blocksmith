3423c213e64e719a8cdb210e2708543a789c5986
fulminazzo <fulminazzo21@gmail.com>
2026-03-29 17:28:34 +0200

    Refactored tests

--- base/src/test/groovy/it/fulminazzo/blocksmith/reflect/ReflectUtilsTest.groovy
+++ base/src/test/groovy/it/fulminazzo/blocksmith/reflect/ReflectUtilsTest.groovy
@@ -3,7 +3,15 @@ package it.fulminazzo.blocksmith.reflect
 
 import spock.lang.Specification
 
+import java.lang.reflect.Type
+
 class ReflectUtilsTest extends Specification {
+    private static final Type listParameterizedType = Mock.getDeclaredMethod('parameterizedType').genericReturnType
+    private static final Type genericArrayType = Mock.getDeclaredMethod('genericArrayType').genericReturnType
+    private static final Type noBoundsWildcardType = Mock.getDeclaredMethod('wildcard').genericReturnType.actualTypeArguments[0]
+    private static final Type upperBoundsWildcardType = Mock.getDeclaredMethod('wildcardExtends').genericReturnType.actualTypeArguments[0]
+    private static final Type lowerBoundsWildcardType = Mock.getDeclaredMethod('wildcardSuper').genericReturnType.actualTypeArguments[0]
+    private static final Type typeVariable = lowerBoundsWildcardType.lowerBounds[0].actualTypeArguments[0]
 
     def 'test that toClass of #type returns #expected'() {
         when:
@@ -13,17 +21,17 @@ class ReflectUtilsTest extends Specification {
         actual == expected
 
         where:
-        type                                                                                                                   || expected
-        Mock                                                                                                                   || Mock
-        Mock.getDeclaredMethod('parameterizedType').genericReturnType                                                          || List
-        Mock.getDeclaredMethod('genericArrayType').genericReturnType                                                           || Set[]
-        Mock.getDeclaredMethod('wildcard').genericReturnType                                                                   || Collection
-        Mock.getDeclaredMethod('wildcard').genericReturnType.actualTypeArguments[0]                                            || Object
-        Mock.getDeclaredMethod('wildcardExtends').genericReturnType                                                            || Collection
-        Mock.getDeclaredMethod('wildcardExtends').genericReturnType.actualTypeArguments[0]                                     || List
-        Mock.getDeclaredMethod('wildcardSuper').genericReturnType                                                              || Collection
-        Mock.getDeclaredMethod('wildcardSuper').genericReturnType.actualTypeArguments[0]                                       || Collection
-        Mock.getDeclaredMethod('wildcardSuper').genericReturnType.actualTypeArguments[0].lowerBounds[0].actualTypeArguments[0] || Number
+        type                                                        || expected
+        Mock                                                        || Mock
+        listParameterizedType                                       || List
+        genericArrayType                                            || Set[]
+        Mock.getDeclaredMethod('wildcard').genericReturnType        || Collection
+        noBoundsWildcardType                                        || Object
+        Mock.getDeclaredMethod('wildcardExtends').genericReturnType || Collection
+        upperBoundsWildcardType                                     || List
+        Mock.getDeclaredMethod('wildcardSuper').genericReturnType   || Collection
+        lowerBoundsWildcardType                                     || Collection
+        typeVariable                                                || Number
     }
 
     def 'test that toString of #type returns #expected'() {
@@ -34,13 +42,13 @@ class ReflectUtilsTest extends Specification {
         actual == expected
 
         where:
-        type                                                          || expected
-        Mock                                                          || "${Mock.canonicalName}"
-        Mock.getDeclaredMethod('parameterizedType').genericReturnType || "${List.canonicalName}<${String.canonicalName}>"
-        Mock.getDeclaredMethod('genericArrayType').genericReturnType  || "${Set.canonicalName}<${Boolean.canonicalName}>[]"
-        Mock.getDeclaredMethod('wildcard').genericReturnType          || "${Collection.canonicalName}<?>"
-        Mock.getDeclaredMethod('wildcardExtends').genericReturnType   || "${Collection.canonicalName}<? extends ${List.canonicalName}<${String.canonicalName}>>"
-        Mock.getDeclaredMethod('wildcardSuper').genericReturnType     || "${Collection.canonicalName}<? super ${Collection.canonicalName}<T extends ${Number.canonicalName} & ${Comparable.canonicalName}>>"
+        type                                                        || expected
+        Mock                                                        || "${Mock.canonicalName}"
+        listParameterizedType                                       || "${List.canonicalName}<${String.canonicalName}>"
+        genericArrayType                                            || "${Set.canonicalName}<${Boolean.canonicalName}>[]"
+        Mock.getDeclaredMethod('wildcard').genericReturnType        || "${Collection.canonicalName}<?>"
+        Mock.getDeclaredMethod('wildcardExtends').genericReturnType || "${Collection.canonicalName}<? extends ${List.canonicalName}<${String.canonicalName}>>"
+        Mock.getDeclaredMethod('wildcardSuper').genericReturnType   || "${Collection.canonicalName}<? super ${Collection.canonicalName}<T extends ${Number.canonicalName} & ${Comparable.canonicalName}>>"
     }
 
     static class Mock<T extends Number & Comparable> {
