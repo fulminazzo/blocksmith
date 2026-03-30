5e7b90085d73b23221a8302720480f4981111327
fulminazzo <fulminazzo21@gmail.com>
2026-03-30 20:13:27 +0200

    Added value field and formatArguments method to correctly format error messages

--- base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ConstraintInfo.java
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ConstraintInfo.java
@@ -4,11 +4,14 @@ import it.fulminazzo.blocksmith.reflect.Reflect;
 import it.fulminazzo.blocksmith.reflect.ReflectException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents information about a constraint.
@@ -16,6 +19,8 @@ import java.lang.annotation.Annotation;
 @Value
 @AllArgsConstructor(access = AccessLevel.NONE)
 class ConstraintInfo {
+    @Getter(AccessLevel.NONE)
+    @Nullable Object value;
     @Nullable String message;
     @NotNull String defaultMessage;
 
@@ -25,9 +30,17 @@ class ConstraintInfo {
      * @param constraint the annotation constraint
      */
     public ConstraintInfo(final @NotNull Annotation constraint) {
+        Reflect reflect = Reflect.on(constraint);
+        Object value;
+        try {
+            value = reflect.invoke("value").get();
+        } catch (ReflectException e) {
+            value = null;
+        }
+        this.value = value;
         String message;
         try {
-            message = Reflect.on(constraint).invoke("message").get();
+            message = reflect.invoke("message").get();
         } catch (ReflectException e) {
             message = null;
         }
@@ -35,6 +48,19 @@ class ConstraintInfo {
         this.defaultMessage = Reflect.on(constraint.annotationType())
                 .getStatic("DEFAULT_MESSAGE", "Invalid value: %s")
                 .get();
+    }
+
+    /**
+     * Formats the arguments for the error message.
+     *
+     * @param value the invalid value
+     * @return the arguments
+     */
+    public @NotNull Object @NotNull [] formatArguments(final Object value) {
+        List<Object> arguments = new ArrayList<>();
+        arguments.add(value);
+        if (this.value != null) arguments.add(this.value);
+        return arguments.toArray();
     }
 
 }
