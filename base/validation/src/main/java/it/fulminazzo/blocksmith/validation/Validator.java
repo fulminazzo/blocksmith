7978c399b831c863feb8ab1b30aff85ba94bd94b
fulminazzo <fulminazzo21@gmail.com>
2026-03-30 19:28:11 +0200

    Created Validator singleton with getValidator and register methods

---
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/Validator.java
@@ -0,0 +1,71 @@
+package it.fulminazzo.blocksmith.validation;
+
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+/**
+ * The registry of validators used to validate objects.
+ */
+@SuppressWarnings("unchecked")
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public final class Validator {
+    private static final Validator INSTANCE = new Validator();
+
+    private final @NotNull Map<Class<? extends Annotation>, Function<? extends Annotation, ConstraintValidator>> validators = new ConcurrentHashMap<>();
+
+    /**
+     * Gets the validator associated with the given annotation.
+     *
+     * @param <A>        the annotation type
+     * @param annotation the annotation
+     * @return the validator (if found)
+     */
+    public <A extends Annotation> @Nullable ConstraintValidator getValidator(final @NotNull A annotation) {
+        Function<A, ConstraintValidator> validatorSupplier = (Function<A, ConstraintValidator>) validators.get(annotation.annotationType());
+        if (validatorSupplier == null) return null;
+        else return validatorSupplier.apply(annotation);
+    }
+
+    /**
+     * Registers a new validator for the provided annotation type.
+     *
+     * @param <A>             the annotation type
+     * @param annotationClass the annotation class
+     * @param validator       the validator
+     * @return this object (for method chaining)
+     */
+    public <A extends Annotation> @NotNull Validator register(final @NotNull Class<A> annotationClass,
+                                                              final @NotNull ConstraintValidator validator) {
+        return register(annotationClass, (Function<A, ConstraintValidator>) a -> validator);
+    }
+
+    /**
+     * Registers a new validator for the provided annotation type.
+     *
+     * @param <A>               the annotation type
+     * @param annotationClass   the annotation class
+     * @param validatorSupplier the function to create a new validator instance from the annotation
+     * @return this object (for method chaining)
+     */
+    public <A extends Annotation> @NotNull Validator register(final @NotNull Class<A> annotationClass,
+                                                              final @NotNull Function<A, ConstraintValidator> validatorSupplier) {
+        validators.put(annotationClass, validatorSupplier);
+        return this;
+    }
+
+    /**
+     * Gets the validator global instance.
+     *
+     * @return the instance
+     */
+    public static @NotNull Validator getInstance() {
+        return INSTANCE;
+    }
+
+}
