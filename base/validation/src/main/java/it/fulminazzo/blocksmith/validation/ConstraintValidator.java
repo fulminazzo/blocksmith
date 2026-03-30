577e54f0c4018d7506ae3bf2237f523d77c64115
fulminazzo <fulminazzo21@gmail.com>
2026-03-30 19:09:17 +0200

    Created ConstraintValidator functional interface

--- base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ConstraintValidator.java
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ConstraintValidator.java
@@ -1,10 +1,5 @@
 package it.fulminazzo.blocksmith.validation;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Optional;
-
 /**
  * A function to validate objects.
  */
@@ -14,15 +9,9 @@ public interface ConstraintValidator {
     /**
      * Validates the given object.
      *
-     * @param value               the value
-     * @param errorMessage        the error message to include in the ConstraintViolation
-     * @param defaultErrorMessage the default error message to fall back to in the ConstraintViolation
-     * @param data                additional data used by the validator to validate the value
-     * @return the constraint violation if the value is invalid, empty otherwise
+     * @param value the value
+     * @return <code>true</code> if the value is valid, <code>false</code> otherwise
      */
-    @NotNull Optional<ConstraintViolation> validate(final Object value,
-                                                    final @Nullable String errorMessage,
-                                                    final @NotNull String defaultErrorMessage,
-                                                    final @Nullable Object @NotNull ... data);
+    boolean isValid(final Object value);
 
 }
