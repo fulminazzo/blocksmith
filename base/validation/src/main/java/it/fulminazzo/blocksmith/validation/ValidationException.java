bca82be661e9efd67f0589300812b5689a05f75c
fulminazzo <fulminazzo21@gmail.com>
2026-03-30 19:33:08 +0200

    Created ValidationException

---
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ValidationException.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.blocksmith.validation;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+/**
+ * An exception thrown when validation fails.
+ */
+public final class ValidationException extends Exception {
+    @Getter
+    private final @NotNull Set<ConstraintViolation> violations;
+
+    /**
+     * Instantiates a new Validation exception.
+     *
+     * @param object     the object that caused the exception
+     * @param violations the violations
+     */
+    ValidationException(final Object object, final @NotNull Set<ConstraintViolation> violations) {
+        super(String.format("Validation failed for object %s", object));
+        this.violations = violations;
+    }
+
+}
