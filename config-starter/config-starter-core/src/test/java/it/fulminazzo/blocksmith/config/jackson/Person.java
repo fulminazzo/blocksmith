08877345c12ec8f1be0af3a8becdd03e8393042e
fulminazzo <fulminazzo21@gmail.com>
2026-03-31 00:54:39 +0200

    Added more validation annotations and updated messages

--- config-starter/config-starter-core/src/test/java/it/fulminazzo/blocksmith/config/jackson/Person.java
+++ config-starter/config-starter-core/src/test/java/it/fulminazzo/blocksmith/config/jackson/Person.java
@@ -2,6 +2,7 @@ package it.fulminazzo.blocksmith.config.jackson;
 
 import it.fulminazzo.blocksmith.validation.annotation.Max;
 import it.fulminazzo.blocksmith.validation.annotation.Min;
+import it.fulminazzo.blocksmith.validation.annotation.NonNull;
 import it.fulminazzo.blocksmith.validation.annotation.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,10 +16,12 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Person {
 
-    @NotBlank(exceptionMessage = "name cannot be null or empty")
+    @NotBlank(exceptionMessage = "name cannot be empty")
+    @NonNull(exceptionMessage = "name cannot be null")
     String name = "Alex";
 
-    @NotBlank(exceptionMessage = "lastname cannot be null or empty")
+    @NotBlank(exceptionMessage = "lastname cannot be empty")
+    @NonNull(exceptionMessage = "lastname cannot be null")
     String lastname = "Fulminazzo";
 
     @Min(value = 18, exceptionMessage = "minimum age must be 18 years")
