4e25d658b325910814c8f343ea7c48976f01f1f8
fulminazzo <fulminazzo21@gmail.com>
2026-04-01 01:05:43 +0200

    Created ComposeValidationException

---
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ComposeValidationException.java
@@ -0,0 +1,28 @@
+package it.fulminazzo.blocksmith.validation;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Special kind of validation exception that aggregates multiple violations for one object fields.
+ */
+public final class ComposeValidationException extends Exception {
+    @Getter
+    private final @NotNull Map<Field, Set<ConstraintViolation>> violations;
+
+    /**
+     * Instantiates a new Compose validation exception.
+     *
+     * @param object     the object that caused the exception
+     * @param violations the violations
+     */
+    public ComposeValidationException(final Object object, final @NotNull Map<Field, Set<ConstraintViolation>> violations) {
+        super(String.format("Validation failed for object %s: %s constraint(s) violated", object, violations.size()));
+        this.violations = violations;
+    }
+
+}
