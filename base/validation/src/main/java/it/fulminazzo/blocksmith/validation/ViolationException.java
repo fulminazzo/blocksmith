4a8b05aa2bc7106281cbe113c4338e01fb6a73dd
fulminazzo <fulminazzo21@gmail.com>
2026-04-01 01:10:42 +0200

    Updated ViolationException with more explicit and clear messages

--- base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ViolationException.java
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/ViolationException.java
@@ -1,8 +1,8 @@
 package it.fulminazzo.blocksmith.validation;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -13,13 +13,32 @@ public final class ViolationException extends RuntimeException {
     /**
      * Instantiates a new Violation exception.
      *
-     * @param violations the violations that triggered the exception
+     * @param object the invalid object
+     * @param cause  the cause exception
      */
-    ViolationException(final @NotNull Set<ConstraintViolation> violations) {
-        super(violations.stream()
-                .map(ConstraintViolation::getExceptionMessage)
-                .collect(Collectors.joining(", "))
-        );
+    ViolationException(final @Nullable Object object, final @NotNull ComposeValidationException cause) {
+        super(String.format("Not valid object '%s': %s",
+                object, cause.getViolations().entrySet().stream()
+                        .map(entry -> String.format("invalid field '%s': %s",
+                                entry.getKey().getName(), entry.getValue().stream()
+                                        .map(ConstraintViolation::getExceptionMessage)
+                                        .collect(Collectors.joining(", "))))
+                        .collect(Collectors.joining("; "))
+        ));
+    }
+
+    /**
+     * Instantiates a new Violation exception.
+     *
+     * @param object the invalid object
+     * @param cause  the cause exception
+     */
+    ViolationException(final @Nullable Object object, final @NotNull ValidationException cause) {
+        super(String.format("Not valid object '%s': %s",
+                object, cause.getViolations().stream()
+                        .map(ConstraintViolation::getExceptionMessage)
+                        .collect(Collectors.joining(", "))
+        ));
     }
 
 }
