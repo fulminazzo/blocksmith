ee5eb1b2beefb3d816f64926a10fba3baa57c632
fulminazzo <fulminazzo21@gmail.com>
2026-03-31 19:07:56 +0200

    Updated documentation

--- base/validation/src/main/java/it/fulminazzo/blocksmith/validation/Constraint.java
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/Constraint.java
@@ -11,23 +11,8 @@ import java.lang.annotation.*;
  * methods to construct a {@link ConstraintViolation}. If those are not given, default values
  * will be used (<code>null</code> for message).
  * <br>
- * Any other non-static method of the annotation will be used as follows:
- * <ul>
- *     <li>if only one method is found (for example <code>value()</code>),
- *     it will be considered the expected value of the constraint,
- *     so it will be put in the arguments map as <code>expected</code>
- *     and will replace the <code>%expected%</code> placeholder in the message;</li>
- *     <li>if only two methods are found (for example <code>min()</code> and <code>max()</code>,
- *     they will be considered as two bound values of the constraint.
- *     As such, they will be put in the arguments map as <code>max</code>
- *     and <code>min</code> and will replace the <code>%max%</code> and <code>%min%</code>
- *     placeholders in the message (the methods will be alphabetically sorted, meaning
- *     the first will be "max" and the second will be "min");</li>
- *     <li>if more than two methods are found, they will be put in the arguments map
- *     (sorted alphabetically) as <code>argument%i%</code> where <code>%i%</code> is
- *     the index of the methods list (starting from 0). Placeholders in the message
- *     will follow the same format (<code>%arg%i%%</code>).</li>
- * </ul>
+ * Any other non-static method of the annotation will be used to define the values
+ * of the constraint which will be available through {@link ConstraintViolation#getArguments()}.
  * Example:
  * <pre>
  * {@code
