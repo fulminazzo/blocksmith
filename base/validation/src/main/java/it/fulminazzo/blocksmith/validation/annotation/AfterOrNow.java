8b66a7d8fd2259ab2764caab15e07bb9ea4db43e
fulminazzo <fulminazzo21@gmail.com>
2026-04-02 12:42:48 +0200

    Created several time related annotations

---
+++ base/validation/src/main/java/it/fulminazzo/blocksmith/validation/annotation/AfterOrNow.java
@@ -0,0 +1,44 @@
+package it.fulminazzo.blocksmith.validation.annotation;
+
+import it.fulminazzo.blocksmith.validation.Constraint;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.*;
+
+/**
+ * Identifies a {@link java.time.temporal.Temporal} parameter or type that
+ * must be after or equal to the current time of execution.
+ * <br>
+ * Supported types are:
+ * <ul>
+ *     <li>{@link java.util.Date};</li>
+ *     <li>{@link java.util.Calendar};</li>
+ *     <li>any {@link java.time.temporal.TemporalAccessor} implementation, like
+ *     {@link java.time.Instant}, {@link java.time.LocalTime} or {@link java.time.LocalDate}.</li>
+ * </ul>
+ * <br>
+ * Accepts <code>null</code> values.
+ */
+@Constraint
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER})
+public @interface AfterOrNow {
+
+    /**
+     * Gets the error message in case of violation.
+     * <br>
+     * By default, the message is a code that will later be translated by an appropriate translator.
+     *
+     * @return the message
+     */
+    @NotNull String message() default "error.validation.required-future-or-now-date";
+
+    /**
+     * Gets the error message that will be shown in the {@link it.fulminazzo.blocksmith.validation.ValidationException} message.
+     *
+     * @return the message
+     */
+    @NotNull String exceptionMessage() default "must be later than or equal to now";
+
+}
