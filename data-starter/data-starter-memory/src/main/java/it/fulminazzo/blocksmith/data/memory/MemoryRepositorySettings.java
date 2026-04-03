cf5edcdbbff17c2c31ad1dcdff82225f40723323
fulminazzo <fulminazzo21@gmail.com>
2026-04-03 02:39:11 +0200

    Added expiry strategy enum and field

--- data-starter/data-starter-memory/src/main/java/it/fulminazzo/blocksmith/data/memory/MemoryRepositorySettings.java
+++ data-starter/data-starter-memory/src/main/java/it/fulminazzo/blocksmith/data/memory/MemoryRepositorySettings.java
@@ -2,10 +2,34 @@ package it.fulminazzo.blocksmith.data.memory;
 
 import it.fulminazzo.blocksmith.data.CacheRepositorySettings;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
 
 @EqualsAndHashCode(callSuper = true, doNotUseGetters = true)
 @ToString(callSuper = true, doNotUseGetters = true)
 public final class MemoryRepositorySettings extends CacheRepositorySettings<MemoryRepositorySettings> {
+
+    @Getter
+    private @Nullable ExpiryStrategy strategy;
+
+    /**
+     * Identifies the strategy to use to clear out expired entities.
+     * <br>
+     * Only valid if {@link #withTtl(Duration)} has been used.
+     */
+    public enum ExpiryStrategy {
+        /**
+         * Entities will be kept in memory until the next operation.
+         */
+        LAZY,
+        /**
+         * Entities will be removed periodically.
+         */
+        SCHEDULED
+
+    }
 
 }
