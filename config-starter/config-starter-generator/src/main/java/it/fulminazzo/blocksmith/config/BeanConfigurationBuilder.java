4f9e655b13495fc1982c9c1cd85c13f552c93b77
fulminazzo <fulminazzo21@gmail.com>
2026-04-04 19:59:32 +0200

    Added addImport methods with getTypeFromObject utility function

--- config-starter/config-starter-generator/src/main/java/it/fulminazzo/blocksmith/config/BeanConfigurationBuilder.java
+++ config-starter/config-starter-generator/src/main/java/it/fulminazzo/blocksmith/config/BeanConfigurationBuilder.java
@@ -24,4 +24,52 @@ public class BeanConfigurationBuilder {
     @NotNull Map<String, FieldDeclaration> fields = new HashMap<>();
     @NotNull Map<String, MethodDeclaration> methods = new HashMap<>();
 
+    /**
+     * Adds an import to the imports list.
+     * If the import belongs to "java.lang",
+     * then nothing is done (as already imported by default).
+     *
+     * @param value the value to get the type from (if <code>null</code>, nothing is done)
+     */
+    void addImport(final @Nullable Object value) {
+        addImport(getTypeFromObject(value));
+    }
+
+    /**
+     * Adds an import to the imports list.
+     * If the import belongs to "java.lang",
+     * then nothing is done (as already imported by default).
+     *
+     * @param type the type to add
+     */
+    void addImport(final @NotNull Class<?> type) {
+        addImport(type.getCanonicalName());
+    }
+
+    /**
+     * Adds an import to the imports list.
+     * If the import belongs to "java.lang",
+     * then nothing is done (as already imported by default).
+     *
+     * @param classCanonicalName the canonical name of the class to add
+     */
+    void addImport(final @NotNull String classCanonicalName) {
+        if (!classCanonicalName.startsWith("java.lang"))
+            imports.computeIfAbsent(
+                    classCanonicalName,
+                    c -> new ImportDeclaration(c, false, false)
+            );
+    }
+
+    /**
+     * Converts the given value to a class.
+     * If the value is <code>null</code>, {@link Object} is returned.
+     *
+     * @param value the value
+     * @return the class
+     */
+    static @NotNull Class<?> getTypeFromObject(final @Nullable Object value) {
+        return value == null ? Object.class : value.getClass();
+    }
+
 }
