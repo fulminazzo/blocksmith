0489bf7336a28fd4a05f996d7bc43797d3c6f072
fulminazzo <fulminazzo21@gmail.com>
2026-04-04 17:17:54 +0200

    Implemented loadComments methods for Properties implementation

--- config-starter/config-starter-properties/src/main/java/it/fulminazzo/blocksmith/config/PropertiesConfigurationAdapter.java
+++ config-starter/config-starter-properties/src/main/java/it/fulminazzo/blocksmith/config/PropertiesConfigurationAdapter.java
@@ -5,17 +5,20 @@ import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
 import it.fulminazzo.blocksmith.config.jackson.CommentPropertyWriter;
 import it.fulminazzo.blocksmith.config.jackson.JacksonConfigurationAdapter;
-import lombok.experimental.Delegate;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 /**
  * Implementation of {@link BaseConfigurationAdapter} for Properties.
  */
 final class PropertiesConfigurationAdapter implements BaseConfigurationAdapter {
-    @Delegate
+    private static final @NotNull List<String> commentIdentifiers = Arrays.asList("#", "!");
+
     private final @NotNull BaseConfigurationAdapter delegate;
 
     /**
@@ -31,6 +34,98 @@ final class PropertiesConfigurationAdapter implements BaseConfigurationAdapter {
         );
     }
 
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull String data) throws IOException {
+        return loadComments(new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull File file) throws IOException {
+        return loadComments(new FileInputStream(file));
+    }
+
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull InputStream stream) throws IOException {
+        return toCommentedMap(stream);
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull String data, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(data, type);
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull File file, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(file, type);
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull InputStream stream, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(stream, type);
+    }
+
+    @Override
+    public @NotNull <T> T loadFromResource(final @NotNull String resource, final @NotNull Class<T> type) throws IOException {
+        return delegate.loadFromResource(resource, type);
+    }
+
+    @Override
+    public @NotNull <T> String serialize(final @NotNull T configuration) throws IOException {
+        return delegate.serialize(configuration);
+    }
+
+    @Override
+    public <T> void store(final @NotNull File file, final @NotNull T configuration) throws IOException {
+        delegate.store(file, configuration);
+    }
+
+    @Override
+    public <T> void store(final @NotNull OutputStream stream, final @NotNull T configuration) throws IOException {
+        delegate.store(stream, configuration);
+    }
+
+    @Override
+    public @NotNull <T> T extractAndLoad(final @NotNull String resource,
+                                         final @NotNull File directory,
+                                         final @NotNull Class<T> type) throws IOException {
+        return delegate.extractAndLoad(resource, directory, type);
+    }
+
+    private static @NotNull Map<String, List<String>> toCommentedMap(final @NotNull InputStream inputStream) throws IOException {
+        final Map<String, List<String>> keysComments = new HashMap<>();
+        List<String> currentComment = new ArrayList<>();
+        try (InputStreamReader streamReader = new InputStreamReader(inputStream);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String identifier = getCommentIdentifier(line);
+                if (identifier != null) currentComment.add(line.substring(identifier.length()).trim());
+                else if (!currentComment.isEmpty()) {
+                    String key = line.split("=")[0].trim();
+                    int index = key.lastIndexOf('.');
+                    if (index != -1) {
+                        String keyName = key.substring(index + 1);
+                        if (keyName.chars().allMatch(Character::isDigit)) {
+                            // ignoring lists as comments are not supported
+                            currentComment.clear();
+                            continue;
+                        }
+                    }
+                    keysComments.put(key, currentComment);
+                    currentComment = new ArrayList<>();
+                }
+            }
+        }
+        return keysComments;
+    }
+
+    private static @Nullable String getCommentIdentifier(final @NotNull String line) {
+        for (String identifier : commentIdentifiers)
+            if (line.startsWith(identifier))
+                return identifier;
+        return null;
+    }
+
     /**
      * An implementation of {@link CommentPropertyWriter} for handling Properties comments.
      */
@@ -43,7 +138,7 @@ final class PropertiesConfigurationAdapter implements BaseConfigurationAdapter {
          * @param comment the comment
          */
         public PropertiesCommentPropertyWriter(final @NotNull BeanPropertyWriter base,
-                                         final @NotNull Comment comment) {
+                                               final @NotNull Comment comment) {
             super(base, comment);
         }
 
