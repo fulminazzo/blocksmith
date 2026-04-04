cc1e3e35489b7e5cb31f202816a8df671edc3361
fulminazzo <fulminazzo21@gmail.com>
2026-04-04 18:30:56 +0200

    Made mergeDataMaps attempt to guess the type of the loaded data

--- config-starter/src/main/java/it/fulminazzo/blocksmith/config/ConfigUtils.java
+++ config-starter/src/main/java/it/fulminazzo/blocksmith/config/ConfigUtils.java
@@ -9,12 +9,30 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Helper functions for this package.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 final class ConfigUtils {
+    private static final @NotNull List<Function<String, Object>> primitiveConverters = List.of(
+            Byte::valueOf,
+            Short::valueOf,
+            Integer::valueOf,
+            Long::valueOf,
+            Float::valueOf,
+            Double::valueOf,
+            s -> {
+                if (s.length() == 1) return s.charAt(0);
+                else throw new IllegalArgumentException(s + " is not a valid character");
+            },
+            s -> {
+                if (s.equals(Boolean.TRUE.toString())) return true;
+                else if (s.equals(Boolean.FALSE.toString())) return false;
+                else throw new IllegalArgumentException(s + " is not a valid boolean");
+            }
+    );
 
     /**
      * Joins the given configuration and comments into a single map.
@@ -31,7 +49,7 @@ final class ConfigUtils {
         final Map<CommentKey, Object> result = new HashMap<>();
         for (final Map.Entry<String, Object> entry : configuration.entrySet()) {
             final String key = entry.getKey();
-            Object value = entry.getValue();
+            Object value = convertValue(entry.getValue());
             final Object comment = comments.get(key);
 
             if (value instanceof Map<?, ?>) {
@@ -46,6 +64,17 @@ final class ConfigUtils {
             else result.put(new CommentKey(key), value);
         }
         return result;
+    }
+
+    private static @Nullable Object convertValue(final @Nullable Object value) {
+        if (value == null) return null;
+        for (Function<String, Object> converter : primitiveConverters) {
+            try {
+                return converter.apply(value.toString());
+            } catch (Exception ignored) {
+            }
+        }
+        return value;
     }
 
 }
