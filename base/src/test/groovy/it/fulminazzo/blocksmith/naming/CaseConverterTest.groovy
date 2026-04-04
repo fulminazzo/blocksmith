57b3fac079bd913eb315e991a248892977a984b4
fulminazzo <fulminazzo21@gmail.com>
2026-04-04 15:06:32 +0200

    Added tests for case conversion

---
+++ base/src/test/groovy/it/fulminazzo/blocksmith/naming/CaseConverterTest.groovy
@@ -0,0 +1,40 @@
+package it.fulminazzo.blocksmith.naming
+
+import spock.lang.Specification
+
+import static it.fulminazzo.blocksmith.naming.Convention.*
+
+class CaseConverterTest extends Specification {
+
+    def 'test that convert #input from #from to #to returns #expected'() {
+        when:
+        def actual = CaseConverter.convert(input, from, to)
+
+        then:
+        actual == expected
+
+        where:
+        input         | from        | to          || expected
+        // camelCase
+        'helloWorld'  | CAMEL_CASE  | CAMEL_CASE  || 'helloWorld'
+        'hello-world' | KEBAB_CASE  | CAMEL_CASE  || 'helloWorld'
+        'hello_world' | SNAKE_CASE  | CAMEL_CASE  || 'helloWorld'
+        'HelloWorld'  | PASCAL_CASE | CAMEL_CASE  || 'helloWorld'
+        // kebab-case
+        'helloWorld'  | CAMEL_CASE  | KEBAB_CASE  || 'hello-world'
+        'hello-world' | KEBAB_CASE  | KEBAB_CASE  || 'hello-world'
+        'hello_world' | SNAKE_CASE  | KEBAB_CASE  || 'hello-world'
+        'HelloWorld'  | PASCAL_CASE | KEBAB_CASE  || 'hello-world'
+        // snake_case
+        'helloWorld'  | CAMEL_CASE  | SNAKE_CASE  || 'hello_world'
+        'hello-world' | KEBAB_CASE  | SNAKE_CASE  || 'hello_world'
+        'hello_world' | SNAKE_CASE  | SNAKE_CASE  || 'hello_world'
+        'HelloWorld'  | PASCAL_CASE | SNAKE_CASE  || 'hello_world'
+        // PascalCase
+        'helloWorld'  | CAMEL_CASE  | PASCAL_CASE || 'HelloWorld'
+        'hello-world' | KEBAB_CASE  | PASCAL_CASE || 'HelloWorld'
+        'hello_world' | SNAKE_CASE  | PASCAL_CASE || 'HelloWorld'
+        'HelloWorld'  | PASCAL_CASE | PASCAL_CASE || 'HelloWorld'
+    }
+
+}
