d49b347fb7472f861950a9f7dba47374e9a0c876
fulminazzo <fulminazzo21@gmail.com>
2026-04-04 16:34:28 +0200

    Implemented loadComments methods for YAML implementation

--- config-starter/config-starter-yaml/src/main/java/it/fulminazzo/blocksmith/config/YamlConfigurationAdapter.java
+++ config-starter/config-starter-yaml/src/main/java/it/fulminazzo/blocksmith/config/YamlConfigurationAdapter.java
@@ -13,21 +13,31 @@ import com.fasterxml.jackson.dataformat.yaml.util.StringQuotingChecker;
 import it.fulminazzo.blocksmith.config.jackson.CommentPropertyWriter;
 import it.fulminazzo.blocksmith.config.jackson.JacksonConfigurationAdapter;
 import it.fulminazzo.blocksmith.reflect.Reflect;
-import lombok.experimental.Delegate;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.comments.CommentLine;
 import org.yaml.snakeyaml.comments.CommentType;
+import org.yaml.snakeyaml.composer.Composer;
 import org.yaml.snakeyaml.events.CommentEvent;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+import org.yaml.snakeyaml.parser.ParserImpl;
+import org.yaml.snakeyaml.reader.StreamReader;
+import org.yaml.snakeyaml.resolver.Resolver;
 
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of {@link BaseConfigurationAdapter} for YAML.
  */
 final class YamlConfigurationAdapter implements BaseConfigurationAdapter {
-    @Delegate
     private final @NotNull BaseConfigurationAdapter delegate;
 
     /**
@@ -48,6 +58,96 @@ final class YamlConfigurationAdapter implements BaseConfigurationAdapter {
                 logger,
                 YamlCommentPropertyWriter.class
         );
+    }
+
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull String data) {
+        return loadComments(new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull File file) throws IOException {
+        return loadComments(new FileInputStream(file));
+    }
+
+    @Override
+    public @NotNull Map<@NotNull String, @NotNull List<@NotNull String>> loadComments(final @NotNull InputStream stream) {
+        final LoaderOptions options = new LoaderOptions().setProcessComments(true);
+        StreamReader reader = new StreamReader(new InputStreamReader(stream));
+        Composer composer = new Composer(new ParserImpl(reader, options), new Resolver(), options);
+        Node root = composer.getSingleNode();
+        if (root instanceof MappingNode) return extractComments((MappingNode) root);
+        else return Collections.emptyMap();
+    }
+
+    private static @NotNull Map<String, List<String>> extractComments(final @NotNull MappingNode node) {
+        final Map<String, List<String>> nodesComments = new HashMap<>();
+        for (NodeTuple nodeTuple : node.getValue()) {
+            ScalarNode keyNode = (ScalarNode) nodeTuple.getKeyNode();
+            String key = keyNode.getValue();
+            @NotNull List<String> comments = extractComments(keyNode);
+            if (!comments.isEmpty()) nodesComments.put(key, comments);
+            Node value = nodeTuple.getValueNode();
+            if (value instanceof MappingNode)
+                extractComments((MappingNode) value).forEach((k, c) ->
+                        nodesComments.put(key + "." + k, c)
+                );
+        }
+        return nodesComments;
+    }
+
+    private static @NotNull List<String> extractComments(final @NotNull Node node) {
+        List<CommentLine> comments = new ArrayList<>();
+        List<CommentLine> inlineComments = node.getInLineComments();
+        if (inlineComments != null) comments.addAll(inlineComments);
+        List<CommentLine> blockComments = node.getBlockComments();
+        if (blockComments != null) comments.addAll(blockComments);
+        return comments.stream()
+                .map(CommentLine::getValue)
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull String data, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(data, type);
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull File file, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(file, type);
+    }
+
+    @Override
+    public @NotNull <T> T load(final @NotNull InputStream stream, final @NotNull Class<T> type) throws IOException {
+        return delegate.load(stream, type);
+    }
+
+    @Override
+    public @NotNull <T> T loadFromResource(final @NotNull String resource, final @NotNull Class<T> type) throws IOException {
+        return delegate.loadFromResource(resource, type);
+    }
+
+    @Override
+    public @NotNull <T> String serialize(final @NotNull T configuration) throws IOException {
+        return delegate.serialize(configuration);
+    }
+
+    @Override
+    public <T> void store(final @NotNull File file, final @NotNull T configuration) throws IOException {
+        delegate.store(file, configuration);
+    }
+
+    @Override
+    public <T> void store(final @NotNull OutputStream stream, final @NotNull T configuration) throws IOException {
+        delegate.store(stream, configuration);
+    }
+
+    @Override
+    public @NotNull <T> T extractAndLoad(final @NotNull String resource,
+                                         final @NotNull File directory,
+                                         final @NotNull Class<T> type) throws IOException {
+        return delegate.extractAndLoad(resource, directory, type);
     }
 
     /**
