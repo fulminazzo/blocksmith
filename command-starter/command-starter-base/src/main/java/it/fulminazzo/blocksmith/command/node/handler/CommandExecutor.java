ec5eb8f22b7a53a75a641747e4471a4ffdb4b5bd
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 22:20:56 +0200

    Populated CommandExecutor#execute method with actual logic from ExecutionInfo

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/handler/CommandExecutor.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/handler/CommandExecutor.java
@@ -1,12 +1,20 @@
 package it.fulminazzo.blocksmith.command.node.handler;
 
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper;
+import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionException;
+import it.fulminazzo.blocksmith.command.visitor.execution.ExecutionContext;
+import it.fulminazzo.blocksmith.message.argument.Placeholder;
 import it.fulminazzo.blocksmith.reflect.Reflect;
+import it.fulminazzo.blocksmith.reflect.ReflectException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.LinkedList;
 
 /**
  * The actual executor of a command.
@@ -28,8 +36,42 @@ public class CommandExecutor {
         this.method = method;
     }
 
-    public void execute() {
-        //TODO: implement
+    /**
+     * Executes the actual command logic.
+     *
+     * @param context the context of execution
+     * @throws CommandExecutionException in case of any errors
+     */
+    public void execute(final @NotNull ExecutionContext context) throws CommandExecutionException {
+        final CommandSenderWrapper<?> sender = context.getCommandSender();
+        try {
+            LinkedList<Object> arguments = context.getArguments();
+            if (arguments.size() != method.getParameterCount()) {
+                Class<?> parameterType = method.getParameterTypes()[0];
+                if (CommandSenderWrapper.class.isAssignableFrom(parameterType)) {
+                    ParameterizedType paramType = (ParameterizedType) method.getGenericParameterTypes()[0];
+                    Type actualSenderType = paramType.getActualTypeArguments()[0];
+                    if (actualSenderType instanceof Class<?> && !sender.extendsType((Class<?>) actualSenderType))
+                        throw new CommandExecutionException(sender.isPlayer()
+                                ? "error.player-cannot-execute"
+                                : "error.console-cannot-execute"
+                        );
+                    arguments.addFirst(sender);
+                } else if (sender.extendsType(parameterType)) arguments.addFirst(sender.getActualSender());
+                else throw new CommandExecutionException(sender.isPlayer()
+                            ? "error.player-cannot-execute"
+                            : "error.console-cannot-execute"
+                    );
+            }
+            Object[] parameterValues = arguments.toArray();
+            //TODO: parameters validation
+            executor.invoke(method, parameterValues);
+        } catch (ReflectException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof CommandExecutionException) throw (CommandExecutionException) cause;
+            throw new CommandExecutionException("error.internal-error", cause)
+                    .arguments(Placeholder.of("message", cause.getMessage()));
+        }
     }
 
 }
