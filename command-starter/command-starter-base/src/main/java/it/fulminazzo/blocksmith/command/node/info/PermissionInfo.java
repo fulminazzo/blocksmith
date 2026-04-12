5e429aa07ca36baf6c249c2cc17ca337a2f4c0e7
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 19:33:58 +0200

    Rewritten PermissionInfo with DataInfo implementation

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/info/PermissionInfo.java
@@ -0,0 +1,75 @@
+package it.fulminazzo.blocksmith.command.node.info;
+
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Carries information about the permission required to execute a command.
+ */
+@Getter
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public final class PermissionInfo extends DataInfo<PermissionInfo> {
+    @Nullable String prefix;
+    @NotNull String actualPermission;
+    @NotNull Permission.Grant grant;
+
+    /**
+     * Instantiates a new Permission info.
+     *
+     * @param prefix     the prefix to prepend to the permission
+     * @param permission the permission
+     * @param grant      who the permission is granted to
+     */
+    public PermissionInfo(final @Nullable String prefix,
+                          final @NotNull String permission,
+                          final @NotNull Permission.Grant grant) {
+        this(prefix, permission, grant, false);
+    }
+
+    /**
+     * Instantiates a new Permission info.
+     *
+     * @param prefix       the prefix to prepend to the permission
+     * @param permission   the permission
+     * @param grant        who the permission is granted to
+     * @param autoComputed if <code>true</code>, will mark the permission as automatically computed,
+     *                     meaning it was not provided by the user, but was instead retrieved from the command route
+     * @deprecated FOR INTERNAL USE ONLY
+     */
+    @Deprecated
+    public PermissionInfo(final @Nullable String prefix,
+                          final @NotNull String permission,
+                          final @NotNull Permission.Grant grant,
+                          final boolean autoComputed) {
+        super(autoComputed);
+        this.prefix = prefix;
+        this.actualPermission = permission;
+        this.grant = grant;
+    }
+
+    @Override
+    public void merge(final @NotNull PermissionInfo permissionInfo) {
+        if (actualPermission.isEmpty() || isAutoComputed()) {
+            prefix = permissionInfo.getPrefix();
+            actualPermission = permissionInfo.getActualPermission();
+            setAutoComputed(permissionInfo.isAutoComputed());
+        }
+        if (grant == Permission.Grant.OP)
+            grant = permissionInfo.getGrant();
+    }
+
+    /**
+     * Gets the permission (prefixed if {@link #prefix} has been provided).
+     *
+     * @return the permission
+     */
+    public @NotNull String getPermission() {
+        return (prefix == null ? "" : prefix + ".") + actualPermission;
+    }
+
+}
---
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/node/info/PermissionInfoTest.groovy
@@ -0,0 +1,90 @@
+//file:noinspection GrDeprecatedAPIUsage
+package it.fulminazzo.blocksmith.command.node.info
+
+import it.fulminazzo.blocksmith.command.annotation.Permission
+import spock.lang.Specification
+
+class PermissionInfoTest extends Specification {
+
+    def 'test that merge overwrites empty permission'() {
+        given:
+        def first = new PermissionInfo(null, '', Permission.Grant.OP, autoComputed)
+
+        and:
+        def second = new PermissionInfo('blocksmith', 'command.permission', Permission.Grant.ALL, true)
+
+        when:
+        first.merge(second)
+
+        then:
+        first.prefix == second.prefix
+        first.actualPermission == second.actualPermission
+        first.autoComputed == second.autoComputed
+        first.grant == second.grant
+
+        where:
+        autoComputed << [true, false]
+    }
+
+    def 'test that merge overwrites auto computed'() {
+        given:
+        def first = new PermissionInfo('blk', 'permission', Permission.Grant.ALL, true)
+
+        and:
+        def second = new PermissionInfo('blocksmith', 'command.permission', Permission.Grant.OP, false)
+
+        when:
+        first.merge(second)
+
+        then:
+        first.prefix == second.prefix
+        first.actualPermission == second.actualPermission
+        first.autoComputed == second.autoComputed
+        first.grant == Permission.Grant.ALL
+    }
+
+    def 'test that merge does not overwrite non-empty, non-autocomputed permission'() {
+        given:
+        def first = new PermissionInfo(null, 'permission', Permission.Grant.OP)
+
+        and:
+        def second = new PermissionInfo('blocksmith', 'command.permission', Permission.Grant.ALL, true)
+
+        when:
+        first.merge(second)
+
+        then:
+        first.prefix == null
+        first.actualPermission == 'permission'
+        !first.autoComputed
+        first.grant == second.grant
+    }
+
+    def 'test that auto computation does not affect equality'() {
+        given:
+        def first = new PermissionInfo('blocksmith', 'permission', Permission.Grant.ALL)
+
+        and:
+        def second = new PermissionInfo('blocksmith', 'permission', Permission.Grant.ALL, true)
+
+        expect:
+        first == second
+
+        and:
+        first.toString() == second.toString()
+    }
+
+    def 'test that permission of #prefix and #permission is #expected'() {
+        given:
+        def info = new PermissionInfo(prefix, permission, Permission.Grant.ALL)
+
+        expect:
+        info.permission == expected
+
+        where:
+        prefix       | permission           || expected
+        null         | 'command.permission' || 'command.permission'
+        'blocksmith' | 'command.permission' || 'blocksmith.command.permission'
+    }
+
+}
