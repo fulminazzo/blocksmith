071a1943f39d4167568e5b9ea1a21265dcb6d861
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 22:12:23 +0200

    Created VisitorContext empty class

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/visitor/context/VisitorContext.java
@@ -0,0 +1,19 @@
+package it.fulminazzo.blocksmith.command.visitor.context;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Contains the current context that a {@link it.fulminazzo.blocksmith.command.visitor.Visitor} should use.
+ */
+@EqualsAndHashCode
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public abstract class VisitorContext {
+    //TODO: application
+    //TODO: registry
+    //TODO: sender
+
+}
