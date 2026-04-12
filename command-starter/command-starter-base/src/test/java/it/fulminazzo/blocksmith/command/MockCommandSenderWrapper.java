a40520260546edbbcabdeed7b01c36d329cfc59c
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 22:30:25 +0200

    Re-introduced and updated MockCommandSenderWrapper

--- command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandSenderWrapper.java
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandSenderWrapper.java
@@ -1,84 +1,106 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command;
-//
-//import it.fulminazzo.blocksmith.ApplicationHandle;//import it.fulminazzo.blocksmith.command.annotation.Permission;
-//import it.fulminazzo.blocksmith.command.node.PermissionInfo;
-//import it.fulminazzo.blocksmith.message.Messenger;
-//import lombok.*;
-//import lombok.extern.slf4j.Slf4j;
-//import org.jetbrains.annotations.NotNull;
-//import org.jspecify.annotations.NonNull;
-//import org.slf4j.Logger;
-//
-///**
-// * A Command sender wrapper for testing purposes.
-// */
-//@Slf4j
-//@ToString(callSuper = true)
-//@EqualsAndHashCode(callSuper = true)
-//public final class MockCommandSenderWrapper extends CommandSenderWrapper<CommandSender> {
-//
-//    /**
-//     * Instantiates a new Mock command sender wrapper.
-//     *
-//     * @param actualSender the actual sender
-//     */
-//    public MockCommandSenderWrapper(final @NonNull CommandSender actualSender) {
-//        this(new ApplicationHandle() {
-//
-//            @Override
-//            public @NotNull Messenger getMessenger() {
-//                return new Messenger(log);
-//            }
-//
-//            @Override
-//            public @NotNull Logger getLog() {
-//                return log;
-//            }
-//
-//            @Override
-//            public @NotNull Object getServer() {
-//                throw new UnsupportedOperationException();
-//            }
-//
-//            @Override
-//            public @NotNull String getName() {
-//                return "blocksmith";
-//            }
-//
-//        }, actualSender);
-//    }
-//
-//    /**
-//     * Instantiates a new Mock command sender wrapper.
-//     *
-//     * @param application  the application
-//     * @param actualSender the actual sender
-//     */
-//    public MockCommandSenderWrapper(final @NotNull ApplicationHandle application,
-//                                    final @NonNull CommandSender actualSender) {
-//        super(application, actualSender);
-//    }
-//
-//    @Override
-//    public @NotNull String getName() {
-//        return actualSender.getName();
-//    }
-//
-//    @Override
-//    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
-//        if (permissionInfo.getGrant() == Permission.Grant.OP && actualSender.isOp()) return true;
-//        return actualSender.hasPermission(permissionInfo.getPermission());
-//    }
-//
-//    @Override
-//    public boolean isPlayer() {
-//        return actualSender instanceof Player;
-//    }
-//
-//    @Override
-//    public @NotNull Object getId() {
-//        return getName();
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.ApplicationHandle;
+import it.fulminazzo.blocksmith.ServerApplication;
+import it.fulminazzo.blocksmith.command.annotation.Permission;
+import it.fulminazzo.blocksmith.command.node.info.PermissionInfo;
+import it.fulminazzo.blocksmith.message.Messenger;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+/**
+ * A Command sender wrapper for testing purposes.
+ */
+@Slf4j
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public final class MockCommandSenderWrapper extends CommandSenderWrapper<CommandSender> {
+
+    /**
+     * Instantiates a new Mock command sender wrapper.
+     *
+     * @param actualSender the actual sender
+     */
+    public MockCommandSenderWrapper(final @NotNull CommandSender actualSender) {
+        this(new ApplicationHandle() { //TODO: optimize
+
+            @Override
+            public @NotNull Messenger getMessenger() {
+                return new Messenger(new ServerApplication() {
+
+                    @Override
+                    public @NotNull <S> S server() {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public @NotNull <T> T as(final @NotNull Class<T> type) {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public @NotNull Logger logger() {
+                        return log;
+                    }
+
+                    @Override
+                    public @NotNull String getName() {
+                        return "blocksmith";
+                    }
+
+                });
+            }
+
+            @Override
+            public @NotNull Logger getLog() {
+                return log;
+            }
+
+            @Override
+            public @NotNull Object getServer() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public @NotNull String getName() {
+                return "blocksmith";
+            }
+
+        }, actualSender);
+    }
+
+    /**
+     * Instantiates a new Mock command sender wrapper.
+     *
+     * @param application  the application
+     * @param actualSender the actual sender
+     */
+    public MockCommandSenderWrapper(final @NotNull ApplicationHandle application,
+                                    final @NotNull CommandSender actualSender) {
+        super(application, actualSender);
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return actualSender.getName();
+    }
+
+    @Override
+    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
+        if (permissionInfo.getGrant() == Permission.Grant.OP && actualSender.isOp()) return true;
+        return actualSender.hasPermission(permissionInfo.getPermission());
+    }
+
+    @Override
+    public boolean isPlayer() {
+        return actualSender instanceof Player;
+    }
+
+    @Override
+    public @NotNull Object getId() {
+        return getName();
+    }
+
+}
