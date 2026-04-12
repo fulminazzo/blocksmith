7525eac80e9cef14c95bb6c0a64db3f9cc5ca488
fulminazzo <fulminazzo21@gmail.com>
2026-04-13 00:51:58 +0200

    Re-introduced CommandParseException and updated tests

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/parser/CommandParseException.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/parser/CommandParseException.java
@@ -1,47 +1,48 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.parser;
-//
-//import it.fulminazzo.blocksmith.util.ReflectionUtils;
-//import org.jetbrains.annotations.NotNull;
-//
-//import java.lang.reflect.Method;
-//
-///**
-// * An exception thrown when the parsing of a command fails.
-// */
-//public final class CommandParseException extends RuntimeException {
-//
-//    /**
-//     * Instantiates a new Command parse exception.
-//     *
-//     * @param message the message
-//     */
-//    public CommandParseException(final @NotNull String message) {
-//        super(message);
-//    }
-//
-//    /**
-//     * Instantiates a new Command parse exception.
-//     * <br>
-//     * The message will be formatted using the provided arguments.
-//     * <ul>
-//     *     <li>If an argument is a method, it will be formatted to be displayed properly.</li>
-//     *     <li>If an argument is a class, it will be replaced with its simple name.</li>
-//     * </ul>
-//     *
-//     * @param format the format of the message
-//     * @param args   the arguments
-//     * @return the command parse exception
-//     */
-//    public static @NotNull CommandParseException of(final @NotNull String format,
-//                                                    final @NotNull Object... args) {
-//        for (int i = 0; i < args.length; i++) {
-//            Object arg = args[i];
-//            if (arg instanceof Method) args[i] = ReflectionUtils.methodToString((Method) arg);
-//            else if (arg instanceof Class) args[i] = ((Class<?>) arg).getSimpleName();
-//            else if (arg instanceof CommandToken) args[i] = ((CommandToken) arg).getToken();
-//        }
-//        return new CommandParseException(String.format(format, args));
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command.parser;
+
+import it.fulminazzo.blocksmith.reflect.ReflectException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * An exception thrown when the parsing of a command fails.
+ */
+public final class CommandParseException extends RuntimeException {
+
+    /**
+     * Instantiates a new Command parse exception.
+     *
+     * @param message the message
+     */
+    public CommandParseException(final @NotNull String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new Command parse exception.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
+    public CommandParseException(final @NotNull String message, final @NotNull Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Instantiates a new Command parse exception.
+     *
+     * @param format the format of the message
+     * @param args   the arguments
+     * @return the command parse exception
+     */
+    public static @NotNull CommandParseException of(final @NotNull String format,
+                                                    final @Nullable Object @NotNull ... args) {
+        for (int i = 0; i < args.length; i++) {
+            Object arg = args[i];
+            if (arg instanceof Class) args[i] = ((Class<?>) arg).getSimpleName();
+            else if (arg instanceof CommandToken) args[i] = ((CommandToken) arg).getToken();
+        }
+        return new CommandParseException(ReflectException.formatMessage(format, args));
+    }
+
+}
--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/parser/CommandParseExceptionTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/parser/CommandParseExceptionTest.groovy
@@ -1,26 +1,26 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.parser
-//
-//import it.fulminazzo.blocksmith.command.annotation.Command
-//import spock.lang.Specification
-//
-//class CommandParseExceptionTest extends Specification {
-//
-//    def 'test that of correctly formats arguments'() {
-//        given:
-//        def expected = 'Invalid method sum(int, double): missing Command annotation'
-//
-//        when:
-//        def exception = CommandParseException.of('Invalid method %s: missing %s annotation',
-//                CommandParseExceptionTest.getMethod('sum', int, double), Command
-//        )
-//
-//        then:
-//        exception.message == expected
-//    }
-//
-//    static void sum(int a, double b) {
-//
-//    }
-//
-//}
+//file:noinspection unused
+package it.fulminazzo.blocksmith.command.parser
+
+import it.fulminazzo.blocksmith.command.annotation.Command
+import spock.lang.Specification
+
+class CommandParseExceptionTest extends Specification {
+
+    def 'test that of correctly formats arguments'() {
+        given:
+        def expected = 'Invalid method void sum(int, double): missing Command annotation'
+
+        when:
+        def exception = CommandParseException.of('Invalid method %s: missing %s %s',
+                CommandParseExceptionTest.getMethod('sum', int, double), Command, 'annotation'
+        )
+
+        then:
+        exception.message == expected
+    }
+
+    static void sum(int a, double b) {
+
+    }
+
+}
