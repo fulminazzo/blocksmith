29f8cd1a1bbb2aa1b42903d6c92dc38d86304ec4
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 22:28:54 +0200

    Fixed invalid usage of NonNull annotations

--- example/example-bukkit/src/main/java/it/fulminazzo/blocksmith/command/HelloCommand.java
+++ example/example-bukkit/src/main/java/it/fulminazzo/blocksmith/command/HelloCommand.java
@@ -7,7 +7,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,19 +16,19 @@ public final class HelloCommand implements TabExecutor {
     private final @NotNull Blocksmith plugin;
 
     @Override
-    public boolean onCommand(final @NonNull CommandSender sender,
-                             final @NonNull Command command,
-                             final @NonNull String label,
-                             final @NonNull String[] args) {
+    public boolean onCommand(final @NotNull CommandSender sender,
+                             final @NotNull Command command,
+                             final @NotNull String label,
+                             final @NotNull String[] args) {
         plugin.getMessenger().sendMessage(sender, "greeting", Placeholder.of("name", sender.getName()));
         return true;
     }
 
     @Override
-    public @NonNull List<String> onTabComplete(final @NonNull CommandSender sender,
-                                               final @NonNull Command command,
-                                               final @NonNull String alias,
-                                               final @NonNull String[] args) {
+    public @NotNull List<String> onTabComplete(final @NotNull CommandSender sender,
+                                               final @NotNull Command command,
+                                               final @NotNull String alias,
+                                               final @NotNull String[] args) {
         return Collections.emptyList();
     }
 
