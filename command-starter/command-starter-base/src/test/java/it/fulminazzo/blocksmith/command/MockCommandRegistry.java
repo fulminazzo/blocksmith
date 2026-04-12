191cee492f18742a34baa437c14e7d9f64de4197
fulminazzo <fulminazzo21@gmail.com>
2026-04-12 19:17:01 +0200

    Commented several classes related to command-starter pending rework

--- command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandRegistry.java
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandRegistry.java
@@ -1,107 +1,108 @@
-package it.fulminazzo.blocksmith.command;
-
-import it.fulminazzo.blocksmith.ApplicationHandle;
-import it.fulminazzo.blocksmith.command.node.CommandInfo;
-import it.fulminazzo.blocksmith.command.node.LiteralNode;
-import it.fulminazzo.blocksmith.message.Messenger;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-
-import java.lang.reflect.Field;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-@Slf4j
-final class MockCommandRegistry extends CommandRegistry {
-    @Getter
-    private final @NotNull Map<String, CommandInfo> registeredCommands = new ConcurrentHashMap<>();
-
-    public MockCommandRegistry() {
-        this(new Messenger(log), log);
-    }
-
-    public MockCommandRegistry(final @NotNull Messenger messenger, final @NotNull Logger logger) {
-        super(new ApplicationHandle() {
-
-            @Override
-            public @NotNull Messenger getMessenger() {
-                return messenger;
-            }
-
-            @Override
-            public @NotNull Logger getLog() {
-                return logger;
-            }
-
-            @Override
-            public @NotNull Object getServer() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public @NotNull String getName() {
-                return "blocksmith";
-            }
-
-        });
-    }
-
-    @SuppressWarnings("unchecked")
-    public @NotNull Map<String, LiteralNode> getCommands() {
-        try {
-            Class<?> clazz = CommandRegistry.class;
-            Field field = clazz.getDeclaredField("commands");
-            field.setAccessible(true);
-            return (Map<String, LiteralNode>) field.get(this);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void setState(final @NotNull State state) {
-        try {
-            Class<?> clazz = CommandRegistry.class;
-            Field field = clazz.getDeclaredField("state");
-            field.setAccessible(true);
-            field.set(this, state);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public State getState() {
-        try {
-            Class<?> clazz = CommandRegistry.class;
-            Field field = clazz.getDeclaredField("state");
-            field.setAccessible(true);
-            return (State) field.get(this);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    protected @NotNull CommandSenderWrapper<?> wrapSender(final @NotNull Object executor) {
-        if (executor instanceof CommandSenderWrapper) return (CommandSenderWrapper<?>) executor;
-        return new MockCommandSenderWrapper(application, (CommandSender) executor);
-    }
-
-    @Override
-    protected void onRegister(final @NotNull String commandName,
-                              final @NotNull LiteralNode command) {
-        registeredCommands.put(commandName, command.getCommandInfo().orElseThrow());
-    }
-
-    @Override
-    protected void onUnregister(final @NotNull String commandName) {
-        registeredCommands.remove(commandName);
-    }
-
-    @Override
-    protected @NotNull Class<?> getSenderType() {
-        return CommandSender.class;
-    }
-
-}
+//TODO: update
+//package it.fulminazzo.blocksmith.command;
+//
+//import it.fulminazzo.blocksmith.ApplicationHandle;
+//import it.fulminazzo.blocksmith.command.node.CommandInfo;
+//import it.fulminazzo.blocksmith.command.node.LiteralNode;
+//import it.fulminazzo.blocksmith.message.Messenger;
+//import lombok.Getter;
+//import lombok.extern.slf4j.Slf4j;
+//import org.jetbrains.annotations.NotNull;
+//import org.slf4j.Logger;
+//
+//import java.lang.reflect.Field;
+//import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//@Slf4j
+//final class MockCommandRegistry extends CommandRegistry {
+//    @Getter
+//    private final @NotNull Map<String, CommandInfo> registeredCommands = new ConcurrentHashMap<>();
+//
+//    public MockCommandRegistry() {
+//        this(new Messenger(log), log);
+//    }
+//
+//    public MockCommandRegistry(final @NotNull Messenger messenger, final @NotNull Logger logger) {
+//        super(new ApplicationHandle() {
+//
+//            @Override
+//            public @NotNull Messenger getMessenger() {
+//                return messenger;
+//            }
+//
+//            @Override
+//            public @NotNull Logger getLog() {
+//                return logger;
+//            }
+//
+//            @Override
+//            public @NotNull Object getServer() {
+//                throw new UnsupportedOperationException();
+//            }
+//
+//            @Override
+//            public @NotNull String getName() {
+//                return "blocksmith";
+//            }
+//
+//        });
+//    }
+//
+//    @SuppressWarnings("unchecked")
+//    public @NotNull Map<String, LiteralNode> getCommands() {
+//        try {
+//            Class<?> clazz = CommandRegistry.class;
+//            Field field = clazz.getDeclaredField("commands");
+//            field.setAccessible(true);
+//            return (Map<String, LiteralNode>) field.get(this);
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public void setState(final @NotNull State state) {
+//        try {
+//            Class<?> clazz = CommandRegistry.class;
+//            Field field = clazz.getDeclaredField("state");
+//            field.setAccessible(true);
+//            field.set(this, state);
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public State getState() {
+//        try {
+//            Class<?> clazz = CommandRegistry.class;
+//            Field field = clazz.getDeclaredField("state");
+//            field.setAccessible(true);
+//            return (State) field.get(this);
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @Override
+//    protected @NotNull CommandSenderWrapper<?> wrapSender(final @NotNull Object executor) {
+//        if (executor instanceof CommandSenderWrapper) return (CommandSenderWrapper<?>) executor;
+//        return new MockCommandSenderWrapper(application, (CommandSender) executor);
+//    }
+//
+//    @Override
+//    protected void onRegister(final @NotNull String commandName,
+//                              final @NotNull LiteralNode command) {
+//        registeredCommands.put(commandName, command.getCommandInfo().orElseThrow());
+//    }
+//
+//    @Override
+//    protected void onUnregister(final @NotNull String commandName) {
+//        registeredCommands.remove(commandName);
+//    }
+//
+//    @Override
+//    protected @NotNull Class<?> getSenderType() {
+//        return CommandSender.class;
+//    }
+//
+//}
--- command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandSenderWrapper.java
+++ command-starter/command-starter-base/src/test/java/it/fulminazzo/blocksmith/command/MockCommandSenderWrapper.java
@@ -1,84 +1,84 @@
-package it.fulminazzo.blocksmith.command;
-
-import it.fulminazzo.blocksmith.ApplicationHandle;
-import it.fulminazzo.blocksmith.command.annotation.Permission;
-import it.fulminazzo.blocksmith.command.node.PermissionInfo;
-import it.fulminazzo.blocksmith.message.Messenger;
-import lombok.*;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
-import org.slf4j.Logger;
-
-/**
- * A Command sender wrapper for testing purposes.
- */
-@Slf4j
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
-public final class MockCommandSenderWrapper extends CommandSenderWrapper<CommandSender> {
-
-    /**
-     * Instantiates a new Mock command sender wrapper.
-     *
-     * @param actualSender the actual sender
-     */
-    public MockCommandSenderWrapper(final @NonNull CommandSender actualSender) {
-        this(new ApplicationHandle() {
-
-            @Override
-            public @NotNull Messenger getMessenger() {
-                return new Messenger(log);
-            }
-
-            @Override
-            public @NotNull Logger getLog() {
-                return log;
-            }
-
-            @Override
-            public @NotNull Object getServer() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public @NotNull String getName() {
-                return "blocksmith";
-            }
-
-        }, actualSender);
-    }
-
-    /**
-     * Instantiates a new Mock command sender wrapper.
-     *
-     * @param application  the application
-     * @param actualSender the actual sender
-     */
-    public MockCommandSenderWrapper(final @NotNull ApplicationHandle application,
-                                    final @NonNull CommandSender actualSender) {
-        super(application, actualSender);
-    }
-
-    @Override
-    public @NotNull String getName() {
-        return actualSender.getName();
-    }
-
-    @Override
-    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
-        if (permissionInfo.getGrant() == Permission.Grant.OP && actualSender.isOp()) return true;
-        return actualSender.hasPermission(permissionInfo.getPermission());
-    }
-
-    @Override
-    public boolean isPlayer() {
-        return actualSender instanceof Player;
-    }
-
-    @Override
-    public @NotNull Object getId() {
-        return getName();
-    }
-
-}
+//TODO: update
+//package it.fulminazzo.blocksmith.command;
+//
+//import it.fulminazzo.blocksmith.ApplicationHandle;//import it.fulminazzo.blocksmith.command.annotation.Permission;
+//import it.fulminazzo.blocksmith.command.node.PermissionInfo;
+//import it.fulminazzo.blocksmith.message.Messenger;
+//import lombok.*;
+//import lombok.extern.slf4j.Slf4j;
+//import org.jetbrains.annotations.NotNull;
+//import org.jspecify.annotations.NonNull;
+//import org.slf4j.Logger;
+//
+///**
+// * A Command sender wrapper for testing purposes.
+// */
+//@Slf4j
+//@ToString(callSuper = true)
+//@EqualsAndHashCode(callSuper = true)
+//public final class MockCommandSenderWrapper extends CommandSenderWrapper<CommandSender> {
+//
+//    /**
+//     * Instantiates a new Mock command sender wrapper.
+//     *
+//     * @param actualSender the actual sender
+//     */
+//    public MockCommandSenderWrapper(final @NonNull CommandSender actualSender) {
+//        this(new ApplicationHandle() {
+//
+//            @Override
+//            public @NotNull Messenger getMessenger() {
+//                return new Messenger(log);
+//            }
+//
+//            @Override
+//            public @NotNull Logger getLog() {
+//                return log;
+//            }
+//
+//            @Override
+//            public @NotNull Object getServer() {
+//                throw new UnsupportedOperationException();
+//            }
+//
+//            @Override
+//            public @NotNull String getName() {
+//                return "blocksmith";
+//            }
+//
+//        }, actualSender);
+//    }
+//
+//    /**
+//     * Instantiates a new Mock command sender wrapper.
+//     *
+//     * @param application  the application
+//     * @param actualSender the actual sender
+//     */
+//    public MockCommandSenderWrapper(final @NotNull ApplicationHandle application,
+//                                    final @NonNull CommandSender actualSender) {
+//        super(application, actualSender);
+//    }
+//
+//    @Override
+//    public @NotNull String getName() {
+//        return actualSender.getName();
+//    }
+//
+//    @Override
+//    protected boolean hasPermissionImpl(final @NotNull PermissionInfo permissionInfo) {
+//        if (permissionInfo.getGrant() == Permission.Grant.OP && actualSender.isOp()) return true;
+//        return actualSender.hasPermission(permissionInfo.getPermission());
+//    }
+//
+//    @Override
+//    public boolean isPlayer() {
+//        return actualSender instanceof Player;
+//    }
+//
+//    @Override
+//    public @NotNull Object getId() {
+//        return getName();
+//    }
+//
+//}
