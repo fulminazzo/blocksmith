a32e6242362b5966fa16f0dacf33beb0c2783766
fulminazzo <fulminazzo21@gmail.com>
2026-04-13 16:16:32 +0200

    Re-introduced tests for AsyncManager

--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/node/handler/AsyncManagerTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/node/handler/AsyncManagerTest.groovy
@@ -1,189 +1,173 @@
-//TODO: update
-////file:noinspection unused
-//package it.fulminazzo.blocksmith.command.node.handler
-//
-//import it.fulminazzo.blocksmith.ApplicationHandle
-//import it.fulminazzo.blocksmith.command.CommandRegistry
-//import it.fulminazzo.blocksmith.command.CommandSenderWrapper
-//import it.fulminazzo.blocksmith.command.visitor.execution.ExecutionContext
-//import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionException
-//import org.jetbrains.annotations.NotNull
-//import spock.lang.Specification
-//
-//import java.time.Duration
-//import java.util.concurrent.ExecutorService
-//import java.util.concurrent.Executors
-//
-//class AsyncManagerTest extends Specification {
-//    private static final waitTime = 50
-//    private static final ExecutorService executorService = Executors.newCachedThreadPool()
-//
-//    private static volatile boolean executed = false
-//
-//    private AsyncManager manager
-//
-//    private CommandRegistry registry
-//    private CommandSenderWrapper sender
-//
-//    private ExecutionContext context
-//
-//    void setup() {
-//        executed = false
-//
-//        manager = new AsyncManager(executorService, Duration.ofSeconds(1))
-//
-//        registry = Mock(CommandRegistry)
-//
-//        sender = Mock(CommandSenderWrapper)
-//        sender.id >> UUID.randomUUID()
-//
-//        context = new ExecutionContext(
-//                Mock(ApplicationHandle),
-//                registry,
-//                sender
-//        ).addInput('execute')
-//    }
-//
-//    void cleanup() {
-//        executed = false
-//    }
-//
-//    void cleanupSpec() {
-//        executorService.shutdown()
-//    }
-//
-//    def 'test that execute does not throw'() {
-//        given:
-//        def executionInfo = getExecutor('execute')
-//
-//        when:
-//        def future = manager.execute(executionInfo, context)
-//
-//        then:
-//        contains()
-//
-//        when:
-//        manager.execute(executionInfo, context)
-//
-//        then:
-//        def e = thrown(CommandExecutionException)
-//        e.message == 'error.await-pending-operation'
-//
-//        when:
-//        future.join()
-//
-//        then:
-//        noExceptionThrown()
-//
-//        and:
-//        executed
-//
-//        and:
-//        !contains()
-//    }
-//
-//    def 'test that #method throws #expected'() {
-//        given:
-//        def executionInfo = getExecutor(method)
-//
-//        when:
-//        def future = manager.execute(executionInfo, context)
-//
-//        then:
-//        contains()
-//
-//        when:
-//        future.join()
-//
-//        and:
-//        sleep(100)
-//
-//        then:
-//        noExceptionThrown()
-//
-//        and:
-//        1 * registry.handleCommandExecutionException(
-//                { CommandExecutionException e ->
-//                    e.message == expected
-//                },
-//                context
-//        )
-//
-//        and:
-//        !contains()
-//
-//        and:
-//        !executed
-//
-//        where:
-//        method                  || expected
-//        'internalThrow'         || 'error.unknown'
-//        'slow'                  || 'error.operation-timeout'
-//        'otherException'        || 'error.internal-error'
-//        'otherRuntimeException' || 'error.internal-error'
-//    }
-//
-//    def 'test that execute re-throws CommandExecutionException on exception'() {
-//        given:
-//        def context = Mock(ExecutionContext)
-//        context.registry >> registry
-//        context.commandSender >> sender
-//
-//        and:
-//        def executionInfo = getExecutor('execute')
-//
-//        when:
-//        manager.execute(executionInfo, context).join()
-//
-//        then:
-//        noExceptionThrown()
-//
-//        and:
-//        1 * registry.handleCommandExecutionException(
-//                { CommandExecutionException e ->
-//                    e.message == 'error.internal-error'
-//                },
-//                context
-//        )
-//
-//        and:
-//        !contains()
-//    }
-//
-//    private boolean contains() {
-//        return manager.pending.contains(sender.id)
-//    }
-//
-//    private static CommandExecutor getExecutor(final @NotNull String method) {
-//        return new CommandExecutor(
-//                AsyncManagerTest,
-//                AsyncManagerTest.getMethod(method)
-//        )
-//    }
-//
-//    static void execute() {
-//        Thread.sleep(waitTime)
-//        executed = true
-//    }
-//
-//    static void internalThrow() {
-//        Thread.sleep(waitTime)
-//        throw new CommandExecutionException('error.unknown')
-//    }
-//
-//    static void slow() {
-//        Thread.sleep(1050)
-//        executed = true
-//    }
-//
-//    static void otherException() {
-//        Thread.sleep(waitTime)
-//        throw new Exception('API error')
-//    }
-//
-//    static void otherRuntimeException() {
-//        Thread.sleep(waitTime)
-//        throw new RuntimeException('API error')
-//    }
-//
-//}
+//file:noinspection unused
+package it.fulminazzo.blocksmith.command.node.handler
+
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper
+import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionVisitor
+import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionException
+import org.jetbrains.annotations.NotNull
+import spock.lang.Specification
+
+import java.time.Duration
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class AsyncManagerTest extends Specification {
+    private static final waitTime = 50
+    private static final ExecutorService executorService = Executors.newCachedThreadPool()
+
+    private static volatile boolean executed = false
+
+    private AsyncManager manager
+
+    private CommandSenderWrapper sender
+
+    private CommandExecutionVisitor visitor
+
+    void setup() {
+        executed = false
+
+        manager = new AsyncManager(executorService, Duration.ofSeconds(1))
+
+        sender = Mock(CommandSenderWrapper)
+        sender.id >> UUID.randomUUID()
+
+        visitor = Mock(CommandExecutionVisitor)
+        visitor.commandSender >> sender
+    }
+
+    void cleanup() {
+        executed = false
+    }
+
+    void cleanupSpec() {
+        executorService.shutdown()
+    }
+
+    def 'test that execute does not throw'() {
+        given:
+        def executor = getExecutor('execute')
+
+        when:
+        def future = manager.execute(executor, visitor)
+
+        then:
+        contains()
+
+        when:
+        manager.execute(executor, visitor)
+
+        then:
+        def e = thrown(CommandExecutionException)
+        e.message == 'error.await-pending-operation'
+
+        when:
+        future.join()
+
+        then:
+        noExceptionThrown()
+
+        and:
+        executed
+
+        and:
+        !contains()
+    }
+
+    def 'test that #method throws #expected'() {
+        given:
+        def executor = getExecutor(method)
+
+        when:
+        def future = manager.execute(executor, visitor)
+
+        then:
+        contains()
+
+        when:
+        future.join()
+
+        and:
+        sleep(100)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        1 * visitor.handleCommandExecutionException(_) >> { a ->
+            a[0].message == expected
+        }
+
+        and:
+        !contains()
+
+        and:
+        !executed
+
+        where:
+        method                  || expected
+        'internalThrow'         || 'error.unknown'
+        'slow'                  || 'error.operation-timeout'
+        'otherException'        || 'error.internal-error'
+        'otherRuntimeException' || 'error.internal-error'
+    }
+
+    def 'test that execute re-throws CommandExecutionException on exception'() {
+        given:
+        def visitor = Mock(CommandExecutionVisitor)
+        visitor.commandSender >> sender
+
+        and:
+        def executor = getExecutor('execute')
+
+        when:
+        manager.execute(executor, visitor).join()
+
+        then:
+        noExceptionThrown()
+
+        and:
+        1 * visitor.handleCommandExecutionException(_) >> { a ->
+            a[0].message == 'error.internal-error'
+        }
+
+        and:
+        !contains()
+    }
+
+    private boolean contains() {
+        return manager.pending.contains(sender.id)
+    }
+
+    private CommandExecutor getExecutor(final @NotNull String method) {
+        return new CommandExecutor(
+                AsyncManagerTest,
+                AsyncManagerTest.getMethod(method)
+        )
+    }
+
+    static void execute() {
+        Thread.sleep(waitTime)
+        executed = true
+    }
+
+    static void internalThrow() {
+        Thread.sleep(waitTime)
+        throw new CommandExecutionException('error.unknown')
+    }
+
+    static void slow() {
+        Thread.sleep(1050)
+        executed = true
+    }
+
+    static void otherException() {
+        Thread.sleep(waitTime)
+        throw new Exception('API error')
+    }
+
+    static void otherRuntimeException() {
+        Thread.sleep(waitTime)
+        throw new RuntimeException('API error')
+    }
+
+}
