56168e6b62cdfc5b7077fe461e3f10ed79d7eb7c
fulminazzo <fulminazzo21@gmail.com>
2026-04-16 13:53:48 +0200

    Re-introduced and updated ArgumentParser with parse and getCompletions method signatures updated with Visitor

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/ArgumentParser.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/ArgumentParser.java
@@ -1,41 +1,33 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.argument;
-//
-//import it.fulminazzo.blocksmith.command.TabCompletable;
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext;
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionException;
-//import org.jetbrains.annotations.NotNull;
-//import org.jetbrains.annotations.Nullable;
-//
-///**
-// * A parser to convert a raw string into a Java type.
-// *
-// * @param <T> the type of the parsed argument
-// */
-//public interface ArgumentParser<T> extends TabCompletable {
-//
-//    /**
-//     * Parses the given string.
-//     *
-//     * @param context the current context of action
-//     * @return the object
-//     * @throws CommandExecutionException in case of parsing errors
-//     */
-//    @Nullable T parse(final @NotNull CommandExecutionContext context) throws CommandExecutionException;
-//
-//    /**
-//     * Validates the current input.
-//     *
-//     * @param context the context
-//     * @return <code>true</code> if it is valid
-//     */
-//    default boolean validateCompletions(final @NotNull CommandExecutionContext context) {
-//        try {
-//            context.addParsedArgument(parse(context));
-//            return true;
-//        } catch (CommandExecutionException e) {
-//            return false;
-//        }
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command.argument;
+
+import it.fulminazzo.blocksmith.command.visitor.Visitor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * A parser to convert raw input to a Java type.
+ *
+ * @param <T> the type of the parsed argument
+ */
+public interface ArgumentParser<T> {
+
+    /**
+     * Parses the given string.
+     *
+     * @param visitor the visitor containing the current input and other useful data
+     * @return the object
+     * @throws ArgumentParseException in case of parsing errors
+     */
+    @Nullable T parse(final @NotNull Visitor<?, ?> visitor) throws ArgumentParseException;
+
+    /**
+     * Gets the tab completions associated with this parser.
+     *
+     * @param visitor the visitor containing the current input and other useful data
+     * @return the completions
+     */
+    @NotNull List<String> getCompletions(final @NotNull Visitor<?, ?> visitor);
+
+}
