6676549d38e03836c8e7d0ad67d625a776038a2e
fulminazzo <fulminazzo21@gmail.com>
2026-04-16 13:54:49 +0200

    Re-introduced and updated NumberArgumentParser with parse and getCompletions method signatures updated with Visitor

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/NumberArgumentParser.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/NumberArgumentParser.java
@@ -1,64 +1,62 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.argument;
-//
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext;
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionException;
-//import it.fulminazzo.blocksmith.message.argument.Placeholder;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import org.jetbrains.annotations.NotNull;
-//import org.jetbrains.annotations.Nullable;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.function.Function;
-//
-///**
-// * A {@link ArgumentParser} for general {@link Number} objects.
-// *
-// * @param <N> the type of the parameter
-// */
-//@RequiredArgsConstructor
-//public final class NumberArgumentParser<N extends Number> implements ArgumentParser<N> {
-//    private final @NotNull N min;
-//    private final @NotNull N max;
-//    @Getter
-//    private final @NotNull Function<String, N> parser;
-//
-//    @Override
-//    public @Nullable N parse(final @NotNull CommandExecutionContext context) throws CommandExecutionException {
-//        String rawArgument = context.getCurrent();
-//        try {
-//            return parser.apply(rawArgument);
-//        } catch (NumberFormatException e) {
-//            throw new CommandExecutionException("error.invalid-number")
-//                    .arguments(
-//                            Placeholder.of("argument", rawArgument),
-//                            Placeholder.of("min", min),
-//                            Placeholder.of("max", max)
-//                    );
-//        }
-//    }
-//
-//    @Override
-//    public @NotNull List<String> getCompletions(final @NotNull CommandExecutionContext context) {
-//        String argument = context.getCurrent();
-//        List<String> completions = new ArrayList<>();
-//        if (argument.isEmpty() || isValid(argument))
-//            for (int i = 0; i < 10; i++) {
-//                String tmp = argument + i;
-//                if (isValid(tmp)) completions.add(tmp);
-//            }
-//        return completions;
-//    }
-//
-//    private boolean isValid(final @NotNull String argument) {
-//        try {
-//            N parsed = parser.apply(argument);
-//            return parsed.floatValue() != Float.POSITIVE_INFINITY && parsed.floatValue() != Float.NEGATIVE_INFINITY;
-//        } catch (NumberFormatException ignored) {
-//            return false;
-//        }
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command.argument;
+
+import it.fulminazzo.blocksmith.command.visitor.Visitor;
+import it.fulminazzo.blocksmith.message.argument.Placeholder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * A {@link ArgumentParser} for general {@link Number} objects.
+ *
+ * @param <N> the type of the parameter
+ */
+@RequiredArgsConstructor
+public final class NumberArgumentParser<N extends Number> implements ArgumentParser<N> {
+    private final @NotNull N min;
+    private final @NotNull N max;
+    @Getter
+    private final @NotNull Function<String, N> parser;
+
+    @Override
+    public @Nullable N parse(final @NotNull Visitor<?, ?> visitor) throws ArgumentParseException {
+        String rawArgument = visitor.getInput().getCurrent();
+        try {
+            return parser.apply(rawArgument);
+        } catch (NumberFormatException e) {
+            throw new ArgumentParseException("error.invalid-number")
+                    .arguments(
+                            Placeholder.of("argument", rawArgument),
+                            Placeholder.of("min", min),
+                            Placeholder.of("max", max)
+                    );
+        }
+    }
+
+    @Override
+    public @NotNull List<String> getCompletions(final @NotNull Visitor<?, ?> visitor) {
+        String argument = visitor.getInput().getCurrent();
+        List<String> completions = new ArrayList<>();
+        if (argument.isEmpty() || isValid(argument))
+            for (int i = 0; i < 10; i++) {
+                String tmp = argument + i;
+                if (isValid(tmp)) completions.add(tmp);
+            }
+        return completions;
+    }
+
+    private boolean isValid(final @NotNull String argument) {
+        try {
+            N parsed = parser.apply(argument);
+            return parsed.floatValue() != Float.POSITIVE_INFINITY && parsed.floatValue() != Float.NEGATIVE_INFINITY;
+        } catch (NumberFormatException ignored) {
+            return false;
+        }
+    }
+
+}
--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/argument/NumberArgumentParserTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/argument/NumberArgumentParserTest.groovy
@@ -1,74 +1,65 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.argument
-//
-//import it.fulminazzo.blocksmith.ApplicationHandle
-//import it.fulminazzo.blocksmith.command.CommandRegistry
-//import it.fulminazzo.blocksmith.command.CommandSender
-//import it.fulminazzo.blocksmith.command.CommandSenderWrapper
-//import it.fulminazzo.blocksmith.command.MockCommandSenderWrapper
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionException
-//import it.fulminazzo.blocksmith.message.argument.Placeholder
-//import org.jetbrains.annotations.NotNull
-//import spock.lang.Specification
-//
-//class NumberArgumentParserTest extends Specification {
-//
-//    private final NumberArgumentParser parser = new NumberArgumentParser<>(Integer.MIN_VALUE, Integer.MAX_VALUE, Integer::valueOf)
-//
-//    private final CommandSenderWrapper sender = new MockCommandSenderWrapper(new CommandSender())
-//
-//    def 'test that parse with valid argument does not throw'() {
-//        when:
-//        parser.parse(prepareContext('1'))
-//
-//        then:
-//        noExceptionThrown()
-//    }
-//
-//    def 'test that parse throws for invalid argument'() {
-//        when:
-//        parser.parse(prepareContext('a'))
-//
-//        then:
-//        def e = thrown(CommandExecutionException)
-//        e.arguments.toList() == [
-//                Placeholder.of('argument', 'a'),
-//                Placeholder.of("min", Integer.MIN_VALUE),
-//                Placeholder.of("max", Integer.MAX_VALUE)
-//        ]
-//    }
-//
-//    def 'test that getCompletions returns #expected for argument #argument'() {
-//        given:
-//        def context = new CommandExecutionContext(
-//                Mock(ApplicationHandle),
-//                Mock(CommandRegistry),
-//                sender
-//        ).addInput(argument)
-//
-//        when:
-//        def actual = parser.getCompletions(context)
-//
-//        then:
-//        actual == expected
-//
-//        where:
-//        argument             || expected
-//        ''                   || (0..9).collect { it.toString() }
-//        '1'                  || (0..9).collect { "1$it".toString() }
-//        '12'                 || (0..9).collect { "12$it".toString() }
-//        '-1'                 || (0..9).collect { "-1$it".toString() }
-//        '-12'                || (0..9).collect { "-12$it".toString() }
-//        'a'                  || []
-//        "$Integer.MAX_VALUE" || []
-//        "$Integer.MIN_VALUE" || []
-//    }
-//
-//    private CommandExecutionContext prepareContext(final @NotNull String argument) {
-//        def context = Mock(CommandExecutionContext)
-//        context.current >> argument
-//        return context
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command.argument
+
+import it.fulminazzo.blocksmith.command.visitor.CommandInput
+import it.fulminazzo.blocksmith.command.visitor.Visitor
+import it.fulminazzo.blocksmith.message.argument.Placeholder
+import org.jetbrains.annotations.NotNull
+import spock.lang.Specification
+
+class NumberArgumentParserTest extends Specification {
+    private final NumberArgumentParser parser = new NumberArgumentParser<>(Integer.MIN_VALUE, Integer.MAX_VALUE, Integer::valueOf)
+
+    def 'test that parse with valid argument does not throw'() {
+        when:
+        parser.parse(prepareVisitor('1'))
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'test that parse throws for invalid argument'() {
+        when:
+        parser.parse(prepareVisitor('a'))
+
+        then:
+        def e = thrown(ArgumentParseException)
+        e.arguments.toList() == [
+                Placeholder.of('argument', 'a'),
+                Placeholder.of("min", Integer.MIN_VALUE),
+                Placeholder.of("max", Integer.MAX_VALUE)
+        ]
+    }
+
+    def 'test that getCompletions returns #expected for argument #argument'() {
+        given:
+        def visitor = prepareVisitor(argument)
+
+        when:
+        def actual = parser.getCompletions(visitor)
+
+        then:
+        actual == expected
+
+        where:
+        argument             || expected
+        ''                   || (0..9).collect { it.toString() }
+        '1'                  || (0..9).collect { "1$it".toString() }
+        '12'                 || (0..9).collect { "12$it".toString() }
+        '-1'                 || (0..9).collect { "-1$it".toString() }
+        '-12'                || (0..9).collect { "-12$it".toString() }
+        'a'                  || []
+        "$Integer.MAX_VALUE" || []
+        "$Integer.MIN_VALUE" || []
+    }
+
+    private Visitor<?, ? extends Exception> prepareVisitor(final @NotNull String argument) {
+        def context = Mock(Visitor)
+        context.input >> {
+            def input = Mock(CommandInput)
+            input.current >> argument
+            return input
+        }
+        return context
+    }
+
+}
