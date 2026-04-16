99b2e2f8758c0031bbf829403fbd3cf45655d1d1
fulminazzo <fulminazzo21@gmail.com>
2026-04-16 13:54:05 +0200

    Re-introduced and updated EnumArgumentParser with parse and getCompletions method signatures updated with Visitor

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/EnumArgumentParser.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/EnumArgumentParser.java
@@ -1,55 +1,53 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command.argument;
-//
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext;
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionException;
-//import it.fulminazzo.blocksmith.message.argument.Placeholder;
-//import it.fulminazzo.blocksmith.reflect.Reflect;
-//import org.jetbrains.annotations.NotNull;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-///**
-// * A dynamic {@link ArgumentParser} for enum types.
-// * Requires the enum type to be passed for each initialization.
-// *
-// * @param <E> the type of the enum
-// */
-//final class EnumArgumentParser<E extends Enum<E>> implements ArgumentParser<E> {
-//    private final @NotNull String enumTypeName;
-//    private final @NotNull Map<String, E> values;
-//
-//    /**
-//     * Instantiates a new Enum argument parser.
-//     *
-//     * @param type the Java class of the enum
-//     */
-//    @SuppressWarnings("unchecked")
-//    public EnumArgumentParser(final @NotNull Class<E> type) {
-//        this.enumTypeName = type.getSimpleName();
-//        this.values = new HashMap<>();
-//        Reflect.on(type).values().forEach(e -> values.put(e.name().toLowerCase(), (E) e));
-//    }
-//
-//    @Override
-//    public @NotNull E parse(final @NotNull CommandExecutionContext context) throws CommandExecutionException {
-//        final String rawArgument = context.getCurrent();
-//        E value = values.get(rawArgument.toLowerCase());
-//        if (value == null)
-//            throw new CommandExecutionException("error.enum-not-found")
-//                    .arguments(
-//                            Placeholder.of("argument", rawArgument),
-//                            Placeholder.of("name", enumTypeName)
-//                    );
-//        else return value;
-//    }
-//
-//    @Override
-//    public @NotNull List<String> getCompletions(final @NotNull CommandExecutionContext context) {
-//        return new ArrayList<>(values.keySet());
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command.argument;
+
+import it.fulminazzo.blocksmith.command.visitor.Visitor;
+import it.fulminazzo.blocksmith.message.argument.Placeholder;
+import it.fulminazzo.blocksmith.reflect.Reflect;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A dynamic {@link ArgumentParser} for enum types.
+ * Requires the enum type to be passed for each initialization.
+ *
+ * @param <E> the type of the enum
+ */
+final class EnumArgumentParser<E extends Enum<E>> implements ArgumentParser<E> {
+    private final @NotNull String enumTypeName;
+    private final @NotNull Map<String, E> values;
+
+    /**
+     * Instantiates a new Enum argument parser.
+     *
+     * @param type the Java class of the enum
+     */
+    @SuppressWarnings("unchecked")
+    public EnumArgumentParser(final @NotNull Class<E> type) {
+        this.enumTypeName = type.getSimpleName();
+        this.values = new HashMap<>();
+        Reflect.on(type).values().forEach(e -> values.put(e.name().toLowerCase(), (E) e));
+    }
+
+    @Override
+    public @NotNull E parse(final @NotNull Visitor<?, ?> visitor) throws ArgumentParseException {
+        final String rawArgument = visitor.getInput().getCurrent();
+        E value = values.get(rawArgument.toLowerCase());
+        if (value == null)
+            throw new ArgumentParseException("error.enum-not-found")
+                    .arguments(
+                            Placeholder.of("argument", rawArgument),
+                            Placeholder.of("name", enumTypeName)
+                    );
+        else return value;
+    }
+
+    @Override
+    public @NotNull List<String> getCompletions(final @NotNull Visitor<?, ?> visitor) {
+        return new ArrayList<>(values.keySet());
+    }
+
+}
