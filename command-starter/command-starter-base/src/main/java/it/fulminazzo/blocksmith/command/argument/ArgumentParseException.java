c263f4f01e8db0bc7f7d6bff71bb1f646f5b5e9f
fulminazzo <fulminazzo21@gmail.com>
2026-04-16 13:49:59 +0200

    Added ArgumentParseException exception

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/ArgumentParseException.java
@@ -0,0 +1,38 @@
+package it.fulminazzo.blocksmith.command.argument;
+
+import it.fulminazzo.blocksmith.message.argument.Argument;
+import lombok.experimental.StandardException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * An exception thrown by {@link ArgumentParser} in case of parsing exceptions.
+ */
+@StandardException
+public final class ArgumentParseException extends Exception {
+    private final @NotNull List<Argument> arguments = new ArrayList<>();
+
+    /**
+     * Adds new arguments to the final message.
+     *
+     * @param arguments the arguments
+     * @return this object (for method chaining)
+     */
+    public @NotNull ArgumentParseException arguments(final @NotNull Argument @NotNull ... arguments) {
+        this.arguments.addAll(Arrays.asList(arguments));
+        return this;
+    }
+
+    /**
+     * Get the arguments.
+     *
+     * @return the arguments
+     */
+    public @NotNull Argument @NotNull [] getArguments() {
+        return arguments.toArray(new Argument[0]);
+    }
+
+}
