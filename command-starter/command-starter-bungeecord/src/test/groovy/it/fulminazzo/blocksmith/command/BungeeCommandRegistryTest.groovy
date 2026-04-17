05834226241f850c90046c6cc4f3bdef137aca7e
fulminazzo <fulminazzo21@gmail.com>
2026-04-18 00:49:19 +0200

    Added more tests

--- command-starter/command-starter-bungeecord/src/test/groovy/it/fulminazzo/blocksmith/command/BungeeCommandRegistryTest.groovy
+++ command-starter/command-starter-bungeecord/src/test/groovy/it/fulminazzo/blocksmith/command/BungeeCommandRegistryTest.groovy
@@ -8,6 +8,7 @@ import it.fulminazzo.blocksmith.command.node.info.CommandInfo
 import it.fulminazzo.blocksmith.command.node.LiteralNode
 import it.fulminazzo.blocksmith.command.node.info.PermissionInfo
 import it.fulminazzo.blocksmith.message.Messenger
+import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.ProxyServer
 import net.md_5.bungee.api.plugin.Plugin
 import net.md_5.bungee.api.plugin.PluginManager
@@ -81,6 +82,51 @@ class BungeeCommandRegistryTest extends Specification {
         and:
         def registeredCommands = registry.registeredCommands
         registeredCommands['help'] == null
+    }
+
+    def 'test that wrapSender instantiates a new Bungee CommandSenderWrapper'() {
+        given:
+        def sender = Mock(CommandSender)
+
+        when:
+        def wrapped = registry.wrapSender(sender)
+
+        then:
+        (wrapped instanceof BungeeCommandSenderWrapper)
+        wrapped.actualSender == sender
+    }
+
+    def 'test that sender type is correct'() {
+        expect:
+        registry.senderType == CommandSender
+    }
+
+    def 'test that BungeeCommand delegates #method to #expected'() {
+        given:
+        def registry = Mock(BungeeCommandRegistry)
+
+        and:
+        def node = new LiteralNode('test')
+        node.commandInfo = new CommandInfo(
+                'test.description',
+                new PermissionInfo(null, 'test.permission', Permission.Grant.NONE)
+        )
+        def sender = Mock(CommandSender)
+        def args = ['first', 'second', 'third'].toArray(String[]::new)
+
+        and:
+        def command = new BungeeCommandRegistry.BungeeCommand(registry, 'test', node)
+
+        when:
+        command."$method"(sender, args)
+
+        then:
+        1 * registry."$expected"(node, sender, 'test', args)
+
+        where:
+        method          || expected
+        'execute'       || 'execute'
+        'onTabComplete' || 'tabComplete'
     }
 
 }
