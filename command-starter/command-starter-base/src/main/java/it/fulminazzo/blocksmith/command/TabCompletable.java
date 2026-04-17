79e1b03ca336f24cac2a16014fef672832baf525
fulminazzo <fulminazzo21@gmail.com>
2026-04-17 18:56:50 +0200

    Re-introduced TabCompletable and updated ArgumentParser

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/TabCompletable.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/TabCompletable.java
@@ -1,23 +1,22 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command;
-//
-//import it.fulminazzo.blocksmith.command.execution.CommandExecutionContext;
-//import org.jetbrains.annotations.NotNull;
-//
-//import java.util.List;
-//
-///**
-// * Represents an object that returns a list of tab completions.
-// */
-//@FunctionalInterface
-//public interface TabCompletable {
-//
-//    /**
-//     * Gets the tab completions.
-//     *
-//     * @param context the current context of action
-//     * @return the completions
-//     */
-//    @NotNull List<String> getCompletions(final @NotNull CommandExecutionContext context);
-//
-//}
+package it.fulminazzo.blocksmith.command;
+
+import it.fulminazzo.blocksmith.command.visitor.Visitor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Represents an object that returns a list of tab completions.
+ */
+@FunctionalInterface
+public interface TabCompletable {
+
+    /**
+     * Gets the tab completions.
+     *
+     * @param visitor the visitor requesting the completions
+     * @return the completions
+     */
+    @NotNull List<String> getCompletions(final @NotNull Visitor<?, ?> visitor);
+
+}
--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/ArgumentParser.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/argument/ArgumentParser.java
@@ -1,17 +1,16 @@
 package it.fulminazzo.blocksmith.command.argument;
 
+import it.fulminazzo.blocksmith.command.TabCompletable;
 import it.fulminazzo.blocksmith.command.visitor.Visitor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 /**
  * A parser to convert raw input to a Java type.
  *
  * @param <T> the type of the parsed argument
  */
-public interface ArgumentParser<T> {
+public interface ArgumentParser<T> extends TabCompletable {
 
     /**
      * Parses the given string.
@@ -21,13 +20,5 @@ public interface ArgumentParser<T> {
      * @throws ArgumentParseException in case of parsing errors
      */
     @Nullable T parse(final @NotNull Visitor<?, ?> visitor) throws ArgumentParseException;
-
-    /**
-     * Gets the tab completions associated with this parser.
-     *
-     * @param visitor the visitor containing the current input and other useful data
-     * @return the completions
-     */
-    @NotNull List<String> getCompletions(final @NotNull Visitor<?, ?> visitor);
 
 }
