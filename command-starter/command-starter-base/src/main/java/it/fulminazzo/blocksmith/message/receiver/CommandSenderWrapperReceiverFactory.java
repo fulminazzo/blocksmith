c94b55e6d997a657e6463f9431994f69aa9fdb19
fulminazzo <fulminazzo21@gmail.com>
2026-04-18 00:05:17 +0200

    Re-introduced CommandSenderWrapperReceiverFactory and updated with setup method

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/message/receiver/CommandSenderWrapperReceiverFactory.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/message/receiver/CommandSenderWrapperReceiverFactory.java
@@ -1,32 +1,39 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.message.receiver;
-//
-//import it.fulminazzo.blocksmith.command.CommandSenderWrapper;
-//import org.jetbrains.annotations.NotNull;
-//
-//import java.util.Collection;
-//import java.util.Collections;
-//
-///**
-// * Implementation of {@link ReceiverFactory} for {@link CommandSenderWrapper} objects.
-// */
-//public final class CommandSenderWrapperReceiverFactory implements ReceiverFactory {
-//
-//    @Override
-//    public @NotNull Collection<Receiver> getAllReceivers() {
-//        return Collections.emptyList();
-//    }
-//
-//    @Override
-//    public @NotNull <R> Receiver create(final @NotNull R receiver) {
-//        CommandSenderWrapper<?> wrapper = (CommandSenderWrapper<?>) receiver;
-//        Object internal = wrapper.getActualSender();
-//        return ReceiverFactories.get(internal.getClass()).create(internal);
-//    }
-//
-//    @Override
-//    public boolean supports(final @NotNull Class<?> receiverType) {
-//        return CommandSenderWrapper.class.isAssignableFrom(receiverType);
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.message.receiver;
+
+import it.fulminazzo.blocksmith.ServerApplication;
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Implementation of {@link ReceiverFactory} for {@link CommandSenderWrapper} objects.
+ */
+public final class CommandSenderWrapperReceiverFactory extends AbstractReceiverFactory {
+    private ServerApplication application;
+
+    @Override
+    public @NotNull ReceiverFactory setup(final @NotNull ServerApplication application) {
+        this.application = application;
+        return this;
+    }
+
+    @Override
+    protected @NotNull Collection<Receiver> getAllReceiversImpl() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected @NotNull <R> Receiver createImpl(final @NotNull R receiver) {
+        CommandSenderWrapper<?> wrapper = (CommandSenderWrapper<?>) receiver;
+        Object internal = wrapper.getActualSender();
+        return ReceiverFactories.get(internal.getClass(), application).create(internal);
+    }
+
+    @Override
+    protected boolean supportsImpl(final @NotNull Class<?> receiverType) {
+        return CommandSenderWrapper.class.isAssignableFrom(receiverType);
+    }
+
+}
