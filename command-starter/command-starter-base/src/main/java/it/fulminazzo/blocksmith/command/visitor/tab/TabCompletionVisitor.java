0c3dc94fdb3bdb3467fb9accebc10457cfb09f6b
fulminazzo <fulminazzo21@gmail.com>
2026-04-17 18:55:38 +0200

    Created empty TabCompletionVisitor

---
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/visitor/tab/TabCompletionVisitor.java
@@ -0,0 +1,53 @@
+package it.fulminazzo.blocksmith.command.visitor.tab;
+
+import it.fulminazzo.blocksmith.ApplicationHandle;
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper;
+import it.fulminazzo.blocksmith.command.node.ArgumentNode;
+import it.fulminazzo.blocksmith.command.node.CommandNode;
+import it.fulminazzo.blocksmith.command.node.LiteralNode;
+import it.fulminazzo.blocksmith.command.visitor.VisitorImpl;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * A special {@link it.fulminazzo.blocksmith.command.visitor.Visitor} handling tab completion.
+ * <br>
+ * Uses {@link RuntimeException} as exception because it should never actually throw anything.
+ */
+public final class TabCompletionVisitor extends VisitorImpl<@NotNull List<String>, RuntimeException> {
+
+    /**
+     * Instantiates a new Tab completion visitor.
+     *
+     * @param application   the application
+     * @param commandSender the command sender
+     * @param commandName   the command name
+     * @param arguments     the arguments
+     */
+    public TabCompletionVisitor(final @NotNull ApplicationHandle application,
+                                final @NotNull CommandSenderWrapper<?> commandSender,
+                                final @NotNull String commandName,
+                                final @NotNull String @NotNull ... arguments) {
+        super(application, commandSender, commandName, arguments);
+    }
+
+    @Override
+    public @NotNull List<String> visitArgumentNode(final @NotNull ArgumentNode<?> node) {
+        //TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NotNull List<String> visitLiteralNode(final @NotNull LiteralNode node) {
+        //TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected @NotNull List<String> visitCommandNode(final @NotNull CommandNode node) {
+        //TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+}
