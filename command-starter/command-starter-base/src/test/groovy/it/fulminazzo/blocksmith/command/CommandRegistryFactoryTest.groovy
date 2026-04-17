758f4c64b092f30cc128d3268d572ad342cea0ca
fulminazzo <fulminazzo21@gmail.com>
2026-04-17 23:41:50 +0200

    Re-introduced CommnadRegistryFactory

--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/CommandRegistryFactoryTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/CommandRegistryFactoryTest.groovy
@@ -1,19 +1,18 @@
-//TODO: update
-//package it.fulminazzo.blocksmith.command
-//
-//import groovy.util.logging.Slf4j
-//import it.fulminazzo.blocksmith.ApplicationHandle
-//import spock.lang.Specification
-//
-//@Slf4j
-//class CommandRegistryFactoryTest extends Specification {
-//
-//    def 'test that newCommandRegistry with no service throws'() {
-//        when:
-//        CommandRegistryFactory.newCommandRegistry(Mock(ApplicationHandle))
-//
-//        then:
-//        thrown(IllegalStateException)
-//    }
-//
-//}
+package it.fulminazzo.blocksmith.command
+
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.ApplicationHandle
+import spock.lang.Specification
+
+@Slf4j
+class CommandRegistryFactoryTest extends Specification {
+
+    def 'test that newCommandRegistry with no service throws'() {
+        when:
+        CommandRegistryFactory.newCommandRegistry(Mock(ApplicationHandle))
+
+        then:
+        thrown(IllegalStateException)
+    }
+
+}
