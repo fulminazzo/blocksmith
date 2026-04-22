7b0c0026ee48763e126dac7caeb27ce04cce5665
fulminazzo <fulminazzo21@gmail.com>
2026-04-22 20:19:39 +0200

    Added tests for visitCommandNode

--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/visitor/usage/UsageVisitorTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/visitor/usage/UsageVisitorTest.groovy
@@ -4,23 +4,120 @@ import it.fulminazzo.blocksmith.command.node.ArgumentNode
 import it.fulminazzo.blocksmith.command.node.CommandNode
 import it.fulminazzo.blocksmith.command.node.LiteralNode
 import it.fulminazzo.blocksmith.command.visitor.Visitor
+import it.fulminazzo.blocksmith.reflect.Reflect
 import spock.lang.Specification
 
 import java.lang.reflect.Parameter
 
 class UsageVisitorTest extends Specification {
-    private static final Parameter parameter = Visitor.getMethod('visitCommandNode', CommandNode).parameters[0]
+    private static final Parameter PARAMETER = Visitor.getMethod('visitCommandNode', CommandNode).parameters[0]
+
+    private static final Visitor<String, ? extends Exception> SINGLE_VISITOR = new UsageVisitor.SimpleUsageVisitor()
+    private static final List<CommandNode> TEST_NODES = [
+            new LiteralNode('message', 'msg', 'm'),
+            ArgumentNode.of('optional', PARAMETER, true),
+            ArgumentNode.of('argument', PARAMETER, false),
+            new LiteralNode('root')
+    ]
+    private static final Map<CommandNode, String> TEST_NODES_USAGES = TEST_NODES
+            .collectEntries { [(it): it.accept(SINGLE_VISITOR)] }
+            .sort { a, b -> a.key.name <=> b.key.name }
 
     private final Visitor<String, ? extends Exception> visitor = new UsageVisitor()
-    private final Visitor<String, ? extends Exception> singleVisitor = new UsageVisitor.SimpleUsageVisitor()
+
+    void cleanup() {
+        TEST_NODES.each {
+            Reflect.on(it).set('parent', null)
+            it.children.clear()
+        }
+    }
+
+    /*
+     * visitCommandNode
+     */
+
+    def 'test visitCommandNode with recursive children'() {
+        given:
+        def node = Mock(CommandNode)
+        node.children >> [
+                childrenSupplier()
+        ].toSet()
+
+        when:
+        def usage = visitor.visitCommandNode(node)
+
+        then:
+        usage == expected
+
+        where:
+        childrenSupplier                   || expected
+        ({
+            def child1 = TEST_NODES[0]
+            def child2 = TEST_NODES[1]
+            child1.addChild(child2)
+            return child1
+        })                                 || " ${TEST_NODES_USAGES[TEST_NODES[0]]} ${TEST_NODES_USAGES[TEST_NODES[1]]}"
+        ({
+            def child1 = TEST_NODES[0]
+            def child2 = TEST_NODES[1]
+            def child3 = TEST_NODES[2]
+            child3.addChild(TEST_NODES[3])
+            child1.addChild(child2)
+            child1.addChild(child3)
+            return child1
+        })                                 ||
+                " ${TEST_NODES_USAGES[TEST_NODES[0]]} ${TEST_NODES_USAGES[TEST_NODES[2]]}<dark_gray>|</dark_gray>${TEST_NODES_USAGES[TEST_NODES[1]]}"
+    }
+
+    def 'test that visitCommandNode of multiple children returns all the children then stops'() {
+        given:
+        def node = Mock(CommandNode)
+        node.children >> TEST_NODES.toSet().sort { a, b -> a.name <=> b.name }
+
+        when:
+        def usage = visitor.visitCommandNode(node)
+
+        then:
+        usage == ' ' + TEST_NODES_USAGES.values()
+                .join('<dark_gray>|</dark_gray>')
+    }
+
+    def 'test that visitCommandNode of one child returns its usage'() {
+        given:
+        def node = Mock(CommandNode)
+        node.children >> [
+                child
+        ].toSet()
+
+        when:
+        def usage = visitor.visitCommandNode(node)
+
+        then:
+        usage == ' ' + TEST_NODES_USAGES[child]
+
+        where:
+        child << TEST_NODES
+    }
+
+    def 'test that visitCommandNode with no children returns empty'() {
+        given:
+        def node = Mock(CommandNode)
+        node.children >> [].toSet()
+
+        when:
+        def usage = visitor.visitCommandNode(node)
+
+        then:
+        usage == ''
+    }
 
     def 'test that visitParentNode returns all usages of parent nodes correctly'() {
         given:
         def node = Mock(CommandNode)
-        def parent1 = new LiteralNode('message', 'msg', 'm')
-        def parent2 = ArgumentNode.of('optional', parameter, true)
-        def parent3 = ArgumentNode.of('argument', parameter, false)
-        def parent4 = new LiteralNode('root')
+        def parent1 = TEST_NODES[0]
+        def parent2 = TEST_NODES[1]
+        def parent3 = TEST_NODES[2]
+        def parent4 = TEST_NODES[3]
 
         and:
         parent4.addChild(parent3).addChild(parent2).addChild(parent1)
@@ -30,10 +127,10 @@ class UsageVisitorTest extends Specification {
         def usage = visitor.visitParentNode(node)
 
         then:
-        usage == parent4.accept(singleVisitor) + ' ' +
-                parent3.accept(singleVisitor) + ' ' +
-                parent2.accept(singleVisitor) + ' ' +
-                parent1.accept(singleVisitor) + ' '
+        usage == TEST_NODES_USAGES[parent4] + ' ' +
+                TEST_NODES_USAGES[parent3] + ' ' +
+                TEST_NODES_USAGES[parent2] + ' ' +
+                TEST_NODES_USAGES[parent1] + ' '
     }
 
 }
