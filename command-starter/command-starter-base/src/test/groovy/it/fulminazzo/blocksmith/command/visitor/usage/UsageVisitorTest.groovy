452594ff16bf53e534ed63c5774341408a7dbca5
fulminazzo <fulminazzo21@gmail.com>
2026-04-22 20:31:00 +0200

    Added tests for visitCommandNode

--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/visitor/usage/UsageVisitorTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/visitor/usage/UsageVisitorTest.groovy
@@ -10,6 +10,7 @@ import spock.lang.Specification
 import java.lang.reflect.Parameter
 
 class UsageVisitorTest extends Specification {
+    private static final UsageStyle USAGE_STYLE = UsageStyle.get()
     private static final Parameter PARAMETER = Visitor.getMethod('visitCommandNode', CommandNode).parameters[0]
 
     private static final Visitor<String, ? extends Exception> SINGLE_VISITOR = new UsageVisitor.SimpleUsageVisitor()
@@ -30,6 +31,100 @@ class UsageVisitorTest extends Specification {
             Reflect.on(it).set('parent', null)
             it.children.clear()
         }
+    }
+
+    def 'test that getUsage returns all the correct usages'() {
+        given:
+        def clan = new LiteralNode('clan', 'team', 'gang')
+        def name = ArgumentNode.of('name', PARAMETER, false)
+        clan.addChild(name)
+
+        def help = new LiteralNode('help', '?')
+        clan.addChild(help)
+
+        def player = new LiteralNode('player')
+        clan.addChild(player)
+        def playerArg = ArgumentNode.of('player', PARAMETER, false)
+        player.addChild(playerArg)
+
+        def invite = new LiteralNode('invite')
+        playerArg.addChild(invite)
+
+        def promote = new LiteralNode('promote')
+        playerArg.addChild(promote)
+        def rank = ArgumentNode.of('rank', PARAMETER, true)
+        promote.addChild(rank)
+
+        def demote = new LiteralNode('demote')
+        playerArg.addChild(demote)
+        rank = ArgumentNode.of('rank', PARAMETER, true)
+        demote.addChild(rank)
+        def reason = ArgumentNode.of('reason', PARAMETER, true)
+        reason.greedy = true
+        demote.addChild(reason)
+
+        def kick = new LiteralNode('kick')
+        playerArg.addChild(kick)
+        reason = ArgumentNode.of('reason', PARAMETER, false)
+        reason.greedy = true
+        kick.addChild(reason)
+
+        and:
+        def separator = "<${USAGE_STYLE.punctuationColor}>|</${USAGE_STYLE.punctuationColor}>"
+        def openBracket = "<${USAGE_STYLE.punctuationColor}>[</${USAGE_STYLE.punctuationColor}>"
+        def closeBracket = "<${USAGE_STYLE.punctuationColor}>]</${USAGE_STYLE.punctuationColor}>"
+        def lessThan = "<${USAGE_STYLE.punctuationColor}><</${USAGE_STYLE.punctuationColor}>"
+        def greaterThan = "<${USAGE_STYLE.punctuationColor}>></${USAGE_STYLE.punctuationColor}>"
+        def slash = "<${USAGE_STYLE.literalColor}>/</${USAGE_STYLE.literalColor}>"
+
+        and:
+        def clanUsage = "${slash}" +
+                "<${USAGE_STYLE.literalColor}>clan</${USAGE_STYLE.literalColor}>" + separator +
+                "<${USAGE_STYLE.literalColor}>gang</${USAGE_STYLE.literalColor}>" + separator +
+                "<${USAGE_STYLE.literalColor}>team</${USAGE_STYLE.literalColor}>"
+
+        and:
+        def playerUsage = "<${USAGE_STYLE.literalColor}>player</${USAGE_STYLE.literalColor}> " +
+                lessThan +
+                "<${USAGE_STYLE.defaultArgumentColor}>player</${USAGE_STYLE.defaultArgumentColor}>" +
+                greaterThan
+
+        and:
+        def helpUsage = "<${USAGE_STYLE.literalColor}>?</${USAGE_STYLE.literalColor}>" + separator +
+                "<${USAGE_STYLE.literalColor}>help</${USAGE_STYLE.literalColor}>"
+
+        expect:
+        invite.accept(visitor) == "${clanUsage} ${playerUsage} " +
+                "<${USAGE_STYLE.literalColor}>invite</${USAGE_STYLE.literalColor}>"
+
+        and:
+        kick.accept(visitor) == "${clanUsage} ${playerUsage} " +
+                "<${USAGE_STYLE.literalColor}>kick</${USAGE_STYLE.literalColor}> " +
+                "$lessThan<${USAGE_STYLE.defaultArgumentColor}>reason...</${USAGE_STYLE.defaultArgumentColor}>$greaterThan"
+
+        and:
+        promote.accept(visitor) == "${clanUsage} ${playerUsage} <${USAGE_STYLE.literalColor}>promote</${USAGE_STYLE.literalColor}> " +
+                "$openBracket<${USAGE_STYLE.defaultOptionalArgumentColor}>rank</${USAGE_STYLE.defaultOptionalArgumentColor}>$closeBracket"
+
+        and:
+        demote.accept(visitor) == "${clanUsage} ${playerUsage} <${USAGE_STYLE.literalColor}>demote</${USAGE_STYLE.literalColor}> " +
+                "$openBracket<${USAGE_STYLE.defaultOptionalArgumentColor}>rank</${USAGE_STYLE.defaultOptionalArgumentColor}>$closeBracket$separator" +
+                "$openBracket<${USAGE_STYLE.defaultOptionalArgumentColor}>reason...</${USAGE_STYLE.defaultOptionalArgumentColor}>$closeBracket"
+
+        and:
+        player.accept(visitor) == "${clanUsage} ${playerUsage} " +
+                "<${USAGE_STYLE.literalColor}>demote</${USAGE_STYLE.literalColor}>$separator" +
+                "<${USAGE_STYLE.literalColor}>invite</${USAGE_STYLE.literalColor}>$separator" +
+                "<${USAGE_STYLE.literalColor}>kick</${USAGE_STYLE.literalColor}>$separator" +
+                "<${USAGE_STYLE.literalColor}>promote</${USAGE_STYLE.literalColor}>"
+
+        and:
+        help.accept(visitor) == "${clanUsage} ${helpUsage}"
+
+        and:
+        clan.accept(visitor) == "${clanUsage} ${helpUsage}$separator" +
+                "$lessThan<${USAGE_STYLE.defaultArgumentColor}>name</${USAGE_STYLE.defaultArgumentColor}>$greaterThan$separator" +
+                "<${USAGE_STYLE.literalColor}>player</${USAGE_STYLE.literalColor}>"
     }
 
     def 'test that visit#type delegates to visitCommandNode'() {
