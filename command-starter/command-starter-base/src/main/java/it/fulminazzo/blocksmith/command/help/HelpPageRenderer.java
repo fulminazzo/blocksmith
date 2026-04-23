029b2aaec2e651f5ee605c7f69d153312fd860b7
fulminazzo <fulminazzo21@gmail.com>
2026-04-24 01:09:27 +0200

    Added getMaxTruncationLength function

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/help/HelpPageRenderer.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/help/HelpPageRenderer.java
@@ -1,9 +1,34 @@
 package it.fulminazzo.blocksmith.command.help;
 
+import it.fulminazzo.blocksmith.message.util.ComponentUtils;
 import org.jetbrains.annotations.NotNull;
 
 public final class HelpPageRenderer {
     private static final int MAX_FONT_WIDTH = 320;
+
+    /**
+     * Given the string, returns the index at which it should be <b>truncated</b>,
+     * so that when rendered it does not exceed {@link #MAX_FONT_WIDTH} pixels.
+     * <br>
+     * Takes into account that the string should end with {@link ComponentUtils#TRUNCATE_SUFFIX}
+     *
+     * @param string the string
+     * @return the index (or {@code -1} if it does not need to be extracted)
+     */
+    static int getMaxTruncationLength(final @NotNull String string) {
+        if (getMaxLength(string) == -1) return -1;
+        final String truncateSuffix = ComponentUtils.TRUNCATE_SUFFIX;
+        final StringBuilder tmp = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            tmp.append(c);
+            if (MinecraftFontWidth.getWidth(tmp.append(truncateSuffix)) > MAX_FONT_WIDTH)
+                return i - 1;
+            else tmp.setLength(tmp.length() - truncateSuffix.length());
+        }
+        // this should be impossible
+        return -1;
+    }
 
     /**
      * Given the string, returns the index at which it should be <b>truncated</b>
--- command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/help/HelpPageRendererTest.groovy
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/help/HelpPageRendererTest.groovy
@@ -4,6 +4,19 @@ import spock.lang.Specification
 
 class HelpPageRendererTest extends Specification {
 
+    def 'test that getMaxTruncationLength of #string returns #expected'() {
+        when:
+        def actual = HelpPageRenderer.getMaxTruncationLength(string)
+
+        then:
+        actual == expected
+
+        where:
+        string                                           || expected
+        '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'  || -1
+        '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' || 44
+    }
+
     def 'test that getMaxLength of #string returns #expected'() {
         when:
         def actual = HelpPageRenderer.getMaxLength(string)
