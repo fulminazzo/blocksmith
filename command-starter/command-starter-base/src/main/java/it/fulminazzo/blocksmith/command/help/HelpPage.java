8aa18a0ba5c6ccf7832774399b1d359055b5bcc2
fulminazzo <fulminazzo21@gmail.com>
2026-04-24 15:38:20 +0200

    Updated HelpPage to store subcommands data as well

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/help/HelpPage.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/help/HelpPage.java
@@ -2,20 +2,21 @@ package it.fulminazzo.blocksmith.command.help;
 
 import it.fulminazzo.blocksmith.command.node.LiteralNode;
 import it.fulminazzo.blocksmith.command.node.info.CommandInfo;
+import it.fulminazzo.blocksmith.command.node.info.PermissionInfo;
 import lombok.Builder;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A holder for information about a help page.
  */
 @Value
-@Builder
 public class HelpPage {
-    @NotNull String name;
-    @NotNull String description;
-    @NotNull String permission;
-    @NotNull String usage;
+    @NotNull CommandData command;
+    @NotNull List<CommandData> subcommands;
 
     /**
      * Creates a new Help page.
@@ -23,14 +24,39 @@ public class HelpPage {
      * @param commandNode the command node
      * @return the help page
      */
-    public @NotNull HelpPage create(final @NotNull LiteralNode commandNode) {
-        CommandInfo commandInfo = commandNode.getCommandInfo();
-        return builder()
-                .name(commandNode.getName())
-                .description(commandInfo.getDescription())
-                .permission(commandInfo.getPermission().getPermission())
-                .usage(commandNode.getUsage())
-                .build();
+    public static @NotNull HelpPage create(final @NotNull LiteralNode commandNode) {
+        return new HelpPage(
+                CommandData.create(commandNode),
+                commandNode.getSubcommands().stream()
+                        .map(CommandData::create)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @Value
+    @Builder
+    static class CommandData {
+        @NotNull String name;
+        @NotNull String description;
+        @NotNull PermissionInfo permission;
+        @NotNull String usage;
+
+        /**
+         * Creates a new Command data.
+         *
+         * @param commandNode the command node
+         * @return the command data
+         */
+        public static @NotNull CommandData create(final @NotNull LiteralNode commandNode) {
+            CommandInfo commandInfo = commandNode.getCommandInfo();
+            return builder()
+                    .name(commandNode.getName())
+                    .description(commandInfo.getDescription())
+                    .permission(commandInfo.getPermission())
+                    .usage(commandNode.getUsage())
+                    .build();
+        }
+
     }
 
 }
