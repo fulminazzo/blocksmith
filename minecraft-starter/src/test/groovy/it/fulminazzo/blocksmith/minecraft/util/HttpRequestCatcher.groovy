b2668e713ca55deecb9312a8636db4b7d422d3e0
fulminazzo <fulminazzo21@gmail.com>
2026-04-27 00:49:13 +0200

    Created HttpRequestCatcher for testing purposes

---
+++ minecraft-starter/src/test/groovy/it/fulminazzo/blocksmith/minecraft/util/HttpRequestCatcher.groovy
@@ -0,0 +1,43 @@
+package it.fulminazzo.blocksmith.minecraft.util
+
+import java.util.concurrent.CompletableFuture
+
+final class HttpRequestCatcher {
+    private ServerSocket socket
+    private CompletableFuture<List<String>> future
+
+    List<String> getLines() {
+        return future?.get()
+    }
+
+    HttpRequestCatcher start() {
+        socket = new ServerSocket(8080)
+        future = CompletableFuture.supplyAsync {
+            def client = socket.accept()
+            def lines = handleClient(client)
+
+            client.close()
+
+            socket.close()
+            socket = null
+
+            return lines
+        }
+        return this
+    }
+
+    void stop() {
+        if (future != null) future.cancel(true)
+        if (socket != null) socket.close()
+    }
+
+    private static List<String> handleClient(final Socket client) {
+        def stream = client.inputStream.newReader()
+        List<String> lines = []
+        String current
+        while ((current = stream.readLine()) != null && !current.empty)
+            lines.add(current)
+        return lines
+    }
+
+}
