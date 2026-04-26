be2f2ea177bfb94cb612fac116c711444165421b
fulminazzo <fulminazzo21@gmail.com>
2026-04-26 14:08:04 +0200

    Replaced injected page node with HelpPageNode

--- command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/HelpNode.java
+++ command-starter/command-starter-base/src/main/java/it/fulminazzo/blocksmith/command/node/HelpNode.java
@@ -1,16 +1,10 @@
 package it.fulminazzo.blocksmith.command.node;
 
 import it.fulminazzo.blocksmith.command.annotation.Help;
-import it.fulminazzo.blocksmith.command.help.HelpPage;
-import it.fulminazzo.blocksmith.command.help.HelpPageRenderer;
-import it.fulminazzo.blocksmith.reflect.Reflect;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 
 /**
  * A special {@link CommandNode} that represents the help command associated with {@link Help}.
@@ -34,19 +28,7 @@ public final class HelpNode extends InjectedNode {
                 parent
         );
         this.helpAnnotation = helpAnnotation;
-        //TODO: dynamic resolution of min and max
-        //TODO: better handling
-        Method method = Reflect.on(this).getMethod("unused", int.class);
-        Parameter parameter = method.getParameters()[0];
-        NumberArgumentNode<?> page = (NumberArgumentNode<?>) (Object) ArgumentNode.of("page", parameter, true);
-        page.setDefaultValue("1");
-        page.setExecutor((n, v) -> {
-            HelpPage helpPage = HelpPage.create(n);
-            int pageArgument = (int) v.getArguments().getLast();
-            new HelpPageRenderer(helpPage, v, pageArgument).render().forEach(l ->
-                    v.getCommandSender().sendMessage(l)
-            );
-        });
+        addChild(new HelpPageNode(parent));
     }
 
     @Override
@@ -56,11 +38,6 @@ public final class HelpNode extends InjectedNode {
             setCommandInfo(null);
         }
         return (HelpNode) super.merge(node);
-    }
-
-    @SuppressWarnings("unused")
-    private static void unused(final int page) {
-        // mock function, used to pass a parameter to the page node
     }
 
 }
