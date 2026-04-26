5a1ef2bdb090ddfc5962085c624df0adc5ca65d6
fulminazzo <fulminazzo21@gmail.com>
2026-04-26 13:09:39 +0200

    Created Help annotation

---
+++ command-starter/src/main/java/it/fulminazzo/blocksmith/command/annotation/Help.java
@@ -0,0 +1,22 @@
+package it.fulminazzo.blocksmith.command.annotation;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//TODO: pending documentation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Help {
+    @NotNull String DEFAULT_NAME = "help";
+
+    @NotNull String @NotNull [] aliases() default {DEFAULT_NAME};
+
+    @NotNull String description() default "";
+
+    @NotNull String permission() default "";
+
+}
