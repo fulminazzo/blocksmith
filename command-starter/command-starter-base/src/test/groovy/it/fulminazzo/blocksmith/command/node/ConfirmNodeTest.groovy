58eb973b3610bd25f6f72f9395aa56fbfcf0ba33
fulminazzo <fulminazzo21@gmail.com>
2026-04-26 12:22:54 +0200

    Created ConfirmNode to handle execution confirmation

---
+++ command-starter/command-starter-base/src/test/groovy/it/fulminazzo/blocksmith/command/node/ConfirmNodeTest.groovy
@@ -0,0 +1,81 @@
+package it.fulminazzo.blocksmith.command.node
+
+import it.fulminazzo.blocksmith.command.CommandMessages
+import it.fulminazzo.blocksmith.command.CommandSenderWrapper
+import it.fulminazzo.blocksmith.command.annotation.Confirm
+import it.fulminazzo.blocksmith.command.annotation.Permission
+import it.fulminazzo.blocksmith.command.node.info.CommandInfo
+import it.fulminazzo.blocksmith.command.node.info.PermissionInfo
+import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionException
+import it.fulminazzo.blocksmith.command.visitor.execution.CommandExecutionVisitor
+import it.fulminazzo.blocksmith.structure.task.PendingTaskManager
+import spock.lang.Specification
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+class ConfirmNodeTest extends Specification {
+    private final PendingTaskManager<Object> confirmationManager = new PendingTaskManager<>()
+
+    private LiteralNode parent
+    private ConfirmNode node
+
+    private CommandExecutionVisitor visitor
+
+    void setup() {
+        parent = new LiteralNode('root')
+        parent.commandInfo = new CommandInfo(
+                'command.description.root',
+                new PermissionInfo('permission', 'root', Permission.Grant.ALL)
+        )
+
+        def annotation = Mock(Confirm)
+        annotation.confirmAliases() >> ['confirm'].toArray()
+        annotation.confirmDescription() >> 'description.confirm'
+        annotation.confirmPermission() >> 'permission.confirm'
+
+        node = new ConfirmNode(annotation, parent, confirmationManager)
+
+        def sender = Mock(CommandSenderWrapper)
+        sender.idImpl >> 1
+        sender.player >> true
+
+        visitor = Mock(CommandExecutionVisitor)
+        visitor.commandSender >> sender
+    }
+
+    def 'test that normal execute works'() {
+        given:
+        def bool = new AtomicBoolean()
+
+        and:
+        confirmationManager.register(visitor.commandSender.id, 10_000, () -> bool.set(true))
+
+        when:
+        node.executor.orElseThrow().execute(parent, visitor)
+
+        then:
+        bool.get()
+    }
+
+    def 'test that execute with expired throws'() {
+        given:
+        confirmationManager.register(visitor.commandSender.id, 1, () -> {})
+
+        when:
+        node.executor.orElseThrow().execute(parent, visitor)
+
+        then:
+        def e = thrown(CommandExecutionException)
+        e.message == CommandMessages.PENDING_ACTION_EXPIRED
+    }
+
+    def 'test that execute with no task throws'() {
+        when:
+        node.executor.orElseThrow().execute(parent, visitor)
+
+        then:
+        def e = thrown(CommandExecutionException)
+        e.message == CommandMessages.NO_PENDING_ACTION
+    }
+
+}
