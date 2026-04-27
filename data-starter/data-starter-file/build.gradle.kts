1c58dd27af27ab7dbce2d25c4518317aac6f5b49
fulminazzo <fulminazzo21@gmail.com>
2026-04-28 01:13:26 +0200

    Fixed invalid dependency

--- data-starter/data-starter-file/build.gradle.kts
+++ data-starter/data-starter-file/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly(libs.slf4j)
 
-    api(projects.configStarter)
+    api(projects.configStarter.configStarterBase)
 
     testImplementation(projects.configStarter.configStarterJson)
 }
