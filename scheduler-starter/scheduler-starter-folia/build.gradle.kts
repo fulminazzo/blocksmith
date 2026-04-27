43ae40639e64a94b301fa6976157ac972f1d8287
fulminazzo <fulminazzo21@gmail.com>
2026-04-27 23:10:54 +0200

    Renamed mockbukkit to mockbukkit20 for more clearance

--- scheduler-starter/scheduler-starter-folia/build.gradle.kts
+++ scheduler-starter/scheduler-starter-folia/build.gradle.kts
@@ -1,10 +1,10 @@
 dependencies {
-    api(projects.schedulerStarter.schedulerStarterBukkit)
-
     compileOnly(libs.folia)
 
+    api(projects.schedulerStarter.schedulerStarterBukkit)
+
     testImplementation(libs.folia)
-    testImplementation(libs.mockbukkit) {
+    testImplementation(libs.mockbukkit20) {
         exclude(group = "io.papermc.paper")
         exclude(group = "net.bytebuddy")
         exclude(group = "org.junit.jupiter")
