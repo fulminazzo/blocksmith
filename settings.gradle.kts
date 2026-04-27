78e60409219dcc34e2d054c74798e20e8ac8837c
fulminazzo <fulminazzo21@gmail.com>
2026-04-28 01:24:21 +0200

    Improved repository handling with resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,20 @@ include(
 
     "base:testing"
 )
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven {
+            name = "spigotmc-repo"
+            url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        }
+        maven {
+            name = "papermc"
+            url = uri("https://repo.papermc.io/repository/maven-public/")
+        }
+    }
+
+}
