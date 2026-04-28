647d6702255aab5b5211f1c7e83089d235ce0215
fulminazzo <fulminazzo21@gmail.com>
2026-04-28 18:26:20 +0200

    Created AbstractMessageChannel with common logics

---
+++ message-broker-starter/message-broker-starter-base/src/main/java/it/fulminazzo/blocksmith/broker/AbstractMessageChannel.java
@@ -0,0 +1,73 @@
+package it.fulminazzo.blocksmith.broker;
+
+import it.fulminazzo.blocksmith.data.mapper.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+/**
+ * Abstract implementation of {@link MessageChannel} with common checks
+ * and support methods.
+ */
+@RequiredArgsConstructor
+public abstract class AbstractMessageChannel implements MessageChannel {
+    protected final @NotNull Map<UUID, Consumer<String>> messageHandlers = new ConcurrentHashMap<>();
+
+    private final @NotNull Mapper mapper;
+
+    @Override
+    public @NotNull <T> CompletableFuture<Void> send(final @NotNull T payload) {
+        return sendRaw(mapper.serialize(payload));
+    }
+
+    @Override
+    public @NotNull <T> UUID subscribe(final @NotNull Class<T> messageType, final @NotNull Consumer<T> consumer) {
+        return subscribeRaw(s -> consumer.accept(mapper.deserialize(s, messageType)));
+    }
+
+    @Override
+    public @NotNull UUID subscribeRaw(final @NotNull Consumer<String> consumer) {
+        UUID id = UUID.randomUUID();
+        messageHandlers.put(id, consumer);
+        return id;
+    }
+
+    @Override
+    public @NotNull MessageChannel unsubscribe(final @NotNull UUID id) {
+        messageHandlers.remove(id);
+        return this;
+    }
+
+    @Override
+    public @NotNull <T, R> CompletableFuture<R> sendAndReceive(final @NotNull T payload,
+                                                               final @NotNull Class<R> responseType,
+                                                               final @NotNull Duration timeout) {
+        return sendAndReceive(payload, responseType, timeout.toMillis());
+    }
+
+    @Override
+    public @NotNull <T, R> CompletableFuture<R> sendAndReceive(final @NotNull T payload,
+                                                               final @NotNull Class<R> responseType,
+                                                               final long timeout) {
+        return sendAndReceiveRaw(mapper.serialize(payload), timeout)
+                .thenApply(r -> mapper.deserialize(r, responseType));
+    }
+
+    @Override
+    public @NotNull CompletableFuture<String> sendAndReceiveRaw(final @NotNull String payload, final @NotNull Duration timeout) {
+        return sendAndReceiveRaw(payload, timeout.toMillis());
+    }
+
+    @Override
+    public @NotNull CompletableFuture<String> sendAndReceiveRaw(final @NotNull String payload, final long timeout) {
+        //TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+}
