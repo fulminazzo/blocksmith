f03b9dbf4ab1b0b4f5d01fe20c7653e06155c0be
fulminazzo <fulminazzo21@gmail.com>
2026-04-29 00:58:34 +0200

    Created static messages for testing purposes

---
+++ message-broker-starter/message-broker-starter-testing/src/main/groovy/it/fulminazzo/blocksmith/broker/Messages.groovy
@@ -0,0 +1,7 @@
+package it.fulminazzo.blocksmith.broker
+
+final class Messages {
+    public static final Message MESSAGE1 = new Message(1, System.currentTimeMillis(), 'Hello, world!')
+    public static final Message MESSAGE2 = new Message(2, System.currentTimeMillis(), 'Goodbye, mars!')
+
+}
