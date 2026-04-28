076b322ae0ab46d2ae5e761cf1836218919c139d
fulminazzo <fulminazzo21@gmail.com>
2026-04-29 00:46:55 +0200

    Created RedisMessageQueryEngine

---
+++ message-broker-starter/message-broker-starter-redis/src/main/java/it/fulminazzo/blocksmith/broker/redis/RedisMessageQueryEngine.java
@@ -0,0 +1,69 @@
+package it.fulminazzo.blocksmith.broker.redis;
+
+import io.lettuce.core.pubsub.RedisPubSubAdapter;
+import io.lettuce.core.pubsub.RedisPubSubListener;
+import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
+import it.fulminazzo.blocksmith.broker.MessageQueryEngine;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+/**
+ * A message query engine with Redis support.
+ * <br>
+ * Uses the <a href="https://lettuce.io">lettuce</a> library under the hood
+ * to leverage the speed and optimizations provided by Netty asynchronous operations.
+ *
+ */
+public final class RedisMessageQueryEngine implements MessageQueryEngine {
+    private final @NotNull Set<RedisPubSubListener<String, String>> listeners = ConcurrentHashMap.newKeySet();
+
+    private final @NotNull StatefulRedisPubSubConnection<String, String> connection;
+    private final @NotNull String channelName;
+
+    /**
+     * Instantiates a new Redis message query engine.
+     *
+     * @param connection  the connection
+     * @param channelName the channel name
+     */
+    RedisMessageQueryEngine(final @NotNull StatefulRedisPubSubConnection<String, String> connection,
+                            final @NotNull String channelName) {
+        this.connection = connection;
+        this.channelName = channelName;
+
+        connection.sync().subscribe(channelName);
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Void> publish(final @NotNull String payload) {
+        return connection.async().publish(channelName, payload)
+                .toCompletableFuture()
+                .thenApply(r -> null);
+    }
+
+    @Override
+    public void listen(final @NotNull Consumer<String> consumer) {
+        RedisPubSubAdapter<String, String> listener = new RedisPubSubAdapter<>() {
+
+            @Override
+            public void message(final @NotNull String channel,
+                                final @NotNull String message) {
+                if (channel.equals(channelName)) consumer.accept(message);
+            }
+
+        };
+        listeners.add(listener);
+        connection.addListener(listener);
+    }
+
+    @Override
+    public void close() {
+        listeners.forEach(connection::removeListener);
+        connection.sync().unsubscribe(channelName);
+    }
+
+}
