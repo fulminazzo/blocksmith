36b83c34a348080ddb518c58ffbf8577606e7da2
fulminazzo <fulminazzo21@gmail.com>
2026-04-29 01:51:01 +0200

    Updated Javadoc Fixed missing initialize call

--- message-broker-starter/message-broker-starter-redis/src/main/java/it/fulminazzo/blocksmith/broker/redis/RedisMessageChannel.java
+++ message-broker-starter/message-broker-starter-redis/src/main/java/it/fulminazzo/blocksmith/broker/redis/RedisMessageChannel.java
@@ -4,8 +4,21 @@ import it.fulminazzo.blocksmith.broker.AbstractMessageChannel;
 import it.fulminazzo.blocksmith.data.mapper.Mapper;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Implementation of {@link it.fulminazzo.blocksmith.broker.MessageChannel} for Redis databases.
+ */
 public class RedisMessageChannel extends AbstractMessageChannel<RedisMessageQueryEngine> {
-    public RedisMessageChannel(@NotNull RedisMessageQueryEngine queryEngine, @NotNull Mapper mapper) {
+
+    /**
+     * Instantiates a new Redis message channel.
+     *
+     * @param queryEngine the query engine
+     * @param mapper      the mapper
+     */
+    protected RedisMessageChannel(final @NotNull RedisMessageQueryEngine queryEngine,
+                                  final @NotNull Mapper mapper) {
         super(queryEngine, mapper);
+        initialize();
     }
+
 }
