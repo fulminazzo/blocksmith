db9158a5a70224f835959c8879f74221b2cf0f5b
fulminazzo <fulminazzo21@gmail.com>
2026-05-06 01:46:47 +0200

    Created ExpiringList interface

---
+++ base/src/main/java/it/fulminazzo/blocksmith/structure/expiring/ExpiringList.java
@@ -0,0 +1,122 @@
+package it.fulminazzo.blocksmith.structure.expiring;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * An expiring list is a special {@link List} whose elements are subject to expiration.
+ * Each can be defined with a time-to-live (TTL) period after which they will not be present anymore.
+ *
+ * @param <E> the type of the elements
+ */
+public interface ExpiringList<E> extends List<E> {
+
+    /**
+     * Adds an element in the list at the given index.
+     * All the elements after the index will be shifted to the right.
+     *
+     * @param index   the index at which to insert the element
+     * @param element the element to add
+     * @param ttl     the time-to-live (after which it will expire)
+     * @return {@code true} if the element was added, {@code false} if it was already present
+     */
+    public boolean add(final int index, final @Nullable E element, final @NotNull Duration ttl);
+
+    /**
+     * Adds an element in the list at the given index.
+     * All the elements after the index will be shifted to the right.
+     *
+     * @param index   the index at which to insert the element
+     * @param element the element to add
+     * @param ttl     the time-to-live (after which it will expire) in milliseconds
+     * @return {@code true} if the element was added, {@code false} if it was already present
+     */
+    public boolean add(final int index, final @Nullable E element, final long ttl);
+
+    /**
+     * Adds an element in the list at the given index.
+     * All the elements after the index will be shifted to the right.
+     *
+     * @param index   the index at which to insert the element
+     * @param element the element to add
+     */
+    @Override
+    public void add(final int index, final @Nullable E element);
+
+    /**
+     * Adds all the elements of the given list to the current one at the given index.
+     *
+     * @param index the index at which to insert the elements
+     * @param list  the list to take elements from
+     * @return {@code true} if the list was modified, {@code false} if it was not modified
+     */
+    public boolean addAll(final int index, final @NotNull ExpiringList<? extends E> list);
+
+    /**
+     * Adds all the elements of the given collection to the current one at the given index.
+     * Each element will have the same expiration time.
+     *
+     * @param index      the index at which to insert the elements
+     * @param collection the collection to take elements from
+     * @param ttl        the time-to-live (after which the elements will expire)
+     * @return {@code true} if the collection was modified, {@code false} if it was not modified
+     */
+    public boolean addAll(final int index, final @NotNull Collection<? extends E> collection, final @NotNull Duration ttl);
+
+    /**
+     * Adds all the elements of the given collection to the current one at the given index.
+     * Each element will have the same expiration time.
+     *
+     * @param index      the index at which to insert the elements
+     * @param collection the collection to take elements from
+     * @param ttl        the time-to-live (after which the elements will expire) in milliseconds
+     * @return {@code true} if the collection was modified, {@code false} if it was not modified
+     */
+    public boolean addAll(final int index, final @NotNull Collection<? extends E> collection, final long ttl);
+
+    /**
+     * Adds all the elements of the given collection to the current one at the given index.
+     * If the collection is not a {@link ExpiringList}, every element will have no expiration time.
+     *
+     * @param index      the index at which to insert the elements
+     * @param collection the collection to take elements from
+     * @return {@code true} if the collection was modified, {@code false} if it was not modified
+     */
+    @Override
+    public boolean addAll(final int index, final @NotNull Collection<? extends E> collection);
+
+    /**
+     * Sets the element at the given index.
+     *
+     * @param index   the index of the element to set
+     * @param element the new element
+     * @param ttl     the time-to-live (after which it will expire)
+     * @return the previous element at the given index
+     */
+    public E set(final int index, final @Nullable E element, final @NotNull Duration ttl);
+
+    /**
+     * Sets the element at the given index.
+     *
+     * @param index   the index of the element to set
+     * @param element the new element
+     * @param ttl     the time-to-live (after which it will expire) in milliseconds
+     * @return the previous element at the given index
+     */
+    public E set(final int index, final @Nullable E element, final long ttl);
+
+    /**
+     * Sets the element at the given index.
+     *
+     * @param index   the index of the element to set
+     * @param element the new element
+     * @return the previous element at the given index
+     */
+    @Override
+    public E set(final int index, final @Nullable E element);
+
+}
