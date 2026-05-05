abb949df46e6cb45dc4a93d1383d747da9c99c3b
fulminazzo <fulminazzo21@gmail.com>
2026-05-06 00:59:18 +0200

    Created MemoryMessageChannelSettings class

---
+++ message-broker-starter/message-broker-starter-memory/src/main/java/it/fulminazzo/blocksmith/broker/MemoryMessageChannelSettings.java
@@ -0,0 +1,10 @@
+package it.fulminazzo.blocksmith.broker;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true, doNotUseGetters = true)
+@ToString(callSuper = true, doNotUseGetters = true)
+public final class MemoryMessageChannelSettings extends MessageChannelSettings<MemoryMessageChannelSettings> {
+
+}
