8f097ca9dff7b149c25e966a84a7ecd6b998ae86
fulminazzo <fulminazzo21@gmail.com>
2026-05-07 00:06:37 +0200

    Updated message-broker-starter to be dependent on plugin-messaging-base rather than main module

--- message-broker-starter/build.gradle.kts
+++ message-broker-starter/build.gradle.kts
@@ -1,1 +1,11 @@
 plugins { id("blocksmith.composite-module") }
+
+dependencies {
+    api(project.projects.messageBrokerStarter.pluginMessaging.pluginMessagingBase)
+}
+
+compositeModule {
+    excludedSubmodules = setOf(
+        project.projects.messageBrokerStarter.pluginMessaging.name
+    )
+}
