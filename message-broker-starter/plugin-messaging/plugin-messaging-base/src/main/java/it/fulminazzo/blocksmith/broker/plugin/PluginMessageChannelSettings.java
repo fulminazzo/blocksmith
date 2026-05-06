d4f8466a5974dac726081511c011300f3e772eef
fulminazzo <fulminazzo21@gmail.com>
2026-05-07 01:43:24 +0200

    Created PluginMessageChannelSettings

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/main/java/it/fulminazzo/blocksmith/broker/plugin/PluginMessageChannelSettings.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.blocksmith.broker.plugin;
+
+import it.fulminazzo.blocksmith.broker.MessageChannelSettings;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true, doNotUseGetters = true)
+@ToString(callSuper = true, doNotUseGetters = true)
+public final class PluginMessageChannelSettings extends MessageChannelSettings<PluginMessageChannelSettings> {
+
+}
