a448d508575c12601d7b1a4fd782592544579cc2
fulminazzo <fulminazzo21@gmail.com>
2026-05-09 00:53:06 +0200

    Added unit tests for FieldsOrderCheck

---
+++ checkstyle-extension/src/test/groovy/it/fulminazzo/blocksmith/checker/FieldsOrderCheckTest.groovy
@@ -0,0 +1,88 @@
+package it.fulminazzo.blocksmith.checker
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST
+import com.puppycrawl.tools.checkstyle.api.FileContents
+import com.puppycrawl.tools.checkstyle.api.FileText
+import com.puppycrawl.tools.checkstyle.api.TokenTypes
+import it.fulminazzo.blocksmith.reflect.Reflect
+import spock.lang.Specification
+
+class FieldsOrderCheckTest extends Specification {
+    private NodeValidator validator
+    private FieldsOrderCheck check
+
+    private DetailAST node = Mock(DetailAST)
+
+    void setup() {
+        check = Mock(FieldsOrderCheck)
+        check.setFileContents(new FileContents(new FileText(
+                File.createTempFile('TestClass', 'java'),
+                ['class TestClass {}']
+        )))
+        check.visitToken(_) >> {
+            callRealMethod()
+        }
+
+        def reflect = Reflect.on(check)
+        validator = Mock(NodeValidator)
+        reflect.set('validator', validator)
+
+        def parent = Mock(DetailAST)
+        node.parent >> parent
+    }
+
+    def 'test that beginTree resets validator'() {
+        given:
+        check.beginTree(_) >> {
+            callRealMethod()
+        }
+
+        when:
+        check.beginTree(node)
+
+        then:
+        1 * validator.reset()
+    }
+
+    def 'test that visitToken does not throw on ValidationException'() {
+        given:
+        node.parent.type >> TokenTypes.OBJBLOCK
+        node.lineNo >> 1
+        node.columnNo >> 2
+
+        and:
+        validator.validateNode(_) >> {
+            throw new ValidationException('exception.message')
+        }
+
+        when:
+        check.visitToken(node)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        1 * check.log(node, "${FieldsOrderCheckTest.packageName}.exception.message")
+    }
+
+    def 'test that visitToken works only if parent is OBJBLOCK'() {
+        when:
+        node.parent.type >> TokenTypes.TEXT_BLOCK_CONTENT
+
+        and:
+        check.visitToken(node)
+
+        then:
+        0 * validator.validateNode(node)
+
+        when:
+        node.parent.type >> TokenTypes.OBJBLOCK
+
+        and:
+        check.visitToken(node)
+
+        then:
+        1 * validator.validateNode(node)
+    }
+
+}
