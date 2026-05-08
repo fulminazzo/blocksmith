94f2d4af61a50df123b1ac29046c96ac1b1bc859
fulminazzo <fulminazzo21@gmail.com>
2026-05-09 00:35:25 +0200

    Created NodeUtils with isModifierPresent utility function

---
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checker/validator/NodeUtils.java
@@ -0,0 +1,29 @@
+package it.fulminazzo.blocksmith.checker.validator;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A collection of utilities for {@link DetailAST}.
+ */
+public final class NodeUtils {
+
+    /**
+     * Checks if a modifier has been declared in the given node.
+     *
+     * @param node     the node to check
+     * @param modifier the modifier
+     * @return {@code true} if the modifier is present, {@code false} otherwise
+     */
+    public static boolean isModifierPresent(final @NotNull DetailAST node, final int modifier) {
+        DetailAST modifiers = node.findFirstToken(TokenTypes.MODIFIERS);
+        if (modifiers == null) return false;
+
+        for (DetailAST child = modifiers.getFirstChild(); child != null; child = child.getNextSibling()) {
+            if (child.getType() == modifier) return true;
+        }
+        return false;
+    }
+
+}
---
+++ checkstyle-extension/src/test/groovy/it/fulminazzo/blocksmith/checker/validator/NodeUtilsTest.groovy
@@ -0,0 +1,45 @@
+package it.fulminazzo.blocksmith.checker.validator
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST
+import com.puppycrawl.tools.checkstyle.api.TokenTypes
+import spock.lang.Specification
+
+class NodeUtilsTest extends Specification {
+
+    def 'test that isModifierPresent works'() {
+        given:
+        def first = Mock(DetailAST)
+        first.type >> TokenTypes.LITERAL_STATIC
+
+        def second = Mock(DetailAST)
+        second.type >> TokenTypes.FINAL
+        first.nextSibling >> second
+
+        def third = Mock(DetailAST)
+        third.type >> TokenTypes.LITERAL_PUBLIC
+        second.nextSibling >> third
+
+        and:
+        def modifiers = Mock(DetailAST)
+        modifiers.firstChild >> first
+
+        and:
+        def node = Mock(DetailAST)
+        node.findFirstToken(TokenTypes.MODIFIERS) >> modifiers
+
+        expect:
+        NodeUtils.isModifierPresent(node, TokenTypes.LITERAL_STATIC)
+        NodeUtils.isModifierPresent(node, TokenTypes.FINAL)
+        NodeUtils.isModifierPresent(node, TokenTypes.LITERAL_PUBLIC)
+        !NodeUtils.isModifierPresent(node, TokenTypes.LITERAL_TRANSIENT)
+    }
+
+    def 'test that isModifierPresent of no modifiers returns false'() {
+        given:
+        def node = Mock(DetailAST)
+
+        expect:
+        !NodeUtils.isModifierPresent(node, TokenTypes.LITERAL_PUBLIC)
+    }
+
+}
