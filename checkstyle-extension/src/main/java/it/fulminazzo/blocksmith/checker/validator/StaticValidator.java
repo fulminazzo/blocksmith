b700afdafc1a3e4a7ac01023364e76e5debc6ca4
fulminazzo <fulminazzo21@gmail.com>
2026-05-08 20:17:11 +0200

    Created StaticValidator for validating based on static modifier

---
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checker/validator/StaticValidator.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.blocksmith.checker.validator;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * {@link RankerValidator} implementation for validating the {@code static} keyword.
+ *
+ * @see RankerValidator
+ */
+public enum StaticValidator implements RankerValidator {
+    /**
+     * {@code static} validator.
+     */
+    STATIC {
+        @Override
+        public boolean validate(final @NotNull DetailAST node) {
+            return node.findFirstToken(TokenTypes.LITERAL_STATIC) != null;
+        }
+    },
+    /**
+     * Non-{@code static} validator.
+     */
+    NON_STATIC {
+        @Override
+        public boolean validate(final @NotNull DetailAST node) {
+            return node.findFirstToken(TokenTypes.LITERAL_STATIC) == null;
+        }
+    }
+
+}
