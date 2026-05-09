e6e78933b0e5c38aa333bc68e769b756ad08f366
fulminazzo <fulminazzo21@gmail.com>
2026-05-09 20:46:54 +0200

    Created OverloadValidator

---
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/validator/OverloadValidator.java
@@ -0,0 +1,81 @@
+package it.fulminazzo.blocksmith.checkstyle.validator;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * An object to validate executable nodes for their overload ordering.
+ *
+ * @see Executable
+ */
+public final class OverloadValidator implements Validator {
+    private final @NotNull Deque<Scope> scopes = new ArrayDeque<>();
+    private @Nullable Validator next;
+
+    @SuppressWarnings("DataFlowIssue")
+    private @NotNull Scope getLastScope() {
+        if (scopes.isEmpty()) enterScope();
+        return scopes.peek();
+    }
+
+    @Override
+    public void validateNode(final @NotNull DetailAST node) throws ValidationException {
+        Executable executable = Executable.of(node);
+        Scope lastScope = getLastScope();
+        Executable lastExecutable = lastScope.getLastExecutable();
+        if (lastExecutable != null && executable.compareTo(lastExecutable) < 0)
+            throw new ValidationException(node, "overload");
+        lastScope.setLastExecutable(executable);
+        lastScope.register(node);
+    }
+
+    @Override
+    public @NotNull OverloadValidator then(final @NotNull Validator validator) {
+        this.next = validator;
+        return this;
+    }
+
+    @Override
+    public void enterScope() {
+        scopes.push(new Scope());
+    }
+
+    @Override
+    public void exitScope() throws ValidationException {
+        if (!scopes.isEmpty()) {
+            Scope scope = scopes.pop();
+            if (next == null) return;
+            for (DetailAST node : scope.getNodes()) next.validateNode(node);
+        }
+    }
+
+    /**
+     * Represents a scope for {@link OverloadValidator}.
+     */
+    private static final class Scope {
+        @Getter
+        private final @NotNull List<DetailAST> nodes = new ArrayList<>();
+        @Getter
+        @Setter
+        public @Nullable Executable lastExecutable;
+
+        /**
+         * Registers a node.
+         *
+         * @param node the node
+         */
+        public void register(final @NotNull DetailAST node) {
+            nodes.add(node);
+        }
+
+    }
+
+}
---
+++ checkstyle-extension/src/test/groovy/it/fulminazzo/blocksmith/checkstyle/validator/OverloadValidatorTest.groovy
@@ -0,0 +1,104 @@
+package it.fulminazzo.blocksmith.checkstyle.validator
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST
+import org.mockito.Mockito
+import spock.lang.Specification
+
+@SuppressWarnings('GroovyAccessibility')
+class OverloadValidatorTest extends Specification {
+    private static final Executable NO_PARAMETERS = new Executable('method', [])
+    private static final Executable PRIMITIVE_PARAMETER = new Executable('method', [new Executable.Parameter('int', 'a', true)])
+    private static final Executable WRAPPER_PARAMETER = new Executable('method', [new Executable.Parameter('Integer', 'b', false)])
+
+    private final DetailAST noParametersNode = Mock(DetailAST)
+    private final DetailAST primitiveParameterNode = Mock(DetailAST)
+    private final DetailAST wrapperParameterNode = Mock(DetailAST)
+
+    private final OverloadValidator validator = new OverloadValidator()
+
+    private def executableMock = Mockito.mockStatic(Executable)
+
+    void setup() {
+        executableMock.when { Executable.of(noParametersNode) }.thenReturn(NO_PARAMETERS)
+        executableMock.when { Executable.of(primitiveParameterNode) }.thenReturn(PRIMITIVE_PARAMETER)
+        executableMock.when { Executable.of(wrapperParameterNode) }.thenReturn(WRAPPER_PARAMETER)
+    }
+
+    void cleanup() {
+        executableMock?.close()
+    }
+
+    def 'test that validateNode does not throw and updates last lastExecutable on valid node'() {
+        when:
+        validator.validateNode(noParametersNode)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        validator.lastScope.lastExecutable == NO_PARAMETERS
+
+        when:
+        validator.validateNode(primitiveParameterNode)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        validator.lastScope.lastExecutable == PRIMITIVE_PARAMETER
+
+        when:
+        validator.validateNode(wrapperParameterNode)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        validator.lastScope.lastExecutable == WRAPPER_PARAMETER
+    }
+
+    def 'test that validateNode when lastExecutable is #lastExecutable throws for node #node'() {
+        given:
+        validator.lastScope.lastExecutable = OverloadValidatorTest."$lastExecutable"
+
+        when:
+        validator.validateNode(this."$node")
+
+        then:
+        thrown(ValidationException)
+
+        where:
+        lastExecutable        | node
+        // primitive parameter
+        'PRIMITIVE_PARAMETER' | 'noParametersNode'
+        // wrapper parameter
+        'WRAPPER_PARAMETER'   | 'noParametersNode'
+        'WRAPPER_PARAMETER'   | 'primitiveParameterNode'
+    }
+
+    def 'test that exitScope throws if nodes are incorrect'() {
+        given:
+        def next = Mock(Validator)
+        next.validateNode(_) >> { a ->
+            throw new ValidationException(a[0], 'mock.exception')
+        }
+
+        and:
+        validator.then(next)
+
+        when:
+        validator.validateNode(noParametersNode)
+
+        then:
+        noExceptionThrown()
+
+        when:
+        validator.exitScope()
+
+        then:
+        def e = thrown(ValidationException)
+        e.node == noParametersNode
+        e.message == 'it.fulminazzo.blocksmith.checkstyle.mock.exception'
+    }
+
+}
