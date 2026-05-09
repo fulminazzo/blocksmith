893458342e0e7362c8d43c2f162d7366d29373f4
fulminazzo <fulminazzo21@gmail.com>
2026-05-10 00:52:29 +0200

    Extracted common validator logic

--- checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/validator/OverloadValidator.java
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/validator/OverloadValidator.java
@@ -6,85 +6,35 @@ import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-
 /**
  * An object to validate executable nodes for their overload ordering.
  *
  * @see Executable
  */
-public final class OverloadValidator implements Validator {
-    private final @NotNull Deque<Scope> scopes = new ArrayDeque<>();
-    private @Nullable Validator next;
-
-    @SuppressWarnings("DataFlowIssue")
-    private @NotNull Scope getLastScope() {
-        if (scopes.isEmpty()) enterScope();
-        return scopes.peek();
-    }
+public final class OverloadValidator extends AbstractValidator<OverloadValidator.Scope, OverloadValidator> {
 
     @Override
-    public void validateNode(final @NotNull DetailAST node) throws ValidationException {
+    public void validateNodeImpl(final @NotNull DetailAST node) throws ValidationException {
         Executable executable = Executable.of(node);
         Scope lastScope = getLastScope();
         Executable lastExecutable = lastScope.getLastExecutable();
         if (lastExecutable != null && executable.compareTo(lastExecutable) < 0)
             throw new ValidationException(node, "executable.overload");
         lastScope.setLastExecutable(executable);
-        lastScope.register(node);
     }
 
     @Override
-    public @NotNull OverloadValidator then(final @NotNull Validator validator) {
-        this.next = validator;
-        return this;
-    }
-
-    @Override
-    public void enterScope() {
-        scopes.push(new Scope());
-    }
-
-    @Override
-    public void exitScope() throws CompositeValidationException {
-        if (!scopes.isEmpty()) {
-            Scope scope = scopes.pop();
-            if (next == null) return;
-            next.enterScope();
-            List<ValidationException> exceptions = new ArrayList<>();
-            for (DetailAST node : scope.getNodes()) {
-                try {
-                    next.validateNode(node);
-                } catch (ValidationException e) {
-                    exceptions.add(e);
-                }
-            }
-            next.exitScope();
-            if (!exceptions.isEmpty()) throw new CompositeValidationException(exceptions);
-        }
+    protected @NotNull Scope newScope() {
+        return new Scope();
     }
 
     /**
      * Represents a scope for {@link OverloadValidator}.
      */
-    private static final class Scope {
-        @Getter
-        private final @NotNull List<DetailAST> nodes = new ArrayList<>();
+    protected static final class Scope extends AbstractValidator.Scope {
         @Getter
         @Setter
         public @Nullable Executable lastExecutable;
-
-        /**
-         * Registers a node.
-         *
-         * @param node the node
-         */
-        public void register(final @NotNull DetailAST node) {
-            nodes.add(node);
-        }
 
     }
 
