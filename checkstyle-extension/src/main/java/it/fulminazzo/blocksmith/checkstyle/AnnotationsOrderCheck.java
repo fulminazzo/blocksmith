95d10d8478b5721462dba0022cb16638454d2ab4
fulminazzo <fulminazzo21@gmail.com>
2026-05-10 14:28:28 +0200

    Updated AnnotationsOrderCheck to handle annotations as independent nodes

--- checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/AnnotationsOrderCheck.java
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/AnnotationsOrderCheck.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.blocksmith.checkstyle;
 
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import it.fulminazzo.blocksmith.checkstyle.validator.OrderValidator;
 import it.fulminazzo.blocksmith.checkstyle.validator.criterion.AnnotationCriterion;
@@ -23,10 +24,16 @@ public final class AnnotationsOrderCheck extends ValidatorCheck {
     @Override
     protected @NotNull List<Integer> getTokens() {
         return List.of(
+                TokenTypes.ANNOTATION,
                 TokenTypes.VARIABLE_DEF,
                 TokenTypes.METHOD_DEF, TokenTypes.CTOR_DEF,
                 TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF, TokenTypes.ENUM_DEF, TokenTypes.RECORD_DEF
         );
+    }
+
+    @Override
+    protected boolean isScopeChanged(final @NotNull DetailAST node) {
+        return node.getType() != TokenTypes.ANNOTATION;
     }
 
 }
