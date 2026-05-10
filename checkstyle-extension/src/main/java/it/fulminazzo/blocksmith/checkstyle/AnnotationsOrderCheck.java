c4f7d73b7841e972c07416dba3669e728e97021e
fulminazzo <fulminazzo21@gmail.com>
2026-05-10 14:17:56 +0200

    Created AnnotationsOrderCheck for annotation ordering

---
+++ checkstyle-extension/src/main/java/it/fulminazzo/blocksmith/checkstyle/AnnotationsOrderCheck.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.blocksmith.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import it.fulminazzo.blocksmith.checkstyle.validator.OrderValidator;
+import it.fulminazzo.blocksmith.checkstyle.validator.criterion.AnnotationCriterion;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Validates the order of annotations.
+ * Check {@link AnnotationCriterion} to see the necessary order.
+ */
+public final class AnnotationsOrderCheck extends ValidatorCheck {
+
+    /**
+     * Instantiates a new Annotations order check.
+     */
+    public AnnotationsOrderCheck() {
+        super(new OrderValidator(AnnotationCriterion.values()));
+    }
+
+    @Override
+    protected @NotNull List<Integer> getTokens() {
+        return List.of(
+                TokenTypes.VARIABLE_DEF,
+                TokenTypes.METHOD_DEF, TokenTypes.CTOR_DEF,
+                TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF, TokenTypes.ENUM_DEF, TokenTypes.RECORD_DEF
+        );
+    }
+
+}
