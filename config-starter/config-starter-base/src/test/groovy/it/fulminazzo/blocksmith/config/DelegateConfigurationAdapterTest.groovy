945187f67591cf7e8bbd04755aef2fb3e7ce042c
fulminazzo <fulminazzo21@gmail.com>
2026-05-13 01:20:06 +0200

    Added more tests for DelegateConfiguration

--- config-starter/config-starter-base/src/test/groovy/it/fulminazzo/blocksmith/config/DelegateConfigurationAdapterTest.groovy
+++ config-starter/config-starter-base/src/test/groovy/it/fulminazzo/blocksmith/config/DelegateConfigurationAdapterTest.groovy
@@ -24,14 +24,41 @@ class DelegateConfigurationAdapterTest extends Specification {
         }
 
         where:
-        method             || arguments
-        'loadComments'     || [new ByteArrayInputStream(''.bytes)]
-        'load'             || ['', MockConfig]
-        'load'             || [new File('build/resources/test/load.json'), MockConfig]
-        'load'             || [new ByteArrayInputStream(''.bytes), MockConfig]
-        'serialize'        || [new MockConfig()]
-        'store'            || [new ByteArrayOutputStream(), new MockConfig()]
-        'store'            || [new File('build/resources/test/store.json'), new MockConfig()]
+        method         || arguments
+        'loadComments' || [new ByteArrayInputStream(''.bytes)]
+        'load'         || ['', MockConfig]
+        'load'         || [new File('build/resources/test/load.json'), MockConfig]
+        'load'         || [new ByteArrayInputStream(''.bytes), MockConfig]
+        'serialize'    || [new MockConfig()]
+        'store'        || [new ByteArrayOutputStream(), new MockConfig()]
+        'store'        || [new File('build/resources/test/store.json'), new MockConfig()]
+    }
+
+    def 'test that #method with #arguments delegates to #delegateMethod with #delegateArguments'() {
+        given:
+        def delegate = Mock(BaseConfigurationAdapter)
+
+        and:
+        def adapter = new DelegateConfigurationAdapter(log)
+        adapter.format = Mock(ConfigurationFormat) {
+            it.getFile(_, _) >> { a ->
+                return new File(a[0], a[1])
+            }
+        }
+        adapter.delegate = delegate
+
+        when:
+        adapter."$method"(*arguments)
+
+        then:
+        1 * delegate."$delegateMethod"(*_) >> { a ->
+            assert a == delegateArguments
+        }
+
+        where:
+        method  | arguments                                                   || delegateMethod | delegateArguments
+        'load' || [new File('build/resources/test'), 'load.json', MockConfig] || 'load'         | [new File('build/resources/test/load.json'), MockConfig]
+        'store' || [new File('build/resources/test'), 'store.json', MockConfig] || 'store'         | [new File('build/resources/test/store.json'), MockConfig]
     }
 
     def 'test that getFormat throws if not initialized'() {
