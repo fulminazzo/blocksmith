26b9e1dd4b4cd23ebba2e392c8625fad239d2071
fulminazzo <fulminazzo21@gmail.com>
2026-05-14 00:09:52 +0200

    Added test containers MongoDB dependency

--- data-starter/data-starter-mongo/build.gradle.kts
+++ data-starter/data-starter-mongo/build.gradle.kts
@@ -1,6 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
 dependencies {
     implementation(platform(libs.mongodb.driver.platform))
 
     api(libs.mongodb.driver.async)
     api(libs.reactor)
+}
+
+testing {
+    suites {
+        withType<JvmTestSuite> {
+            dependencies {
+                implementation(libs.test.containers.mongodb)
+            }
+        }
+    }
 }
