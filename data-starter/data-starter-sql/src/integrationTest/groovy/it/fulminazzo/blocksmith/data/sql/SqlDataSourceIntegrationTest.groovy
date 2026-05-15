d46a58c0222ea042dacd4e6616cd214f07e8d06f
fulminazzo <fulminazzo21@gmail.com>
2026-05-15 17:55:58 +0200

    Re-introduced and updated tests for init execute script

--- data-starter/data-starter-sql/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/sql/SqlDataSourceIntegrationTest.groovy
+++ data-starter/data-starter-sql/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/sql/SqlDataSourceIntegrationTest.groovy
@@ -31,6 +31,40 @@ abstract class SqlDataSourceIntegrationTest extends DataSourceIntegrationTest<Sq
         builder.SQLDialect == testHelper.dialect
     }
 
+    def 'test #methodName of #argument correctly updates database'() {
+        given:
+        final context = testHelper.context
+
+        and:
+        def dataSource = newDataSourceBuilder().build()
+
+        when:
+        dataSource."$methodName"(argument)
+
+        then:
+        noExceptionThrown()
+
+        when:
+        def results = context.selectFrom('logins').fetchMany()
+
+        then:
+        results.size() == 1
+
+        and:
+        def result = results[0]
+        result.getValue(0, 'name') == 'Alex'
+        result.getValue(0, 'count') == 3
+
+        cleanup:
+        context.dropTableIfExists('logins').execute()
+
+        where:
+        methodName                  | argument
+        'executeScriptFromFile'     | 'build/resources/integrationTest/schema.sql'
+        'executeScriptFromFile'     | new File('build/resources/integrationTest/schema.sql')
+        'executeScriptFromResource' | '/schema.sql'
+    }
+
     protected abstract <B extends ASqlDataSourceBuilder<B>> B newDataSourceBuilderImpl()
 
     @Override
