5b61e369497053279eda05ce9a2ea64d362e5116
fulminazzo <fulminazzo21@gmail.com>
2026-05-16 15:19:31 +0200

    Replaced CachedRepositoryBuilder tests with more comprehensive integration tests

--- data-starter/data-starter-cache/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
+++ data-starter/data-starter-cache/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
@@ -6,8 +6,16 @@ import it.fulminazzo.blocksmith.data.User
 import it.fulminazzo.blocksmith.data.Users
 import it.fulminazzo.blocksmith.data.cache.helper.CachedIntegrationTestHelper
 import it.fulminazzo.blocksmith.data.entity.EntityMapper
+import it.fulminazzo.blocksmith.data.memory.MemoryDataSource
+import it.fulminazzo.blocksmith.data.memory.MemoryRepository
+import it.fulminazzo.blocksmith.data.memory.MemoryRepositorySettings
+import it.fulminazzo.blocksmith.data.redis.RedisRepository
+import it.fulminazzo.blocksmith.data.redis.RedisRepositorySettings
+import it.fulminazzo.blocksmith.data.sql.SqlRepository
 import org.jetbrains.annotations.NotNull
 import spock.lang.Shared
+
+import java.time.Duration
 
 class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> {
     private static final CachedIntegrationTestHelper TEST_HELPER = new CachedIntegrationTestHelper()
@@ -39,6 +47,44 @@ class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> 
     void setupRepository() {
         super.setupRepository()
         cache.delete(Users.SAVED2.id).join()
+    }
+
+    def 'test that wrap allows creation of a cached repository wrapping the original'() {
+        given:
+        def memoryDataSource = MemoryDataSource.create(TEST_HELPER.executor)
+
+        when:
+        def repository = CachedRepository.wrap(base)
+                ."$entityMapperMethod"(*entityMapperArguments)
+                .cacheRepository(
+                        TEST_HELPER.cacheDataSource,
+                        new RedisRepositorySettings()
+                                .withDatabaseName('test')
+                                .withCollectionName('users')
+                                .withTtl(Duration.ofMinutes(1L))
+                )
+                .hybrid(
+                        memoryDataSource,
+                        new MemoryRepositorySettings()
+                                .withTtl(Duration.ofSeconds(5L))
+                )
+
+        then:
+        repository != null
+
+        and:
+        MemoryRepository.isInstance(repository.cacheRepository)
+
+        and:
+        def baseRepository = repository.repository
+        CachedRepository.isInstance(baseRepository)
+        RedisRepository.isInstance(baseRepository.cacheRepository)
+        SqlRepository.isInstance(baseRepository.repository)
+
+        where:
+        entityMapperMethod | entityMapperArguments
+        'entityType'       | [User]
+        'entityMapper'     | [EntityMapper.create(User)]
     }
 
     @Override
