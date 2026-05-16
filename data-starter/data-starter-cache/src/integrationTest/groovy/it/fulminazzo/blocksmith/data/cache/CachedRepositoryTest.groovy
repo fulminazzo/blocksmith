c51a173cb16d958fe39e3791d41a693dbcc68d94
fulminazzo <fulminazzo21@gmail.com>
2026-05-16 15:31:10 +0200

    Added execution time tests for hybrid cached repository

--- data-starter/data-starter-cache/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
+++ data-starter/data-starter-cache/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/cache/CachedRepositoryTest.groovy
@@ -13,6 +13,7 @@ import it.fulminazzo.blocksmith.data.redis.RedisRepository
 import it.fulminazzo.blocksmith.data.redis.RedisRepositorySettings
 import it.fulminazzo.blocksmith.data.sql.SqlRepository
 import org.jetbrains.annotations.NotNull
+import org.spockframework.util.Pair
 import spock.lang.Shared
 
 import java.time.Duration
@@ -87,6 +88,74 @@ class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> 
         'entityMapper'     | [EntityMapper.create(User)]
     }
 
+    def 'test that fetch from hybrid repository is quicker for higher cache layer'() {
+        given:
+        final entity = new User(10L, 'Smith', 24)
+        def entityId = entity.id
+        final cacheTtl = 4L
+
+        and:
+        def memoryDataSource = MemoryDataSource.create(TEST_HELPER.executor)
+
+        and:
+        def repository = CachedRepository.wrap(base)
+                .entityType(User)
+                .cacheRepository(
+                        TEST_HELPER.cacheDataSource,
+                        new RedisRepositorySettings()
+                                .withDatabaseName('test')
+                                .withCollectionName('users')
+                                .withTtl(Duration.ofSeconds(cacheTtl))
+                )
+                .hybrid(
+                        memoryDataSource,
+                        new MemoryRepositorySettings()
+                                .withTtl(Duration.ofSeconds((long) (cacheTtl / 2)))
+                ) as Repository<User, Long>
+
+        when:
+        repository.save(entity).join()
+
+        then:
+        noExceptionThrown()
+
+        when: 'Fetch from first layer: in-memory repository'
+        def memory = timed { repository.findById(entityId).join() }
+
+        then:
+        def result1 = memory.first()
+        result1.present
+        result1.get() == entity
+
+        when: 'Fetch from second layer: Redis repository'
+        sleep((long) (cacheTtl / 2 * 1_000))
+        def redis = timed { repository.findById(entityId).join() }
+
+        then:
+        def result2 = redis.first()
+        result2.present
+        result2.get() == entity
+
+        and: 'in-memory lookup should be faster than Redis'
+        memory.second() <= redis.second()
+
+        when: 'Fetch from third layer: SQL repository'
+        sleep((long) (cacheTtl + cacheTtl / 2) * 1_000)
+        def sql = timed { repository.findById(entityId).join() }
+
+        then:
+        def result3 = sql.first()
+        result3.present
+        result3.get() == entity
+
+        and: 'Redis lookup should be faster than SQL'
+        redis.second() <= sql.second()
+
+        cleanup:
+        base.delete(entityId).join()
+        cache.delete(entityId).join()
+    }
+
     @Override
     CachedRepository<User, Long> initializeRepository() {
         return new CachedRepository<User, Long>(
@@ -111,6 +180,17 @@ class CachedRepositoryTest extends RepositoryTest<CachedRepository<User, Long>> 
     void remove(final @NotNull Long id) {
         cache.delete(id).get()
         base.delete(id).get()
+    }
+
+    private static <E> Pair<E, Long> timed(final Closure<E> function) {
+        def start = now
+        def e = function()
+        def end = now
+        return Pair.of(e, end - start)
+    }
+
+    private static long getNow() {
+        return System.currentTimeMillis()
     }
 
 }
