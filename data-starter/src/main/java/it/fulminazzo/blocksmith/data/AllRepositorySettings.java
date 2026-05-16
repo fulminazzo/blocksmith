198008795a8c35ff9439c5bd84ac5ba6cbed8148
fulminazzo <fulminazzo21@gmail.com>
2026-05-16 17:10:42 +0200

    Updated AllRepositorySettings Javadoc and style

--- data-starter/src/main/java/it/fulminazzo/blocksmith/data/AllRepositorySettings.java
+++ data-starter/src/main/java/it/fulminazzo/blocksmith/data/AllRepositorySettings.java
@@ -2,6 +2,8 @@ package it.fulminazzo.blocksmith.data;
 
 import it.fulminazzo.blocksmith.data.cache.CachedDataSource;
 import it.fulminazzo.blocksmith.data.cache.CachedRepositorySettings;
+import it.fulminazzo.blocksmith.data.config.DataSourceConfig;
+import it.fulminazzo.blocksmith.data.config.DataSourceFactories;
 import it.fulminazzo.blocksmith.data.file.FileDataSource;
 import it.fulminazzo.blocksmith.data.file.FileRepositorySettings;
 import it.fulminazzo.blocksmith.data.memory.MemoryDataSource;
@@ -19,12 +21,52 @@ import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * This is a special class to encapsulate all the {@link RepositorySettings} implementations.
+ * This is a special class encapsulating all {@link RepositorySettings} implementations.
+ * The correct settings are then picked up according to the provided {@link RepositoryDataSource}.
  * <br>
- * Then, only the interested repository, according to the Datasource, is taken.
+ * This ensures a Write-Once-Run-Everywhere approach and gives the developer the possibility to
+ * integrate with any data source provided.
+ * <br>
+ * Imagine this scenario: the developer loads a {@link DataSourceConfig} provided from the user
+ * (via a configuration file). It is impossible to know prior the type of {@link RepositoryDataSource}
+ * that will be created (unless only one {@code data-starter} module is being used).
+ * Therefore, this class provides support for all possible combinations and implementations,
+ * so that the developer does not have to worry about the underlying database and can deal with the
+ * {@code data-starter} API with ease.
+ * <br>
+ * Example:
+ * <pre>{@code
+ * final MemoryRepositorySettings memoryRepositorySettings = ...;
+ * final FileRepositorySettings fileRepositorySettings = ...;
+ * final SqlRepositorySettings sqlRepositorySettings = ...;
+ * final RedisRepositorySettings redisRepositorySettings = ...;
+ * final MongoRepositorySettings mongoRepositorySettings = ...;
+ *
+ * final AllRepositorySettings repositorySettings = new AllRepositorySettings(
+ *         memoryRepositorySettings,
+ *         fileRepositorySettings,
+ *         sqlRepositorySettings,
+ *         redisRepositorySettings,
+ *         mongoRepositorySettings
+ * );
+ *
+ * DataSourceConfig dataSourceConfig = ...; // loaded from a configuration provider
+ * RepositoryDataSource<RepositorySettings> dataSource = DataSourceFactories.build(dataSourceConfig);
+ *
+ * // We don't care what the data source settings type are, they are fetched automatically
+ * Repository<?, ?> repository = dataSource.newRepository(
+ *         EntityMapper.create(User.class),
+ *         repositorySettings.getRepositorySettings(dataSource)
+ * );
+ * }</pre>
+ *
+ * @see RepositorySettings
+ * @see RepositoryDataSource
+ * @see DataSourceConfig
+ * @see DataSourceFactories
  */
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class AllRepositorySettings {
 
     @NotNull MemoryRepositorySettings memory;
@@ -50,19 +92,22 @@ public final class AllRepositorySettings {
         else if (dataSource instanceof RedisDataSource) return redis;
         else if (dataSource instanceof MongoDataSource) return mongo;
         else if (dataSource instanceof CachedDataSource) return getCachedRepositorySettings(dataSource);
-        else throw new IllegalArgumentException("Unsupported data source type: " + dataSource.getClass().getCanonicalName());
+        else throw new IllegalArgumentException("Unsupported data source type: "
+                    + dataSource.getClass().getCanonicalName());
     }
 
     @SuppressWarnings("unchecked")
     private <
-            CS extends CacheRepositorySettings<CS>,
+            C extends CacheRepositorySettings<C>,
             S extends RepositorySettings
-            > @NotNull CachedRepositorySettings<CS, S> getCachedRepositorySettings(final @NotNull RepositoryDataSource<?> dataSource) {
+            > @NotNull CachedRepositorySettings<C, S> getCachedRepositorySettings(
+            final @NotNull RepositoryDataSource<?> dataSource
+    ) {
         Reflect reflect = Reflect.on(dataSource);
         RepositoryDataSource<?> cacheRepositoryDataSource = reflect.get("cacheRepositoryDataSource").get();
         RepositoryDataSource<?> repositoryDataSource = reflect.get("repositoryDataSource").get();
         return CachedRepositorySettings.combine(
-                (CS) getRepositorySettings(cacheRepositoryDataSource),
+                (C) getRepositorySettings(cacheRepositoryDataSource),
                 (S) getRepositorySettings(repositoryDataSource)
         );
     }
