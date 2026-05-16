a69b083655d364941fd008228a871ed346ec8002
fulminazzo <fulminazzo21@gmail.com>
2026-05-16 20:42:27 +0200

    Added more tests for memory TTL save-find cycle

--- data-starter/data-starter-memory/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/memory/MemoryDataSourceIntegrationTest.groovy
+++ data-starter/data-starter-memory/src/integrationTest/groovy/it/fulminazzo/blocksmith/data/memory/MemoryDataSourceIntegrationTest.groovy
@@ -1,6 +1,7 @@
 package it.fulminazzo.blocksmith.data.memory
 
 import it.fulminazzo.blocksmith.data.User
+import it.fulminazzo.blocksmith.data.Users
 import spock.lang.Specification
 
 import java.time.Duration
@@ -16,7 +17,7 @@ class MemoryDataSourceIntegrationTest extends Specification {
                 User,
                 new MemoryRepositorySettings()
                         .withTtl(Duration.ofSeconds(1))
-                        .withExpirationStrategy(MemoryRepositorySettings.ExpiryStrategy.SCHEDULED)
+                        .withExpirationStrategy(MemoryRepositorySettings.ExpiryStrategy.LAZY)
         )
 
         then:
@@ -61,6 +62,45 @@ class MemoryDataSourceIntegrationTest extends Specification {
 
         then:
         dataSource.executor.shutdown
+    }
+
+    def 'test expiry save-find cycle with #expirationStrategy'() {
+        given:
+        final ttl = 2_000L
+        final entity = new User(Users.SAVED1.id, Users.SAVED1.username + '_', Users.SAVED1.age + 1)
+
+        and:
+        final repository = MemoryDataSource.create().newRepository(
+                User,
+                new MemoryRepositorySettings()
+                        .withTtl(Duration.ofMillis(ttl))
+                        .withExpirationStrategy(expirationStrategy)
+        )
+
+        when:
+        def saved = repository.save(entity).get()
+
+        then:
+        saved == entity
+
+        when:
+        def first = repository.findById(entity.id).get()
+
+        then:
+        first.present
+        first.get() == entity
+
+        when:
+        sleep(ttl)
+
+        and:
+        def second = repository.findById(entity.id).get()
+
+        then:
+        second.empty
+
+        where:
+        expirationStrategy << MemoryRepositorySettings.ExpiryStrategy.values()
     }
 
 }
