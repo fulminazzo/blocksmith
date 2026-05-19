c98dcbeea1c70f5838d58283fbf11bb40669df39
fulminazzo <fulminazzo21@gmail.com>
2026-05-20 01:19:58 +0200

    Optimized MemoryChannel integration tests helper

--- message-broker-starter/message-broker-starter-memory/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/memory/MemoryChannelIntegrationTestHelper.groovy
+++ message-broker-starter/message-broker-starter-memory/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/memory/MemoryChannelIntegrationTestHelper.groovy
@@ -1,54 +1,39 @@
 package it.fulminazzo.blocksmith.broker.memory
 
 import it.fulminazzo.blocksmith.broker.Message
-import it.fulminazzo.blocksmith.broker.MessageChannelIntegrationTestUtils
+import it.fulminazzo.blocksmith.broker.MessageChannelIntegrationTestHelper
 import it.fulminazzo.blocksmith.broker.memory.MemoryMessageQueryEngine.MemoryChannel
 import org.slf4j.Logger
 
-import java.util.function.BiConsumer
+import java.util.function.Consumer
 
-class MemoryChannelIntegrationTestHelper {
-    private final Queue<Message> receivedMessages = [] as Queue
-    private final List<BiConsumer<Message, UUID>> consumers = []
-
-    private final MemoryChannel memoryChannel
+class MemoryChannelIntegrationTestHelper extends MessageChannelIntegrationTestHelper {
+    private MemoryMessageQueryEngine queryEngine
+    private MemoryChannel memoryChannel
 
     MemoryChannelIntegrationTestHelper(
             final MemoryMessageQueryEngine queryEngine,
             final String channelName,
             final Logger logger
     ) {
-        memoryChannel = MemoryChannel.getChannel(channelName)
-        memoryChannel.register(
-                queryEngine,
-                p -> {
-                    logger.debug("Received raw: $p")
-                    def pair = MessageChannelIntegrationTestUtils.deserializeMessage(p)
-
-                    def message = pair.first
-                    logger.info("Received message with id=$message.id")
-                    receivedMessages.add(message)
-
-                    consumers.forEach { it.accept(message, pair.second) }
-                }
-        )
+        super(channelName, logger)
+        this.queryEngine = queryEngine
     }
 
-    MemoryChannelIntegrationTestHelper registerConsumer(final BiConsumer<Message, UUID> consumer) {
-        consumers.add(consumer)
+    @Override
+    MessageChannelIntegrationTestHelper start(
+            final String channelName,
+            final Logger logger,
+            final Consumer<String> consumer
+    ) {
+        memoryChannel = MemoryChannel.getChannel(channelName)
+        memoryChannel.register(queryEngine, consumer)
         return this
     }
 
-    boolean received(final Long id) {
-        return receivedMessages.any { it.id == id }
-    }
-
+    @Override
     void send(final Message message, final UUID conversationId) {
-        memoryChannel.publish(MessageChannelIntegrationTestUtils.serializeMessage(message, conversationId))
-    }
-
-    void clear() {
-        receivedMessages.clear()
+        memoryChannel.publish(serializeMessage(message, conversationId))
     }
 
 }
