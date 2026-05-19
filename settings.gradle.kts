2589e1ed671e3f8a7ac29b15f07233dd3bc818cb
fulminazzo <fulminazzo21@gmail.com>
2026-05-19 13:29:00 +0200

    Re-instantiated message-broker-starter-memory module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -83,7 +83,7 @@ include(
 
     "message-broker-starter:message-broker-starter-base",
 
-//    "message-broker-starter:message-broker-starter-memory", //TODO: re-enable
+    "message-broker-starter:message-broker-starter-memory",
 //    "message-broker-starter:message-broker-starter-redis", //TODO: re-enable
 
     "message-broker-starter:message-broker-starter-testing"
