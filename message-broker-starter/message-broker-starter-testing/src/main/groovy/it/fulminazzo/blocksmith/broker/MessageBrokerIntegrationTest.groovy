9cab9dfc6fd4cfb805fb2396be0984ab6ad6a1c2
fulminazzo <fulminazzo21@gmail.com>
2026-05-19 13:53:10 +0200

    Created abstract MessageBroker integration tests

---
+++ message-broker-starter/message-broker-starter-testing/src/main/groovy/it/fulminazzo/blocksmith/broker/MessageBrokerIntegrationTest.groovy
@@ -0,0 +1,40 @@
+package it.fulminazzo.blocksmith.broker
+
+import spock.lang.Specification
+
+abstract class MessageBrokerIntegrationTest<S extends MessageChannelSettings<S>> extends Specification {
+
+    def 'test broker life cycle'() {
+        given:
+        def builder = newMessageBrokerBuilder()
+
+        when:
+        def broker = builder.build()
+
+        then:
+        noExceptionThrown()
+
+        when:
+        def messageChannel = broker.newChannel(settings)
+
+        then:
+        messageChannel != null
+
+        when:
+        def message = messageChannel.sendAndReceiveRaw('hello', 10_000).get()
+
+        then:
+        message == 'world'
+
+        when:
+        broker.close()
+
+        then:
+        noExceptionThrown()
+    }
+
+    protected abstract MessageBrokerBuilder<MessageBroker<S>> newMessageBrokerBuilder()
+
+    protected abstract S getSettings()
+
+}
