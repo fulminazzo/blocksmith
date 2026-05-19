2e3667046efd78ad0635d0603873b7b7e669527b
fulminazzo <fulminazzo21@gmail.com>
2026-05-19 13:09:37 +0200

    Removed duplicate repositories declaration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -76,20 +76,3 @@ include(
 
     "data-starter:data-starter-testing"
 )
-
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        maven {
-            name = "spigotmc-repo"
-            url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-        }
-        maven {
-            name = "papermc"
-            url = uri("https://repo.papermc.io/repository/maven-public/")
-        }
-    }
-
-}
