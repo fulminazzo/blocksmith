59677a13143e16ae49a3dd75dc561f75454f1e27
fulminazzo <fulminazzo21@gmail.com>
2026-05-25 02:26:19 +0200

    Created RabbitMQMessageBroker with newChannel method

---
+++ message-broker-starter/message-broker-starter-rabbitmq/src/main/java/it/fulminazzo/blocksmith/broker/rabbitmq/RabbitMQMessageBroker.java
@@ -0,0 +1,130 @@
+package it.fulminazzo.blocksmith.broker.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import it.fulminazzo.blocksmith.broker.AbstractMessageBroker;
+import it.fulminazzo.blocksmith.broker.MessageChannel;
+import it.fulminazzo.blocksmith.broker.MessageChannelType;
+import it.fulminazzo.blocksmith.data.mapper.Mapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.concurrent.ExecutorService;
+import java.util.function.BiFunction;
+
+/**
+ * RabbitMQ message broker for handling connections and creating RabbitMQ channels.
+ * <br>
+ * The channels are configured with the following rules:
+ * <ul>
+ *     <li>the {@link RabbitMQMessageChannelSettings#getChannelName()} is the <b>exchange name</b>;</li>
+ *     <li>if the {@link RabbitMQMessageChannelSettings#getChannelType()} is {@link MessageChannelType#DIRECT},
+ *     the {@link RabbitMQMessageChannelSettings#getSubchannelName()} is used as <b>routing key</b>;</li>
+ *     <li><b>consumer tags</b> are programmatically generated;</li>
+ *     <li>the <b>queue name</b> is configured from the user.</li>
+ * </ul>
+ * //TODO: creation examples
+ * Examples:
+ * <ul>
+ *     <li>creating a standard channel:
+ *         <pre>{@code
+ *         RabbitMQMessageBroker messageBroker = ...;
+ *         MessageChannel channel = messageBroker.newChannel(
+ *                 new RabbitMQMessageChannelSettings()
+ *                         .withChannelName("rabbitmq_channel")
+ *                         .direct("private_channel")
+ *                         .withQueueName("rabbitmq_queue")
+ *         );
+ *         }</pre>
+ *     </li>
+ *     <li>creating a custom channel:
+ *         <pre>{@code
+ *         RabbitMQMessageBroker messageBroker = ...;
+ *         MessageChannel channel = messageBroker.newChannel(
+ *                 (engine, mapper) -> new CustomRabbitMQMessageChannel(engine, mapper),
+ *                 new RabbitMQMessageChannelSettings()
+ *                         .withChannelName("rabbitmq_channel")
+ *                         .direct("private_channel")
+ *                         .withQueueName("rabbitmq_queue")
+ *         );
+ *         }</pre>
+ *         where CustomRabbitMQMessageChannel extends RabbitMQMessageChannel and adds custom behavior.
+ *     </li>
+ * </ul>
+ *
+ * @see RabbitMQMessageChannel
+ * @see RabbitMQMessageChannelSettings
+ * @see RabbitMQMessageQueryEngine
+ */
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
+public final class RabbitMQMessageBroker extends AbstractMessageBroker<RabbitMQMessageChannelSettings> {
+    private final @NotNull ExecutorService executor;
+
+    private final @NotNull Connection connection;
+
+    private final @NotNull Mapper mapper;
+
+    /**
+     * Instantiates a new RabbitMQ message broker.
+     *
+     * @param executor   the executor
+     * @param connection the connection
+     * @param mapper     the mapper
+     */
+    RabbitMQMessageBroker(
+            final @NotNull ExecutorService executor,
+            final @NotNull Connection connection,
+            final @NotNull Mapper mapper
+    ) {
+        this.executor = executor;
+        this.connection = connection;
+        this.mapper = mapper;
+    }
+
+    /**
+     * Creates a new custom channel.
+     *
+     * @param <C>            the type of the channel
+     * @param channelBuilder the channel creation function
+     * @param settings       the settings to build the channel with
+     * @return the channel
+     */
+    public <C extends RabbitMQMessageChannel> C newChannel(
+            final @NotNull BiFunction<RabbitMQMessageQueryEngine, Mapper, C> channelBuilder,
+            final @NotNull RabbitMQMessageChannelSettings settings
+    ) {
+        try {
+            final String exchangeName = settings.getChannelName();
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(
+                    exchangeName,
+                    settings.getChannelType() == MessageChannelType.DIRECT ? "direct" : "fanout",
+                    true
+            );
+            RabbitMQMessageQueryEngine queryEngine = new RabbitMQMessageQueryEngine(
+                    executor,
+                    exchangeName,
+                    channel,
+                    settings.getSubchannelNameOrNull(),
+                    settings.getQueueName()
+            );
+            return registerChannel(channelBuilder.apply(queryEngine, mapper));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @Override
+    public @NotNull MessageChannel newChannel(final @NotNull RabbitMQMessageChannelSettings settings) {
+        return newChannel(RabbitMQMessageChannel::new, settings);
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        connection.close();
+        executor.shutdown();
+    }
+
+}
