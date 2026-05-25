b9c15f71bd961631475b85eb02ec4c6ab58c4cfa
fulminazzo <fulminazzo21@gmail.com>
2026-05-26 00:31:57 +0200

    Added integration tests for RabbitMQ message broker

---
+++ message-broker-starter/message-broker-starter-rabbitmq/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/rabbitmq/RabbitMQMessageBrokerIntegrationTest.groovy
@@ -0,0 +1,54 @@
+package it.fulminazzo.blocksmith.broker.rabbitmq
+
+import com.rabbitmq.client.ConnectionFactory
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.broker.MessageBroker
+import it.fulminazzo.blocksmith.broker.MessageBrokerBuilder
+import it.fulminazzo.blocksmith.broker.MessageBrokerIntegrationTest
+import it.fulminazzo.blocksmith.broker.MessageChannelIntegrationTestHelper
+import it.fulminazzo.blocksmith.data.mapper.MapperFormat
+
+import java.util.concurrent.Executors
+
+@Slf4j
+class RabbitMQMessageBrokerIntegrationTest extends MessageBrokerIntegrationTest<RabbitMQMessageChannelSettings> {
+
+    void setup() {
+        setupSingle()
+    }
+
+    void cleanup() {
+        cleanupSingle()
+    }
+
+    @Override
+    protected MessageBrokerBuilder<MessageBroker<RabbitMQMessageChannelSettings>> newMessageBrokerBuilder() {
+        return RabbitMQMessageBroker.builder()
+                .host(RabbitMQChannelIntegrationTestHelper.serverHost)
+                .port(RabbitMQChannelIntegrationTestHelper.serverPort)
+                .username(ConnectionFactory.DEFAULT_USER)
+                .password(ConnectionFactory.DEFAULT_PASS)
+                .configure {
+                    it.connectionTimeout = 60_000
+                }
+                .executor(Executors.newCachedThreadPool())
+                .mapper(MapperFormat.JSON.newMapper())
+    }
+
+    @Override
+    protected MessageChannelIntegrationTestHelper newTestHelper(final String channelName) {
+        return new RabbitMQChannelIntegrationTestHelper(channelName, log)
+    }
+
+    @Override
+    protected RabbitMQMessageChannelSettings getSettings() {
+        return new RabbitMQMessageChannelSettings()
+                .withChannelName(CHANNEL_NAME)
+                .withQueueSettings { RabbitMQMessageChannelSettings.QueueSettings queueSettings ->
+                    queueSettings
+                            .withQueueName(RabbitMQChannelIntegrationTestHelper.QUEUE_NAME)
+                            .durable()
+                }
+    }
+
+}
