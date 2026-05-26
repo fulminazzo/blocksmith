f76f252111cc2b3e3443e133397498d04b1886e6
fulminazzo <fulminazzo21@gmail.com>
2026-05-26 02:01:38 +0200

    Optimized RabbitMQMessageBrokerException handling with unwrapping of null message exception and injection of cause message

--- message-broker-starter/message-broker-starter-rabbitmq/src/main/java/it/fulminazzo/blocksmith/broker/rabbitmq/RabbitMQMessageBrokerException.java
+++ message-broker-starter/message-broker-starter-rabbitmq/src/main/java/it/fulminazzo/blocksmith/broker/rabbitmq/RabbitMQMessageBrokerException.java
@@ -2,6 +2,8 @@ package it.fulminazzo.blocksmith.broker.rabbitmq;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 /**
  * Unchecked exception used throughout this package to capture any RabbitMQ related exceptions.
  */
@@ -22,24 +24,22 @@ public final class RabbitMQMessageBrokerException extends RuntimeException {
             final @NotNull Integer port,
             final @NotNull Throwable cause
     ) {
-        return new RabbitMQMessageBrokerException(
-                "An exception occurred while instantiating a new connection to '%s:%s': %s",
+        return newException(
+                "An exception occurred while instantiating a new connection to '%s:%s'",
                 cause,
                 host,
-                port,
-                getCauseMessage(cause)
+                port
         );
     }
 
     static @NotNull RabbitMQMessageBrokerException createChannelException(
             final @NotNull String exchangeName,
-            final @NotNull Throwable cause
+            @NotNull Throwable cause
     ) {
-        return new RabbitMQMessageBrokerException(
-                "An exception occurred while creating a new channel for exchange '%s': %s",
+        return newException(
+                "An exception occurred while creating a new channel for exchange '%s'",
                 cause,
-                exchangeName,
-                getCauseMessage(cause)
+                exchangeName
         );
     }
 
@@ -47,46 +47,56 @@ public final class RabbitMQMessageBrokerException extends RuntimeException {
             final @NotNull String payload,
             final @NotNull Throwable cause
     ) {
-        return new RabbitMQMessageBrokerException(
-                "An exception occurred while publishing payload '%s': %s",
-                cause,
-                payload,
-                getCauseMessage(cause)
-        );
+        return newException("An exception occurred while publishing payload '%s'", cause, payload);
     }
 
     static @NotNull RabbitMQMessageBrokerException queueDeclareException(
             final @NotNull String queueName,
             final @NotNull String exchangeName,
             final @NotNull String routingKey,
-            final @NotNull Throwable cause
+            @NotNull Throwable cause
     ) {
-        return new RabbitMQMessageBrokerException(
-                "An exception occurred while declaring and binding queue '%s' "
-                        + "to exchange '%s' with routing key '%s': %s",
+        return newException(
+                "An exception occurred while declaring and binding queue '%s' to exchange '%s' with routing key '%s'",
                 cause,
                 queueName,
                 exchangeName,
-                routingKey,
-                getCauseMessage(cause)
+                routingKey
         );
     }
 
     static @NotNull RabbitMQMessageBrokerException registerConsumerException(
             final @NotNull String queueName,
-            final @NotNull Throwable cause
+            @NotNull Throwable cause
     ) {
-        return new RabbitMQMessageBrokerException(
-                "An exception occurred while registering consumer for queue '%s': %s",
-                cause,
-                queueName,
-                getCauseMessage(cause)
-        );
+        return newException("An exception occurred while registering consumer for queue '%s'", cause, queueName);
+    }
+
+    private static @NotNull RabbitMQMessageBrokerException newException(
+            final @NotNull String message,
+            @NotNull Throwable cause,
+            @NotNull Object... args
+    ) {
+        cause = unwrapCause(cause);
+        args = Arrays.copyOfRange(args, 0, args.length + 1);
+        args[args.length - 1] = getCauseMessage(cause);
+        RabbitMQMessageBrokerException exception = new RabbitMQMessageBrokerException(message + ": %s", cause, args);
+        // remove static StackTrace elements
+        StackTraceElement[] stackTrace = exception.getStackTrace();
+        stackTrace = Arrays.copyOfRange(stackTrace, 2, stackTrace.length);
+        exception.setStackTrace(stackTrace);
+        return exception;
     }
 
     private static @NotNull String getCauseMessage(final @NotNull Throwable cause) {
         String message = cause.getMessage();
         return message != null ? message : cause.getClass().getSimpleName();
+    }
+
+    private static @NotNull Throwable unwrapCause(final @NotNull Throwable cause) {
+        Throwable inner = cause.getCause();
+        String message = cause.getMessage();
+        return (message == null || message.isEmpty()) && inner != null ? unwrapCause(inner) : cause;
     }
 
 }
