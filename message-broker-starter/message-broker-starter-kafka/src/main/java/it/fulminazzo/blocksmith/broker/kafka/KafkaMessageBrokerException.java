05e7ad58fba50d2974684b4a2ade84bccef8b463
fulminazzo <fulminazzo21@gmail.com>
2026-05-28 01:19:46 +0200

    Updated KafkaMessageBroker exception and added publishException method

--- message-broker-starter/message-broker-starter-kafka/src/main/java/it/fulminazzo/blocksmith/broker/kafka/KafkaMessageBrokerException.java
+++ message-broker-starter/message-broker-starter-kafka/src/main/java/it/fulminazzo/blocksmith/broker/kafka/KafkaMessageBrokerException.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.blocksmith.broker.kafka;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,9 +16,23 @@ public final class KafkaMessageBrokerException extends RuntimeException {
 
     private KafkaMessageBrokerException(
             final @NotNull String message,
+            final @Nullable Throwable cause,
             final @NotNull Object... args
     ) {
-        super(String.format(message, args));
+        super(String.format(message, args), cause);
+    }
+
+    static @NotNull KafkaMessageBrokerException publishException(
+            final @NotNull String topic,
+            final @NotNull String payload,
+            final @NotNull Throwable cause
+    ) {
+        return newException(
+                "An exception occurred while publishing payload '%s' to topic '%s'",
+                cause,
+                payload,
+                topic
+        );
     }
 
     static @NotNull KafkaMessageBrokerException getAssignmentException(
@@ -26,21 +41,40 @@ public final class KafkaMessageBrokerException extends RuntimeException {
     ) {
         return newException(
                 "Unable to get assignment for topics '%s' after %s ms",
+                null,
                 String.join(", ", topics),
                 waitInterval
         );
     }
 
     private static @NotNull KafkaMessageBrokerException newException(
-            final @NotNull String message,
-            final @NotNull Object... args
+            @NotNull String message,
+            @Nullable Throwable cause,
+            @NotNull Object... args
     ) {
-        KafkaMessageBrokerException exception = new KafkaMessageBrokerException(message, args);
+        if (cause != null) {
+            message = message + ": %s";
+            cause = unwrapCause(cause);
+            args = Arrays.copyOfRange(args, 0, args.length + 1);
+            args[args.length - 1] = getCauseMessage(cause);
+        }
+        KafkaMessageBrokerException exception = new KafkaMessageBrokerException(message, cause, args);
         // remove static StackTrace elements
         StackTraceElement[] stackTrace = exception.getStackTrace();
         stackTrace = Arrays.copyOfRange(stackTrace, 2, stackTrace.length);
         exception.setStackTrace(stackTrace);
         return exception;
+    }
+
+    private static @NotNull String getCauseMessage(final @NotNull Throwable cause) {
+        String message = cause.getMessage();
+        return message != null ? message : cause.getClass().getSimpleName();
+    }
+
+    private static @NotNull Throwable unwrapCause(final @NotNull Throwable cause) {
+        Throwable inner = cause.getCause();
+        String message = cause.getMessage();
+        return (message == null || message.isEmpty()) && inner != null ? unwrapCause(inner) : cause;
     }
 
 }
