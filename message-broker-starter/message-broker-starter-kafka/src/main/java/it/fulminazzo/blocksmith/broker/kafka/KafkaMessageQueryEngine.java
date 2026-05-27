0af777386246f814ef64a27234737010dd45d563
fulminazzo <fulminazzo21@gmail.com>
2026-05-28 00:49:47 +0200

    Created KafkaMessageQueryEngine with consumer handlers support

---
+++ message-broker-starter/message-broker-starter-kafka/src/main/java/it/fulminazzo/blocksmith/broker/kafka/KafkaMessageQueryEngine.java
@@ -0,0 +1,113 @@
+package it.fulminazzo.blocksmith.broker.kafka;
+
+import it.fulminazzo.blocksmith.broker.MessageQueryEngine;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+
+/**
+ * A message query engine with Kafka support.
+ * <br>
+ * The internal producer and consumer will be configured with the provided properties.
+ * The <b>channel name</b> will be used as the <b>topic name</b>.
+ *
+ * @see KafkaMessageChannel
+ * @see KafkaMessageBroker
+ */
+public final class KafkaMessageQueryEngine extends MessageQueryEngine {
+    private static final @NotNull String SERIALIZER = StringSerializer.class.getCanonicalName();
+    private static final @NotNull String DESERIALIZER = StringDeserializer.class.getCanonicalName();
+
+    private final @NotNull List<KafkaConsumerHandler<String, String>> consumers = new CopyOnWriteArrayList<>();
+
+    private final @NotNull ExecutorService executor;
+
+    private final @NotNull KafkaProducer<String, String> producer;
+
+    private final @Nullable String key;
+
+    private final @NotNull Properties properties;
+    private final long assignmentWaitTime;
+    private final long pollInterval;
+
+    /**
+     * Instantiates a new Kafka message query engine.
+     *
+     * @param executor           the executor
+     * @param properties         the properties to apply to both the producer and the consumer
+     * @param channelName        the channel name
+     * @param key                the key to include in the message
+     * @param assignmentWaitTime the time to wait for the first assignment
+     * @param pollInterval       the time between polls
+     */
+    public KafkaMessageQueryEngine(
+            final @NotNull ExecutorService executor,
+            final @NotNull Properties properties,
+            final @NotNull String channelName,
+            final @Nullable String key,
+            final long assignmentWaitTime,
+            final long pollInterval
+    ) {
+        super(channelName);
+        this.executor = executor;
+
+        this.properties = properties;
+        this.assignmentWaitTime = assignmentWaitTime;
+        this.pollInterval = pollInterval;
+
+        properties.put("key.serializer", SERIALIZER);
+        properties.put("value.serializer", SERIALIZER);
+        properties.put("key.deserializer", DESERIALIZER);
+        properties.put("value.deserializer", DESERIALIZER);
+
+        this.producer = new KafkaProducer<>(properties);
+
+        this.key = key;
+    }
+
+    @Override
+    public @NotNull CompletableFuture<Void> publish(final @NotNull String payload) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(getChannelName(), key, payload);
+                producer.send(producerRecord).get();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new CompletionException(e.getCause());
+            }
+        }, executor);
+    }
+
+    @Override
+    public void listen(final @NotNull Consumer<String> consumer) {
+        consumers.add(new KafkaConsumerHandler<>(
+                properties,
+                List.of(getChannelName()),
+                assignmentWaitTime,
+                pollInterval
+        ) {
+            @Override
+            protected void handle(final @NotNull String key, final @NotNull String value) {
+                consumer.accept(value);
+            }
+        });
+    }
+
+    @Override
+    public void close() {
+        consumers.forEach(KafkaConsumerHandler::close);
+        producer.close();
+        executor.shutdown();
+    }
+
+}
