f6a4578ba7a1620a8c7da7a15667f563173c9abf
fulminazzo <fulminazzo21@gmail.com>
2026-05-29 01:01:50 +0200

    Created TcpMessageServer

---
+++ message-broker-starter/message-broker-starter-tcp/src/main/java/it/fulminazzo/blocksmith/broker/tcp/server/TcpMessageServer.java
@@ -0,0 +1,97 @@
+package it.fulminazzo.blocksmith.broker.tcp.server;
+
+import it.fulminazzo.blocksmith.data.mapper.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * TCP server to handle incoming messages.
+ * The message protocol is very basic as the main concern for this module is testing purposes.
+ * In production environments a more sophisticated module should be used.
+ *
+ * @see TcpMessageClientHandler
+ */
+@RequiredArgsConstructor
+public final class TcpMessageServer implements Runnable, Closeable {
+    private final @NotNull Map<String, List<TcpMessageClientHandler>> handlers = new ConcurrentHashMap<>();
+
+    private final @NotNull Logger logger;
+    private final @NotNull Mapper mapper;
+    private final int port;
+
+    private @Nullable ServerSocket socket;
+
+    /**
+     * Broadcasts a message to all the clients subscribed to the specified channel.
+     *
+     * @param channel the channel to which the message should be sent
+     * @param message the message to send (without the leading {@code \n})
+     */
+    void broadcast(final @NotNull String channel, final @NotNull String message) {
+        handlers.computeIfPresent(channel, (c, l) -> {
+            l.removeIf(TcpMessageClientHandler::isClosed);
+            l.forEach(h -> h.write(message + "\n"));
+            return l;
+        });
+    }
+
+    private @NotNull String formatLog(final @NotNull String message) {
+        return String.format("|TCP Server (%s)|: %s", port, message);
+    }
+
+    private void registerHandler(final @NotNull String channel, final @NotNull TcpMessageClientHandler handler) {
+        handlers.computeIfAbsent(channel, c -> new ArrayList<>()).add(handler);
+        handler.write("OK\n");
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new ServerSocket(port);
+            logger.info(formatLog("Server started on port {}"), port);
+        } catch (IOException e) {
+            /*
+             * Either the port is already in use or the system refused to bind it.
+             * Both cases are handled by the clients.
+             */
+        }
+        while (socket != null && !socket.isClosed())
+            try {
+                Socket socket = this.socket.accept();
+                logger.debug(
+                        formatLog("New client connected on {}:{}"),
+                        socket.getInetAddress().getHostAddress(),
+                        socket.getPort()
+                );
+                new TcpMessageClientHandler(logger, mapper, socket)
+                        .onRead(this::broadcast)
+                        .start(this::registerHandler);
+            } catch (IOException e) {
+                // client closed connection abruptly
+            }
+    }
+
+    @Override
+    public void close() {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // do nothing
+            }
+            socket = null;
+            logger.info(formatLog("TCP server stopped"));
+        }
+    }
+
+}
