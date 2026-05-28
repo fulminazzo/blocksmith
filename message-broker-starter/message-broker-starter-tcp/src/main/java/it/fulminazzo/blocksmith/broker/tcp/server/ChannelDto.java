350c58e2bf72abd9988059af48b9b0b22b36402e
fulminazzo <fulminazzo21@gmail.com>
2026-05-29 00:56:02 +0200

    Created ChannelDto

---
+++ message-broker-starter/message-broker-starter-tcp/src/main/java/it/fulminazzo/blocksmith/broker/tcp/server/ChannelDto.java
@@ -0,0 +1,16 @@
+package it.fulminazzo.blocksmith.broker.tcp.server;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Simple DTO for sharing the channel name.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+final class ChannelDto {
+    private String channelName;
+
+}
