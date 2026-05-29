1e5b2f6cddbfba03686c5f215250310af2fee5b9
fulminazzo <fulminazzo21@gmail.com>
2026-05-29 22:50:24 +0200

    Created TcpConnection interface

---
+++ message-broker-starter/message-broker-starter-tcp/src/main/java/it/fulminazzo/blocksmith/broker/tcp/peer_rework/TcpConnection.java
@@ -0,0 +1,33 @@
+package it.fulminazzo.blocksmith.broker.tcp.peer_rework;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Closeable;
+
+/**
+ * Represents a TCP connection.
+ */
+public interface TcpConnection extends Closeable {
+
+    /**
+     * Gets the host address.
+     *
+     * @return the host
+     */
+    @NotNull String getHost();
+
+    /**
+     * Gets the port.
+     *
+     * @return the port
+     */
+    int getPort();
+
+    /**
+     * Checks if the connection is closed.
+     *
+     * @return {@code true} if it is
+     */
+    boolean isClosed();
+
+}
