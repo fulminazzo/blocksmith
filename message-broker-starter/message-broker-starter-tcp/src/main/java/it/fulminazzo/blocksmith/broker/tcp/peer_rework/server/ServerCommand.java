679ffd0891c051a58342cee240e710aa5f8692d7
fulminazzo <fulminazzo21@gmail.com>
2026-05-29 23:19:44 +0200

    Created ServerCommand enum with SUBSCRIBE, UNSUBSCRIBE and MESSAGE commands

---
+++ message-broker-starter/message-broker-starter-tcp/src/main/java/it/fulminazzo/blocksmith/broker/tcp/peer_rework/server/ServerCommand.java
@@ -0,0 +1,69 @@
+package it.fulminazzo.blocksmith.broker.tcp.peer_rework.server;
+
+import it.fulminazzo.blocksmith.broker.tcp.peer_rework.MessageDto;
+import it.fulminazzo.blocksmith.data.mapper.Mapper;
+import it.fulminazzo.blocksmith.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/**
+ * Represents a command that can be executed by the server.
+ *
+ * @see TcpMessageServer
+ * @see TcpMessageServerClient
+ */
+@RequiredArgsConstructor
+enum ServerCommand {
+    /**
+     * Subscribes to a channel.
+     * <br>
+     * Syntax: {@code SUBSCRIBE <channel>}
+     */
+    SUBSCRIBE((client, args) -> {
+        if (!args.isEmpty()) client.subscribe(args.get(0)).send(ServerResponse.SUCCESS);
+        else client.send(ServerResponse.NOT_ENOUGH_ARGUMENTS);
+    }),
+    /**
+     * Unsubscribes from a channel.
+     * <br>
+     * Syntax: {@code UNSUBSCRIBE <channel>}
+     */
+    UNSUBSCRIBE((client, args) -> {
+        if (!args.isEmpty()) client.unsubscribe(args.get(0)).send(ServerResponse.SUCCESS);
+        else client.send(ServerResponse.NOT_ENOUGH_ARGUMENTS);
+    }),
+    /**
+     * Sends a message to all the clients listening on the channel.
+     * <br>
+     * Syntax: {@code MESSAGE <channel> <message...>}
+     */
+    MESSAGE((client, args) -> {
+        if (args.size() > 1) {
+            String channel = args.get(0);
+            if (client.isSubscribed(channel)) {
+                String message = String.join(" ", args.subList(1, args.size()));
+                Mapper mapper = client.getMapper();
+                client.send(mapper.serialize(new MessageDto(channel, message)));
+            }
+        } else client.send(ServerResponse.NOT_ENOUGH_ARGUMENTS);
+    });
+
+    private final @NotNull BiConsumer<TcpMessageServerClient, List<String>> executor;
+
+    /**
+     * Executes the command.
+     *
+     * @param client    the client
+     * @param arguments the arguments for the command
+     */
+    public void execute(final @NotNull TcpMessageServerClient client, final @NotNull String arguments) {
+        executor.accept(
+                client,
+                StringUtils.split(arguments, " ", "'", "\"")
+        );
+    }
+
+}
