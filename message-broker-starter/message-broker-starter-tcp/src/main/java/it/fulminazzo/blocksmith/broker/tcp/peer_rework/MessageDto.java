50f8adb6e9b041a02182aaf71167a8b02f6e27a7
fulminazzo <fulminazzo21@gmail.com>
2026-05-29 23:15:33 +0200

    Created MessageDto

---
+++ message-broker-starter/message-broker-starter-tcp/src/main/java/it/fulminazzo/blocksmith/broker/tcp/peer_rework/MessageDto.java
@@ -0,0 +1,18 @@
+package it.fulminazzo.blocksmith.broker.tcp.peer_rework;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Base DTO for sharing messages across the network.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public final class MessageDto {
+    @NotNull String channel;
+    @NotNull String message;
+
+}
