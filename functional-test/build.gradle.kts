a0a132ba91592a9dcf1001cb7f2534503d5ab454
fulminazzo <fulminazzo21@gmail.com>
2026-06-08 23:12:49 +0200

    Added shadowjar and resources processing configurations

--- functional-test/build.gradle.kts
+++ functional-test/build.gradle.kts
@@ -1,3 +1,43 @@
 plugins {
+    alias(libs.plugins.shadow)
+
     id("blocksmith.minecraft-module")
+}
+
+val baseModuleName: String by extra
+
+subprojects {
+    if (name.endsWith("-$baseModuleName")) return@subprojects
+
+    apply { plugin(rootProject.libs.plugins.shadow.get().pluginId) }
+
+    tasks.jar {
+        dependsOn(tasks.shadowJar)
+
+        archiveClassifier = "original"
+        archiveBaseName = rootProject.name
+    }
+
+    tasks.shadowJar {
+        archiveClassifier = ""
+        archiveBaseName = rootProject.name
+
+        mergeServiceFiles()
+    }
+
+    tasks.processResources {
+        val group = "${rootProject.group}"
+        val author = group.split(".").last().replaceFirstChar { it.uppercase() }
+        val properties = mapOf(
+            "group" to group,
+            "name" to rootProject.name.replaceFirstChar { it.titlecase() },
+            "name_lower" to rootProject.name,
+            "description" to "${rootProject.name} plugin for functional testing purposes",
+            "version" to "${rootProject.version}",
+            "author" to author
+        )
+        filesMatching("*.yml") { expand(properties) }
+        filesMatching("*.json") { expand(properties) }
+    }
+
 }
