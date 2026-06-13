0ca85cbb634910902e4cb9f82b198c36facd8930
fulminazzo <fulminazzo21@gmail.com>
2026-06-14 01:01:20 +0200

    Created abstract ProxyPluginMessageChannelCoordinator to better handle proxies PluginMessageChannelCoordinator implementations

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/main/java/it/fulminazzo/blocksmith/broker/plugin/coordinator/ProxyPluginMessageChannelCoordinator.java
@@ -0,0 +1,62 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A special {@link PluginMessageChannelCoordinator} prepared for proxies configurations.
+ * Contains an internal map for handling all the nodes of a proxy as instances of {@link PluginMessagePublisher},
+ * so that each node can handle failed publications accordingly.
+ *
+ * @param <N> the type of the nodes (IDs are preferred for memory efficiency)
+ * @see PluginMessageChannelCoordinator
+ */
+public abstract class ProxyPluginMessageChannelCoordinator<N> extends PluginMessageChannelCoordinator {
+    private final @NotNull Map<N, PluginMessagePublisher> nodes = new ConcurrentHashMap<>();
+
+    /**
+     * Instantiates a new Proxy plugin message channel coordinator.
+     *
+     * @param registrar the registrar
+     */
+    protected ProxyPluginMessageChannelCoordinator(final @NotNull PluginMessageRegistrar registrar) {
+        super(registrar);
+    }
+
+    /**
+     * Instantiates a new publisher for the given node.
+     *
+     * @param node the node
+     * @return the publisher
+     */
+    protected abstract @NotNull PluginMessagePublisher newNodePublisher(final @NotNull N node);
+
+    /**
+     * Fetches all the currently connected nodes.
+     *
+     * @return the nodes
+     */
+    protected abstract @NotNull Collection<N> getAllNodes();
+
+    private void refreshNodes() {
+        @NotNull Collection<N> currNodes = getAllNodes();
+        nodes.keySet().removeIf(n -> !currNodes.contains(n));
+        currNodes.forEach(n -> nodes.put(n, newNodePublisher(n)));
+    }
+
+    @Override
+    public boolean publish(final @NotNull String channelName, final @NotNull String message) {
+        refreshNodes();
+        return nodes.values().stream().anyMatch(n -> n.publish(channelName, message));
+    }
+
+    @Override
+    public void republishFailedMessages() {
+        refreshNodes();
+        nodes.values().forEach(PluginMessagePublisher::republishFailedMessages);
+    }
+
+}
---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/test/groovy/it/fulminazzo/blocksmith/broker/plugin/coordinator/ProxyPluginMessageChannelCoordinatorTest.groovy
@@ -0,0 +1,54 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator
+
+import it.fulminazzo.blocksmith.reflect.Reflect
+import spock.lang.Specification
+
+class ProxyPluginMessageChannelCoordinatorTest extends Specification {
+    private final ProxyPluginMessageChannelCoordinator<String> coordinator = Spy(
+            ProxyPluginMessageChannelCoordinator,
+            constructorArgs : [Mock(PluginMessageRegistrar)]
+    )
+
+    def 'test that refreshNodes removes disconnected nodes and updates with new ones'() {
+        given:
+        def nodes = Reflect.on(coordinator).get('nodes').get() as Map
+        for (def i in 1..5)
+            nodes.put("$i", Mock(PluginMessagePublisher))
+
+        and:
+        final newNodes = (1..3).collect { "${it * 2}" }
+
+        and:
+        coordinator.allNodes >> newNodes
+        coordinator.newNodePublisher(_) >> Mock(PluginMessagePublisher)
+
+        when:
+        Reflect.on(coordinator).invoke('refreshNodes')
+
+        then:
+        nodes.size() == 3
+        nodes.keySet() == newNodes.toSet()
+    }
+
+    def 'test that #method delegates to nodes'() {
+        given:
+        def nodes = (1..5).collect { "$it".toString() }
+        def publishers = nodes.collectEntries { [it, Mock(PluginMessagePublisher)] }
+        coordinator.allNodes >> nodes
+        coordinator.newNodePublisher(_) >> { a -> publishers[a[0]] }
+
+        when:
+        coordinator."$method"(*arguments)
+
+        then:
+        nodes.forEach {
+            1 * publishers[it]."$method"(*_)
+        }
+
+        where:
+        method                    | arguments
+        'publish'                 | ['channel', 'message']
+        'republishFailedMessages' | []
+    }
+
+}
