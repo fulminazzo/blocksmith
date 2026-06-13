743d3316b7fc860d18bff03524fd8d2622eed9b8
fulminazzo <fulminazzo21@gmail.com>
2026-06-14 00:18:11 +0200

    Created PluginMessagePublisher to handle publication of messages

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/main/java/it/fulminazzo/blocksmith/broker/plugin/coordinator/PluginMessagePublisher.java
@@ -0,0 +1,35 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Identifies an object capable of publishing to a plugin messaging channel.
+ * <br>
+ * Must provide a recovery method in case of failed sending.
+ *
+ * @see AbstractPluginMessagePublisher
+ * @see PluginMessageChannelCoordinator
+ * @see ProxyPluginMessageChannelCoordinator
+ */
+public interface PluginMessagePublisher {
+
+    /**
+     * Attempts to publish a message to the channel.
+     *
+     * @param channelName the channel
+     * @param message     the message
+     * @return {@code true} if the message was successfully published;
+     *         {@code false} if the message could not be sent at this time
+     *         (probably due to missing bridge between connections)
+     */
+    boolean publish(final @NotNull String channelName, final @NotNull String message);
+
+    /**
+     * Attempts to publish once again (in order) the messages whose {@link #publish(String, String)}
+     * returned {@code false}.
+     * <br>
+     * <b>WARNING</b>: it is <b>not guaranteed</b> that the publication will be successful.
+     */
+    void republishFailedMessages();
+
+}
