bb2a78c5e5589d291c6f21b4bd95c4a84abba823
fulminazzo <fulminazzo21@gmail.com>
2026-06-14 01:35:30 +0200

    Created Bukkit PluginMessageChannelCoordinator implementation with PluginMessageListener#onPluginMessageReceived and resend of messages on player join event

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-bukkit/src/main/java/it/fulminazzo/blocksmith/broker/plugin/coordinator/bukkit/BukkitPluginMessageChannelCoordinator.java
@@ -0,0 +1,109 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator.bukkit;
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.AbstractPluginMessagePublisher;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageChannelCoordinator;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessagePublisher;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageRegistrar;
+import lombok.experimental.Delegate;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Comparator;
+
+/**
+ * Bukkit implementation of {@link PluginMessageChannelCoordinator}.
+ *
+ * @see PluginMessageChannelCoordinator
+ */
+public final class BukkitPluginMessageChannelCoordinator
+        extends PluginMessageChannelCoordinator
+        implements PluginMessageListener, Listener {
+    private final @NotNull Server server;
+    @Delegate
+    private final @NotNull PluginMessagePublisher publisher;
+
+    /**
+     * Instantiates a new Bukkit plugin message channel coordinator.
+     *
+     * @param registrar the registrar
+     */
+    BukkitPluginMessageChannelCoordinator(final @NotNull PluginMessageRegistrar registrar) {
+        super(registrar);
+        this.server = this.registrar.server();
+        this.publisher = new BukkitPluginMessageChannelPublisher();
+    }
+
+    /**
+     * Attempts to resend the failed messages whenever a player joins
+     * (will only have effect if no other player was online at the time of publishing).
+     *
+     * @param event the event
+     */
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void on(final @NotNull PlayerJoinEvent event) {
+        publisher.republishFailedMessages();
+    }
+
+    @Override
+    public void onPluginMessageReceived(
+            final @NotNull String channel,
+            final @NotNull Player player,
+            final byte @NotNull [] message
+    ) {
+        handleIncomingMessage(channel, message);
+    }
+
+    @Override
+    protected void registerChannel(final @NotNull String channelName) {
+        Plugin plugin = registrar.plugin();
+        server.getMessenger().registerIncomingPluginChannel(
+                plugin,
+                channelName,
+                this
+        );
+        server.getMessenger().registerOutgoingPluginChannel(plugin, channelName);
+        server.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @Override
+    protected void unregisterChannel(final @NotNull String channelName) {
+        Plugin plugin = registrar.plugin();
+        server.getMessenger().unregisterIncomingPluginChannel(plugin, channelName);
+        server.getMessenger().unregisterOutgoingPluginChannel(plugin, channelName);
+        HandlerList.unregisterAll(this);
+    }
+
+    /**
+     * Bukkit implementation of {@link PluginMessagePublisher}.
+     *
+     * @see PluginMessagePublisher
+     */
+    private final class BukkitPluginMessageChannelPublisher extends AbstractPluginMessagePublisher {
+
+        @Override
+        protected boolean publishImpl(final @NotNull String channelName, final byte @NotNull [] message) {
+            Player player = server.getOnlinePlayers().stream()
+                    /*
+                     * We are sorting for OP players first, so that if an OP player is present,
+                     * they will be chosen as bridge for the proxy, in an attempt to limit
+                     * malicious actors intercepting messages.
+                     */
+                    .sorted(Comparator.comparing(p -> !p.isOp()))
+                    .findAny().orElse(null);
+            if (player != null) {
+                player.sendPluginMessage(registrar.plugin(), channelName, message);
+                return true;
+            } else return false;
+        }
+
+    }
+
+}
---
+++ message-broker-starter/plugin-messaging/plugin-messaging-bukkit/src/test/groovy/it/fulminazzo/blocksmith/broker/plugin/coordinator/bukkit/BukkitPluginMessageChannelCoordinatorTest.groovy
@@ -0,0 +1,162 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator.bukkit
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.AbstractPluginMessagePublisher
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessagePublisher
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageRegistrar
+import it.fulminazzo.blocksmith.reflect.Reflect
+import org.bukkit.Server
+import org.bukkit.entity.Player
+import org.bukkit.event.HandlerList
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.messaging.Messenger
+import org.mockito.Mockito
+import spock.lang.Specification
+
+class BukkitPluginMessageChannelCoordinatorTest extends Specification {
+    private static final String CHANNEL_NAME = 'test-channel'
+
+    private final PluginMessageRegistrar registrar = Mock(PluginMessageRegistrar) {
+        def messenger = Mock(Messenger)
+        def pluginManager = Mock(PluginManager)
+
+        def server = Mock(Server)
+        server.messenger >> messenger
+        server.pluginManager >> pluginManager
+
+        def plugin = Mock(Plugin)
+        plugin.server >> server
+
+        it.server() >> server
+        it.plugin() >> plugin
+    }
+
+    private final BukkitPluginMessageChannelCoordinator coordinator = new BukkitPluginMessageChannelCoordinator(registrar)
+
+    def 'test that coordinator attempts to publish failed messages on player join'() {
+        given:
+        final message = 'message'.bytes
+
+        and:
+        def publisher = Reflect.on(coordinator).get('publisher').get()
+        Reflect.on(publisher)
+                .get('failedMessages')
+                .invoke('put', CHANNEL_NAME, new LinkedList<>([message]))
+
+        and:
+        def player = Mock(Player)
+        def event = new PlayerJoinEvent(player, 'Player joined!')
+
+        and:
+        registrar.server().onlinePlayers >> [player]
+
+        when:
+        coordinator.on(event)
+
+        then:
+        1 * player.sendPluginMessage(
+                registrar.plugin(),
+                CHANNEL_NAME,
+                message
+        )
+    }
+
+    def 'test that onPluginMessageReceived delegates to handleIncomingMessage'() {
+        given:
+        def coordinator = Mock(BukkitPluginMessageChannelCoordinator)
+        coordinator.onPluginMessageReceived(*_) >> { callRealMethod() }
+
+        and:
+        final message = 'Hello, world!'.bytes
+
+        when:
+        coordinator.onPluginMessageReceived(
+                CHANNEL_NAME,
+                Mock(Player),
+                message
+        )
+
+        then:
+        1 * coordinator.handleIncomingMessage(CHANNEL_NAME, message)
+    }
+
+    def 'test that registerChannel correctly registers all necessary channels'() {
+        when:
+        coordinator.registerChannel(CHANNEL_NAME)
+
+        then:
+        1 * registrar.server().messenger.registerIncomingPluginChannel(
+                registrar.plugin(),
+                CHANNEL_NAME,
+                coordinator
+        )
+        1 * registrar.server().messenger.registerOutgoingPluginChannel(registrar.plugin(), CHANNEL_NAME)
+        1 * registrar.server().pluginManager.registerEvents(coordinator, registrar.plugin())
+    }
+
+    def 'test that unregisterChannel correctly unregisters all previously registered channels'() {
+        given:
+        def handlerList = Mockito.mockStatic(HandlerList)
+
+        when:
+        coordinator.unregisterChannel(CHANNEL_NAME)
+
+        then:
+        1 * registrar.server().messenger.unregisterIncomingPluginChannel(registrar.plugin(), CHANNEL_NAME)
+        1 * registrar.server().messenger.unregisterOutgoingPluginChannel(registrar.plugin(), CHANNEL_NAME)
+
+        and:
+        handlerList.verify(
+                { HandlerList.unregisterAll(coordinator) },
+                Mockito.only()
+        )
+
+        cleanup:
+        handlerList?.close()
+    }
+
+    def 'test that BukkitPluginMessageChannelPublisher prioritizes OP players for publishing'() {
+        given:
+        def nonOp = Mock(Player)
+        def op = Mock(Player) { it.op >> true }
+
+        and:
+        def publisher = Reflect.on(coordinator).get('publisher').get() as AbstractPluginMessagePublisher
+
+        and:
+        def players = [nonOp, op]
+        def message = 'Hello, world'.bytes
+
+        and:
+        registrar.server().onlinePlayers >> players
+
+        when:
+        def result1 = publisher.publishImpl(CHANNEL_NAME, message)
+
+        then:
+        result1
+
+        and:
+        1 * op.sendPluginMessage(registrar.plugin(), CHANNEL_NAME, message)
+        0 * nonOp.sendPluginMessage(registrar.plugin(), CHANNEL_NAME, message)
+
+        when:
+        players.remove(op)
+        def result2 = publisher.publishImpl(CHANNEL_NAME, message)
+
+        then:
+        result2
+
+        and:
+        0 * op.sendPluginMessage(registrar.plugin(), CHANNEL_NAME, message)
+        1 * nonOp.sendPluginMessage(registrar.plugin(), CHANNEL_NAME, message)
+
+        when:
+        players.remove(nonOp)
+
+        then:
+        !publisher.publishImpl(CHANNEL_NAME, message)
+    }
+
+}
