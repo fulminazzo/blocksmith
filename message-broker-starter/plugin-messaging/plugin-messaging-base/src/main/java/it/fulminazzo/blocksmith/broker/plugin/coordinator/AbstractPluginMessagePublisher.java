c8bad9a7081c4444f5e6e82fa8ce791e9bb65080
fulminazzo <fulminazzo21@gmail.com>
2026-06-14 00:22:55 +0200

    Created AbstractPluginMessagePublisher to handle failed publications in an internal map

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/main/java/it/fulminazzo/blocksmith/broker/plugin/coordinator/AbstractPluginMessagePublisher.java
@@ -0,0 +1,61 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Abstract implementation of {@link PluginMessagePublisher} that stores failed messages in
+ * a {@link Map} for later republications.
+ *
+ * @see PluginMessagePublisher
+ */
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class AbstractPluginMessagePublisher implements PluginMessagePublisher {
+    private final @NotNull Map<String, Queue<String>> failedMessages = new ConcurrentHashMap<>();
+
+    /**
+     * Attempts to publish a message to the channel.
+     * <br>
+     * Implementation of {@link #publish(String, String)} that will <b>not</b> store
+     * messages to {@link #failedMessages}.
+     *
+     * @param channelName the channel
+     * @param message     the message
+     * @return {@code true} if the message was successfully published;
+     *         {@code false} if the message could not be sent at this time
+     *         (probably due to missing bridge between connections)
+     */
+    protected abstract boolean publishImpl(final @NotNull String channelName, final @NotNull String message);
+
+    @Override
+    public boolean publish(final @NotNull String channelName, final @NotNull String message) {
+        if (publishImpl(channelName, message)) return true;
+        else {
+            failedMessages.computeIfAbsent(
+                    channelName,
+                    c -> new ConcurrentLinkedQueue<>()
+            ).offer(message);
+            return false;
+        }
+    }
+
+    @Override
+    public void republishFailedMessages() {
+        for (Map.Entry<String, Queue<String>> entry : failedMessages.entrySet()) {
+            // We are creating a copy of the queue to avoid infinite recursion between this method and publish
+            Queue<String> queue = entry.getValue();
+            Queue<String> copy = new LinkedList<>(queue);
+            queue.clear();
+            while (!copy.isEmpty())
+                publish(entry.getKey(), copy.poll());
+        }
+    }
+
+}
---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/test/groovy/it/fulminazzo/blocksmith/broker/plugin/coordinator/AbstractPluginMessagePublisherTest.groovy
@@ -0,0 +1,41 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator
+
+import it.fulminazzo.blocksmith.reflect.Reflect
+import spock.lang.Specification
+
+class AbstractPluginMessagePublisherTest extends Specification {
+
+    def 'test that republishFailedMessages adds failed messages a second time if publication fails'() {
+        given:
+        def validator = a -> a[0] == 'valid'
+
+        and:
+        def publisher = Spy(AbstractPluginMessagePublisher)
+        publisher.publishImpl(_, _) >> { a -> return validator(a) }
+
+        and:
+        final failedMessages = Reflect.on(publisher).get('failedMessages').get()
+
+        when:
+        publisher.publish('valid', 'Hello')
+        publisher.publish('valid', 'world')
+        publisher.publish('invalid', 'valid:Hello')
+        publisher.publish('invalid', 'invalid:world')
+
+        then:
+        failedMessages.size() == 1
+        failedMessages['invalid'].size() == 2
+
+        when:
+        validator = a -> a[1].startsWith('valid:')
+
+        and:
+        publisher.republishFailedMessages()
+
+        then:
+        failedMessages.size() == 1
+        failedMessages['invalid'].size() == 1
+    }
+
+
+}
