299d908c43aaca47276fb5ae3483ac15c6d0e3c3
fulminazzo <fulminazzo21@gmail.com>
2026-06-14 13:28:33 +0200

    Created Bungeecord PluginMessageChannelCoordinator implementation with PluginMessageEvent and resend of messages on player connect to server event

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-bungeecord/src/main/java/it/fulminazzo/blocksmith/broker/plugin/coordinator/bungee/BungeecordPluginMessageChannelCoordinator.java
@@ -0,0 +1,112 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator.bungee;
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.*;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.event.PluginMessageEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+/**
+ * Bungeecord implementation of {@link PluginMessageChannelCoordinator}.
+ * <br>
+ * Uses the name of the servers as node IDs.
+ *
+ * @see PluginMessageChannelCoordinator
+ * @see ProxyPluginMessageChannelCoordinator
+ */
+public final class BungeecordPluginMessageChannelCoordinator
+        extends ProxyPluginMessageChannelCoordinator<String>
+        implements Listener {
+    private final @NotNull ProxyServer server;
+
+    /**
+     * Instantiates a new Bungeecord plugin message channel coordinator.
+     *
+     * @param registrar the registrar
+     */
+    BungeecordPluginMessageChannelCoordinator(final @NotNull PluginMessageRegistrar registrar) {
+        super(registrar);
+        this.server = this.registrar.server();
+    }
+
+    /**
+     * Attempts to resend the failed messages whenever a player joins a node
+     * (will only have an effect if no other player was online at the time of publishing).
+     *
+     * @param event the event
+     */
+    @EventHandler
+    public void on(final @NotNull ServerConnectedEvent event) {
+        getNodePublisher(event.getServer().getInfo().getName()).republishFailedMessages();
+    }
+
+    /**
+     * Handles incoming plugin messages.
+     *
+     * @param event the event
+     */
+    @EventHandler
+    public void on(final @NotNull PluginMessageEvent event) {
+        handleIncomingMessage(event.getTag(), event.getData());
+    }
+
+    @Override
+    protected @NotNull PluginMessagePublisher newNodePublisher(final @NotNull String node) {
+        return new BungeePluginMessagePublisher(node);
+    }
+
+    @Override
+    protected void registerChannel(final @NotNull String channelName) {
+        server.registerChannel(channelName);
+        server.getPluginManager().registerListener(registrar.plugin(), this);
+    }
+
+    @Override
+    protected void unregisterChannel(final @NotNull String channelName) {
+        server.unregisterChannel(channelName);
+        server.getPluginManager().unregisterListener(this);
+    }
+
+    @Override
+    protected @NotNull Collection<String> getAllNodes() {
+        return server.getServers().keySet();
+    }
+
+    /**
+     * Bungeecord implementation of {@link PluginMessagePublisher}.
+     * <br>
+     * Uses the name of the server as the node ID.
+     *
+     * @see PluginMessagePublisher
+     */
+    @RequiredArgsConstructor
+    private final class BungeePluginMessagePublisher extends AbstractPluginMessagePublisher {
+        private final @NotNull String serverId;
+
+        /**
+         * Gets the node.
+         *
+         * @return the node
+         */
+        private @NotNull ServerInfo getNode() {
+            return server.getServerInfo(serverId);
+        }
+
+        @Override
+        protected boolean publishImpl(final @NotNull String channelName, final byte @NotNull [] message) {
+            return getNode().sendData(
+                    channelName,
+                    message,
+                    false // leave handling of failures to our own implementation
+            );
+        }
+
+    }
+
+}
---
+++ message-broker-starter/plugin-messaging/plugin-messaging-bungeecord/src/test/groovy/it/fulminazzo/blocksmith/broker/plugin/coordinator/bungee/BungeecordPluginMessageChannelCoordinatorTest.groovy
@@ -0,0 +1,118 @@
+package it.fulminazzo.blocksmith.broker.plugin.coordinator.bungee
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageRegistrar
+import it.fulminazzo.blocksmith.reflect.Reflect
+import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.config.ServerInfo
+import net.md_5.bungee.api.connection.ProxiedPlayer
+import net.md_5.bungee.api.connection.Server
+import net.md_5.bungee.api.event.PluginMessageEvent
+import net.md_5.bungee.api.event.ServerConnectedEvent
+import net.md_5.bungee.api.plugin.PluginManager
+import spock.lang.Specification
+
+class BungeecordPluginMessageChannelCoordinatorTest extends Specification {
+    private static final String CHANNEL_NAME = 'test-channel'
+    private static final List<String> SERVER_NAMES = ['hub', 'factions']
+
+    private final PluginMessageRegistrar registrar = Mock(PluginMessageRegistrar) {
+        def servers = SERVER_NAMES.collectEntries { [it, newServer(it)] }
+        def pluginManager = Mock(PluginManager)
+
+        def server = Mock(ProxyServer)
+        server.pluginManager >> pluginManager
+        server.servers >> servers
+        server.getServerInfo(_) >> { a -> servers[a[0]] }
+
+        it.server() >> server
+    }
+
+    private final BungeecordPluginMessageChannelCoordinator coordinator = new BungeecordPluginMessageChannelCoordinator(registrar)
+
+    def 'test that coordinator attempts to publish failed messages on player connect to server'() {
+        given:
+        final message = 'message'.bytes
+
+        and:
+        SERVER_NAMES.forEach {
+            def publisher = coordinator.getNodePublisher(it)
+            Reflect.on(publisher)
+                    .get('failedMessages')
+                    .invoke('put', CHANNEL_NAME, new LinkedList<>([message]))
+        }
+
+        and:
+        def player = Mock(ProxiedPlayer)
+        def server = Mock(Server) {
+            it.info >> registrar.server().servers[name]
+        }
+        def event = new ServerConnectedEvent(player, server)
+
+        when:
+        coordinator.on(event)
+
+        then:
+        1 * registrar.server().servers[name].sendData(CHANNEL_NAME, message, false)
+
+        where:
+        name << SERVER_NAMES
+    }
+
+    def 'test that on PluginMessageEvent delegates to handleIncomingMessage'() {
+        given:
+        def coordinator = Mock(BungeecordPluginMessageChannelCoordinator)
+        coordinator.on(_ as PluginMessageEvent) >> { callRealMethod() }
+
+        and:
+        final message = 'Hello, world!'.bytes
+
+        when:
+        coordinator.on(Mock(PluginMessageEvent) {
+            it.tag >> CHANNEL_NAME
+            it.data >> message
+        })
+
+        then:
+        1 * coordinator.handleIncomingMessage(CHANNEL_NAME, message)
+    }
+
+    def 'test that publish calls on all nodes'() {
+        given:
+        def message = 'Hello, world'.bytes
+
+        and:
+        def server = registrar.server() as ProxyServer
+
+        when:
+        coordinator.publish(CHANNEL_NAME, message)
+
+        then:
+        1 * server.servers['hub'].sendData(CHANNEL_NAME, message, false)
+        1 * server.servers['factions'].sendData(CHANNEL_NAME, message, false)
+    }
+
+    def 'test that registerChannel correctly registers all necessary channels'() {
+        when:
+        coordinator.registerChannel(CHANNEL_NAME)
+
+        then:
+        1 * registrar.server().registerChannel(CHANNEL_NAME)
+        1 * registrar.server().pluginManager.registerListener(registrar.plugin(), coordinator)
+    }
+
+    def 'test that unregisterChannel correctly unregisters all previously registered channels'() {
+        when:
+        coordinator.unregisterChannel(CHANNEL_NAME)
+
+        then:
+        1 * registrar.server().unregisterChannel(CHANNEL_NAME)
+        1 * registrar.server().pluginManager.unregisterListener(coordinator)
+    }
+
+    private ServerInfo newServer(final String name) {
+        return Mock(ServerInfo) {
+            it.name >> name
+        }
+    }
+
+}
