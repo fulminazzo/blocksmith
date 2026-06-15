d8849040d50fe487e6eab89f686430f7b13bf1ab
fulminazzo <fulminazzo21@gmail.com>
2026-06-15 23:39:29 +0200

    Created MockPluginMessageChannelCoordinatorFactory for testing purposes

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/integrationTest/java/it/fulminazzo/blocksmith/broker/plugin/MockPluginMessageChannelCoordinatorFactory.java
@@ -0,0 +1,42 @@
+package it.fulminazzo.blocksmith.broker.plugin;
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageChannelCoordinator;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageChannelCoordinatorFactory;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Mock implementation of {@link PluginMessageChannelCoordinatorFactory}.
+ *
+ * @see MockPluginMessageChannelCoordinator
+ */
+public final class MockPluginMessageChannelCoordinatorFactory implements PluginMessageChannelCoordinatorFactory {
+
+    @Override
+    public @NotNull PluginMessageChannelCoordinator create(final @NotNull PluginMessageRegistrar registrar) {
+        return new MockPluginMessageChannelCoordinator();
+    }
+
+    @Override
+    public @NotNull PluginMessageRegistrar createRegistrar(@NotNull Object owner) {
+        return new PluginMessageRegistrar() {
+
+            @Override
+            public @NotNull <P> P plugin() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public @NotNull <S> S server() {
+                throw new UnsupportedOperationException();
+            }
+
+        };
+    }
+
+    @Override
+    public boolean supportsOwner(final @NotNull Class<?> ownerType) {
+        return true;
+    }
+
+}
