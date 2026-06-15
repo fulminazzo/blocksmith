ede6cd3b591e0c6e418cb28ec86a0511afc3fd70
fulminazzo <fulminazzo21@gmail.com>
2026-06-15 23:15:18 +0200

    Created mock implementation of PluginMessageChannelCoordinator for integration tests

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/integrationTest/java/it/fulminazzo/blocksmith/broker/plugin/MockPluginMessageChannelCoordinator.java
@@ -0,0 +1,67 @@
+package it.fulminazzo.blocksmith.broker.plugin;
+
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageChannelCoordinator;
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * Mock implementation of {@link PluginMessageChannelCoordinator}.
+ */
+public final class MockPluginMessageChannelCoordinator extends PluginMessageChannelCoordinator {
+    private static final @NotNull Map<
+            String,
+            Set<MockPluginMessageChannelCoordinator>
+            > COORDINATORS = new ConcurrentHashMap<>();
+
+    /***
+     * Instantiates a new Mock plugin message channel coordinator.
+     */
+    public MockPluginMessageChannelCoordinator() {
+        super(new PluginMessageRegistrar() {
+
+            @Override
+            public @NotNull <P> P plugin() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public @NotNull <S> S server() {
+                throw new UnsupportedOperationException();
+            }
+
+        });
+    }
+
+    @Override
+    protected void registerChannel(final @NotNull String channelName) {
+        getChannelCoordinators(channelName).add(this);
+    }
+
+    @Override
+    protected void unregisterChannel(final @NotNull String channelName) {
+        getChannelCoordinators(channelName).remove(this);
+    }
+
+    @Override
+    public boolean publish(final @NotNull String channelName, final byte @NotNull [] message) {
+        getChannelCoordinators(channelName).forEach(c ->
+                c.handleIncomingMessage(channelName, message)
+        );
+        return true;
+    }
+
+    @Override
+    public void republishFailedMessages() {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Set<MockPluginMessageChannelCoordinator> getChannelCoordinators(final @NotNull String channelName) {
+        return COORDINATORS.computeIfAbsent(channelName, _ -> new CopyOnWriteArraySet<>());
+    }
+
+}
