a6152572e8d0983ccdb9e877a3fdf2080a3e5bd6
fulminazzo <fulminazzo21@gmail.com>
2026-06-15 23:39:46 +0200

    Added PluginMessageBroker integration test suite

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/plugin/PluginMessageBrokerIntegrationTest.groovy
@@ -0,0 +1,42 @@
+package it.fulminazzo.blocksmith.broker.plugin
+
+import groovy.util.logging.Slf4j
+import it.fulminazzo.blocksmith.broker.MessageBroker
+import it.fulminazzo.blocksmith.broker.MessageBrokerBuilder
+import it.fulminazzo.blocksmith.broker.MessageBrokerIntegrationTest
+import it.fulminazzo.blocksmith.broker.MessageChannelIntegrationTestHelper
+import it.fulminazzo.blocksmith.data.mapper.MapperFormat
+
+import java.util.concurrent.Executors
+
+@Slf4j
+class PluginMessageBrokerIntegrationTest extends MessageBrokerIntegrationTest<PluginMessageChannelSettings> {
+
+    void setup() {
+        setupSingle()
+    }
+
+    void cleanup() {
+        cleanupSingle()
+    }
+
+    @Override
+    protected MessageBrokerBuilder<MessageBroker<PluginMessageChannelSettings>> newMessageBrokerBuilder() {
+        return PluginMessageBroker.builder()
+                .owner(new Object())
+                .executor(Executors.newCachedThreadPool())
+                .mapper(MapperFormat.JSON.newMapper())
+
+    }
+
+    @Override
+    protected MessageChannelIntegrationTestHelper newTestHelper(final String channelName) {
+        return new PluginChannelIntegrationTestHelper(channelName, log)
+    }
+
+    @Override
+    protected PluginMessageChannelSettings getSettings() {
+        return new PluginMessageChannelSettings()
+    }
+
+}
