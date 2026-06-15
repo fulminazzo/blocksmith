c093887f732b1f993349e322447d15b3a5754ebb
fulminazzo <fulminazzo21@gmail.com>
2026-06-15 23:17:36 +0200

    Created PluginChannelIntegrationTestHelper

---
+++ message-broker-starter/plugin-messaging/plugin-messaging-base/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/plugin/PluginChannelIntegrationTestHelper.groovy
@@ -0,0 +1,48 @@
+package it.fulminazzo.blocksmith.broker.plugin
+
+import com.google.common.io.ByteStreams
+import it.fulminazzo.blocksmith.broker.Message
+import it.fulminazzo.blocksmith.broker.MessageChannelIntegrationTestHelper
+import it.fulminazzo.blocksmith.broker.plugin.coordinator.PluginMessageChannelCoordinator
+import org.slf4j.Logger
+
+import java.util.function.Consumer
+
+@SuppressWarnings('CloseWithoutCloseable')
+class PluginChannelIntegrationTestHelper extends MessageChannelIntegrationTestHelper {
+    private final PluginMessageChannelCoordinator coordinator
+
+    PluginChannelIntegrationTestHelper(final String channelName, final Logger logger) {
+        super(channelName, logger)
+
+        coordinator = new MockPluginMessageChannelCoordinator()
+    }
+
+    @Override
+    void send(final Message message, final UUID conversationId) {
+        def payload = serializeMessage(message, conversationId)
+        def stream = ByteStreams.newDataOutput()
+        stream.writeUTF(payload)
+        coordinator.publish(channelName, stream.toByteArray())
+    }
+
+    @Override
+    void close() throws IOException {
+        coordinator?.close()
+        super.close()
+    }
+
+    @Override
+    protected MessageChannelIntegrationTestHelper start(
+            final String channelName,
+            final Logger logger,
+            final Consumer<String> consumer
+    ) {
+        coordinator.registerHandler(
+                channelName,
+                m -> consumer.accept(m)
+        )
+        return this
+    }
+
+}
