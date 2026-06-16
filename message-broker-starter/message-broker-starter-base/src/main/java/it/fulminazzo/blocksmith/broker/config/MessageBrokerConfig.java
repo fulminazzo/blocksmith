d15b434e9dba39f9283d20caadcc125702a0498e
fulminazzo <fulminazzo21@gmail.com>
2026-06-16 13:24:28 +0200

    Updated MessageBrokerConfig to abstract class with internal mapper field

--- message-broker-starter/message-broker-starter-base/src/main/java/it/fulminazzo/blocksmith/broker/config/MessageBrokerConfig.java
+++ message-broker-starter/message-broker-starter-base/src/main/java/it/fulminazzo/blocksmith/broker/config/MessageBrokerConfig.java
@@ -1,16 +1,48 @@
 package it.fulminazzo.blocksmith.broker.config;
 
 import it.fulminazzo.blocksmith.broker.MessageBroker;
+import it.fulminazzo.blocksmith.data.mapper.Mapper;
+import it.fulminazzo.blocksmith.data.mapper.MapperFormat;
+import it.fulminazzo.blocksmith.validation.annotation.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents the configuration for a {@link MessageBroker}.
  *
+ * @param <C> the type of this configuration
  * @see MessageBrokerFactory
  * @see MessageBroker
  */
-public interface MessageBrokerConfig {
-    /*
-     * This is a marker interface.
-     * Submodules should use it to provide their own implementation.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class MessageBrokerConfig<C extends MessageBrokerConfig<C>> {
+
+    @NonNull(exceptionMessage = "'mapper' must be declared")
+    private @NotNull MapperFormat mapper = MapperFormat.JSON;
+
+    /**
+     * Gets the mapper.
+     *
+     * @return the mapper
      */
+    public @NotNull Mapper getMapper() {
+        return mapper.newMapper();
+    }
+
+    /**
+     * Sets the mapper.
+     *
+     * @param mapper the mapper
+     * @return this object (for method chaining)
+     */
+    @SuppressWarnings("unchecked")
+    public @NotNull C setMapper(final @NotNull MapperFormat mapper) {
+        this.mapper = mapper;
+        return (C) this;
+    }
+
 }
