ebcf034590acb1b43ce7ef98e9b14a31427b87bf
fulminazzo <fulminazzo21@gmail.com>
2026-06-16 13:01:35 +0200

    Created MessageBrokerConfig

---
+++ message-broker-starter/message-broker-starter-base/src/main/java/it/fulminazzo/blocksmith/broker/config/MessageBrokerConfig.java
@@ -0,0 +1,16 @@
+package it.fulminazzo.blocksmith.broker.config;
+
+import it.fulminazzo.blocksmith.broker.MessageBroker;
+
+/**
+ * Represents the configuration for a {@link MessageBroker}.
+ *
+ * @see MessageBrokerFactory
+ * @see MessageBroker
+ */
+public interface MessageBrokerConfig {
+    /*
+     * This is a marker interface.
+     * Submodules should use it to provide their own implementation.
+     */
+}
