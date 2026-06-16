ca5c412e75ff3268d43d28c43ab625efa906c27b
fulminazzo <fulminazzo21@gmail.com>
2026-06-16 22:55:44 +0200

    Added message-broker-starter integration tests dependencies

--- message-broker-starter/build.gradle.kts
+++ message-broker-starter/build.gradle.kts
@@ -1,1 +1,6 @@
 plugins { id("blocksmith.composite-module") }
+
+dependencies {
+    integrationTestImplementation(libs.test.containers.rabbitmq)
+    integrationTestImplementation(libs.test.containers.kafka)
+}
