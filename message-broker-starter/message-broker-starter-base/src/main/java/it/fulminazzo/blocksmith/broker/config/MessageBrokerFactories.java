60aa228b7d86195aef923366f4f901a90d4dbfc5
fulminazzo <fulminazzo21@gmail.com>
2026-06-16 13:02:07 +0200

    Created MessageBrokerFactories class to manage message broker factories

---
+++ message-broker-starter/message-broker-starter-base/src/main/java/it/fulminazzo/blocksmith/broker/config/MessageBrokerFactories.java
@@ -0,0 +1,60 @@
+package it.fulminazzo.blocksmith.broker.config;
+
+import it.fulminazzo.blocksmith.broker.MessageBroker;
+import it.fulminazzo.blocksmith.broker.MessageChannelSettings;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Collects all the {@link MessageBrokerFactory} implementations.
+ *
+ * @see MessageBrokerConfig
+ * @see MessageBrokerFactory
+ * @see MessageBroker
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MessageBrokerFactories {
+    private static final @NotNull Map<
+            Class<? extends MessageBrokerConfig>,
+            MessageBrokerFactory
+            > FACTORIES = new ConcurrentHashMap<>();
+
+    /**
+     * Instantiates a new {@link MessageBroker} from the given configuration.
+     *
+     * @param messageBrokerConfig the message broker configuration
+     * @param <S>                 the type of the channel settings
+     * @return the message broker
+     */
+    @SuppressWarnings("unchecked")
+    public static <S extends MessageChannelSettings<S>> @NotNull MessageBroker<S> build(
+            final @NotNull MessageBrokerConfig messageBrokerConfig
+    ) {
+        MessageBrokerFactory messageBrokerFactory = FACTORIES.get(messageBrokerConfig.getClass());
+        if (messageBrokerFactory == null)
+            throw new IllegalArgumentException(String.format(
+                    "No %s factory currently registered for configuration type: %s",
+                    MessageBroker.class.getSimpleName(),
+                    messageBrokerConfig.getClass().getCanonicalName()
+            ));
+        else return (MessageBroker<S>) messageBrokerFactory.build(messageBrokerConfig);
+    }
+
+    /**
+     * Registers a new factory for the given {@link MessageBrokerConfig} type.
+     *
+     * @param configClass the config class
+     * @param factory     the factory
+     */
+    public static void registerFactory(
+            final @NotNull Class<? extends MessageBrokerConfig> configClass,
+            final @NotNull MessageBrokerFactory factory
+    ) {
+        FACTORIES.put(configClass, factory);
+    }
+
+}
