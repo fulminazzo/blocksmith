004c5c292db06314e01b755b88077170895127b6
fulminazzo <fulminazzo21@gmail.com>
2026-06-16 23:37:38 +0200

    Added missing dependencies

--- message-broker-starter/build.gradle.kts
+++ message-broker-starter/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins { id("blocksmith.composite-module") }
 
 dependencies {
+    integrationTestImplementation(libs.guava)
     integrationTestImplementation(libs.test.containers.rabbitmq)
     integrationTestImplementation(libs.test.containers.kafka)
 }
