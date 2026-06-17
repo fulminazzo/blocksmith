b737fba2941f4453ff07169b527ab47e1178d1dc
fulminazzo <fulminazzo21@gmail.com>
2026-06-17 11:40:49 +0200

    Added more tests

--- message-broker-starter/message-broker-starter-rabbitmq/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/rabbitmq/config/RabbitMQMessageBrokerFactoryIntegrationTest.groovy
+++ message-broker-starter/message-broker-starter-rabbitmq/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/rabbitmq/config/RabbitMQMessageBrokerFactoryIntegrationTest.groovy
@@ -4,8 +4,18 @@ import it.fulminazzo.blocksmith.broker.config.MessageBrokerConfig
 import it.fulminazzo.blocksmith.broker.config.MessageBrokerFactory
 import it.fulminazzo.blocksmith.broker.config.MessageBrokerFactoryIntegrationTest
 import it.fulminazzo.blocksmith.broker.rabbitmq.RabbitMQIntegrationTest
+import it.fulminazzo.blocksmith.broker.rabbitmq.RabbitMQMessageBrokerException
 
 class RabbitMQMessageBrokerFactoryIntegrationTest extends MessageBrokerFactoryIntegrationTest implements RabbitMQIntegrationTest {
+
+    def 'test that build with no port specified works'() {
+        when:
+        factory.build(config.setPort(0))
+
+        then: 'Exception should be thrown because of a connection refused, not a misconfiguration'
+        def e = thrown(RabbitMQMessageBrokerException)
+        ConnectException.isInstance(e.cause)
+    }
 
     @Override
     protected MessageBrokerFactory getFactory() {
--- message-broker-starter/message-broker-starter-redis/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/redis/config/RedisMessageBrokerFactoryIntegrationTest.groovy
+++ message-broker-starter/message-broker-starter-redis/src/integrationTest/groovy/it/fulminazzo/blocksmith/broker/redis/config/RedisMessageBrokerFactoryIntegrationTest.groovy
@@ -1,11 +1,55 @@
 package it.fulminazzo.blocksmith.broker.redis.config
 
+import it.fulminazzo.blocksmith.ProjectInfo
 import it.fulminazzo.blocksmith.broker.config.MessageBrokerConfig
 import it.fulminazzo.blocksmith.broker.config.MessageBrokerFactory
 import it.fulminazzo.blocksmith.broker.config.MessageBrokerFactoryIntegrationTest
 import it.fulminazzo.blocksmith.broker.redis.RedisIntegrationTest
 
 class RedisMessageBrokerFactoryIntegrationTest extends MessageBrokerFactoryIntegrationTest implements RedisIntegrationTest {
+
+    def 'test build with clientName=#clientName and database#database'() {
+        given:
+        def config = new RedisMessageBrokerConfig()
+                .setHost(RedisIntegrationTest.serverHost)
+                .setPort(RedisIntegrationTest.serverPort)
+                .setClientName(clientName)
+                .setSsl(false)
+                .setDatabase(database)
+
+        when:
+        def dataSource = new RedisMessageBrokerFactory().build(config)
+
+        then:
+        dataSource != null
+
+        cleanup:
+        dataSource?.close()
+
+        where:
+        database | clientName
+        null     | null
+        0        | null
+        null     | ProjectInfo.PROJECT_NAME
+        0        | ProjectInfo.PROJECT_NAME
+    }
+
+    /**
+     * These tests are purely for coverage purposes.
+     * Although, they ensure that if the port has not been specified, the default one is used.
+     */
+    def 'test build with unspecified port'() {
+        given:
+        def config = new RedisMessageBrokerConfig()
+                .setHost(RedisIntegrationTest.serverHost)
+                .setPort(null)
+
+        when:
+        new RedisMessageBrokerFactory().build(config)
+
+        then:
+        thrown(io.lettuce.core.RedisConnectionException)
+    }
 
     @Override
     protected MessageBrokerFactory getFactory() {
