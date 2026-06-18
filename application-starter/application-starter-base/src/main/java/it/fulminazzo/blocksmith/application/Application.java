34491cc8161ebf5a71090a707ff1f61340f19da3
fulminazzo <fulminazzo21@gmail.com>
2026-06-18 23:48:06 +0200

    Created Application interface

---
+++ application-starter/application-starter-base/src/main/java/it/fulminazzo/blocksmith/application/Application.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.blocksmith.application;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.File;
+
+/**
+ * The basic entry point of a program. Contains useful data and basic functioning information.
+ */
+public interface Application {
+
+    /**
+     * Gets the directory where the application should operate.
+     *
+     * @return the directory
+     */
+    @NotNull File directory();
+
+    /**
+     * Gets the logger associated with the application.
+     *
+     * @return the logger
+     */
+    @NotNull Logger logger();
+
+}
