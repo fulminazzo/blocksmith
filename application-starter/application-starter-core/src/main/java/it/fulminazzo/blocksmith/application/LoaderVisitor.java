6a075539ff345d9b006d4deb8ae682c23319f7a4
fulminazzo <fulminazzo21@gmail.com>
2026-06-20 00:36:52 +0200

    Added Throwable parameter to throw exceptions during visits

--- application-starter/application-starter-core/src/main/java/it/fulminazzo/blocksmith/application/LoaderVisitor.java
+++ application-starter/application-starter-core/src/main/java/it/fulminazzo/blocksmith/application/LoaderVisitor.java
@@ -8,25 +8,28 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Identifies a visitor for a {@link LoaderNode} dependency tree.
  *
+ * @param <X> the type of the exception to throw in case of errors during visits
  * @see ApplicationLoader
  * @see LoaderNode
  * @see RootLoaderNode
  * @see FieldAnnotationNode
  */
-public interface LoaderVisitor {
+public interface LoaderVisitor<X extends Throwable> {
 
     /**
      * Visits a general {@link FieldAnnotationNode}.
      *
      * @param node the node
+     * @throws X the exception thrown in case of visit errors
      */
-    void visitField(final @NotNull FieldAnnotationNode node);
+    void visitField(final @NotNull FieldAnnotationNode node) throws X;
 
     /**
      * Visits a general {@link RootLoaderNode}.
      *
      * @param node the node
+     * @throws X the exception thrown in case of visit errors
      */
-    void visitRoot(final @NotNull RootLoaderNode node);
+    void visitRoot(final @NotNull RootLoaderNode node) throws X;
 
 }
