80727872b44279e2f6db08c1ae0ef9a69936c2f3
fulminazzo <fulminazzo21@gmail.com>
2026-09-17 22:45:51 +0200

    Added more constructors

--- application-starter/application-starter-base/src/main/java/it/fulminazzo/blocksmith/application/ApplicationInitializeException.java
+++ application-starter/application-starter-base/src/main/java/it/fulminazzo/blocksmith/application/ApplicationInitializeException.java
@@ -1,29 +1,12 @@
 package it.fulminazzo.blocksmith.application;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.experimental.StandardException;
 
 /**
  * An exception thrown during a failed initialization of the application.
  */
+@StandardException
 public final class ApplicationInitializeException extends Exception {
     private static final long serialVersionUID = 6819735949224600789L;
-
-    /**
-     * Instantiates a new Application initialize exception.
-     *
-     * @param message the message
-     */
-    public ApplicationInitializeException(final @NotNull String message) {
-        super(message);
-    }
-
-    /**
-     * Instantiates a new Application initialize exception.
-     *
-     * @param cause the cause
-     */
-    public ApplicationInitializeException(final @NotNull Throwable cause) {
-        super(cause);
-    }
 
 }
