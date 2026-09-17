00f0e25c4504706626591be1cf66e1cb87130096
fulminazzo <fulminazzo21@gmail.com>
2026-09-17 22:46:17 +0200

    Updated InitializationContext#get method to attempt to recursively fetch nested data

--- application-starter/application-starter-base/src/main/java/it/fulminazzo/blocksmith/application/InitializationContext.java
+++ application-starter/application-starter-base/src/main/java/it/fulminazzo/blocksmith/application/InitializationContext.java
@@ -1,5 +1,7 @@
 package it.fulminazzo.blocksmith.application;
 
+import it.fulminazzo.blocksmith.reflect.Reflect;
+import it.fulminazzo.blocksmith.reflect.ReflectException;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
@@ -23,14 +25,55 @@ public class InitializationContext {
 
     /**
      * Gets the requested environment variable value.
+     * If the variable is dotted ("variable.subvariable"),
+     * it will be resolved recursively (only works for maps and objects).
      *
      * @param key the name of the variable
      * @param <T> the type of the value
      * @return the value
+     * @throws ApplicationInitializeException if the variable is not found
      */
+    public <T> T get(final @NotNull String key) throws ApplicationInitializeException {
+        try {
+            return get(environment, key);
+        } catch (ApplicationInitializeException e) {
+            throw new ApplicationInitializeException(
+                    String.format("Could not find variable with path '%s'", key),
+                    e
+            );
+        }
+    }
+
     @SuppressWarnings("unchecked")
-    public <T> T get(final @NotNull String key) {
-        return (T) environment.get(key);
+    private static <T> T get(
+            final @NotNull Object object,
+            final @NotNull String key
+    ) throws ApplicationInitializeException {
+        final String firstKey;
+        final String remainder;
+        if (key.contains(".")) {
+            int idx = key.indexOf(".");
+            firstKey = key.substring(0, idx);
+            remainder = key.substring(idx + 1);
+        } else {
+            firstKey = key;
+            remainder = null;
+        }
+        T result;
+        if (object instanceof Map) {
+            result = (T) ((Map<?, ?>) object).get(firstKey);
+        } else
+            try {
+                result = Reflect.on(object).get(firstKey).get();
+            } catch (ReflectException e) {
+                result = null;
+            }
+        if (remainder == null) return result;
+        else if (result != null) return get(result, remainder);
+        else throw new ApplicationInitializeException(String.format(
+                    "Could not find '%s' in object of type %s: %s",
+                    key, object.getClass().getCanonicalName(), object
+            ));
     }
 
 }
